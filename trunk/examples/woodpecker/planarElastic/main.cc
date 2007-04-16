18f3ec387140216b3656f979b64b051d808a8aa0
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2007-04-16 18:17:20 +0000

    Updated README. Changed solver in the woodpecker-example.

--- trunk/examples/woodpecker/planarElastic/main.cc
+++ trunk/examples/woodpecker/planarElastic/main.cc
@@ -8,14 +8,11 @@ using namespace MBSim;
 int main (int argc, char* argv[]) {
 
   Woodpecker sys("WP");
-  sys.setSolver(RootFinding);
-//  sys.setSolver(solveGS);
-//  sys.setSolver(projectGS);
   sys.init();
 
   TimeSteppingIntegrator integrator;
   integrator.setdt(1.e-5);
-  integrator.settEnd(5.0);
+  integrator.settEnd(4.5);
   integrator.setdtPlot(1.e-3);
   
 // integrator.setWarnLevel(0); defautl: 0
