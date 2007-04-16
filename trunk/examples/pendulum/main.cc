a90d3170754867be7cf49484ece2beb5198afcbb
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2007-04-16 18:05:03 +0000

    Changed integrator to DOPRI5

--- trunk/examples/pendulum/main.cc
+++ trunk/examples/pendulum/main.cc
@@ -12,18 +12,16 @@ int main (int argc, char* argv[])
   // Bausteine zum Gesamtsystem zusammenfuegen (zu einem DGL-System) 
   sys->init();
   
-  //LSODARIntegrator integrator;
-  //LSODEIntegrator integrator;
- // RKSuiteIntegrator integrator;
- // RADAU5Integrator integrator;
-  //DOPRI5Integrator integrator;
-  TimeSteppingIntegrator integrator;
+  // LSODEIntegrator integrator;
+  // RKSuiteIntegrator integrator;
+  // RADAU5Integrator integrator;
+  // TimeSteppingIntegrator integrator;
+  // integrator.setdt(1e-4);
+  //
+  DOPRI5Integrator integrator;
 
   integrator.settEnd(10.0);
   integrator.setdtPlot(1e-3);
- integrator.setdt(1e-4);
-//  integrator.setrTol(1e-10);
- // integrator.setaTol(1e-10);
 
   integrator.integrate(*sys);
   cout << "finished"<<endl;
