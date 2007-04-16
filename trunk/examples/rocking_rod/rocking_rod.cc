b0e83a40bcdc5dc4831e8e63ec7d05805575420b
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2007-04-16 20:14:36 +0000

    Clean up.

--- trunk/examples/rocking_rod/rocking_rod.cc
+++ trunk/examples/rocking_rod/rocking_rod.cc
@@ -9,36 +9,36 @@ using namespace AMVis;
 
 RockingRod::RockingRod(const string &projectName) : MultiBodySystem(projectName) {
   setProjectDirectory("plot");
-  setPlotLevel(2);
 
+  // Gravitation
   Vec grav(3,INIT,0.0);
   grav(1)=-9.81;
   setGrav(grav);
 
-  double l = 0.8;              // Laenge Balken
-  double alpha = 3.0 * M_PI/180.; // Winkel der Anfangslage
-  double deltax = 0.2;             // Abstand der Kontaktpunkte
-
-  double   h =  0.02;
-  double   d = 0.1;
-
-  double mu  = 0.3;
-
-  BodyRigidAbs *balken = new BodyRigidAbs("Rod");
+  // Parameters
+  double l = 0.8;              		
+  double h =  0.02;
+  double d = 0.1;
   double m = 0.7;
-  balken->setMass(m);
   SymMat Theta(3);
   Theta(1,1) = m*l*l/12.;
   Theta(2,2) = Theta(1,1);
-  balken->setInertia(Theta);
+  double alpha = 3.0 * M_PI/180.; 
+  double deltax = 0.2;           
+  double mu  = 0.3;
 
+  // Body with relative kinematics
+  BodyRigidAbs *balken = new BodyRigidAbs("Rod");
+  addObject(balken);
+  balken->setMass(m);
+  balken->setInertia(Theta);
   balken->setJT(Mat("[1,0; 0,1; 0,0]"));
   balken->setJR(Mat("[0; 0; 1]"));
 
+  // Initial translation and rotation
   Vec WrOS0(3);
   WrOS0(1) = .5;
   WrOS0(2) = d/2;
-  balken->setWrOK0(WrOS0);
   SqrMat AWK0(3);
   AWK0(0,0) = cos(alpha);
   AWK0(0,1) = -sin(alpha);
@@ -46,8 +46,9 @@ RockingRod::RockingRod(const string &projectName) : MultiBodySystem(projectName)
   AWK0(1,1) = cos(alpha);
   AWK0(2,2) = 1;
   balken->setAWK0(AWK0);
-  addObject(balken);
+  balken->setWrOK0(WrOS0);
 
+  // Contour definition
   Line *line = new Line("Line");
   Vec KrSC(3);
   KrSC(1) = -0.5*h;
@@ -58,20 +59,7 @@ RockingRod::RockingRod(const string &projectName) : MultiBodySystem(projectName)
   line->setCn(n);
   line->setCb(b);
 
- // Cuboid *cubeoid = new Cuboid(balken->getFullName(),1,false);
- // cubeoid->setSize(l,h,d);
- // cubeoid->setColor(0);
- // balken->setAMVisBody(cubeoid);
-  ObjObject *obj = new ObjObject(balken->getFullName(),1,false);
-  obj->setObjFilename("objects/rod.obj");
-  balken->setAMVisBody(obj);
-  obj->setInitialRotation(M_PI/2,0,0);
-  obj->setScaleFactor(0.1);
-  obj->setCalculationOfNormals(3);
-  obj->setVertexEPS(1e-5);
-  obj-> setNormalEPS(1e-5);
-  obj-> setAngleEPS(M_PI*2/9);
-
+  // Obstacles
   Vec delta1(3); 
   delta1(0) = -deltax/2.;
   Point* point1 = new Point("Point1");
@@ -82,6 +70,7 @@ RockingRod::RockingRod(const string &projectName) : MultiBodySystem(projectName)
   Point* point2 = new Point("Point2");
   addContour(point2,delta2);
 
+  // Contacts
   ContactRigid *cr1S = new ImpactRigid("Contact1"); 
   cr1S->setFrictionDirections(1);
   cr1S->setFrictionCoefficient(mu);
@@ -93,6 +82,22 @@ RockingRod::RockingRod(const string &projectName) : MultiBodySystem(projectName)
   cr2S->setFrictionCoefficient(mu);
   cr2S->connect(point2,balken->getContour("Line"));
   addLink(cr2S);
+
+  // Visualisation with AMVis
+  ObjObject *obj = new ObjObject(balken->getFullName(),1,false);
+  obj->setObjFilename("objects/rod.obj");
+  balken->setAMVisBody(obj);
+  obj->setInitialRotation(M_PI/2,0,0);
+  obj->setScaleFactor(0.1);
+  obj->setCalculationOfNormals(3);
+  obj->setVertexEPS(1e-5);
+  obj-> setNormalEPS(1e-5);
+  obj-> setAngleEPS(M_PI*2/9);
+  // Cuboid *cubeoid = new Cuboid(balken->getFullName(),1,false);
+  // cubeoid->setSize(l,h,d);
+  // cubeoid->setColor(0);
+  // balken->setAMVisBody(cubeoid);
+
 
 }
 
