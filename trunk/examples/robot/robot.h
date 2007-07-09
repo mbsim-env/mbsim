fa1d100fe75ab6865bf909bc8a2b705191ddaedc
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2007-07-09 18:57:41 +0000

    added control files

---
+++ trunk/examples/robot/robot.h
@@ -0,0 +1,19 @@
+#ifndef _ROBOT_H_
+#define _ROBOT_H_
+
+#include "multi_body_system.h"
+#include <string>
+
+
+using namespace std;
+using namespace MBSim;
+
+class Robot : public MultiBodySystem {
+  private:
+  public:
+    Robot(const string &projectName); 
+    
+ 
+};
+
+#endif
