088769ac0eb7a35139c34b793f1515f31f1c55e5
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2008-05-27 16:56:51 +0000

    Added two contact-examples

---
+++ trunk/examples_dev/rocking_rod_with_flexible_contacts/main.cc
@@ -0,0 +1,48 @@
+#include "system.h"
+#include <integrators.h>
+
+using namespace std;
+using namespace MBSim;
+
+int main (int argc, char* argv[])
+{
+  // Einzelne Bausteine des MKS erschaffen
+  MultiBodySystem *sys = new System("TS");
+
+  // Bausteine zum Gesamtsystem zusammenfuegen (zu einem DGL-System) 
+  sys->init();
+  
+  // LSODEIntegrator integrator;
+  // RKSuiteIntegrator integrator;
+  // RADAU5Integrator integrator;
+  // TimeSteppingIntegrator integrator;
+  // integrator.setdt(1e-4);
+  //
+  DOPRI5Integrator integrator;
+
+  //integrator.settEnd(10.0);
+  //integrator.setdtPlot(1e-3);
+
+   integrator.setdtPlot(1e-2);
+   integrator.settEnd(2.5);
+
+ // Vec z(4);
+ // z(0) = 0.13;
+ // z(1) = -1.13;
+ // z(2) = -1.1;
+ // z(3) = 3.1;
+
+ // cout << sys->zdot(z,0)<< endl;
+ // cout << sys->getM()<<endl;
+ // cout << sys->geth()<<endl;
+ // throw 5;
+  
+  integrator.integrate(*sys);
+  cout << "finished"<<endl;
+
+  delete sys;
+
+  return 0;
+
+}
+
