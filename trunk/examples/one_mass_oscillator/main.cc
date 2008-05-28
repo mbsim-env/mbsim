a95306f712e1eec2835f2717e6bbe9f510bf3227
thschindler <thschindler@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2008-05-28 10:19:58 +0000

    example for spring potential

--- trunk/examples/one_mass_oscillator/main.cc
+++ trunk/examples/one_mass_oscillator/main.cc
@@ -10,10 +10,10 @@ int main (int argc, char* argv[]) {
   sys.setProjectDirectory("plot");
   sys.setPlotLevel(3);
   sys.init();
-
-  LSODEIntegrator integrator;
-  integrator.settEnd(5);
-  integrator.setaTol(1.e-7);
+  
+  TimeSteppingIntegrator integrator;
+  integrator.settEnd(2.);
+  integrator.setdt(1.e-7);
   integrator.setdtPlot(2.5e-4);
 
   integrator.integrate(sys);
