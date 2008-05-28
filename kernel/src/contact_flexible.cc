739947501b607ff46f3c29f0666baae35b9cd6cb
thschindler <thschindler@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2008-05-28 09:51:15 +0000

    contact with userfunction and potential

--- kernel/src/contact_flexible.cc
+++ kernel/src/contact_flexible.cc
@@ -19,22 +19,31 @@
  *   mfoerg@users.berlios.de
  *
  */
-#include <config.h>
+#include<config.h>
 #define FMATVEC_NO_BOUNDS_CHECK
 #include "contact_flexible.h"
 #include "multi_body_system.h"
+#include "eps.h"
 
 namespace MBSim {
 
-  ContactFlexible::ContactFlexible(const string &name) : Contact(name,false), gdT_grenz(0.1) {
+  ContactFlexible::ContactFlexible(const string &name) : Contact(name,false), gdT_grenz(0.1), flag_c(false) {
+    V_fun = new FuncConst(Vec(1,INIT,0.));
+    c_fun = new FuncConst(Vec(1,INIT,0.));
+    d_fun = new FuncConst(Vec(1,INIT,0.));
     active = false;
   }
 
-  ContactFlexible::ContactFlexible(const ContactFlexible *master,const string &name_) : Contact(master,name_), gdT_grenz(0.1) {
-    c = master->c;
-    d = master->d;
+  ContactFlexible::ContactFlexible(const ContactFlexible *master,const string &name_) : Contact(master,name_) {
+    V_fun = master->V_fun;
+    c_fun = master->c_fun;
+    d_fun = master->d_fun;
+    flag_c = master->flag_c;
+    gdT_grenz = master->gdT_grenz;
     active = false;
   }
+  
+  ContactFlexible::~ContactFlexible() {}
 
   void ContactFlexible::init() {
     Contact::init();
@@ -46,28 +55,32 @@ namespace MBSim {
   }
 
   void ContactFlexible::updateKinetics(double t) {
-    double mue0 = mue;
+  	static const double eps = epsroot();
+  	if(flag_c)
+    	if(gd(0)<0) la(0) = ((*c_fun)(g(0)))(0) + ((*d_fun)(gd(0)))(0); // normal direction
+    	else la(0) = ((*c_fun)(g(0)))(0);
+	else
+		if(gd(0)<0) la(0) = (((*V_fun)(g(0)))(0)-((*V_fun)(g(0)+eps))(0))/eps + ((*d_fun)(gd(0)))(0); // normal direction
+    	else la(0) = (((*V_fun)(g(0)))(0)-((*V_fun)(g(0)+eps))(0))/eps;
+		
 
-    if(gd(0)<0) 
-      la(0) = -c*g(0) - d*gd(0);
-    else
-      la(0) = -c*g(0);
-
-    if(nFric == 1) { 
-      if(fabs(gd(1)) < gdT_grenz)
-	la(1) = -la(0)*mue0*gd(1)/gdT_grenz;
-      else
-	la(1) = gd(1)>0?-la(0)*mue:la(0)*mue;
-    } else if(nFric == 2) {
+    if(nFric == 1) { // tangential directions
+	  if(fabs(gd(1)) < gdT_grenz) la(1) = -la(0)*((*mue_fun)(gdT_grenz))(0)*gd(1)/gdT_grenz;
+	  else la(1) = gd(1)>0?-la(0)*((*mue_fun)(gd(1)))(0):la(0)*((*mue_fun)(-gd(1)))(0);
+    }
+    else if(nFric == 2) {
       double norm_gdT = nrm2(gd(1,2));
-      if(norm_gdT < gdT_grenz)
-	la(1,2) = gd(1,2)*(-la(0)*mue0/gdT_grenz);
-      else
-	la(1,2) = gd(1,2)*(-la(0)*mue/norm_gdT);
+      if(norm_gdT < gdT_grenz) la(1,2) = gd(1,2)*(-la(0)*((*mue_fun)(gdT_grenz))(0)/gdT_grenz);
+      else la(1,2) = gd(1,2)*(-la(0)*((*mue_fun)(norm_gdT))(0)/norm_gdT);
     }
 
     WF[0] = getContourPointData(0).Wn*la(0) + getContourPointData(0).Wt*la(iT);
     WF[1] = -WF[0];
+  }
+  
+  double ContactFlexible::computePotentialEnergy() {
+    if(g(0)<0.) return ((*V_fun)(g(0)))(0);
+    else return 0.;
   }
 
 }
