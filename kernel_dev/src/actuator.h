fe827ccbbaf31ad4906cca1655fbb0f11c176650
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2008-11-23 21:04:28 +0000

    clean up

--- kernel_dev/src/actuator.h
+++ kernel_dev/src/actuator.h
@@ -1,3 +1,25 @@
+/* Copyright (C) 2004-2008  Martin Förg
+ 
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+ *
+ * Contact:
+ *   mfoerg@users.berlios.de
+ *
+ */
+
 #ifndef _ACTUATOR_H_
 #define _ACTUATOR_H_
 
@@ -33,8 +55,6 @@ namespace MBSim {
       void calclaSize();
       void init();
 
-      void initPlotFiles() {}
-      void plot(double t, double dt=1) {}
       void setKOSY(int);
       void setUserFunction(DataInterfaceBase *func_);
       void setSignal(DataInterfaceBase *func_);
