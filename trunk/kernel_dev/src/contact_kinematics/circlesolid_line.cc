6faa9dbffe85c6ac495fd0166528d305444cbf43
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2008-12-13 14:16:20 +0000

    new convetion for contour coordinate systems

--- trunk/kernel_dev/src/contact_kinematics/circlesolid_line.cc
+++ trunk/kernel_dev/src/contact_kinematics/circlesolid_line.cc
@@ -46,14 +46,14 @@ namespace MBSim {
     cpData[icircle].cosy.getOrientation().col(1) = -line->getCoordinateSystem()->getOrientation().col(1);
     cpData[icircle].cosy.getOrientation().col(2) = line->getCoordinateSystem()->getOrientation().col(2);
 
-    Vec Wn = cpData[iline].cosy.getOrientation().col(1);
+    Vec Wn = cpData[iline].cosy.getOrientation().col(0);
 
-    Vec Wd = line->getCoordinateSystem()->getPosition() - circlesolid->getCoordinateSystem()->getPosition();
+    Vec Wd = circlesolid->getCoordinateSystem()->getPosition() - line->getCoordinateSystem()->getPosition();
 
     g(0) = trans(Wn)*Wd - circlesolid->getRadius();
 
-    cpData[icircle].cosy.setPosition(circlesolid->getCoordinateSystem()->getPosition() + Wn*circlesolid->getRadius());
-    cpData[iline].cosy.setPosition(cpData[icircle].cosy.getPosition() + Wn*g(0));
+    cpData[icircle].cosy.setPosition(circlesolid->getCoordinateSystem()->getPosition() - Wn*circlesolid->getRadius());
+    cpData[iline].cosy.setPosition(cpData[icircle].cosy.getPosition() - Wn*g(0));
   }
 
   void ContactKinematicsCircleSolidLine::updategd(const Vec &g, Vec &gd, ContourPointData *cpData) {}
@@ -62,10 +62,10 @@ namespace MBSim {
 
     Vec b1 = cpData[iline].cosy.getOrientation().col(2);
     Vec b2 = cpData[icircle].cosy.getOrientation().col(2);
-    Vec n1 = cpData[iline].cosy.getOrientation().col(1);
-    Vec n2 = cpData[icircle].cosy.getOrientation().col(1);
-    Vec t1 = cpData[iline].cosy.getOrientation().col(0);
-    Vec t2 = cpData[icircle].cosy.getOrientation().col(0);
+    Vec n1 = cpData[iline].cosy.getOrientation().col(0);
+    Vec n2 = cpData[icircle].cosy.getOrientation().col(0);
+    Vec t1 = cpData[iline].cosy.getOrientation().col(1);
+    Vec t2 = cpData[icircle].cosy.getOrientation().col(1);
     Vec vC1 = cpData[iline].cosy.getVelocity();
     Vec vC2 = cpData[icircle].cosy.getVelocity();
     Vec Om1 = cpData[iline].cosy.getAngularVelocity();
@@ -80,11 +80,11 @@ namespace MBSim {
     //double sd1 = (kap2*trans(t1)*(vC2-vC1)+trans(b1)*(Om2-Om1))/(kap1+kap2);
     //double sd2 = (kap1*trans(t2)*(vC1-vC2)+trans(b2)*(Om1-Om2))/(kap1+kap2);
 
-    wb(0) += -trans(n1)*(-crossProduct(Om1,vC2-vC1) - crossProduct(Om1,t1)*ad1 + crossProduct(Om2,s2)*ad2);
+    wb(0) += trans(n1)*(-crossProduct(Om1,vC2-vC1) - crossProduct(Om1,t1)*ad1 + crossProduct(Om2,s2)*ad2);
     //wb(0) += trans(n1)*(-crossProduct(Om1,vC1)) - kap1*sd1*trans(t1)*vC1 + sd1*trans(b1)*Om1 + trans(n2)*(-crossProduct(Om2,vC2)) - kap2*sd2*trans(t2)*vC2 + sd2*trans(b1)*Om2;
     
     if(wb.size() > 1) 
-      wb(1) += -trans(t1)*(-crossProduct(Om1,vC2-vC1) - crossProduct(Om1,t1)*ad1 + crossProduct(Om2,s2)*ad2);
+      wb(1) += trans(t1)*(-crossProduct(Om1,vC2-vC1) - crossProduct(Om1,t1)*ad1 + crossProduct(Om2,s2)*ad2);
      // wb(1) += trans(t1)*(-crossProduct(Om1,vC1)) + kap1*sd1*trans(n1)*vC1 + trans(t2)*(-crossProduct(Om2,vC2)) + kap2*sd2*trans(n2)*vC2;
     
   }
--- trunk/kernel_dev/src/contact_kinematics/point_line.cc
+++ trunk/kernel_dev/src/contact_kinematics/point_line.cc
@@ -46,14 +46,14 @@ namespace MBSim {
     cpData[ipoint].cosy.getOrientation().col(1) = -line->getCoordinateSystem()->getOrientation().col(1);
     cpData[ipoint].cosy.getOrientation().col(2) = line->getCoordinateSystem()->getOrientation().col(2);
 
-    Vec Wn = cpData[iline].cosy.getOrientation().col(1);
+    Vec Wn = cpData[iline].cosy.getOrientation().col(0);
 
-    Vec Wd =  line->getCoordinateSystem()->getPosition() - point->getCoordinateSystem()->getPosition();
+    Vec Wd =  point->getCoordinateSystem()->getPosition() - line->getCoordinateSystem()->getPosition();
 
     g(0) = trans(Wn)*Wd;
 
     cpData[ipoint].cosy.setPosition(point->getCoordinateSystem()->getPosition());
-    cpData[iline].cosy.setPosition(cpData[ipoint].cosy.getPosition() + Wn*g(0));
+    cpData[iline].cosy.setPosition(cpData[ipoint].cosy.getPosition() - Wn*g(0));
   }
 
   void ContactKinematicsPointLine::updategd(const Vec& g, Vec &gd, ContourPointData *cpData) {}
@@ -61,10 +61,10 @@ namespace MBSim {
   void ContactKinematicsPointLine::updatewb(Vec &wb, const Vec &g, ContourPointData *cpData) {
 
     Vec b1 = cpData[iline].cosy.getOrientation().col(2);
-    Vec n1 = cpData[iline].cosy.getOrientation().col(1);
-    Vec n2 = cpData[ipoint].cosy.getOrientation().col(1);
-    Vec t1 = cpData[iline].cosy.getOrientation().col(0);
-    Vec t2 = cpData[ipoint].cosy.getOrientation().col(0);
+    Vec n1 = cpData[iline].cosy.getOrientation().col(0);
+    Vec n2 = cpData[ipoint].cosy.getOrientation().col(0);
+    Vec t1 = cpData[iline].cosy.getOrientation().col(1);
+    Vec t2 = cpData[ipoint].cosy.getOrientation().col(1);
     Vec vC1 = cpData[iline].cosy.getVelocity();
     Vec vC2 = cpData[ipoint].cosy.getVelocity();
     Vec Om1 = cpData[iline].cosy.getAngularVelocity();
@@ -73,11 +73,11 @@ namespace MBSim {
    // double kapsd2 = -trans(b1)*(Om2 - Om1);
     double sd1 = trans(t1)*(vC2 - vC1); //- g(0)*trans(b1)*Om1;
     // TODO: Vorzeichenkonvention anpassen
-    wb(0) += -trans(n1)*(-crossProduct(Om1,vC2-vC1) - crossProduct(Om1,t1)*sd1);
+    wb(0) += trans(n1)*(-crossProduct(Om1,vC2-vC1) - crossProduct(Om1,t1)*sd1);
     //wb(0) += trans(n1)*(-crossProduct(Om1,vC1)) + sd1 * trans(b1) * Om1
     //+ trans(n2)*(-crossProduct(Om2,vC2)) - kapsd2*trans(t2)*vC2;
     if(wb.size() > 1) 
-      wb(1) += -trans(t1)*(-crossProduct(Om1,vC2-vC1) - crossProduct(Om1,t1)*sd1);
+      wb(1) += trans(t1)*(-crossProduct(Om1,vC2-vC1) - crossProduct(Om1,t1)*sd1);
     //wb(1) += trans(t1)*(-crossProduct(Om1,vC1)) + trans(t2)*(-crossProduct(Om2,vC2)) + kapsd2*trans(n2)*vC2;
   }
 }
