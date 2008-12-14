243838fd4aa8ff211f940405d42d15cd37d103f3
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2008-12-14 13:53:47 +0000

    Added new example

---
+++ trunk/examples_dev/sphere_on_plane_with_rigid_contact/main.cc
@@ -0,0 +1,56 @@
+#include "system.h"
+#include <integrators.h>
+
+using namespace std;
+using namespace MBSim;
+
+int main (int argc, char* argv[])
+{
+  MultiBodySystem *sys = new System("TS");
+
+  sys->init();
+  
+  bool eventDriven = true;
+
+  if(eventDriven) { // Event driven time integration
+    LSODARIntegrator integrator;
+    integrator.setPlotOnRoot(false);
+    integrator.setdtPlot(1e-2);
+    integrator.settEnd(1.5);
+    integrator.integrate(*sys);
+  } 
+  else { // time stepping integration
+    double dt = 1e-4;
+    sys->setLaTol(1e-2*dt);
+    sys->setgdTol(1e-8);
+    TimeSteppingIntegrator integrator;
+    integrator.setdt(dt);
+    integrator.setdtPlot(1e-2);
+    integrator.settEnd(1.5);
+    integrator.integrate(*sys);
+  }
+
+  // RKSuiteIntegrator integrator;
+  // RADAU5Integrator integrator;
+  // TimeSteppingIntegrator integrator;
+  // integrator.setdt(1e-4);
+  //
+  //DOPRI5Integrator integrator;
+
+  //integrator.settEnd(10.0);
+
+//  TimeSteppingIntegrator integrator;
+//  integrator.setdt(1e-4);
+//  integrator.setdtPlot(1e-2);
+//  integrator.settEnd(1.8);
+//
+//  integrator.integrate(*sys);
+//  cout << "finished"<<endl;
+//  cout << sys->getq() << endl;
+//  cout << sys->getu() << endl;
+  delete sys;
+
+  return 0;
+
+}
+
---
+++ trunk/examples_dev/sphere_on_plane_with_rigid_contact/system.cc
@@ -0,0 +1,78 @@
+#include "system.h"
+#include "rigid_body.h"
+#include "objobject.h"
+#include "contour.h"
+#include "constitutive_laws.h"
+#include "contact.h"
+#include "load.h"
+#include "cube.h"
+#include "sphere.h"
+
+System::System(const string &projectName) : MultiBodySystem(projectName) {
+ // Gravitation
+  Vec grav(3);
+  grav(1)=-9.81;
+  setAccelerationOfGravity(grav);
+
+  addCoordinateSystem("Os",Vec(3),SqrMat(3,EYE));
+
+  Plane *plane = new Plane("Plane");
+  plane->setCn(Vec("[0;-1;0]"));
+  double phi = M_PI/2;
+  SqrMat AWK(3);
+  AWK(0,0) = cos(phi);
+  AWK(0,1) = -sin(phi);
+  AWK(1,1) = cos(phi);
+  AWK(1,0) = sin(phi);
+  AWK(2,2) = 1;
+  addContour(plane,Vec(3),AWK);
+
+  RigidBody* body = new RigidBody("Body");
+  addObject(body);
+
+  body->setFrameOfReference(getCoordinateSystem("I"));
+  body->setCoordinateSystemForKinematics(body->getCoordinateSystem("C"));
+
+  Mat J("[1,0,0;0,1,0;0,0,1]");
+  body->setTranslation(new LinearTranslation(J));
+  body->setRotation(new CardanAngles);
+  double m = 0.1;
+  double r = 0.1;
+  Vec q0(6);
+  q0(0) = 0;
+  q0(1) = 2.2*r;
+  q0(2) = 0;
+  body->setq0(q0);
+  //body->setu0("[0;0;0;0;0;-30]");
+  body->setu0("[3;0;1;0;0;0]");
+  //body->setu0("[0;0;0;20;0;-30]");
+ //body->setu0("[0;0;0;20;10;-30]");
+  //body->setu0("[3;0;0;4;-3;2]");
+  //body->setu0("[0;0;0;20;10;-30]");
+  body->setMass(m);
+  SymMat Theta(3);
+  Theta(0,0) =  2./5.*m*r*r;
+  Theta(1,1) =  2./5.*m*r*r;
+  Theta(2,2) =  2./5.*m*r*r;
+  body->setInertiaTensor(Theta);
+  Sphere *sphere = new Sphere("Sphere");
+  sphere->setRadius(r);
+  body->addContour(sphere,Vec(3),SqrMat(3,EYE));
+
+    Contact *cnf = new Contact("Contact");
+    cnf->setContactForceLaw(new UnilateralConstraint);
+    cnf->setContactImpactLaw(new UnilateralNewtonImpact(0.0));
+    //cnf->setContactForceLaw(new LinearRegularizedUnilateralConstraint(1e-6,100));
+    //cnf->setFrictionForceLaw(new LinearRegularizedSpatialCoulombFriction(0.3));
+    cnf->setFrictionForceLaw(new SpatialCoulombFriction(0.1));
+    cnf->setFrictionImpactLaw(new SpatialCoulombImpact(0.1));
+    cnf->connect(getContour("Plane"), body->getContour("Sphere"));
+    // cnf->setFrictionCoefficient(0.3);
+    cnf->setPlotLevel(2);
+    addLink(cnf);
+
+    AMVis::Sphere *obj = new AMVis::Sphere(body->getName(),1,false);
+    body->setAMVisBody(obj);
+    obj->setRadius(r);
+}
+
