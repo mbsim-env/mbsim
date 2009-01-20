0f8fa4c8829671fd9c2b5f0e4a68ce5996c54ee5
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-01-20 18:33:15 +0000

    changed F77 to FC

--- kernel_dev/src/integrators/fortran/fortran_wrapper.h
+++ kernel_dev/src/integrators/fortran/fortran_wrapper.h
@@ -8,72 +8,72 @@ extern "C" {
 #endif
 
 
-#define DLSODAR F77_FUNC(dlsodar,DLSODAR)
+#define DLSODAR FC_FUNC(dlsodar,DLSODAR)
 void DLSODAR(void (*)(int*,double*,double*,double*), int*, double*, double*,
             double*, int*, double*, double*, int*, int*, int*, double*, int*,
             int*, int*, void(*)(int*,double*,double*,int*,int*,double*,int*),
 	    int*, void(*)(int*, double*, double*,
             int*, double*), int*, int*);
 
-#define DLSODER F77_FUNC(dlsoder,DLSODER)
+#define DLSODER FC_FUNC(dlsoder,DLSODER)
 void DLSODER(void (*)(int*,double*,double*,double*), int*, double*, double*,
             double*, int*, double*, double*, int*, int*, int*, double*, int*,
             int*, int*, void(*)(int*,double*,double*,int*,int*,double*,int*),
 	    int*, void(*)(int*, double*, double*,
             int*, double*), int*, int*);
 
-#define DLSODE F77_FUNC(dlsode,DLSODE)
+#define DLSODE FC_FUNC(dlsode,DLSODE)
 void DLSODE(void (*)(int*,double*,double*,double*), int*, double*, double*,
             double*, int*, double*, double*, int*, int*, int*, double*, int*,
             int*, int*, void(*)(int*,double*,double*,int*,int*,double*,int*), int*);
 
-#define SETUP F77_FUNC(setup,SETUP)
+#define SETUP FC_FUNC(setup,SETUP)
 void SETUP(int*, double*, double*, double*,
            double*, double*, int*, char*, int*,
            double*, double*, int*, int*);
 
-#define UT F77_FUNC(ut,UT)
+#define UT FC_FUNC(ut,UT)
 void UT(void (*)(double*,double*,double*), double*, double*, double*, double*,
         double*, double*, int*, double*);
 
-#define DOPRI5 F77_FUNC(dopri5,DOPRI5)
+#define DOPRI5 FC_FUNC(dopri5,DOPRI5)
 void DOPRI5(int*,void(*)(int*,double*,double*,double*,double*,int*),double*,double*,
             double*, double*,double*,int*,
 	    void(*)(int*,double*,double*,double*,int*,double*,int*,int*,double*,int*,int*),
             int*,double*,int*,int*,int*,double*,int*,int*);
 
-#define CONTD5 F77_FUNC(contd5,CONTD5)
+#define CONTD5 FC_FUNC(contd5,CONTD5)
 double CONTD5(int*,double*,double*,int*,int*);
 
-#define DOP853 F77_FUNC(dop853,DOP853)
+#define DOP853 FC_FUNC(dop853,DOP853)
 void DOP853(int*,void(*)(int*,double*,double*,double*,double*,int*),double*,double*,
             double*, double*,double*,int*,
 	    void(*)(int*,double*,double*,double*,int*,double*,int*,int*,double*,int*,int*),
             int*,double*,int*,int*,int*,double*,int*,int*);
 
-#define CONTD8 F77_FUNC(contd8,CONTD8)
+#define CONTD8 FC_FUNC(contd8,CONTD8)
 double CONTD8(int*,double*,double*,int*,int*);
 
-#define ODEX F77_FUNC(odex,ODEX)
+#define ODEX FC_FUNC(odex,ODEX)
 void ODEX(int*,void(*)(int*,double*,double*,double*,double*,int*),double*,double*,
             double*, double*,double*,double*,int*,
 	    void(*)(int*,double*,double*,double*,int*,double*,int*,int*,int*,double*,int*,int*),
             int*,double*,int*,int*,int*,double*,int*,int*);
 
-#define CONTEX F77_FUNC(contex,CONTEX)
+#define CONTEX FC_FUNC(contex,CONTEX)
 double CONTEX(int*,double*,double*,int*,int*,int*);
 
-#define RADAU5 F77_FUNC(radau5,RADAU5)
+#define RADAU5 FC_FUNC(radau5,RADAU5)
 void RADAU5(int*,void(*)(int*,double*,double*,double*,double*,int*),double*,double*,double*,
     double*,double*,double*,int*,void(*)(int*,double*,double*,double*,int*,double*,int*),
     int*,int*,int*,void(*)(int*,double*,int*,double*,int*),int*,int*,int*,
     void(*)(int*,double*,double*,double*,double*,int*,int*,double*,int*,int*),
     int*,double*,int*,int*,int*,double*,int*,int*);
 
-#define CONTR5 F77_FUNC(contr5,CONTR5)
+#define CONTR5 FC_FUNC(contr5,CONTR5)
 double CONTR5(int*,double*,double*,int*);
 
-#define DDASKR F77_FUNC(ddaskr,DDASKR)
+#define DDASKR FC_FUNC(ddaskr,DDASKR)
 void DDASKR(void(*)(double*,double*,double*,double*,double*, double*,int*), int*, double*, double*, double*, double*, int*, double*, double*,
         int*, double*, int*, int*, int*, double*, int*, void(*)(double*, double*, double*, double*, double*, double*, int*), double*,
         void(*)(int*,double*,double*,double*,int*, double*,double*,int*),int*, int*);
