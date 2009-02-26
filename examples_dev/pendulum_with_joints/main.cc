19a9089b1c3ed1a599b6f0493d27c499d3239a05
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-02-26 19:14:01 +0000

    merged two examples

---
+++ examples_dev/pendulum_with_joints/main.cc
@@ -0,0 +1,42 @@
+#include "system.h"
+#include <integrators.h>
+
+using namespace std;
+
+bool rigidJoints;
+
+int main (int argc, char* argv[]) {
+
+  char dummy[10000];
+  double tEnd, dt, dtPlot;
+  bool eventDriven;
+
+  // Beginn input
+  ifstream is("input.asc");
+  is >> rigidJoints;
+  is.getline(dummy,10000);
+  is >> tEnd;
+  is.getline(dummy,10000);
+  is >> dtPlot;
+  is.getline(dummy,10000);
+  is.close();
+
+  MultiBodySystem *sys = new System("MBS");
+
+  sys->init();
+  
+  DOPRI5Integrator integrator;
+
+  integrator.settEnd(tEnd);
+  integrator.setdtPlot(dtPlot);
+
+  integrator.integrate(*sys);
+
+  cout << "finished"<<endl;
+
+  delete sys;
+
+  return 0;
+
+}
+
