c67f71e9f38728b8959a3e602749f2fba302d5bd
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-02-26 18:59:14 +0000

    Merged to examples.

---
+++ examples_dev/cylinder_on_plane/main.cc
@@ -0,0 +1,63 @@
+#include "system.h"
+#include <integrators.h>
+
+using namespace std;
+using namespace MBSim;
+
+bool rigidContact;
+
+int main (int argc, char* argv[]) {
+
+  char dummy[10000];
+  string linAlg, solver, strategy, useOldla;
+  double tEnd, dt, dtPlot;
+  bool eventDriven;
+
+  // Beginn input
+  ifstream is("input.asc");
+  is >> rigidContact;
+  is.getline(dummy,10000);
+  is >> eventDriven;
+  is.getline(dummy,10000);
+  is >> tEnd;
+  is.getline(dummy,10000);
+  is >> dtPlot;
+  is.getline(dummy,10000);
+  is >> dt;
+  is.getline(dummy,10000);
+  is.close();
+
+  MultiBodySystem *sys = new System("TS");
+
+  sys->init();
+
+  Integrator* integrator;
+
+  if(!rigidContact) {
+    integrator = new LSODEIntegrator;
+  } 
+  else if(eventDriven) { // Event driven time integration
+    integrator = new LSODARIntegrator;
+    static_cast<LSODARIntegrator*>(integrator)->setPlotOnRoot(false);
+    static_cast<LSODARIntegrator*>(integrator)->setdt0(1e-13);
+  } 
+  else { // time stepping integration
+    sys->setLaTol(1e-2*dt);
+    sys->setgdTol(1e-8);
+    integrator = new TimeSteppingIntegrator;
+    static_cast<TimeSteppingIntegrator*>(integrator)->setdt(dt);
+  }
+
+  integrator->settEnd(tEnd);
+  integrator->setdtPlot(dtPlot);
+  integrator->integrate(*sys);
+
+  cout << "finished"<<endl;
+
+  delete integrator;
+  delete sys;
+
+  return 0;
+
+}
+
