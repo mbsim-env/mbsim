73570b9daf5f15a41183a27fa0277c8ee76b7d63
thschindler <thschindler@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-03-24 16:32:40 +0000

    planar beam example

---
+++ trunk/examples_dev/planar_beam_with_large_deflections/main.cc
@@ -0,0 +1,31 @@
+#include "system.h"
+#include <mbsim/integrators/integrators.h>
+
+using namespace std;
+
+int main (int argc, char* argv[]) {
+
+  MultiBodySystem *sys = new System("MBS");
+
+  sys->setStopIfNoConvergence(true,true);
+  sys->init();
+
+  
+  TimeSteppingIntegrator integrator;
+
+  integrator.settEnd(5);
+  integrator.setdt(5.e-4);
+  integrator.setdtPlot(5e-3);
+
+  integrator.integrate(*sys);
+
+  sys->closePlot();
+
+  cout << "finished"<<endl;
+
+  delete sys;
+
+  return 0;
+
+}
+
---
+++ trunk/examples_dev/planar_beam_with_large_deflections/system.cc
@@ -0,0 +1,58 @@
+#include "system.h"
+#include "mbsim/flexible_body/flexible_body_1s_21_rcm.h"
+#include "mbsim/joint.h"
+#include "mbsim/constitutive_laws.h"
+
+using namespace AMVis;
+
+System::System(const string &projectName) : MultiBodySystem(projectName) {
+
+  Vec q02D, u02D;
+
+  double l0 = 1.5; // length
+  double b0 = 0.1; // width
+  double E = 5.e7; // E-Modul alu 7.e10; E-Modul iron 2.1e11; E-Modul rubber 5.e7
+  double A = b0*b0; // cross-section area
+  double I1 = 1./12.*b0*b0*b0*b0; // moment inertia
+  double rho = 9.2e2; // density alu 2.7e3; density iron 7.9e3; density rubber 9.2e2
+  int elements = 2; // number of finite elements
+
+
+  /* 2D-Beam */ 
+  FlexibleBody1s21RCM *rod2D = new FlexibleBody1s21RCM("Rod2D", true);
+  rod2D->setLength(l0);
+  rod2D->setEModul(E);
+  rod2D->setCrossSectionalArea(A);
+  rod2D->setMomentInertia(I1);
+  rod2D->setDensity(rho);
+  rod2D->setStationaryFrameOfReference(this->getFrame("I"));
+
+  rod2D->setNumberElements(elements);
+  rod2D->setAMVisCuboid(b0,b0);
+
+  q02D = Vec(5*elements+3,INIT,0.);
+  for(int i=1;i<=elements;i++) q02D(5*i) = l0*i/elements;
+
+  u02D = Vec(5*elements+3,INIT,0.);
+  for(int i=0;i<=elements;i++) {
+    u02D(5*i+2) = 0.1;
+    u02D(5*i+1) = 0.1*l0*i/elements;
+  }
+
+  ContourPointData cpdata;
+  cpdata.alpha = Vec(1,INIT,0.);
+  cpdata.type = CONTINUUM;
+  rod2D->addFrame("RJ",cpdata);
+  Joint *joint2D = new Joint("Clamping_2D");
+  joint2D->connect(this->getFrame("I"),rod2D->getFrame("RJ")); 
+  joint2D->setForceDirection(Mat("[1,0; 0,1; 0,0]"));
+  joint2D->setForceLaw(new BilateralConstraint);
+  joint2D->setImpactForceLaw(new BilateralImpact);
+  this->addLink(joint2D);
+  
+  rod2D->setq0(q02D);
+  rod2D->setu0(u02D);
+  rod2D->createAMVisBody(true);
+  this->addObject(rod2D);
+}
+
