2f5cf68d71cc9a2c8a86029f5c9e44bf7dfae1d2
thschindler <thschindler@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-03-25 13:00:04 +0000

    svn conflicts solved some comments

--- kernel_dev/mbsim/class_factory.h
+++ kernel_dev/mbsim/class_factory.h
@@ -1,23 +1,20 @@
-/* Copyright (C) 2004-2006  Martin Förg
- 
+/* Copyright (C) 2004-2009 MBSim Development Team
+ * 
  * This library is free software; you can redistribute it and/or 
  * modify it under the terms of the GNU Lesser General Public 
  * License as published by the Free Software Foundation; either 
  * version 2.1 of the License, or (at your option) any later version. 
- *  
+ * 
  * This library is distributed in the hope that it will be useful, 
  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. 
- *  
+ *
  * You should have received a copy of the GNU Lesser General Public 
  * License along with this library; if not, write to the Free Software 
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
-
  *
- * Contact:
- *   mfoerg@users.berlios.de
- *
+ * Contact: mfoerg@users.berlios.de
  */
 
 #ifndef _CLASS_FACTORY_H_
@@ -39,8 +36,14 @@ namespace MBSim {
   class GeneralizedImpactLaw;
   class FrictionImpactLaw;
 
+  /**
+   * \brief creates default MBSim incredients for topological load mechanism
+   * \author Martin Foerg
+   * \date 2009-03-25 some comments (Thorsten Schindler)
+   */
   class ClassFactory {
     public:
+      /* GETTER */
       Subsystem* getSubsystem(const string &type);
       Object* getObject(const string &type);
       Link* getLink(const string &type);
--- kernel_dev/mbsim/link.cc
+++ kernel_dev/mbsim/link.cc
@@ -1,24 +1,22 @@
-/* Copyright (C) 2004-2006  Martin Förg
- 
+/* Copyright (C) 2004-2009 MBSim Development Team
+ * 
  * This library is free software; you can redistribute it and/or 
  * modify it under the terms of the GNU Lesser General Public 
  * License as published by the Free Software Foundation; either 
  * version 2.1 of the License, or (at your option) any later version. 
- *  
+ * 
  * This library is distributed in the hope that it will be useful, 
  * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details. 
- *  
+ *
  * You should have received a copy of the GNU Lesser General Public 
  * License along with this library; if not, write to the Free Software 
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
-
  *
- * Contact:
- *   mfoerg@users.berlios.de
- *
+ * Contact: mfoerg@users.berlios.de
  */
+
 #include <config.h>
 #include <string.h>
 #include <mbsim/link.h>
@@ -36,10 +34,10 @@ using namespace AMVis;
 
 namespace MBSim {
 
-  Link::Link(const string &name) : Element(name), parent(0), xSize(0), xInd(0), svSize(0), svInd(0), gSize(0), gInd(0), gdSize(0), gdInd(0), laSize(0), laInd(0), rFactorSize(0), scaleTolQ(1e-9), scaleTolp(1e-5), gdTol(1e-6), gddTol(1e-6), laTol(1e-6), LaTol(1e-6), rMax(1.0), HSLink(0), checkHSLink(false) { // , active(true), parent(0), 
-  }
+  Link::Link(const string &name) : Element(name), parent(0), xSize(0), xInd(0), svSize(0), svInd(0), gSize(0), gInd(0), gdSize(0), gdInd(0), laSize(0), laInd(0), rFactorSize(0), scaleTolQ(1e-9), scaleTolp(1e-5), gdTol(1e-6), gddTol(1e-6), laTol(1e-6), LaTol(1e-6), rMax(1.0), HSLink(0), checkHSLink(false) {}
 
   Link::~Link() { 
+
 #ifdef HAVE_AMVIS   
     for (unsigned int i=0; i<arrowAMVis.size(); i++) {
       delete arrowAMVis[i];
@@ -150,86 +148,86 @@ namespace MBSim {
         plotVector.push_back(computePotentialEnergy()); 
       }
 
-/*#ifdef HAVE_AMVIS
-      Vec WrOToPoint;
-      Vec LoadArrow(6,NONINIT);
-      for (unsigned int i=0; i<arrowAMVis.size(); i++) {
+      /*#ifdef HAVE_AMVIS
+        Vec WrOToPoint;
+        Vec LoadArrow(6,NONINIT);
+        for (unsigned int i=0; i<arrowAMVis.size(); i++) {
         WrOToPoint = port[arrowAMVisID[i]]->getPosition();
         if(setValued){ 
-          if (isActive()) {
-            LoadArrow(0,2) = fF[arrowAMVisID[i]]*la/dt;
-            LoadArrow(3,5) = fM[arrowAMVisID[i]]*la/dt;
-          }
-          else {
-            LoadArrow = Vec(6,INIT,0.0);
-            WrOToPoint= Vec(3,INIT,0.0);
-          }
+        if (isActive()) {
+        LoadArrow(0,2) = fF[arrowAMVisID[i]]*la/dt;
+        LoadArrow(3,5) = fM[arrowAMVisID[i]]*la/dt;
         }
         else {
-          LoadArrow(0,2) = WF[arrowAMVisID[i]];
-          LoadArrow(3,5) = WM[arrowAMVisID[i]];
+        LoadArrow = Vec(6,INIT,0.0);
+        WrOToPoint= Vec(3,INIT,0.0);
         }
-        // Scaling: 1KN or 1KNm scaled to arrowlenght one
-        LoadArrow= LoadArrow/1000*arrowAMVisScale[i];
+        }
+        else {
+        LoadArrow(0,2) = WF[arrowAMVisID[i]];
+        LoadArrow(3,5) = WM[arrowAMVisID[i]];
+        }
+      // Scaling: 1KN or 1KNm scaled to arrowlenght one
+      LoadArrow= LoadArrow/1000*arrowAMVisScale[i];
 
-        arrowAMVis[i]->setTime(t);
-        arrowAMVis[i]->setToPoint(WrOToPoint(0),WrOToPoint(1),WrOToPoint(2));
-        double color;
-        if (arrowAMVisMoment[i]) {
-          arrowAMVis[i]->setDirection(LoadArrow(3),LoadArrow(4),LoadArrow(5));
-          color=0.5;
-        }
-        else {
-          arrowAMVis[i]->setDirection(LoadArrow(0),LoadArrow(1),LoadArrow(2));
-          color =1.0;
-        }
-        if (arrowAMVisUserFunctionColor[i]) {
-          color = (*arrowAMVisUserFunctionColor[i])(t)(0);
-          if (color>1) color=1;
-          if (color<0) color=0;
-        }  
-        arrowAMVis[i]->setColor(color);
-        arrowAMVis[i]->appendDataset(0);
+      arrowAMVis[i]->setTime(t);
+      arrowAMVis[i]->setToPoint(WrOToPoint(0),WrOToPoint(1),WrOToPoint(2));
+      double color;
+      if (arrowAMVisMoment[i]) {
+      arrowAMVis[i]->setDirection(LoadArrow(3),LoadArrow(4),LoadArrow(5));
+      color=0.5;
+      }
+      else {
+      arrowAMVis[i]->setDirection(LoadArrow(0),LoadArrow(1),LoadArrow(2));
+      color =1.0;
+      }
+      if (arrowAMVisUserFunctionColor[i]) {
+      color = (*arrowAMVisUserFunctionColor[i])(t)(0);
+      if (color>1) color=1;
+      if (color<0) color=0;
+      }  
+      arrowAMVis[i]->setColor(color);
+      arrowAMVis[i]->appendDataset(0);
       }
 
       for (unsigned int i=0; i<arrowAMVis.size(); i++) {
-       // WrOToPoint = cpData[arrowAMVisID[i]].WrOC;
-        if(setValued) { 
-          if (isActive()) {
-            LoadArrow(0,2) = fF[arrowAMVisID[i]]*la/dt;
-            LoadArrow(3,5) = fM[arrowAMVisID[i]]*la/dt;
-          }
-          else {
-            LoadArrow = Vec(6,INIT,0.0);
-            WrOToPoint= Vec(3,INIT,0.0);
-          }
-        }
-        else {
-          LoadArrow(0,2) = WF[arrowAMVisID[i]];
-          LoadArrow(3,5) = WM[arrowAMVisID[i]];
-        }
-        // Scaling: 1KN or 1KNm scaled to arrowlenght one
-        LoadArrow(0,2)= LoadArrow(0,2)/1000*arrowAMVisScale[i];
-
-        arrowAMVis[i]->setTime(t);
-        arrowAMVis[i]->setToPoint(WrOToPoint(0),WrOToPoint(1),WrOToPoint(2));
-        double color;
-        if (arrowAMVisMoment[i]) {
-          arrowAMVis[i]->setDirection(LoadArrow(3),LoadArrow(4),LoadArrow(5));
-          color=0.5;
-        }
-        else {
-          arrowAMVis[i]->setDirection(LoadArrow(0),LoadArrow(1),LoadArrow(2));
-          color =1.0;
-        }
-        if (arrowAMVisUserFunctionColor[i]) {
-          color = (*arrowAMVisUserFunctionColor[i])(t)(0);
-          if (color>1) color=1;
-          if (color<0) color=0;
-        }  
-        arrowAMVis[i]->setColor(color);
-        arrowAMVis[i]->appendDataset(0);
+      // WrOToPoint = cpData[arrowAMVisID[i]].WrOC;
+      if(setValued) { 
+      if (isActive()) {
+      LoadArrow(0,2) = fF[arrowAMVisID[i]]*la/dt;
+      LoadArrow(3,5) = fM[arrowAMVisID[i]]*la/dt;
       }
+      else {
+      LoadArrow = Vec(6,INIT,0.0);
+      WrOToPoint= Vec(3,INIT,0.0);
+      }
+      }
+      else {
+      LoadArrow(0,2) = WF[arrowAMVisID[i]];
+      LoadArrow(3,5) = WM[arrowAMVisID[i]];
+      }
+      // Scaling: 1KN or 1KNm scaled to arrowlenght one
+      LoadArrow(0,2)= LoadArrow(0,2)/1000*arrowAMVisScale[i];
+
+      arrowAMVis[i]->setTime(t);
+      arrowAMVis[i]->setToPoint(WrOToPoint(0),WrOToPoint(1),WrOToPoint(2));
+      double color;
+      if (arrowAMVisMoment[i]) {
+      arrowAMVis[i]->setDirection(LoadArrow(3),LoadArrow(4),LoadArrow(5));
+      color=0.5;
+      }
+      else {
+      arrowAMVis[i]->setDirection(LoadArrow(0),LoadArrow(1),LoadArrow(2));
+      color =1.0;
+    }
+    if (arrowAMVisUserFunctionColor[i]) {
+      color = (*arrowAMVisUserFunctionColor[i])(t)(0);
+      if (color>1) color=1;
+      if (color<0) color=0;
+    }  
+    arrowAMVis[i]->setColor(color);
+    arrowAMVis[i]->appendDataset(0);
+    }
 #endif*/
       Element::plot(t,dt);
     }
@@ -258,14 +256,14 @@ namespace MBSim {
         plotColumns.push_back("V");
       }
 
-/*#ifdef HAVE_AMVIS
-      for (unsigned int i=0; i<arrowAMVis.size(); i++)
+      /*#ifdef HAVE_AMVIS
+        for (unsigned int i=0; i<arrowAMVis.size(); i++)
         arrowAMVis[i]->writeBodyFile();
 #endif*/
       Element::initPlot(parent);
     }
   }
-  
+
   void Link::closePlot() {
     if(getPlotFeature(plotRecursive)==enabled) {
       Element::closePlot();
@@ -287,7 +285,7 @@ namespace MBSim {
   void Link::decreaserFactors() {
     for(int i=0; i<rFactor.size(); i++)
       if(rFactorUnsure(i))
-	rFactor(i) *= 0.9;
+        rFactor(i) *= 0.9;
   }
 
 
@@ -424,7 +422,7 @@ namespace MBSim {
 
     for(unsigned i=0; i<port.size(); i++) 
       r[i] += W[i]*la;
-    
+
     for(unsigned i=0; i<contour.size(); i++) 
       r[i] += W[i]*la;
   }
