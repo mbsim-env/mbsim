8f5e5f345bd7ec53fd3362a784872a715f6d7fe8
rzander <rzander@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-03-26 23:18:33 +0000

    added and enhanced comments

--- trunk/kernel_dev/mbsim/interfaces.h
+++ trunk/kernel_dev/mbsim/interfaces.h
@@ -201,83 +201,101 @@ namespace MBSim {
   };
 
   /*!
+   * all Link%s use this common interface regardless whether single- or set-valued
+   * / smooth or non-smooth interactions are defined
    * \brief interface for links
    * \author Martin Foerg
    * \date 2009-03-09 some comments (Thorsten Schindler)
+   * \date 2009-03-26 enhanced comments (Roland Zander)
    */
   class LinkInterface {
     public:
-      /**
+      /*!
        * \brief constructor 
        */
       LinkInterface() {}
 
-      /**
+      /*!
        * \brief destructor
        */
       virtual ~LinkInterface() {}
 
-      /**
-       * \brief update nonsmooth right hand side
-       * \param simulation time 
+      /*!
+	   * for links holding non-smooth contributions \f$\dd\vLambda\f$, the respective forces are projected
+	   * into the minimal coordinate representation of the associated Body%s using the
+	   * JACOBIAN matrices \f$\vJ\f$.
+	   * \f[ \vr = \vJ\dd\vLambda \f]
+	   * The JACOBIAN is provided by the connected Frame%s (which might be
+	   * user-defined for Joint%s or internally defined for Contact%s).
+       * \brief update smooth link force law
+       * \param simulation time
        */
       virtual void updater(double t) = 0;
 
-      /**
+      /*!
        * \brief updates nonlinear relative acceleration term
        * \param simulation time
        */
       virtual void updatewb(double t) = 0;
 
-      /**
-       * \brief updates JACOBIAN matrix between Lagrangian multipliers and generalised velocities
+      /*!
+       * \brief updates JACOBIAN matrix \f$\vW\f$ between Lagrangian multipliers and generalised velocities
        * \param simulation time
        */
       virtual void updateW(double t) = 0;
 
-      /**
+      /*!
+	   * for event driven integration, \f$\vW\f$ can be condensed regarding dependent
+	   * tangential contacts, where \f$\Lambda_N\f$ also occures in evaluation of tangential
+	   * force when sliding; in this case, \f$\vV\f$ might hold less columns than \f$\vW\f$
        * \brief updates condensed JACOBIAN matrix between condensed Lagrangian multipliers and generalised velocities
        * \param simulation time
        */
       virtual void updateV(double t) = 0;
 
-      /**
-       * \brief update smooth link law
+      /*!
+	   * for links holding smooth contributions \f$\vF\f$, the respective forces are projected
+	   * into the minimal coordinate representation of the associated Body%s using the
+	   * JACOBIAN matrices \f$\vJ\f$.
+	   * \f[ \vh = \vJ\vF \f]
+	   * The JACOBIAN is provided by the connected Frame%s (which might be
+	   * user-defined for Joint%s or internally defined for Contact%s).
+       * \brief update smooth link force law
        * \param simulation time
        */
       virtual void updateh(double t) = 0;
 
-      /**
+      /*!
        * \brief update relative distance
        * \param simulation time
        */
       virtual void updateg(double t) = 0;
       
-      /**
+      /*!
        * \brief update relative velocity
        * \param simulation time
        */
       virtual void updategd(double t) = 0;
 
-      /**
+      /*!
        * \brief update TODO
        * \param simulation time
        */
       virtual void updatedx(double t, double dt) = 0;
       
-      /**
+      /*!
        * \brief update TODO
        * \param simulation time
        */
       virtual void updatexd(double t) = 0;
 
-      /**
+      /*!
        * \brief update stop vector (root functions for event driven integration)
        * \param simulation time
        */
       virtual void updateStopVector(double t) = 0;
 
-      /**
+      /*!
        * \brief update acceleration description of frames and contours
        * \param simulation time
        */
@@ -294,87 +312,87 @@ namespace MBSim {
    */
   class DiscretizationInterface {
     public:
-      /**
+      /*!
        * \brief constructor
        */
       DiscretizationInterface() {}    
 
-      /** 
+      /*! 
        * \brief destructor
        */
       virtual ~DiscretizationInterface() {}    
 
-      /**
+      /*!
        * \return mass matrix of discretization
        */
       virtual fmatvec::SymMat getMassMatrix() const = 0;    
 
-      /**
+      /*!
        * \return smooth right hand side of discretization
        */
       virtual fmatvec::Vec getGeneralizedForceVector() const = 0;
 
-      /**
+      /*!
        * \return Jacobian of implicit integration regarding position
        */
       virtual fmatvec::SqrMat getJacobianForImplicitIntegrationRegardingPosition() const = 0;    
 
-      /**
+      /*!
        * \return Jacobian of implicit integration regarding velocity
        */
       virtual fmatvec::SqrMat getJacobianForImplicitIntegrationRegardingVelocity() const = 0;
 
-      /**
+      /*!
        * \return dimension of positions
        */
       virtual int getSizeOfPositions() const = 0;
 
-      /**
+      /*!
        * \return dimension of velocities
        */
       virtual int getSizeOfVelocities() const = 0;
 
-      /**
+      /*!
        * \brief compute equations of motion
        * \param generalised positions
        * \param generalised velocities
        */
       virtual void computeEquationsOfMotion(const fmatvec::Vec& q,const fmatvec::Vec& u) = 0;
 
-      /**
+      /*!
        * \brief compute kinetic energy
        * \param generalised positions
        * \param generalised velocities
        */
       virtual double computeKineticEnergy(const fmatvec::Vec& q,const fmatvec::Vec& u) = 0;
 
-      /** 
+      /*! 
        * \brief compute gravitational energy
        * \param generalised positions
        */
       virtual double computeGravitationalEnergy(const fmatvec::Vec& q) = 0;    
 
-      /**
+      /*!
        * \brief compute elastic energy
        * \param generalised positions
        */
       virtual double computeElasticEnergy(const fmatvec::Vec& q) = 0;
 
-      /** 
+      /*! 
        * \brief compute position of contour in physical representation
        * \param generalised positions
        * \param contour location
        */
       virtual fmatvec::Vec computeTranslation(const fmatvec::Vec& q, const ContourPointData &data) = 0;
 
-      /**
+      /*!
        * \brief compute orientation of contour in physical representation
        * \param generalised coordiantes
        * \param contour location
        */
       virtual fmatvec::SqrMat computeOrientation(const fmatvec::Vec& q, const ContourPointData &data) = 0;
 
-      /**
+      /*!
        * \brief compute translational velocity of contour in physical representation
        * \param generalised positions
        * \param generalised velocities
@@ -382,7 +400,7 @@ namespace MBSim {
        */
       virtual fmatvec::Vec computeTranslationalVelocity(const fmatvec::Vec& q, const fmatvec::Vec& u, const ContourPointData &data) = 0;
 
-      /**
+      /*!
        * \brief compute angular velocity of contour in physical representation
        * \param generalised positions
        * \param generalised velocities
@@ -390,7 +408,7 @@ namespace MBSim {
        */
       virtual fmatvec::Vec computeAngularVelocity(const fmatvec::Vec& q, const fmatvec::Vec& u, const ContourPointData &data) = 0;
 
-      /**
+      /*!
        * \brief compute Jacobian of minimal representation regarding physical representation
        * \param generalised positions
        * \param contour location
