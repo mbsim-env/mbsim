f86a8eb0b49fb1e7d5f5b6cec2bab13733cf02a5
mafriedrich <mafriedrich@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-03-27 11:29:45 +0000

    Added diff with reference soulution in subdir reference

--- examples_dev/runexamples.sh
+++ examples_dev/runexamples.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 FAILED=""
-GOOD=""
+DIFF=""
 find -name "*.d" -exec rm -f {} \;
 
 for D in $(find -maxdepth 1 -type d | grep -v "^\.$" | grep -v "^\./\."); do
@@ -14,16 +14,33 @@ for D in $(find -maxdepth 1 -type d | grep -v "^\.$" | grep -v "^\./\."); do
   ./main && \
   ERROR=0
 
-  if [ $ERROR -ne 0 ]; then
-    echo "ERROR RUNNING EXMAPLE $D"
-    FAILED="$FAILED\n$D"
+  if [ $ERROR -eq 0 ]; then
+    echo "EXAMPLE $D PASSED COMPILING AND RUNNING"
   else
-    echo "EXAMPLE $D PASSED"
-    GOOD="$GOOD\n$D"
+    echo "EXAMPLE $D FAILED COMPILING OR RUNNING"
+    FAILED="$FAILED\n$D"
+  fi
+
+  if test -d reference; then
+    for H5F in $(cd reference && find -name "*.h5"); do
+      for DS in $(../../local/bin/h5lsserie $H5F | sed -nre "s|^.*\(Path: (.*)\)|\1|p"); do
+        P=$(echo $DS | sed -re "s|^.*\.h5/(.*)|\1|")
+        ../../local/bin/h5diff --relative=1e-6 $H5F reference/$H5F $P $P
+        RET=$?
+        if [ $RET -ne 0 ]; then
+          echo "EXAMPLE $DS FAILED DIFF WITH REFERENCE"
+          DIFF="$DIFF\n$D/$DS"
+        else
+          echo "EXAMPLE $DS PASSED DIFF WITH REFERENCE"
+        fi
+      done
+    done
   fi
 
   cd ..
 done
 
-echo -e "EXAMPLES PASSED:$GOOD"
-echo -e "EXAMPLES FAILED:$FAILED"
+echo -e "\n\n\n\n\n\n\n\n\n\n"
+echo -e "EXAMPLES FAILED COMPINLING OR RUNNING:$FAILED"
+echo -e "\n"
+echo -e "EXAMPLES FAILED DIFF:$DIFF"
