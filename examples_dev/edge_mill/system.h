e0f7ff9f9c1995c8c6d4e512d563c2cefe3296c4
thschindler <thschindler@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-04-29 15:02:03 +0000

    VRML examples "really" added

---
+++ examples_dev/edge_mill/system.h
@@ -0,0 +1,21 @@
+#ifndef _SYS_H
+#define _SYS_H
+
+#include "mbsim/dynamic_system_solver.h"
+#include <string>
+
+class System : public MBSim::DynamicSystemSolver {
+  public:
+    System(const std::string &projectName);
+
+  private:
+    struct Vertex { // data structure for vertices
+      float x;
+      float y;
+      float z;
+      int num;
+    };
+};
+
+#endif
+
