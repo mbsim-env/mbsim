845bebc51b2233702c063774545c119d580162a9
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-06-25 18:02:45 +0000

    reorganization of hierarchy available on top level. Just call setReorganizeHierarchy(true) before init.

--- kernel_dev/mbsim/dynamic_system_solver.cc
+++ kernel_dev/mbsim/dynamic_system_solver.cc
@@ -23,6 +23,7 @@
 #include "mbsim/frame.h"
 #include "mbsim/contour.h"
 #include "mbsim/link.h"
+#include "mbsim/tree.h"
 #include "mbsim/order_one_dynamics.h"
 #include "mbsim/integrators/integrator.h"
 #include "mbsim/flexible_body.h"
@@ -59,12 +60,12 @@ namespace MBSim {
 
   bool DynamicSystemSolver::exitRequest=false;
 
-  DynamicSystemSolver::DynamicSystemSolver() : Group("Default"), grav(3), maxIter(10000), highIter(1000), maxDampingSteps(3), lmParm(0.001), contactSolver(FixedPointSingle), impactSolver(FixedPointSingle), strategy(local), linAlg(LUDecomposition), stopIfNoConvergence(false), dropContactInfo(false), useOldla(true), numJac(false), checkGSize(true), limitGSize(500), warnLevel(0), /*directoryName("Default"),*/ pinf(0.), preIntegrator(NULL), peds(false), impact(false), sticking(false), k(1) { 
+  DynamicSystemSolver::DynamicSystemSolver() : Group("Default"), grav(3), maxIter(10000), highIter(1000), maxDampingSteps(3), lmParm(0.001), contactSolver(FixedPointSingle), impactSolver(FixedPointSingle), strategy(local), linAlg(LUDecomposition), stopIfNoConvergence(false), dropContactInfo(false), useOldla(true), numJac(false), checkGSize(true), limitGSize(500), warnLevel(0), /*directoryName("Default"),*/ pinf(0.), preIntegrator(NULL), peds(false), impact(false), sticking(false), k(1), reorganizeHierarchy(false) { 
 
     constructor();
   } 
 
-  DynamicSystemSolver::DynamicSystemSolver(const string &projectName) : Group(projectName), grav(3), maxIter(10000), highIter(1000), maxDampingSteps(3), lmParm(0.001), contactSolver(FixedPointSingle), impactSolver(FixedPointSingle), strategy(local), linAlg(LUDecomposition), stopIfNoConvergence(false), dropContactInfo(false), useOldla(true), numJac(false), checkGSize(true), limitGSize(500), warnLevel(0), /* directoryName("Default"),*/ pinf(0.), preIntegrator(NULL), peds(false), impact(false), sticking(false), k(1) { 
+  DynamicSystemSolver::DynamicSystemSolver(const string &projectName) : Group(projectName), grav(3), maxIter(10000), highIter(1000), maxDampingSteps(3), lmParm(0.001), contactSolver(FixedPointSingle), impactSolver(FixedPointSingle), strategy(local), linAlg(LUDecomposition), stopIfNoConvergence(false), dropContactInfo(false), useOldla(true), numJac(false), checkGSize(true), limitGSize(500), warnLevel(0), /* directoryName("Default"),*/ pinf(0.), preIntegrator(NULL), peds(false), impact(false), sticking(false), k(1), reorganizeHierarchy(false) { 
 
     constructor();
   }
@@ -79,6 +80,104 @@ namespace MBSim {
 #endif
 
     Group::preinit();
+
+    if(reorganizeHierarchy) {
+      cout <<name << " (special group) preinit():" << endl;
+
+      vector<Object*>  objList;
+      vector<Link*>  lnkList;
+      vector<OrderOneDynamics*>  oodList;
+      buildListOfObjects(objList,true);
+      buildListOfLinks(lnkList,true);
+      buildListOfOrderOneDynamics(oodList,true);
+
+      vector<ModellingInterface*> modellList;
+      buildListOfModels(modellList,true);
+      if(modellList.size())
+	do {
+	  modellList[0]->processModellList(modellList,objList,lnkList);
+	} while(modellList.size());
+
+      link.clear(); // Alte link-Liste löschen
+      orderOneDynamics.clear(); // Alte ood-Liste löschen
+      cout << "object List:" << endl;
+      for(unsigned int i=0; i<objList.size(); i++) {
+	cout << objList[i]->getName() << endl;
+	stringstream str;
+	str << objList[i]->getName() << "#" << i;
+	objList[i]->setName(str.str());
+      }
+      cout << "link List:" << endl;
+      for(unsigned int i=0; i<lnkList.size(); i++) {
+	cout << lnkList[i]->getName() << endl;
+	stringstream str;
+	str << lnkList[i]->getName() << "#" << i;
+	lnkList[i]->setName(str.str());
+	addLink(lnkList[i]);
+      }
+      cout << "ood List:" << endl;
+      for(unsigned int i=0; i<oodList.size(); i++) {
+	cout << oodList[i]->getName() << endl;
+	stringstream str;
+	str << oodList[i]->getName() << "#" << i;
+	oodList[i]->setName(str.str());
+	addOrderOneDynamics(oodList[i]);
+      }
+      // Matrix anlegen, zeigt die Abhängigkeiten der Körper
+      SqrMat A(objList.size());
+      for(unsigned int i=0; i<objList.size(); i++) {
+
+	Object* parentBody = objList[i]->getObjectDependingOn();
+
+	if(parentBody) { // Körper hat Relativkinematik
+	  unsigned int j=0;
+	  bool foundBody = false;
+	  for(unsigned int k=0; k<objList.size(); k++, j++) {
+	    if(objList[k] == parentBody) {
+	      foundBody = true;
+	      break;
+	    }
+	  }
+
+	  if(foundBody) {
+	    A(i,j) = 2; // 2 bedeuted Vorgänger
+	    A(j,i) = 1; // 1 bedeuted Nachfolger
+	  }
+	}
+      }
+      // Matrix der Abhängigkeiten
+      //cout << "A=" << A << endl;
+      // Tree Liste
+      vector<Tree*> bufTree;
+      int nt = 0;
+      // Lege unsichtbare Group an 
+      Group* group = new Group("InvisibleGroup");
+      object.clear(); // Alte Object-Liste löschen
+      dynamicsystem.clear(); // Alte DynamicSystem-Liste löschen
+      // Starte Aufbau
+      for(int i=0; i<A.size(); i++) {
+	double a = max(trans(A).col(i));
+	if(a==1) { // Root einer Relativkinematik
+	  stringstream str;
+	  str << "InvisibleTree" << nt++;
+	  // Lege unsichtbaren Tree an 
+	  Tree *tree = new Tree(str.str());
+	  bufTree.push_back(tree);
+	  addToTree(tree, 0, A, i, objList);
+	} 
+	else if(a==0) // Absolutkinematik
+	  addObject(objList[i]);
+	//group->addObject(objList[i]);
+      }
+
+      addDynamicSystem(group,Vec(3),SqrMat(3));
+
+      for(unsigned int i=0; i<bufTree.size(); i++) {
+	addDynamicSystem(bufTree[i],Vec(3),SqrMat(3));
+      }
+
+      cout << "End of special group preinit()" << endl;
+    }
 
     calcqSize();
     calcuSize(0);
@@ -1321,6 +1420,15 @@ namespace MBSim {
     TiXmlElement *e;
     e=element->FirstChildElement(MBSIMNS"accelerationOfGravity");
     setAccelerationOfGravity(Vec(e->GetText()));
+  }
+
+  void DynamicSystemSolver::addToTree(Tree* tree, Node* node, SqrMat &A, int i, vector<Object*>& objList) {
+
+    Node *nextNode = tree->addObject(node,objList[i]);
+
+    for(int j=0; j<A.cols(); j++)
+      if(A(i,j) == 1) // Child node of object i
+	addToTree(tree, nextNode, A, j,objList);
   }
 
 }
