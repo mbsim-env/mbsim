32648f5e1e2c1866b464550b9d94da8cca05991b
thschindler <thschindler@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-07-09 11:49:50 +0000

    point frustum contact seems to work

--- trunk/kernel_dev/mbsim/contact_kinematics/point_frustum.h
+++ trunk/kernel_dev/mbsim/contact_kinematics/point_frustum.h
@@ -34,14 +34,14 @@ namespace MBSim {
    * \author Martin Foerg
    * \author Thorsten Schindler
    * \date 2009-04-02 some comments (Thorsten Schindler)
-   * \todo change stage to new interface TODO
+   * \date 2009-07-08 new interface (Thorsten Schindler)
    */
   class ContactKinematicsPointFrustum : public ContactKinematics {     
     public:
       /* INHERITED INTERFACE */
       virtual void assignContours(const std::vector<Contour*> &contour);
-      virtual void stage1(fmatvec::Vec &g, std::vector<ContourPointData> &cpData);
-      virtual void stage2(const fmatvec::Vec &g, fmatvec::Vec &gd, std::vector<ContourPointData> &cpData);
+      virtual void updateg(fmatvec::Vec &g, ContourPointData* cpData);
+      virtual void updatewb(fmatvec::Vec &wb, const fmatvec::Vec &g, ContourPointData *cpData) { throw new MBSimError("ERROR(ContactKinematicsPointFrustum:updatewb): Not implemented!"); }
       /***************************************************/
 
     private:
