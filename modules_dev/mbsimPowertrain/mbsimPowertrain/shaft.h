b93e6c0b177b209f139a96030e4212175a805c11
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-10-01 18:19:13 +0000

    added module mbsimPowertrain

---
+++ modules_dev/mbsimPowertrain/mbsimPowertrain/shaft.h
@@ -0,0 +1,23 @@
+#ifndef SHAFT_H_
+#define SHAFT_H_
+
+#include "rigid_body_with_1d_relative_motion.h"
+#include <vector.h>
+
+namespace MBSim {
+
+  class Shaft : public RigidBodyWith1DRelativeMotion { 
+    protected:
+
+    public:
+
+      Shaft(const std::string &name) : RigidBodyWith1DRelativeMotion(name) {}
+
+      void updateKinematicsForSelectedFrame(double t); 
+      void updateJacobiansForSelectedFrame(double t); 
+      void init(InitStage stage);
+  };
+
+}
+
+#endif
