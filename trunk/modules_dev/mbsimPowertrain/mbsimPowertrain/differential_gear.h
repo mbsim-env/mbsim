775b1102e412603a13490d25e01a9e8cf4aadf92
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-10-05 16:26:22 +0000

    added differential gear.

---
+++ trunk/modules_dev/mbsimPowertrain/mbsimPowertrain/differential_gear.h
@@ -0,0 +1,19 @@
+#ifndef DIFFERENTIAL_GEAR_H_
+#define DIFFERENTIAL_GEAR_H_
+
+#include "mbsim/group.h"
+
+namespace MBSim {
+
+  class DifferentialGear : public Group { 
+    protected:
+
+
+    public:
+
+      DifferentialGear(const std::string &name);
+  };
+
+}
+
+#endif
