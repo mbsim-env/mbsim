e1b758ea753d7429467d830112ce5d2971eaaca9
mafriedrich <mafriedrich@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-10-06 12:36:30 +0000

    Added option --donotintegrate to mbsimxml

--- mbsimxml/mbsimxml/mbsimflatxml-main.cc
+++ mbsimxml/mbsimxml/mbsimflatxml-main.cc
@@ -16,15 +16,22 @@ int main(int argc, char *argv[]) {
   Integrator *integrator;
   DynamicSystemSolver *dss;
 
+  bool doNotIntegrate=false;
+  if(argc>=2 && strcmp(argv[1],"--donotintegrate")==0)
+    doNotIntegrate=true;
+
   int ret;
   ret=MBSimXML::preInitDynamicSystemSolver(argc, argv, dss);
+  if(ret==-1) return 0; // help message was printed
   if(ret!=0) return ret;
   ret=MBSimXML::initDynamicSystemSolver(argc, argv, dss);
   if(ret!=0) return ret;
   ret=MBSimXML::initIntegrator(argc, argv, integrator);
   if(ret!=0) return ret;
-  ret=MBSimXML::main(integrator, dss);
-  if(ret!=0) return ret;
+  if(doNotIntegrate==false) {
+    ret=MBSimXML::main(integrator, dss);
+    if(ret!=0) return ret;
+  }
   ret=MBSimXML::postMain(integrator, dss);
   if(ret!=0) return ret;
 
