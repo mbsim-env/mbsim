cbc41af8144fda90d4e4060a1f8c9c7b71e3ff77
thschindler <thschindler@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-11-02 11:51:51 +0000

    renaming kernel a.s.o.

--- examples/mechanics_contacts_cylinder_on_plane/Readme.txt
+++
@@ -1,2 +0,0 @@
-This example deals with a planar contact problem. You can choose between rigid
-and flexible contact laws using the file "input.asc".
---
+++ release-2.0.0/mbsim/utils/mbsiminit.bat
@@ -0,0 +1,39 @@
+@echo off
+rem Windows batch variant of mbsiminit.bat
+rem If editing this file, edit also the file mbsiminit!
+rem
+rem Copyright (C) 2009 Markus Friedrich
+rem
+rem This library is free software; you can redistribute it and/or 
+rem modify it under the terms of the GNU Lesser General Public 
+rem License as published by the Free Software Foundation; either 
+rem version 2.1 of the License, or (at your option) any later version. 
+rem  
+rem This library is distributed in the hope that it will be useful, 
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of 
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+rem Lesser General Public License for more details. 
+rem  
+rem You should have received a copy of the GNU Lesser General Public 
+rem License along with this library; if not, write to the Free Software 
+rem Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+rem
+rem Contact:
+rem   mafriedrich@users.berlios.de
+rem
+rem
+rem This file is only usefull for binary installations which are origianlly made
+rem to the directory c:/WINDOWS/Temp/mbsim_local. In this case copy this directory to some
+rem other directory on some other machine and run this script
+rem ("path\mbsiminit.bat") ones in the shell to initialize the installation.
+
+rem get BINDIR
+set BINDIR=%~dp0
+rem get PREFIX
+set PREFIX=%BINDIR:~0,-4%
+
+rem setup link
+del c:\WINDOWS\Temp\mbsim_local
+if not exist c:\WINDOWS\Temp\mbsim_local %BINDIR%ln.exe %PREFIX% c:\WINDOWS\Temp\mbsim_local
+rem setup PATH
+set PATH=%PATH%;c:\WINDOWS\Temp\mbsim_local\bin
