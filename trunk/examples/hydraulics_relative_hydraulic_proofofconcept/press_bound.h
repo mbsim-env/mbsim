0b1049027ada2ef19c8d23b49a0453259ee90541
mafriedrich <mafriedrich@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-11-16 09:18:54 +0000

    Added a "relative hydraulic" example as a "Proof Of Conecpt" for the graph structure in mbsim-kernel.

---
+++ trunk/examples/hydraulics_relative_hydraulic_proofofconcept/press_bound.h
@@ -0,0 +1,35 @@
+#ifndef PRESS_BOUND_H
+#define PRESS_BOUND_H
+
+#include <mbsim/link.h>
+#include <vector>
+#include "line.h"
+
+namespace MBSim {
+
+class PressBound : public Link {
+  protected:
+    double p;
+    std::vector<Line*> conIn, conOut;
+  public:
+    PressBound(std::string name);
+    void updateg(double) {}
+    void updategd(double) {}
+    void updateWRef(const fmatvec::Mat&, int) {}
+    void updateVRef(const fmatvec::Mat&, int) {}
+    void updatehRef(const fmatvec::Vec&, const fmatvec::Vec&, int) {}
+    void updatedhdqRef(const fmatvec::Mat&, int) {}
+    void updatedhduRef(const fmatvec::SqrMat&, int) {}
+    void updatedhdtRef(const fmatvec::Vec&, int) {}
+    void updaterRef(const fmatvec::Vec&) {}
+    bool isActive() const { return false; }
+    bool gActiveChanged() { return false; }
+    void setPressure(double p_) { p=p_; }
+    void addInConnection(Line *l) { conIn.push_back(l); }
+    void addOutConnection(Line *l) { conOut.push_back(l); }
+    void updateh(double);
+};
+
+}
+
+#endif
