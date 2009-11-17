3439d0971221440c3de4ebfd33bebced9a35a886
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-11-17 15:21:03 +0000

    Revision.

--- trunk/examples/electronics_dcdc_buck_converter/main.cc
+++ trunk/examples/electronics_dcdc_buck_converter/main.cc
@@ -4,28 +4,39 @@
 using namespace std;
 using namespace MBSim;
 
+bool setValued = true;
+
 int main (int argc, char* argv[])
 {
   // Einzelne Bausteine des MKS erschaffen
   DynamicSystemSolver *sys = new System("TS");
+  sys->setConstraintSolver(GaussSeidel);
+  sys->setImpactSolver(GaussSeidel);
 
   // Bausteine zum Gesamtsystem zusammenfuegen (zu einem DGL-System) 
   sys->initialize();
   
-//   LSODEIntegrator integrator;
-  // RKSuiteIntegrator integrator;
-   RADAU5Integrator integrator;
- //  TimeSteppingIntegrator integrator;
- //  integrator.setdt(1e-3);
-  
+   Integrator *integrator;
+   if(setValued) {
+     integrator = new TimeSteppingIntegrator;
+     double dt = 1e-6;
+     static_cast<TimeSteppingIntegrator*>(integrator)->setStepSize(dt);
+     sys->setLaTol(1e-2*dt);
+     sys->setgdTol(1e-8);
+   }
+   else {
+
+   integrator = new RADAU5Integrator;
+   static_cast<RADAU5Integrator*>(integrator)->setMaximalStepSize(1e-5);
+   static_cast<RADAU5Integrator*>(integrator)->setRelativeTolerance(1e-10);
+   }
+
   //DOPRI5Integrator integrator;
+   //LSODEIntegrator integrator;
 
-   integrator.setMaximalStepSize(1e-5);
-  integrator.setEndTime(1e-2);
-  integrator.setPlotStepSize(1e-5);
-  integrator.setRelativeTolerance(1e-10);
-
-  integrator.integrate(*sys);
+  integrator->setPlotStepSize(1e-5);
+  integrator->setEndTime(1e-2);
+  integrator->integrate(*sys);
 
   sys->closePlot();
 
