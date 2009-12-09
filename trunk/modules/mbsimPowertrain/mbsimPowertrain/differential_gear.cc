bfd64a1f4c543f0399acd280029a7270da055f41
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-12-09 17:54:24 +0000

    Extensions and improvement of parametrization.

--- trunk/modules/mbsimPowertrain/mbsimPowertrain/differential_gear.cc
+++ trunk/modules/mbsimPowertrain/mbsimPowertrain/differential_gear.cc
@@ -3,6 +3,7 @@
 #include "mbsim/utils/rotarymatrices.h"
 #ifdef HAVE_OPENMBVCPPINTERFACE
 #include "openmbvcppinterface/frustum.h"
+#include "openmbvcppinterface/cube.h"
 #include "openmbvcppinterface/compoundrigidbody.h"
 #endif
 
@@ -12,165 +13,169 @@ using namespace MBSim;
 
 namespace MBSimPowertrain {
 
-  DifferentialGear::DifferentialGear(const std::string &name) : Group(name) {
+  DifferentialGear::Data::Data() {
+    massHousing = 2;
+    massInputShaft = -1;
+    massLeftOutputShaft = -1;
+    massRightOutputShaft = -1;
+    massPlanet = -1;
+    radiusInputShaft = 0.15;
+    radiusLeftOutputShaft = 0.05;
+    radiusRightOutputShaft = 0.05;
+    radiusPlanet = 0.05;
+    lengthInputShaft = radiusInputShaft/4.;
+    lengthLeftOutputShaft = radiusLeftOutputShaft*3;
+    lengthRightOutputShaft = radiusRightOutputShaft*3;
+    lengthPlanet = radiusPlanet*2;
+  }
 
-    double m0 = 1;
-    double m1 = 1;
-    double m2 = 1.5;
-    double mP = 0.3;
-    double m4 = 1;
-    double m5 = m4;
-    double l = 1;
-    double l2 = l/10;
-    double l3 = 0.3;
-    double l4 = 1;
-    double l5 = l4;
-    double R1 = 0.2;
-    double R2 = 2*R1;
-    double RP = 0.1;
-    double R4 = RP;
-    double R5 = R4;
-    double J = 0.5*m1*R1*R1; 
-    double J2 = 0.5*m2*R2*R2; 
-    double JP = 0.5*mP*RP*RP; 
-    double J4 = 0.5*m4*R4*R4; 
-    double J5 = 0.5*m5*R5*R5; 
+  DifferentialGear::DifferentialGear(const std::string &name, Data param) : Group(name), data(param) {
+    double density = 785;
 
-    SymMat Theta(3);
+    if(data.massInputShaft == -1) 
+      data.massInputShaft = density*data.lengthInputShaft*pow(data.radiusInputShaft,2)*M_PI;
+    if(data.massLeftOutputShaft == -1) 
+      data.massLeftOutputShaft = density*data.lengthLeftOutputShaft*pow(data.radiusLeftOutputShaft,2)*M_PI;
+    if(data.massRightOutputShaft == -1) 
+      data.massRightOutputShaft = density*data.lengthRightOutputShaft*pow(data.radiusRightOutputShaft,2)*M_PI;
+    if(data.massPlanet == -1) 
+      data.massPlanet = density*data.lengthPlanet*pow(data.radiusPlanet,2)*M_PI;
+    if(data.inertiaTensorHousing.size() == 0) {
+      data.inertiaTensorHousing = SymMat(3,EYE)*data.massHousing/10.;
+    }
+    if(data.inertiaTensorInputShaft.size() == 0) {
+      data.inertiaTensorInputShaft.resize(3);
+      data.inertiaTensorInputShaft(0,0) = data.massInputShaft*pow(data.lengthInputShaft,2)/12.0;
+      data.inertiaTensorInputShaft(1,1) = data.massInputShaft*pow(data.lengthInputShaft,2)/12.0;
+      data.inertiaTensorInputShaft(2,2) = data.massInputShaft*pow(data.radiusInputShaft,2)/2.0;
+    }
+    if(data.inertiaTensorLeftOutputShaft.size() == 0) {
+      data.inertiaTensorLeftOutputShaft.resize(3);
+      data.inertiaTensorLeftOutputShaft(0,0) = data.massLeftOutputShaft*pow(data.lengthLeftOutputShaft,2)/12.0;
+      data.inertiaTensorLeftOutputShaft(1,1) = data.massLeftOutputShaft*pow(data.lengthLeftOutputShaft,2)/12.0;
+      data.inertiaTensorLeftOutputShaft(2,2) = data.massLeftOutputShaft*pow(data.radiusLeftOutputShaft,2)/2.0;
+    }
+    if(data.inertiaTensorRightOutputShaft.size() == 0) {
+      data.inertiaTensorRightOutputShaft.resize(3);
+      data.inertiaTensorRightOutputShaft(0,0) = data.massRightOutputShaft*pow(data.lengthRightOutputShaft,2)/12.0;
+      data.inertiaTensorRightOutputShaft(1,1) = data.massRightOutputShaft*pow(data.lengthRightOutputShaft,2)/12.0;
+      data.inertiaTensorRightOutputShaft(2,2) = data.massRightOutputShaft*pow(data.radiusRightOutputShaft,2)/2.0;
+    }
+    if(data.inertiaTensorPlanet.size() == 0) {
+      data.inertiaTensorPlanet.resize(3);
+      data.inertiaTensorPlanet(0,0) = data.massPlanet*pow(data.lengthPlanet,2)/12.0;
+      data.inertiaTensorPlanet(1,1) = data.massPlanet*pow(data.lengthPlanet,2)/12.0;
+      data.inertiaTensorPlanet(2,2) = data.massPlanet*pow(data.radiusPlanet,2)/2.0;
+    }
+
     RigidBody* housing = new RigidBody("Housing");
     addObject(housing);
 
     housing->setFrameOfReference(getFrame("I"));
     housing->setFrameForKinematics(housing->getFrame("C"));
 
-    housing->setMass(m0);
-    Theta(2,2) = J;
-    housing->setInertiaTensor(Theta);
-    housing->getFrame("C")->enableOpenMBV(0.3);
+    housing->setMass(data.massHousing);
+    housing->setInertiaTensor(data.inertiaTensorHousing);
 
-    Shaft* shaft1 = new Shaft("Shaft1");
-    addObject(shaft1);
-
-    shaft1->setFrameOfReference(housing->getFrame("C"));
-    shaft1->setFrameForKinematics(shaft1->getFrame("C"));
-
-    shaft1->setMass(m1);
-    Theta(2,2) = J;
-    shaft1->setInertiaTensor(Theta);
-    //shaft1->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
-    Vec r(3);
-    r(2) = l/2;
-    shaft1->addFrame("Q",r,SqrMat(3,EYE));
-    shaft1->getFrame("Q")->enableOpenMBV(0.3);
-    shaft1->getFrame("C")->enableOpenMBV(0.3);
-
-    Shaft* shaft2 = new Shaft("Shaft2");
+    shaft2 = new Shaft("InputShaft");
     addObject(shaft2);
 
-    r(2) = l;
-    r(1) = 1.5*R1;
-
-    housing->addFrame("Q",r,BasicRotAKIy(M_PI/2));
+    double a = data.lengthInputShaft/2+data.radiusPlanet*1.2;
+    Vec r(3);
+    r(2) = -a;
+    housing->addFrame("Q",r,SqrMat(3,EYE));
     shaft2->setFrameOfReference(housing->getFrame("Q"));
-
     shaft2->setFrameForKinematics(shaft2->getFrame("C"));
-    shaft2->getFrame("C")->enableOpenMBV(0.3);
 
-    shaft2->setMass(m2);
-    Theta(2,2) = J2;
-    shaft2->setInertiaTensor(Theta);
-    shaft2->addDependecy(shaft1,-R1/R2);
+    shaft2->setMass(data.massInputShaft);
+    shaft2->setInertiaTensor(data.inertiaTensorInputShaft);
 
     Shaft* planet = new Shaft("Planet");
     addObject(planet);
 
     r.init(0);
-    r(2) = l2/2+RP*1.2;
+    r(2) = a;
     shaft2->addFrame("Q",r,BasicRotAKIy(M_PI/2));
-    shaft2->getFrame("Q")->enableOpenMBV(0.3);
     planet->setFrameOfReference(shaft2->getFrame("Q"));
     planet->setFrameForKinematics(planet->getFrame("C"));
-    planet->getFrame("C")->enableOpenMBV(0.3);
+    planet->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
 
-    planet->setMass(mP);
-    Theta(2,2) = JP;
-    planet->setInertiaTensor(Theta);
-    //planet->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
+    planet->setMass(data.massPlanet);
+    planet->setInertiaTensor(data.inertiaTensorPlanet);
 
-    Shaft* shaft4 = new Shaft("Shaft4");
+    shaft4 = new Shaft("LeftOutputShaft");
     addObject(shaft4);
 
     r.init(0);
-    r(2) = -l4/2-0.2;
-    housing->addFrame("L",r,BasicRotAKIy(0),housing->getFrame("Q"));
+    r(2) = -data.lengthLeftOutputShaft/2+a;
+    housing->addFrame("L",r,SqrMat(3,EYE),housing->getFrame("Q"));
     shaft4->setFrameOfReference(housing->getFrame("L"));
-
     shaft4->setFrameForKinematics(shaft4->getFrame("C"));
-    shaft4->getFrame("C")->enableOpenMBV(0.3);
+    r(2) = -data.lengthLeftOutputShaft/2;
+    shaft4->addFrame("Q",r,BasicRotAKIy(M_PI));
+    shaft4->getFrame("Q")->enableOpenMBV(0.3);
 
-    shaft4->setMass(m4);
-    Theta(2,2) = J4;
-    shaft4->setInertiaTensor(Theta);
+    shaft4->setMass(data.massLeftOutputShaft);
+    shaft4->setInertiaTensor(data.inertiaTensorLeftOutputShaft);
     shaft4->addDependecy(shaft2,1);
-    shaft4->addDependecy(planet,RP/R4);
+    shaft4->addDependecy(planet,data.radiusPlanet/data.radiusLeftOutputShaft);
 
-    Shaft* shaft5 = new Shaft("Shaft5");
+    shaft5 = new Shaft("RightOutputShaft");
     addObject(shaft5);
 
     r.init(0);
-    r(2) = +l5/2+0.2;
-    housing->addFrame("R",r,BasicRotAKIy(0),housing->getFrame("Q"));
+    r(2) = data.lengthRightOutputShaft/2+a;
+    housing->addFrame("R",r,SqrMat(3,EYE),housing->getFrame("Q"));
     shaft5->setFrameOfReference(housing->getFrame("R"));
-
     shaft5->setFrameForKinematics(shaft5->getFrame("C"));
-    shaft5->getFrame("C")->enableOpenMBV(0.3);
+    r(2) = data.lengthRightOutputShaft/2;
+    shaft5->addFrame("Q",r,SqrMat(3,EYE));
+    shaft5->getFrame("Q")->enableOpenMBV(0.3);
 
-    shaft5->setMass(m5);
-    Theta(2,2) = J5;
-    shaft5->setInertiaTensor(Theta);
+    shaft5->setMass(data.massRightOutputShaft);
+    shaft5->setInertiaTensor(data.inertiaTensorRightOutputShaft);
     shaft5->addDependecy(shaft2,1);
-    shaft5->addDependecy(planet,-RP/R4);
+    shaft5->addDependecy(planet,-data.radiusPlanet/data.radiusRightOutputShaft);
 
 #ifdef HAVE_OPENMBVCPPINTERFACE
-    OpenMBV::Frustum *cylinder=new OpenMBV::Frustum;
-    cylinder->setTopRadius(R1);
-    cylinder->setBaseRadius(R1);
-    cylinder->setHeight(l);
-    cylinder->setStaticColor(0.1);
-    shaft1->setOpenMBVRigidBody(cylinder);
-    cylinder->setInitialTranslation(0,0,l/2);
+    OpenMBV::Cube *cube=new OpenMBV::Cube;
+    cube->setLength(0.01);
+    cube->setStaticColor(0.1);
+    housing->setOpenMBVRigidBody(cube);
+
+    OpenMBV::Frustum *cylinder;
 
     cylinder=new OpenMBV::Frustum;
-    cylinder->setTopRadius(R2);
-    cylinder->setBaseRadius(R2);
-    cylinder->setHeight(l2);
-    cylinder->setStaticColor(0.4);
+    cylinder->setTopRadius(data.radiusInputShaft);
+    cylinder->setBaseRadius(data.radiusInputShaft);
+    cylinder->setHeight(data.lengthInputShaft);
+    cylinder->setStaticColor(0.2);
     shaft2->setOpenMBVRigidBody(cylinder);
-    cylinder->setInitialTranslation(0,0,l2/2);
+    cylinder->setInitialTranslation(0,0,data.lengthInputShaft/2);
 
     cylinder=new OpenMBV::Frustum;
-    cylinder->setTopRadius(RP);
-    cylinder->setBaseRadius(RP);
-    cylinder->setHeight(l3);
-    cylinder->setStaticColor(0.5);
+    cylinder->setTopRadius(data.radiusPlanet);
+    cylinder->setBaseRadius(data.radiusPlanet);
+    cylinder->setHeight(data.lengthPlanet);
+    cylinder->setStaticColor(0.88);
     planet->setOpenMBVRigidBody(cylinder);
-    cylinder->setInitialTranslation(0,0,l3/2);
+    cylinder->setInitialTranslation(0,0,data.lengthPlanet/2);
 
     cylinder=new OpenMBV::Frustum;
-    cylinder->setTopRadius(R4);
-    cylinder->setBaseRadius(R4);
-    cylinder->setHeight(l4);
+    cylinder->setTopRadius(data.radiusLeftOutputShaft);
+    cylinder->setBaseRadius(data.radiusLeftOutputShaft);
+    cylinder->setHeight(data.lengthLeftOutputShaft);
     cylinder->setStaticColor(0.5);
     shaft4->setOpenMBVRigidBody(cylinder);
+    cylinder->setInitialTranslation(0,0,data.lengthLeftOutputShaft/2);
 
-    cylinder->setInitialTranslation(0,0,l5/2);
     cylinder=new OpenMBV::Frustum;
-    cylinder->setTopRadius(R5);
-    cylinder->setBaseRadius(R5);
-    cylinder->setHeight(l5);
-    cylinder->setStaticColor(0.5);
+    cylinder->setTopRadius(data.radiusRightOutputShaft);
+    cylinder->setBaseRadius(data.radiusRightOutputShaft);
+    cylinder->setHeight(data.lengthRightOutputShaft);
+    cylinder->setStaticColor(0.7);
     shaft5->setOpenMBVRigidBody(cylinder);
-    cylinder->setInitialTranslation(0,0,l5/2);
+    cylinder->setInitialTranslation(0,0,data.lengthRightOutputShaft/2);
 #endif
 
   }
