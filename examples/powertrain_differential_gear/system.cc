15e69e3faf62108bf73b2460c17ca985d6325ed4
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-12-09 17:56:46 +0000

    Changed parameters.

--- examples/powertrain_differential_gear/system.cc
+++ examples/powertrain_differential_gear/system.cc
@@ -4,6 +4,7 @@
 #include "mbsim/utils/rotarymatrices.h"
 #include "mbsim/rigid_body.h"
 #include "mbsimPowertrain/differential_gear.h"
+#include "mbsimPowertrain/shaft.h"
 
 #ifdef HAVE_OPENMBVCPPINTERFACE
 #include "openmbvcppinterface/frustum.h"
@@ -26,31 +27,67 @@ class Moment : public Function1<fmatvec::Vec, double> {
 };
 
 Pendulum::Pendulum(const string &projectName) : DynamicSystemSolver(projectName) {
+  double R1 = 0.02;
+  double m1 = 1;
+  double J = 0.5*m1*R1*R1; 
+  SymMat Theta(3);
+  double l = 0.1;
 
   Vec grav(3);
   grav(1)=-9.81;
   MBSimEnvironment::getInstance()->setAccelerationOfGravity(grav);
 
+  Vec r(3);
+  r(0) = l/2;
+  addFrame("Q",r,BasicRotAKIy(M_PI/2));
+
+  Shaft* shaft1 = new Shaft("Shaft1");
+  addObject(shaft1);
+
+
+  shaft1->setFrameOfReference(getFrame("Q"));
+  shaft1->setFrameForKinematics(shaft1->getFrame("C"));
+
+
+  shaft1->setMass(m1);
+  Theta(2,2) = J;
+  shaft1->setInertiaTensor(Theta);
+  shaft1->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
+  r.init(0);
+  r(2) = l/2;
+  shaft1->addFrame("Q",r,SqrMat(3,EYE));
+  shaft1->getFrame("Q")->enableOpenMBV(0.3);
+  shaft1->getFrame("C")->enableOpenMBV(0.3);
+
+
   DifferentialGear* differentialGear = new DifferentialGear("PlanetaryGear");
   addGroup(differentialGear);
-  static_cast<RigidBody*>(differentialGear->getObject("Shaft1"))->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
-  static_cast<RigidBody*>(differentialGear->getObject("Planet"))->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
+   double R2 = differentialGear->getRadiusInputShaft();
+  static_cast<Shaft*>(differentialGear->getObject("InputShaft"))->addDependecy(shaft1,-R1/R2);
 
   KineticExcitation* ke;
   ke = new KineticExcitation("MAn");
   addLink(ke);
-  ke->connect(static_cast<RigidBody*>(differentialGear->getObject("Shaft1"))->getFrame("C"));
-  ke->setMoment("[0;0;1]", new Moment(1.2));
+  ke->connect(shaft1->getFrame("C"));
+  ke->setMoment("[0;0;1]", new Moment(1.1/100.));
 
   ke = new KineticExcitation("MAbL");
   addLink(ke);
-  ke->connect(static_cast<RigidBody*>(differentialGear->getObject("Shaft4"))->getFrame("C"));
-  ke->setMoment("[0;0;1]", new Moment(0.99));
+  ke->connect(static_cast<RigidBody*>(differentialGear->getObject("LeftOutputShaft"))->getFrame("C"));
+  ke->setMoment("[0;0;1]", new Moment(0.99/100.));
 
   ke = new KineticExcitation("MAbR");
   addLink(ke);
-  ke->connect(static_cast<RigidBody*>(differentialGear->getObject("Shaft5"))->getFrame("C"));
-  ke->setMoment("[0;0;1]", new Moment(1));
+  ke->connect(static_cast<RigidBody*>(differentialGear->getObject("RightOutputShaft"))->getFrame("C"));
+  ke->setMoment("[0;0;1]", new Moment(1/100.));
+
+  OpenMBV::Frustum *cylinder=new OpenMBV::Frustum;
+  cylinder->setTopRadius(R1);
+  cylinder->setBaseRadius(R1);
+  cylinder->setHeight(l);
+  cylinder->setStaticColor(0.1);
+  shaft1->setOpenMBVRigidBody(cylinder);
+  cylinder->setInitialTranslation(0,0,l/2);
 
 }
 
