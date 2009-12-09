58b3994e94dfe4d4f87dcf9ba6992124d161aed3
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2009-12-09 17:54:24 +0000

    Extensions and improvement of parametrization.

--- modules/mbsimPowertrain/mbsimPowertrain/differential_gear.h
+++ modules/mbsimPowertrain/mbsimPowertrain/differential_gear.h
@@ -2,14 +2,43 @@
 #define DIFFERENTIAL_GEAR_H_
 
 #include "mbsim/group.h"
+#include "mbsimPowertrain/shaft.h"
 
 namespace MBSimPowertrain {
 
   class DifferentialGear : public MBSim::Group { 
+    public:
+      struct Data {
+	double massHousing;
+	double massInputShaft;
+	double massRightOutputShaft;
+	double massLeftOutputShaft;
+	double massPlanet;
+	fmatvec::SymMat inertiaTensorHousing;
+	fmatvec::SymMat inertiaTensorInputShaft;
+	fmatvec::SymMat inertiaTensorRightOutputShaft;
+	fmatvec::SymMat inertiaTensorLeftOutputShaft;
+	fmatvec::SymMat inertiaTensorPlanet;
+	double lengthInputShaft;
+	double lengthRightOutputShaft;
+	double lengthLeftOutputShaft;
+	double lengthPlanet;
+	double radiusInputShaft;
+	double radiusLeftOutputShaft;
+	double radiusRightOutputShaft;
+	double radiusPlanet;
+	Data();
+      };
     protected:
+      Data data;
+      Shaft *shaft2, *shaft4, *shaft5;
 
     public:
-      DifferentialGear(const std::string &name);
+      DifferentialGear(const std::string &name, Data data=Data());
+      double getRadiusInputShaft() const {return data.radiusInputShaft;}
+      Shaft* getInputShaft() {return shaft2;}
+      Shaft* getRightOutputShaft() {return shaft5;}
+      Shaft* getLeftOutputShaft() {return shaft4;}
   };
 
 }
