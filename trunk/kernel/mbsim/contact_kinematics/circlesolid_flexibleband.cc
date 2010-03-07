02bb135b436a7776a701aec21ea4c03f16e297fb
rzander <rzander@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2010-03-07 11:30:48 +0000

    initial commit for contact kinematics circlesolid flexibleband; still minor bugs

---
+++ trunk/kernel/mbsim/contact_kinematics/circlesolid_flexibleband.cc
@@ -0,0 +1,214 @@
+/* Copyright (C) 2004-2009 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: rzander@users.berlios.de
+ */
+
+#include<config.h>
+#include "mbsim/contact_kinematics/circlesolid_flexibleband.h"
+#include "mbsim/contour.h"
+#include "mbsim/contours/flexible_band.h"
+#include "mbsim/contours/circle_solid.h"
+#include "mbsim/contours/point.h"
+#include "mbsim/functions_contact.h"
+
+using namespace std;
+using namespace fmatvec;
+
+namespace MBSim {
+
+  ContactKinematicsCircleSolidFlexibleBand::ContactKinematicsCircleSolidFlexibleBand() : ContactKinematics(),icircle(0),icontour(0),possibleContactsPerNode(1),circle(0),band(0) {}
+  ContactKinematicsCircleSolidFlexibleBand::~ContactKinematicsCircleSolidFlexibleBand() {}
+
+  void ContactKinematicsCircleSolidFlexibleBand::assignContours(const vector<Contour*>& contour) {	
+    if(dynamic_cast<CircleSolid*>(contour[0])) {
+      icircle = 0;
+      icontour = 1;
+      circle = static_cast<CircleSolid*>(contour[0]);
+      band = static_cast<FlexibleBand*>(contour[1]);
+    }
+    else {
+      icircle = 1;
+      icontour = 0;
+      circle = static_cast<CircleSolid*>(contour[1]);
+      band = static_cast<FlexibleBand*>(contour[0]);
+    }
+    wBand   = band->getWidth();
+    hBand   = band->getNormalDistance();
+    rCircle = circle->getRadius();
+
+    numberOfPotentialContactPoints = 2*possibleContactsPerNode*band->getNodes().size()-1;  // dies braeuchte einen eigenen init-Call
+    l0     =        (band->getAlphaEnd()-band->getAlphaStart()); /* bandwidth of mesh deformer: higher values leads to stronger attraction of last contact points */
+    epsTol = 1.0e-3*l0;
+
+    static int nr = 0;
+    cout << "ContactKinematicsCircleSolidFlexibleBand " <<  nr++ << endl;
+    cout <<  " wBand                          = "  << wBand                           << endl;
+    cout <<  " hBand                          = "  << hBand                           << endl;
+    cout <<  " rCircle                        = "  << rCircle                         << endl;
+    cout <<  " numberOfPotentialContactPoints = "  << numberOfPotentialContactPoints  << endl;
+    cout <<  " epsTol                         = "  << epsTol                          << endl;
+  }
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//  void ContactKinematicsCircleSolidFlexibleBand::updateg(Vec& g, ContourPointData *cpData) {
+  void ContactKinematicsCircleSolidFlexibleBand::updateg(std::vector<fmatvec::Vec>::iterator ig, std::vector<ContourPointData*>::iterator icpData) {
+    int inContact = 0;
+    Vec nodes = band->getNodes();
+    int nrNodes = nodes.size();
+
+    // always use nodes for contact kinematics
+#pragma omp parallel for schedule(dynamic) shared(ig,icpData,nodes,nrNodes,inContact) default(none) 
+    for(int i=0;i<nrNodes;i++) {
+      ContourPointData* cpData = icpData[i];
+
+      if(cpData[icontour].getLagrangeParameterPosition().size() != 2)
+        cpData[icontour].getLagrangeParameterPosition().resize() = Vec(2,NONINIT);
+      cpData[icontour].getLagrangeParameterPosition()(0) = nodes(i);
+      cpData[icontour].getLagrangeParameterPosition()(1) = 0.0;
+
+      if(cpData[icontour].getLagrangeParameterPosition()(0) < band->getAlphaStart() || cpData[icontour].getLagrangeParameterPosition()(0) > band->getAlphaEnd())
+        (ig[i])(0) = 1.0;
+      else {
+        band->updateKinematicsForFrame(cpData[icontour],position_cosy);
+        Vec Wd = circle->getFrame()->getPosition() - cpData[icontour].getFrameOfReference().getPosition();
+        (ig[i])(0) = nrm2(Wd) - rCircle - hBand;
+        if((ig[i])(0) <= 0.0) {
+
+          Vec Wb = cpData[icontour].getFrameOfReference().getOrientation().col(2).copy();
+          cpData[icontour].getLagrangeParameterPosition()(1) = trans(Wb)*Wd; // get contact parameter of second tangential direction
+
+          if(cpData[icontour].getLagrangeParameterPosition()(1) > 0.5*wBand || -cpData[icontour].getLagrangeParameterPosition()(1) > 0.5*wBand)
+            (ig[i])(0) = 1.0;
+          else { // calculate the normal distance
+            cpData[icontour].getFrameOfReference().getPosition()           += cpData[icontour].getLagrangeParameterPosition()(1)*Wb; 
+            cpData[icircle] .getFrameOfReference().getOrientation().col(0)  = -cpData[icontour].getFrameOfReference().getOrientation().col(0);
+            cpData[icircle] .getFrameOfReference().getOrientation().col(1)  = -cpData[icontour].getFrameOfReference().getOrientation().col(1);
+            cpData[icircle] .getFrameOfReference().getOrientation().col(2)  =  cpData[icontour].getFrameOfReference().getOrientation().col(2);
+            cpData[icircle] .getFrameOfReference().getPosition()            =  circle->getFrame()->getPosition() + cpData[icircle].getFrameOfReference().getOrientation().col(0)*rCircle;
+
+          }
+          inContact++;
+        }
+      }
+    }
+
+    FuncPairContour1sCircleSolid *func= new FuncPairContour1sCircleSolid(circle,band); // root function for searching contact parameters
+    Contact1sSearch search(func);
+
+    // deform mesh
+    for(int i=0;i<nodes.size()-1;i++) {
+      double node_i = nodes(i);
+      for(int j=0;j<lastContactPositions.size();j++)
+        nodes(i) += (lastContactPositions(j)-node_i) * exp( - fabs((lastContactPositions(j)-node_i)/ l0));
+    }
+    Vec cp = band->getNodes();
+    search.setNodes(nodes); // defining search areas for contacts
+
+	Mat result = search.slvAll();
+
+    delete func;
+
+#if 0
+    if(result.rows()>0 && false) {
+      cout << endl <<endl<< "----------------------------------------------\nlast hits: "<< trans(lastContactPositions) << endl;
+      cout << "original Nodes" << endl << trans( cp ) << endl;
+      cout << "modified Nodes" << endl << trans( localNodes ) << endl;
+      cout << "  Loop all "<< result.rows() <<" possible contact point ...\n";
+      cout << trans(result) << endl;
+    }
+#endif
+
+    Vec innerContactPositions(result.rows());
+    int innerContacts = 0;
+
+//#pragma omp parallel for schedule(dynamic) shared(ig,icpData,inContact,cp,result,innerContacts,innerContactPositions,cout) default(none) 
+    for(int i=0;i<result.rows();i++) {
+      ContourPointData* cpData = icpData[nrNodes + i];
+
+      if(cpData[icontour].getLagrangeParameterPosition().size() != 2)
+        cpData[icontour].getLagrangeParameterPosition().resize() = Vec(2,NONINIT);
+      cpData[icontour].getLagrangeParameterPosition()(0) = result(i,0);
+      cpData[icontour].getLagrangeParameterPosition()(1) = 0.0;
+
+      if(cpData[icontour].getLagrangeParameterPosition()(0) < band->getAlphaStart() || cpData[icontour].getLagrangeParameterPosition()(0) > band->getAlphaEnd())
+        (ig[nrNodes + i])(0) = 1.0;
+      else {
+        band->updateKinematicsForFrame(cpData[icontour],position_cosy);
+        Vec Wd = circle->getFrame()->getPosition() - cpData[icontour].getFrameOfReference().getPosition();
+        (ig[nrNodes + i])(0) = nrm2(Wd) - rCircle - hBand;
+
+        if((ig[nrNodes + i])(0) <= 0.0) {
+          Vec Wb = cpData[icontour].getFrameOfReference().getOrientation().col(2).copy();
+          cpData[icontour].getLagrangeParameterPosition()(1) = trans(Wb)*Wd; // get contact parameter of second tangential direction
+
+          if(cpData[icontour].getLagrangeParameterPosition()(1) > 0.5*wBand || -cpData[icontour].getLagrangeParameterPosition()(1) > 0.5*wBand)
+            (ig[i])(0) = 1.0;
+          else { // calculate the normal distance
+            cpData[icontour].getFrameOfReference().getPosition()           += cpData[icontour].getLagrangeParameterPosition()(1)*Wb; 
+            cpData[icircle] .getFrameOfReference().getOrientation().col(0)  = -cpData[icontour].getFrameOfReference().getOrientation().col(0);
+            cpData[icircle] .getFrameOfReference().getOrientation().col(1)  = -cpData[icontour].getFrameOfReference().getOrientation().col(1);
+            cpData[icircle] .getFrameOfReference().getOrientation().col(2)  =  cpData[icontour].getFrameOfReference().getOrientation().col(2);
+            cpData[icircle] .getFrameOfReference().getPosition()            =  circle->getFrame()->getPosition() + cpData[icircle].getFrameOfReference().getOrientation().col(0)*rCircle;
+
+#if 0
+            if(false && (ig[i])(0) < 0.)//1.0e-4)
+            {
+               cout << endl;
+              cout << " g(0)  = " << (ig[i])(0) << "\n------------------------------------------\n" << endl;
+              cout << " " << i << " - alpha = " << cpData[icontour].getLagrangeParameterPosition()(0) << endl;
+              cout << "     Wb = " << trans(Wb) << "\n Wd = " << trans(Wd) << endl;
+              cout << "     rCircle  = " << rCircle << endl;
+              cout << " cpData[icontour].r = \n" << trans(cpData[icontour].getFrameOfReference().getPosition()   ) << endl;
+              cout << " cpData[icontour].A = \n" <<       cpData[icontour].getFrameOfReference().getOrientation()  << endl;
+              cout << " cpData[icircle].r  = \n" << trans(cpData[icircle] .getFrameOfReference().getPosition()   ) << endl;
+              cout << " cpData[icircle].A  = \n" <<       cpData[icircle] .getFrameOfReference().getOrientation()  << endl;
+              cpData[icontour].getLagrangeParameterPosition()(0) = 0.0;
+              band->updateKinematicsForFrame(cpData[icontour],position_cosy);
+              cout << " cpData(alpha=0).r = \n" << trans(cpData[icontour].getFrameOfReference().getPosition()   ) << endl;
+              cout << " cpData(alpha=0).A = \n" <<       cpData[icontour].getFrameOfReference().getOrientation()  << endl;
+              throw 1;
+            }
+#endif
+          }
+          inContact++;
+        }
+      }
+    }
+    lastContactPositions.resize(innerContacts);
+    lastContactPositions = innerContactPositions(Index(0,innerContacts-1));
+
+    for(int i=nrNodes + result.rows();i<numberOfPotentialContactPoints;i++)
+    {
+      // flush data not being necessary
+      ContourPointData* cpData = icpData[i];
+
+      // positions -> dummy and centerOfCircle
+      if(cpData[icontour].getLagrangeParameterPosition().size() != 2)
+        cpData[icontour].getLagrangeParameterPosition().resize() = Vec(2,INIT,0.0);
+      cpData[icontour].getLagrangeParameterPosition()(0) = 0.0;//result(0,0);
+
+      cpData[icontour].getFrameOfReference().setPosition(Vec("[100.0;0.0;0.0]"));//Vec(3,INIT,0.0));//
+      cpData[icircle ].getFrameOfReference().setPosition(Vec("[100.0;0.0;0.0]"));//Vec(3,INIT,0.0));//
+      cpData[icontour].getFrameOfReference().getOrientation() = -SqrMat(3,EYE);
+      cpData[icircle ].getFrameOfReference().getOrientation() = -SqrMat(3,EYE);
+      (ig[i])(0) = 1.0;
+    }
+
+  }
+
+}
+
---
+++ trunk/kernel/mbsim/contact_kinematics/circlesolid_flexibleband.h
@@ -0,0 +1,101 @@
+/* Copyright (C) 2004-2009 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: rzander@users.berlios.de
+ */
+
+#ifndef CIRCLESOLID_FLEXIBLEBAND_H_
+#define CIRCLESOLID_FLEXIBLEBAND_H_
+
+#include "mbsim/contact_kinematics/contact_kinematics.h"
+#include "mbsim/mbsim_event.h"
+
+namespace MBSim {
+
+  class FlexibleBand;
+  class CircleSolid;
+
+  /**
+   * \brief pairing solid cirlce to flexible band, planar only
+   * \author Roland Zander
+   * \date 2009-xx-yy initial commit
+   */
+  class ContactKinematicsCircleSolidFlexibleBand : public ContactKinematics {
+    public:
+      /**
+       * \brief constructor
+       */
+      ContactKinematicsCircleSolidFlexibleBand();
+      
+      /**
+       * \brief destructor
+       */
+      virtual ~ContactKinematicsCircleSolidFlexibleBand();
+
+      /*!
+       */
+      void setNumberOfPossibleContactPerNode(int n) {possibleContactsPerNode = n;}
+
+      /* INHERITED INTERFACE OF CONTACTKINEAMTICS */
+      virtual void assignContours(const std::vector<Contour*>& contour);
+//      virtual void updateg(fmatvec::Vec& g, ContourPointData *cpData);   
+      virtual void updateg(std::vector<fmatvec::Vec>::iterator ig, std::vector<ContourPointData*>::iterator icpData);   
+//      virtual void updatewb(fmatvec::Vec& wb, const fmatvec::Vec &g,ContourPointData *cpData) { throw new MBSimError("ERROR (ContactKinematicsCircleSolidFlexibleBand::updatewb): not implemented!"); }   
+      virtual void updateg(fmatvec::Vec &g, ContourPointData *cpData) { throw new MBSimError("ERROR (ContactKinematicsCircleSolidFlexibleBand::updateg): Not implemented!"); }
+      virtual void updatewb(fmatvec::Vec &wb, const fmatvec::Vec &g, ContourPointData* cpData) { throw new MBSimError("ERROR (ContactKinematicsCircleSolidFlexibleBand::updatewb): Not implemented!"); };
+      /***************************************************/
+
+    private:
+      /** 
+       * \brief contour index 
+       */
+      int icircle, icontour;
+
+      /**
+       * \brief possible contacts regarded per node
+       */
+      int possibleContactsPerNode;
+
+      /**
+	   * \brief radius of circle
+	   */
+	  double rCircle;
+
+      /**
+	   * \brief half thickness of band
+	   */
+	  double hBand;
+
+      /**
+	   * \brief width of band
+	   */
+	  double wBand;
+
+      /** 
+       * \brief contour classes 
+       */
+      CircleSolid *circle;
+      FlexibleBand *band;
+
+      fmatvec::Vec lastContactPositions;
+      double epsTol;
+      double l0;
+  };
+
+}
+
+#endif /* CIRCLESOLID_FLEXIBLEBAND_H_ */
+
