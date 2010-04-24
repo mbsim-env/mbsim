f5986e2bd2ba38df3486756f29bdacdf15bd86ec
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2010-04-24 17:31:57 +0000

    Substituted Shaft.

--- trunk/modules/mbsimPowertrain/mbsimPowertrain/planetary_gear.cc
+++ trunk/modules/mbsimPowertrain/mbsimPowertrain/planetary_gear.cc
@@ -1,5 +1,6 @@
 #include "planetary_gear.h"
-#include "shaft.h"
+#include "mbsim/rigid_body.h"
+#include "mbsim/constraint.h"
 #include "mbsim/utils/rotarymatrices.h"
 #ifdef HAVE_OPENMBVCPPINTERFACE
 #include "openmbvcppinterface/frustum.h"
@@ -43,11 +44,12 @@ namespace MBSimPowertrain {
     housing->setInertiaTensor(Theta);
     housing->getFrame("C")->enableOpenMBV(0.3);
 
-    Shaft* sun = new Shaft("Sun");
+    RigidBody* sun = new RigidBody("Sun");
     addObject(sun);
 
     sun->setFrameOfReference(housing->getFrame("C"));
     sun->setFrameForKinematics(sun->getFrame("C"));
+    sun->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
 
     sun->setMass(mS);
     Theta(2,2) = JS;
@@ -56,7 +58,7 @@ namespace MBSimPowertrain {
     r(2) = lS/2;
     sun->getFrame("C")->enableOpenMBV(0.3);
 
-    Shaft* annulus = new Shaft("Annulus");
+    RigidBody* annulus = new RigidBody("Annulus");
     addObject(annulus);
 
     r(2) = lS/2-lH/2*1.01;
@@ -65,35 +67,39 @@ namespace MBSimPowertrain {
 
     annulus->setFrameOfReference(housing->getFrame("Q"));
     annulus->setFrameForKinematics(annulus->getFrame("C"));
+    annulus->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
     annulus->getFrame("C")->enableOpenMBV(0.3);
 
     annulus->setMass(mH);
     Theta(2,2) = JH;
     annulus->setInertiaTensor(Theta);
 
-    Shaft* carrier = new Shaft("Carrier");
+    RigidBody* carrier = new RigidBody("Carrier");
     addObject(carrier);
 
     r.init(0);
     r(2) = lS/2+lT/2;
     housing->addFrame("R",r,BasicRotAKIy(0));
     carrier->setFrameOfReference(housing->getFrame("R"));
-
     carrier->setFrameForKinematics(carrier->getFrame("C"));
+    carrier->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
     carrier->getFrame("C")->enableOpenMBV(0.3);
 
     carrier->setMass(mT);
     Theta(2,2) = JT;
     carrier->setInertiaTensor(Theta);
-    carrier->addDependecy(sun,rS/(rS+rH));
-    carrier->addDependecy(annulus,rH/(rS+rH));
+
+    Constraint2* constraint = new Constraint2("C1",carrier);
+    addObject(constraint);
+    constraint->addDependency(sun,0.5*rS/rT2);
+    constraint->addDependency(annulus,0.5*rH/rT2);
 
     r.init(0);
     r(2) = -lT/2-lP/2;
     r(1) = rT2;
 
     vector<Frame*> P;
-    vector<Shaft*> planet;
+    vector<RigidBody*> planet;
     int numP = 3;
     for(int i=0; i<numP; i++) {
       stringstream str;
@@ -102,17 +108,21 @@ namespace MBSimPowertrain {
       shaftName << "Planet" << i;
       P.push_back(new Frame(str.str()));
       carrier->addFrame(P[i],BasicRotAKIz(M_PI*2*i/3.)*r,BasicRotAKIy(0));
-      planet.push_back(new Shaft(shaftName.str()));
+      planet.push_back(new RigidBody(shaftName.str()));
       addObject(planet[i]);
       planet[i]->setFrameOfReference(carrier->getFrame(str.str()));
       planet[i]->setFrameForKinematics(planet[i]->getFrame("C"));
+      planet[i]->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
       planet[i]->getFrame("C")->enableOpenMBV(0.3);
 
       planet[i]->setMass(mP);
       Theta(2,2) = JP;
       planet[i]->setInertiaTensor(Theta);
-      planet[i]->addDependecy(sun,-rS/(2*rP)-rS/(rS+rH));
-      planet[i]->addDependecy(annulus,rH/(2*rP)-rH/(rS+rH));
+
+      Constraint2* constraint = new Constraint2(string("C_")+shaftName.str(),planet[i]);
+      addObject(constraint);
+      constraint->addDependency(sun,-0.5*(rS/rP+rS/rT2));
+      constraint->addDependency(annulus,0.5*(rH/rP-rH/rT2));
     }
 
 #ifdef HAVE_OPENMBVCPPINTERFACE
