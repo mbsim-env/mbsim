366ca658a7be4629816f0f7b1775ed2b38f0b190
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2010-04-25 20:09:13 +0000

    Prettified visualisation

--- examples/mechanics_basics_simple_gear/system.cc
+++ examples/mechanics_basics_simple_gear/system.cc
@@ -7,6 +7,7 @@
 
 #ifdef HAVE_OPENMBVCPPINTERFACE
 #include "openmbvcppinterface/frustum.h"
+#include "openmbvcppinterface/compoundrigidbody.h"
 #endif
 
 using namespace std;
@@ -25,9 +26,15 @@ class Moment : public Function1<fmatvec::Vec, double> {
 };
 
 Gear::Gear(const string &projectName) : DynamicSystemSolver(projectName) {
-  double R1 = 0.02;
+  double r1 = 0.02;
+  double r2 = 0.02;
+  double r3 = 0.02;
+  double R1 = 0.02*2;
+  double R2a = 0.04*2;
+  double R2b = 0.02*2;
+  double R3 = 0.04*2;
   double m1 = 1;
-  double J = 0.5*m1*R1*R1; 
+  double J = 0.5*m1*r1*r1; 
   SymMat Theta(3);
   double l = 0.1;
 
@@ -35,9 +42,6 @@ Gear::Gear(const string &projectName) : DynamicSystemSolver(projectName) {
   grav(1)=-9.81;
   MBSimEnvironment::getInstance()->setAccelerationOfGravity(grav);
 
-  Vec r(3);
-  r(0) = l/2;
-  addFrame("Q",r,BasicRotAKIy(M_PI/2));
 
   RigidBody* shaft1 = new RigidBody("Shaft1");
   addObject(shaft1);
@@ -46,9 +50,14 @@ Gear::Gear(const string &projectName) : DynamicSystemSolver(projectName) {
   shaft1->setMass(m1);
   Theta(2,2) = J;
   shaft1->setInertiaTensor(Theta);
-  shaft1->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
-  shaft1->getFrame("C")->enableOpenMBV(0.3);
+  shaft1->setRotation(new RotationAboutZAxis);
+  shaft1->getFrame("C")->enableOpenMBV(0.2);
   //shaft1->setInitialGeneralizedVelocity("[1]");
+
+  Vec r(3);
+  r(1) = R1+R2a;
+  r(2) = l;
+  addFrame("Q",r,SqrMat(3,EYE));
 
   RigidBody* shaft2 = new RigidBody("Shaft2");
   addObject(shaft2);
@@ -57,12 +66,12 @@ Gear::Gear(const string &projectName) : DynamicSystemSolver(projectName) {
   shaft2->setMass(m1);
   Theta(2,2) = J;
   shaft2->setInertiaTensor(Theta);
-  shaft2->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
-  shaft2->getFrame("C")->enableOpenMBV(0.3);
+  shaft2->setRotation(new RotationAboutZAxis);
+  shaft2->getFrame("C")->enableOpenMBV(0.2);
 
-  r(0) = 2*l;
-  r(1) = l/5;
-  addFrame("P",r,BasicRotAKIy(M_PI/2));
+  r(1) = R1+R2a-R2b-R3;
+  r(2) = 2*l;
+  addFrame("P",r,SqrMat(3,EYE));
   RigidBody* shaft3 = new RigidBody("Shaft3");
   addObject(shaft3);
   shaft3->setFrameOfReference(getFrame("P"));
@@ -70,12 +79,16 @@ Gear::Gear(const string &projectName) : DynamicSystemSolver(projectName) {
   shaft3->setMass(m1);
   Theta(2,2) = J;
   shaft3->setInertiaTensor(Theta);
-  shaft3->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
-  shaft3->getFrame("C")->enableOpenMBV(0.3);
+  shaft3->setRotation(new RotationAboutZAxis);
+  shaft3->getFrame("C")->enableOpenMBV(0.2);
 
   Constraint2 *constraint = new Constraint2("C1",shaft2);
   addObject(constraint);
-  constraint->addDependency(shaft1,0.5);
+  constraint->addDependency(shaft1,-R1/R2a);
+
+  constraint = new Constraint2("C2",shaft3);
+  addObject(constraint);
+  constraint->addDependency(shaft2,-R2b/R3);
 
   KineticExcitation* ke;
   ke = new KineticExcitation("MAn");
@@ -83,38 +96,70 @@ Gear::Gear(const string &projectName) : DynamicSystemSolver(projectName) {
   ke->connect(shaft1->getFrame("C"));
   ke->setMoment("[0;0;1]", new Moment(1.1/100.));
 
-  constraint = new Constraint2("C2",shaft3);
-  addObject(constraint);
-  constraint->addDependency(shaft2,0.5);
-
   ke = new KineticExcitation("MAbL");
   addLink(ke);
   ke->connect(shaft3->getFrame("C"));
   ke->setMoment("[0;0;1]", new Moment(-4.0/100.));
 
-  OpenMBV::Frustum *cylinder=new OpenMBV::Frustum;
-  cylinder->setTopRadius(R1);
-  cylinder->setBaseRadius(R1);
-  cylinder->setHeight(l);
-  cylinder->setStaticColor(0.1);
-  shaft1->setOpenMBVRigidBody(cylinder);
-  cylinder->setInitialTranslation(0,0,l/2);
+  OpenMBV::Frustum *c1=new OpenMBV::Frustum;
+  c1->setTopRadius(r1);
+  c1->setBaseRadius(r1);
+  c1->setHeight(l);
+  c1->setInitialTranslation(0,0,l/2);
 
-  cylinder=new OpenMBV::Frustum;
-  cylinder->setTopRadius(R1);
-  cylinder->setBaseRadius(R1);
-  cylinder->setHeight(l);
-  cylinder->setStaticColor(0.1);
-  shaft2->setOpenMBVRigidBody(cylinder);
-  cylinder->setInitialTranslation(0,0,l/2);
+  OpenMBV::Frustum *c2=new OpenMBV::Frustum;
+  c2->setTopRadius(R1);
+  c2->setBaseRadius(R1);
+  c2->setHeight(l/10);
+  c2->setInitialTranslation(0,0,l/2+l/20);
 
-  cylinder=new OpenMBV::Frustum;
-  cylinder->setTopRadius(R1);
-  cylinder->setBaseRadius(R1);
-  cylinder->setHeight(l);
-  cylinder->setStaticColor(0.1);
-  shaft3->setOpenMBVRigidBody(cylinder);
-  cylinder->setInitialTranslation(0,0,l/2);
+  OpenMBV::CompoundRigidBody *c = new OpenMBV::CompoundRigidBody;
+  c->addRigidBody(c1);
+  c->addRigidBody(c2);
+  c->setStaticColor(0.1);
+  shaft1->setOpenMBVRigidBody(c);
 
+  c1=new OpenMBV::Frustum;
+  c1->setTopRadius(r2);
+  c1->setBaseRadius(r2);
+  c1->setHeight(l);
+  c1->setInitialTranslation(0,0,l/2);
+
+  c2=new OpenMBV::Frustum;
+  c2->setTopRadius(R2a);
+  c2->setBaseRadius(R2a);
+  c2->setHeight(l/10);
+  c2->setInitialTranslation(0,0,-l/2+l/20);
+
+  OpenMBV::Frustum* c3=new OpenMBV::Frustum;
+  c3->setTopRadius(R2b);
+  c3->setBaseRadius(R2b);
+  c3->setHeight(l/10);
+  c3->setInitialTranslation(0,0,l/2+l/20);
+
+  c = new OpenMBV::CompoundRigidBody;
+  c->addRigidBody(c1);
+  c->addRigidBody(c2);
+  c->addRigidBody(c3);
+  c->setStaticColor(0.3);
+  shaft2->setOpenMBVRigidBody(c);
+
+  c1=new OpenMBV::Frustum;
+  c1->setTopRadius(r3);
+  c1->setBaseRadius(r3);
+  c1->setHeight(l);
+  c1->setInitialTranslation(0,0,l/2);
+
+  c2=new OpenMBV::Frustum;
+  c2->setTopRadius(R3);
+  c2->setBaseRadius(R3);
+  c2->setHeight(l/10);
+  c2->setInitialTranslation(0,0,-l/2+l/20);
+
+  c = new OpenMBV::CompoundRigidBody;
+  c->addRigidBody(c1);
+  c->addRigidBody(c2);
+  c->setStaticColor(0.5);
+  shaft3->setOpenMBVRigidBody(c);
 }
 
