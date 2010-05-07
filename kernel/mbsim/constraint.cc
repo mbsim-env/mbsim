129bf2c70d530d6cf91cbbb8b89b4207e4fa942a
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2010-05-07 17:10:51 +0000

    Working on constraints.

--- kernel/mbsim/constraint.cc
+++ kernel/mbsim/constraint.cc
@@ -27,171 +27,41 @@ using namespace std;
 
 namespace MBSim {
 
-  class Residuum : public Function1<Vec,Vec> {
-    RigidBody *body1, *body2;
-    Mat d;
-    Frame *frame1, *frame2;
-    double t;
-    int i1,i2;
-    public:
-    Residuum(RigidBody* body1_, RigidBody* body2_, const Mat &d_,Frame *frame1_, Frame *frame2_,double t_,int i1_, int i2_) : body1(body1_),body2(body2_),d(d_),frame1(frame1_), frame2(frame2_), t(t_), i1(i1_), i2(i2_) {}
-    Vec operator()(const Vec &x, const void * =NULL) {
-      Vec res(x.size()); 
-      int nq1 = body1->getqRel().size();
-      int nq2 = body2->getqRel().size();
+  JointConstraint::Residuum::Residuum(vector<RigidBody*> body1_, vector<RigidBody*> body2_, const Mat &dT_, const Mat &dR_,Frame *frame1_, Frame *frame2_,double t_,vector<int> i1_, vector<int> i2_) : body1(body1_),body2(body2_),dT(dT_),dR(dR_),frame1(frame1_), frame2(frame2_), t(t_), i1(i1_), i2(i2_) {}
+  Vec JointConstraint::Residuum::operator()(const Vec &x, const void *) {
+    Vec res(x.size()); 
+    int nq = 0;
+    for(unsigned int i=0; i<body1.size(); i++) {
+      int dq = body1[i]->getqRel().size();
+      body1[i]->getqRel() = x(nq,nq+dq-1);
+      nq += dq;
+    }
+    for(unsigned int i=0; i<body2.size(); i++) {
+      int dq = body2[i]->getqRel().size();
+      body2[i]->getqRel() = x(nq,nq+dq-1);
+      nq += dq;
+    }
 
-      body1->getqRel() = x(0,nq1-1);
-      body2->getqRel() = x(nq1,nq1+nq2-1);
+    for(unsigned int i=0; i<body1.size(); i++) {
+      body1[i]->updatePositionAndOrientationOfFrame(t,i1[i]);
+    }
+    for(unsigned int i=0; i<body2.size(); i++) {
+      body2[i]->updatePositionAndOrientationOfFrame(t,i2[i]);
+    }
 
-      body1->updatePositionAndOrientationOfFrame(t,i1);
-      body2->updatePositionAndOrientationOfFrame(t,i2);
+    int nT = dT.cols();
+    int nR = dR.cols();
+    if(nT) {
+      res(0,nT-1) = dT.T()*(frame1->getPosition()-frame2->getPosition()); 
+    }
+    if(nR) { // TODO
+      res(nT,nT+nR-1) = dR.T()*(frame1->getPosition()-frame2->getPosition()); 
+    }
 
-      res = d.T()*(frame1->getPosition()-frame2->getPosition()); 
-
-      return res;
-    } 
-  };
-
-  class Residuum2 : public Function1<Vec,Vec> {
-    RigidBody *body1, *body2;
-    Mat d;
-    Frame *frame1, *frame2, *frame3;
-    double t;
-    int i1,i2;
-    public:
-    Residuum2(RigidBody* body1_, RigidBody* body2_, const Mat &d_,Frame *frame1_, Frame *frame2_, Frame *frame3_, double t_,int i1_, int i2_) : body1(body1_),body2(body2_),d(d_),frame1(frame1_), frame2(frame2_), frame3(frame3_), t(t_), i1(i1_), i2(i2_) {}
-    Vec operator()(const Vec &x, const void * =NULL) {
-      Vec res(x.size()); 
-      int nq1 = body1->getqRel().size();
-      int nq2 = body2->getqRel().size();
-
-      body1->getqRel() = x(0,nq1-1);
-      body2->getqRel() = x(nq1,nq1+nq2-1);
-
-      body1->updatePositionAndOrientationOfFrame(t,i1);
-      body2->updatePositionAndOrientationOfFrame(t,i2);
-
-      res = d.T()*(frame2->getPosition()-frame3->getPosition()); 
-
-      return res;
-    } 
-  };
-
-  class Residuum3 : public Function1<Vec,Vec> {
-    vector<RigidBody*> body1, body2;
-    Mat dT, dR;
-    Frame *frame1, *frame2;
-    double t;
-    vector<int> i1,i2;
-    public:
-    Residuum3(vector<RigidBody*> body1_, vector<RigidBody*> body2_, const Mat &dT_, const Mat &dR_,Frame *frame1_, Frame *frame2_,double t_,vector<int> i1_, vector<int> i2_) : body1(body1_),body2(body2_),dT(dT_),dR(dR_),frame1(frame1_), frame2(frame2_), t(t_), i1(i1_), i2(i2_) {}
-    Vec operator()(const Vec &x, const void * =NULL) {
-      Vec res(x.size()); 
-      int nq = 0;
-      for(unsigned int i=0; i<body1.size(); i++) {
-	int dq = body1[i]->getqRel().size();
-	body1[i]->getqRel() = x(nq,nq+dq-1);
-	nq += dq;
-      }
-      for(unsigned int i=0; i<body2.size(); i++) {
-	int dq = body2[i]->getqRel().size();
-	body2[i]->getqRel() = x(nq,nq+dq-1);
-	nq += dq;
-      }
-
-      for(unsigned int i=0; i<body1.size(); i++) {
-	body1[i]->updatePositionAndOrientationOfFrame(t,i1[i]);
-      }
-      for(unsigned int i=0; i<body2.size(); i++) {
-	body2[i]->updatePositionAndOrientationOfFrame(t,i2[i]);
-      }
-
-      int nT = dT.cols();
-      int nR = dR.cols();
-      if(nT) {
-	res(0,nT-1) = dT.T()*(frame1->getPosition()-frame2->getPosition()); 
-      }
-      if(nR) { // TODO
-	res(nT,nT+nR-1) = dR.T()*(frame1->getPosition()-frame2->getPosition()); 
-      }
-
-      return res;
-    } 
-  };
+    return res;
+  } 
 
   Constraint::Constraint(const std::string &name) : Object(name) {
-  }
-
-  Constraint1::Constraint1(const std::string &name, RigidBody *b0, RigidBody* b1, RigidBody* b2, Frame* frame1_, Frame* frame2_) : Constraint(name), frame1(frame1_), frame2(frame2_) {
-    bi = b0;
-    bd1 = b1;
-    bd2 = b2;
-    bd1->addDependency(this);
-    bd2->addDependency(this);
-    if1 = bd1->frameIndex(frame1);
-    if2 = bd2->frameIndex(frame2);
-  }
-
-  void Constraint1::init(InitStage stage) {
-    if(stage==preInit) {
-      Constraint::init(stage);
-      dependency.push_back(bi);
-    }
-    else
-      Constraint::init(stage);
-  }
-
-  void Constraint1::initz() {
-    int nq1 = bd1->getqRel().size();
-    int nq2 = bd2->getqRel().size();
-    int nu1 = bd1->getuRel().size();
-    int nu2 = bd2->getuRel().size();
-    q12.resize(nq1+nq2);
-    u12.resize(nu1+nu2);
-    int nh = bd1->getJRel().cols();
-    J12.resize(nu1+nu2,nh);
-    j12.resize(nu1+nu2);
-
-    bd1->getqRel() >> q12(0,nq1-1);
-    bd2->getqRel() >> q12(nq1,nq1+nq2-1);
-    bd1->getuRel() >> u12(0,nu1-1);
-    bd2->getuRel() >> u12(nu1,nu1+nu2-1);
-    bd1->getJRel() >> J12(Index(0,nu1-1),Index(0,nh-1)); //(Index(0,nu1-1),Index(0,nh1-1))
-    bd2->getJRel() >> J12(Index(nu1,nu1+nu2-1),Index(0,nh-1));
-    bd1->getjRel() >> j12(Index(0,nu1-1)); // (Index(0,nu1-1))
-    bd2->getjRel() >> j12(Index(nu1,nu1+nu2-1));
-    J12t.resize(3,nu1+nu2);
-  }
-
-  void Constraint1::updateStateDependentVariables(double t){
-    Residuum* f = new Residuum(bd1,bd2,d,frame1,frame2,t,if1,if2);
-    MultiDimNewtonMethod newton(f);
-    q12 = newton.solve(q12);
-
-    bd1->updateRelativeJacobians(t,if1);
-    bd2->updateRelativeJacobians(t,if2);
-    int nu1 = bd1->getWJTrel().cols();
-    int nu2 = bd2->getWJTrel().cols();
-
- //   Mat J12(3,nu1+nu2);
-    J12t(Index(0,2),Index(0,nu1-1)) = bd1->getWJTrel();
-    J12t(Index(0,2),Index(nu1,nu1+nu2-1)) = -bd2->getWJTrel();
-    u12 = slvLU(SqrMat(d.T()*J12t),-d.T()*(frame1->getVelocity()-frame2->getVelocity())); 
-  }
-
-  void Constraint1::updateJacobians(double t){
-    //int nu1 = bd1->getWJTrel().cols();
-    //int nu2 = bd2->getWJTrel().cols();
- //   Mat J12t(3,nu1+nu2);
- //   J12t(Index(0,2),Index(0,nu1-1)) = bd1->getWJTrel();
- //   J12t(Index(0,2),Index(nu1,nu1+nu2-1)) = -bd2->getWJTrel();
-
-    bd1->updateAcclerations(t,if1);
-    bd2->updateAcclerations(t,if2);
-
-    J12 = slvLU(SqrMat(d.T()*J12t),-d.T()*(frame1->getJacobianOfTranslation()-frame2->getJacobianOfTranslation())); 
-    j12 = slvLU(SqrMat(d.T()*J12t),-d.T()*(frame1->getGyroscopicAccelerationOfTranslation()-frame2->getGyroscopicAccelerationOfTranslation()));
-
   }
 
   Constraint2::Constraint2(const std::string &name, RigidBody* body) : Constraint(name), bd(body) {
@@ -242,92 +112,7 @@ namespace MBSim {
     bd->getJRel().init(0); 
   }
 
-  Constraint4::Constraint4(const std::string &name, RigidBody *b0, RigidBody* b1, RigidBody* b2, Frame* frame1_, Frame* frame2_, Frame* frame3_) : Constraint(name), frame1(frame1_), frame2(frame2_), frame3(frame3_) {
-    bi = b0;
-    bd1 = b1;
-    bd2 = b2;
-    bd1->addDependency(this);
-    bd2->addDependency(this);
-    if1 = bd1->frameIndex(frame1);
-    if2 = bd2->frameIndex(frame2);
-  }
-
-  void Constraint4::initz() {
-      int nq1 = bd1->getqRel().size();
-      int nq2 = bd2->getqRel().size();
-      x0.resize(nq1+nq2);
-      x0(0,nq1-1).init(0.52);
-      x0(nq1,nq1+nq2-1).init(-2.2);
-  }
-
-  void Constraint4::init(InitStage stage) {
-    if(stage==preInit) {
-      Constraint::init(stage);
-      dependency.push_back(bi);
-    } 
-    else
-      Constraint::init(stage);
-  }
-
-  void Constraint4::updateStateDependentVariables(double t){
-    Residuum2* f = new Residuum2(bd1,bd2,d,frame1,frame2,frame3,t,if1,if2);
-    MultiDimNewtonMethod newton(f);
-    int nq1 = bd1->getqRel().size();
-    int nq2 = bd2->getqRel().size();
-    //Vec x0(nq1+nq2);
-  //  if(true || t>0) {
-  //  x0(0,nq1-1) = bd1->getqRel();
-  //  x0(nq1,nq1+nq2-1) = bd2->getqRel();
-  //  }
-  //  else {
-  //  x0(0,nq1-1).init(0.52);
-  //  x0(nq1,nq1+nq2-1).init(-2.2);
-  //  }
-    //Vec q12 = newton.solve(x0);
-    //bd1->getqRel() = q12(0,nq1-1);
-    //bd2->getqRel() = q12(nq1,nq1+nq2-1);
-    x0 = newton.solve(x0);
-    bd1->getqRel() = x0(0,nq1-1);
-    bd2->getqRel() = x0(nq1,nq1+nq2-1);
-
-    bd1->updateRelativeJacobians(t,if1);
-    bd2->updateRelativeJacobians(t,if2);
-    bd2->updateRelativeJacobians(t,if2,bd1->getWJTrel(),bd1->getWJRrel());
-
-    int nu1 = bd1->getWJTrel().cols();
-    int nu2 = bd2->getWJTrel().cols();
-
-    Mat J12(3,nu1+nu2);
-    J12(Index(0,2),Index(0,nu1-1)) = bd1->getWJTrel();
-    J12(Index(0,2),Index(nu1,nu1+nu2-1)) = bd2->getWJTrel();
-    Vec u12 = slvLU(SqrMat(d.T()*J12),-d.T()*frame2->getVelocity()); 
-    bd1->getuRel() = u12(0,nu1-1);
-    bd2->getuRel() = u12(nu1,nu1+nu2-1);
-  }
-
-  void Constraint4::updateJacobians(double t){
-    int nu1 = bd1->getWJTrel().cols();
-    int nu2 = bd2->getWJTrel().cols();
-    Mat J12t(3,nu1+nu2);
-    J12t(Index(0,2),Index(0,nu1-1)) = bd1->getWJTrel();
-    J12t(Index(0,2),Index(nu1,nu1+nu2-1)) = bd2->getWJTrel();
-
-    bd1->updateAcclerations(t,if1);
-    bd2->updateAcclerations(t,if2);
-
-    Mat J12 = slvLU(SqrMat(d.T()*J12t),-d.T()*frame2->getJacobianOfTranslation()); 
-    Vec j12 = slvLU(SqrMat(d.T()*J12t),-d.T()*frame2->getGyroscopicAccelerationOfTranslation());
-
-    int nh1 = bd1->getJRel().cols();
-    int nh2 = bd2->getJRel().cols();
-    bd1->getJRel() = J12(Index(0,nu1-1),Index(0,nh1-1)); 
-    bd2->getJRel() = J12(Index(nu1,nu1+nu2-1),Index(0,nh2-1));
-    bd1->getjRel() = j12(Index(0,nu1-1)); 
-    bd2->getjRel() = j12(Index(nu1,nu1+nu2-1));
-
-  }
-
-  Constraint5::Constraint5(const std::string &name, RigidBody* bi_, std::vector<RigidBody*> bd1_, std::vector<RigidBody*> bd2_, Frame* frame1_, Frame* frame2_) : Constraint(name), frame1(frame1_), frame2(frame2_) {
+  JointConstraint::JointConstraint(const std::string &name, RigidBody* bi_, std::vector<RigidBody*> bd1_, std::vector<RigidBody*> bd2_, Frame* frame1_, Frame* frame2_) : Constraint(name), frame1(frame1_), frame2(frame2_) {
     bi = bi_;
     bd1 = bd1_;
     bd2 = bd2_;
@@ -354,7 +139,7 @@ namespace MBSim {
     }
   }
 
-  void Constraint5::init(InitStage stage) {
+  void JointConstraint::init(InitStage stage) {
     if(stage==preInit) {
       Constraint::init(stage);
       dependency.push_back(bi);
@@ -368,7 +153,7 @@ namespace MBSim {
       Constraint::init(stage);
   }
 
-  void Constraint5::initz() {
+  void JointConstraint::initz() {
     nq = 0;
     nu = 0;
     for(unsigned int i=0; i<bd1.size(); i++) {
@@ -379,14 +164,14 @@ namespace MBSim {
       nq += dq;
       nu += du;
     }
-     for(unsigned int i=0; i<bd2.size(); i++) {
-	int dq = bd2[i]->getqRel().size();
-	int du = bd2[i]->getuRel().size();
-	Iq2.push_back(Index(nq,nq+dq-1));
-	Iu2.push_back(Index(nu,nu+du-1));
-	nq += dq;
-	nu += du;
-      }
+    for(unsigned int i=0; i<bd2.size(); i++) {
+      int dq = bd2[i]->getqRel().size();
+      int du = bd2[i]->getuRel().size();
+      Iq2.push_back(Index(nq,nq+dq-1));
+      Iu2.push_back(Index(nu,nu+du-1));
+      nq += dq;
+      nu += du;
+    }
     int nh = bd1[0]->getJRel().cols();
     q.resize(nq);
     u.resize(nu);
@@ -410,8 +195,8 @@ namespace MBSim {
   }
 
 
-  void Constraint5::updateStateDependentVariables(double t){
-    Residuum3* f = new Residuum3(bd1,bd2,dT,dR,frame1,frame2,t,if1,if2);
+  void JointConstraint::updateStateDependentVariables(double t){
+    Residuum* f = new Residuum(bd1,bd2,dT,dR,frame1,frame2,t,if1,if2);
     MultiDimNewtonMethod newton(f);
     q = newton.solve(q);
 
@@ -420,7 +205,7 @@ namespace MBSim {
       for(unsigned int j=i+1; j<bd1.size(); j++) 
 	bd1[j]->updateRelativeJacobians(t,if1[j],bd1[i]->getWJTrel(),bd1[i]->getWJRrel());
     }
-    
+
     for(unsigned int i=0; i<bd2.size(); i++) {
       bd2[i]->updateRelativeJacobians(t,if2[i]);
       for(unsigned int j=i+1; j<bd2.size(); j++) 
@@ -444,7 +229,7 @@ namespace MBSim {
     u = slvLU(A,b); 
   }
 
-  void Constraint5::updateJacobians(double t) {
+  void JointConstraint::updateJacobians(double t) {
 
     for(unsigned int i=0; i<bd1.size(); i++)
       bd1[i]->updateAcclerations(t,if1[i]);
