570f3de7bccbbce525e8df094e383711eb78b1e7
mfoerg <mfoerg@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2010-05-15 15:37:31 +0000

    update

--- trunk/examples/powertrain_vehicle_dynamics/system.cc
+++ trunk/examples/powertrain_vehicle_dynamics/system.cc
@@ -409,7 +409,7 @@ System::System(const string &projectName) : DynamicSystemSolver(projectName) {
       bd2.push_back(psR->getInputShaft());
       bd2.push_back(psR->getIntermediateShaft());
       bd2.push_back(psR->getOutputShaft());
-      JointConstraint* jointconstraint = new JointConstraint("CR",hr,bd1,bd2,hr->getFrame("K"),psR->getOutputShaft()->getFrame("Q"));
+      JointConstraint* jointconstraint = new JointConstraint("CR",bd1,bd2,hr->getFrame("K"),psR->getOutputShaft()->getFrame("Q"));
       addObject(jointconstraint);
       jointconstraint->setForceDirection(Mat(3,3,EYE));
       jointconstraint->setMomentDirection(Mat(3,3,EYE));
@@ -422,7 +422,7 @@ System::System(const string &projectName) : DynamicSystemSolver(projectName) {
       bd2.push_back(psL->getInputShaft());
       bd2.push_back(psL->getIntermediateShaft());
       bd2.push_back(psL->getOutputShaft());
-      JointConstraint* jointconstraint = new JointConstraint("CL",hl,bd1,bd2,hl->getFrame("K"),psL->getOutputShaft()->getFrame("Q"));
+      JointConstraint* jointconstraint = new JointConstraint("CL",bd1,bd2,hl->getFrame("K"),psL->getOutputShaft()->getFrame("Q"));
       addObject(jointconstraint);
       jointconstraint->setForceDirection(Mat(3,3,EYE));
       jointconstraint->setMomentDirection(Mat(3,3,EYE));
