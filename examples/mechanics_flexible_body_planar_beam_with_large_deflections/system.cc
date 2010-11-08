8a82395ce8619d96a96610a53c57c4312c7e8283
schneidm <schneidm@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2010-11-08 17:22:50 +0000

    fix

--- examples/mechanics_flexible_body_planar_beam_with_large_deflections/system.cc
+++ examples/mechanics_flexible_body_planar_beam_with_large_deflections/system.cc
@@ -4,8 +4,7 @@
 #include "mbsim/joint.h"
 #include "mbsim/contact.h"
 #include "mbsim/contours/point.h"
-#include "mbsim/contours/flexible_band.h"
-#include "mbsim/contact_kinematics/point_flexibleband.h"
+#include "mbsimFlexibleBody/contours/flexible_band.h"
 #include "mbsim/constitutive_laws.h"
 #include "mbsim/environment.h"
 
@@ -107,9 +106,7 @@ System::System(const string &projectName) : DynamicSystemSolver(projectName) {
   ball->setOpenMBVRigidBody(sphere);
 #endif
 
-  ContactKinematicsPointFlexibleBand *ck = new ContactKinematicsPointFlexibleBand();
   Contact *contact = new Contact("Contact");
-  contact->setContactKinematics(ck);
   contact->setContactForceLaw(new UnilateralConstraint);
   contact->setContactImpactLaw(new UnilateralNewtonImpact(1.0));
   contact->connect(ball->getContour("Point"),rod->getContour("Top"));
