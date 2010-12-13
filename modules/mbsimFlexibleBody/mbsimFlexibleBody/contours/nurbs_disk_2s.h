abb6530537bfc3f316d274b5635a4d2103907c8e
thschindler <thschindler@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2010-12-13 23:12:58 +0000

    fix

--- modules/mbsimFlexibleBody/mbsimFlexibleBody/contours/nurbs_disk_2s.h
+++ modules/mbsimFlexibleBody/mbsimFlexibleBody/contours/nurbs_disk_2s.h
@@ -23,6 +23,7 @@
 #include "fmatvec.h"
 #include "mbsim/mbsim_event.h"
 #include "mbsim/contours/contour2s.h"
+#include "mbsimFlexibleBody/utils/contact_utils.h"
 
 #ifdef HAVE_NURBS
 #define MY_PACKAGE_BUGREPORT PACKAGE_BUGREPORT
@@ -101,6 +102,9 @@ namespace MBSimFlexibleBody {
       /* INHERITED INTERFACE OF CONTOUR */
       virtual void updateKinematicsForFrame(MBSim::ContourPointData &cp, MBSim::FrameFeature ff);
       virtual void updateJacobiansForFrame(MBSim::ContourPointData &cp);
+      virtual MBSim::ContactKinematics * findContactPairingWith(std::string type0, std::string type1) {
+        return findContactPairingFlexible(type0.c_str(), type1.c_str());
+      }
       /***************************************************/
 
 #ifdef HAVE_NURBS
