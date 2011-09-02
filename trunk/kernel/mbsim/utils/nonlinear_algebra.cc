83ad69a2396954c5116face03585fb58bba02039
cebulla <cebulla@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2011-09-02 11:49:10 +0000

    Added Lemke-Solver for solving LCPs directly to nonlinear algebra Added Multi-dimensional fixpoint iteration to nonlinear algebra Enahnced Multi-Dimensional Newton Method with more info for the info-variable

--- trunk/kernel/mbsim/utils/nonlinear_algebra.cc
+++ trunk/kernel/mbsim/utils/nonlinear_algebra.cc
@@ -20,9 +20,12 @@
 #include <config.h>
 #include "mbsim/utils/nonlinear_algebra.h"
 #include "mbsim/utils/eps.h"
+#include <mbsim/utils/utils.h>
+
 #include <cmath>
 
 using namespace fmatvec;
+using namespace std;
 
 namespace MBSim {
 
@@ -58,6 +61,43 @@ namespace MBSim {
     }
     info = -1;
     return u; 
+  }
+
+  MultiDimFixPointIteration::MultiDimFixPointIteration(Function1<Vec, Vec> *function_) :
+      function(function_), tol(1e-10), iter(0), itermax(30000), norms(0), info(1) {
+  }
+
+  /*
+   * \brief finds a fixpoint starting on the initialGuess values
+   * \param intialGuess starting value for the fixpoint iteration
+   * \return vector after iteration (solution or currentGuess-value)
+   */
+  Vec MultiDimFixPointIteration::solve(const Vec & initialGuess) {
+    Vec currentGuess = initialGuess.copy();
+    Vec lastGuess;
+    norms.clear();
+    for (iter = 0; iter < itermax; iter++) {
+      lastGuess = currentGuess.copy();
+
+      currentGuess = (*function)(currentGuess);
+
+      norms.push_back(nrmInf(currentGuess - lastGuess));
+      if (norms[iter] < tol) {
+        info = 0;
+        return currentGuess;
+      }
+
+    }
+
+    info = 1; //convergence (needs to be true for all steps)
+    for (size_t i = 1; i < norms.size(); i++) {
+      if (norms[i - 1] <= norms[i]) {
+        info = -1; //no convergence
+        break;
+      }
+    }
+
+    return currentGuess;
   }
 
   NewtonMethod::NewtonMethod(Function1<double,double> *fct_, Function1<double,double> *jac_) : fct(fct_), jac(jac_), itmax(300), iter(0), kmax(100), tol(1e-10) {}
@@ -117,7 +157,7 @@ namespace MBSim {
     return 0;
   }
 
-  MultiDimNewtonMethod::MultiDimNewtonMethod(Function1<Vec,Vec> *fct_, Function1<SqrMat,Vec> *jac_) : fct(fct_), jac(jac_), itmax(300), iter(0), kmax(100), tol(1e-10) {}
+  MultiDimNewtonMethod::MultiDimNewtonMethod(Function1<Vec,Vec> *fct_, Function1<SqrMat,Vec> *jac_) : fct(fct_), jac(jac_), itmax(300), iter(0), kmax(100), norms(0), tol(1e-10) {}
 
   Vec MultiDimNewtonMethod::solve(const Vec &x0) {
 
@@ -126,11 +166,12 @@ namespace MBSim {
     x = x0;
 
     Vec f = (*fct)(x);
-    double nrmf0 = nrmInf(f);
+    norms.clear();
+    norms.push_back(nrmInf(f));
     SqrMat J;
-    for (iter=1; iter <= itmax; iter++) {
+    for (iter = 0; iter <= itmax; iter++) {
 
-      if (nrmf0 <= tol) {
+      if (norms[iter] <= tol) {
         info = 0;
         return x;
       }
@@ -166,19 +207,199 @@ namespace MBSim {
         x = xold - alpha*dx;
         f = (*fct)(x);
         nrmf = nrmInf(f);
-        if(nrmf < nrmf0)
+        if (nrmf < norms[iter])
           break;
         alpha *= 0.5;
       }
-      nrmf0 = nrmf;
+      norms.push_back(nrmf);
       if (nrmf <= tol) {
         info = 0;
         return x;
       }
     }
-    info = -1;
 
-    return 0;
+    info = 1; //convergence (needs to be true for all steps)
+    for (size_t i = 1; i < norms.size(); i++) {
+      if (norms[i - 1] <= norms[i]) { //divergence
+        info = -1;
+        break;
+      }
+    }
+
+    return x;
+  }
+
+  Vec LemkeAlgorithm::solve() {
+    /*REMARK:
+     * This algorithm is taken from:
+     * Das lineare Komplementaritätsproblem: Eine Einführung  (Uwe Schäfer) pages 7 ff
+     */
+
+    size_t dim = q.size();
+
+    Vec solutionVector(2 * dim, INIT, 0.);
+
+    Mat A(dim, 2 * dim + 2, NONINIT);
+    A(0, 0, dim - 1, dim - 1) = Mat(dim, dim, EYE);
+    A(0, dim, dim - 1, 2 * dim - 1) = (-M).copy();
+    A(0, 2 * dim, dim - 1, 2 * dim) = Vec(dim, INIT, -1.);
+    A(0, 2 * dim + 1, dim - 1, 2 * dim + 1) = q.copy();
+
+    Vec q_;
+    q_.resize() >> A(0, 2 * dim + 1, dim - 1, 2 * dim + 1);
+
+    vector<size_t> basis;
+    //At first, all w-values are in the basis
+    for (size_t i = 0; i < dim; i++)
+      basis.push_back(i);
+
+    int pivotRowIndex = minIndex(q);     // first row is that with lowest q-value
+    int pivotColIndex = 2 * dim;         //first col is that of z0
+
+    if (INFO) {
+      cout << "A: " << A << endl;
+      cout << "pivotRowIndex " << pivotRowIndex << endl;
+      cout << "pivotColIndex " << pivotColIndex << endl;
+      cout << "Basis: ";
+      for (size_t i = 0; i < basis.size(); i++)
+        cout << basis[i];
+      cout << endl;
+    }
+
+    while (!greaterZero(q_) or !validBasis(basis)) {
+
+      GaussJordanEliminationStep(A, pivotRowIndex, pivotColIndex, basis);
+
+      if (INFO) {
+        cout << "A: " << A << endl;
+        cout << "pivotRowIndex " << pivotRowIndex << endl;
+        cout << "pivotColIndex " << pivotColIndex << endl;
+        cout << "Basis: ";
+        for (size_t i = 0; i < basis.size(); i++)
+          cout << basis[i];
+        cout << endl;
+      }
+
+      int pivotColIndexOld = pivotColIndex;
+
+      /*find new column index */
+      if (basis[pivotRowIndex] < dim) //if a w-value left the basis get in the correspondent z-value
+        pivotColIndex = basis[pivotRowIndex] + dim;
+      else
+        //else do it the other way round and get in the corresponding w-value
+        pivotColIndex = basis[pivotRowIndex] - dim;
+
+      /*the column becomes part of the basis*/
+      basis[pivotRowIndex] = pivotColIndexOld;
+
+      pivotRowIndex = findLexicographicMinimum(A, pivotColIndex);
+
+    }
+
+    if (INFO) {
+      cout << "A: " << A << endl;
+      cout << "pivotRowIndex " << pivotRowIndex << endl;
+      cout << "pivotColIndex " << pivotColIndex << endl;
+    }
+
+    for (size_t i = 0; i < basis.size(); i++)
+      solutionVector(basis[i]) = q_(i);
+
+    return solutionVector;
+  }
+
+  int LemkeAlgorithm::findLexicographicMinimum(const fmatvec::Mat &A, const int & pivotColIndex) {
+    int RowIndex = 0;
+    size_t dim = A.rows();
+    vector<Vec> Rows;
+    for (size_t row = 0; row < dim; row++) {
+      Rows.push_back(Vec(dim + 1, INIT, 0.));
+      double a = A(row, pivotColIndex);
+      if (a > 0) {
+        Rows[row](0) = A(row, 2 * dim + 1) / a;
+        Rows[row](1) = A(row, 2 * dim) / a;
+        for (size_t j = 2; j < dim + 1; j++)
+          Rows[row](j) = A(row, j - 1) / a;
+
+        if (INFO) {
+          cout << "Rows(" << row << ") = " << Rows[row] << endl;
+        }
+      }
+    }
+
+    for (size_t i = 0; i < Rows.size(); i++) {
+      if (nrm2(Rows[i]) > 0.) {
+
+        size_t j = 0;
+        for (; j < Rows.size(); j++) {
+          if(i != j)
+            if(nrm2(Rows[j]) > 0.)
+              if (! LexicographicPositive(Rows[j] - Rows[i]))
+                break;
+        }
+
+        if (j == Rows.size()) {
+          RowIndex += i;
+          break;
+        }
+      }
+    }
+
+    return RowIndex;
+  }
+
+  bool LemkeAlgorithm::LexicographicPositive(const fmatvec::Vec & v) {
+    int i = 0;
+    if (INFO)
+      cout << "v " << v << endl;
+
+    while (fabs(v(i)) < macheps() and i < v.size())
+      i++;
+    if (v(i) > 0)
+      return true;
+
+    return false;
+  }
+
+  void LemkeAlgorithm::GaussJordanEliminationStep(Mat &A, int pivotRowIndex, int pivotColumnIndex, const vector<size_t> & basis) {
+    double a = -1 / A(pivotRowIndex, pivotColumnIndex);
+    for (int i = 0; i < A.rows(); i++)
+      if (i != pivotRowIndex)
+        for (int j = 0; j < A.cols(); j++)
+          if (j != pivotColumnIndex)
+            A(i, j) += A(pivotRowIndex, j) * A(i, pivotColumnIndex) * a;
+
+    for (int i = 0; i < A.cols(); i++) {
+      A(pivotRowIndex, i) *= -a;
+    }
+
+    for (int i = 0; i < A.rows(); i++)
+      if (i != pivotRowIndex)
+        A(i, pivotColumnIndex) = 0;
+  }
+
+  bool LemkeAlgorithm::greaterZero(const Vec & vector) {
+    bool isGreater = true;
+    for (int i = 0; i < vector.size(); i++) {
+      if (vector(i) < 0) {
+        isGreater = false;
+        break;
+      }
+    }
+
+    return isGreater;
+  }
+
+  bool LemkeAlgorithm::validBasis(const vector<size_t> & basis) {
+    bool isValid = true;
+    for (size_t i = 0; i < basis.size(); i++) {
+      if (basis[i] >= basis.size() * 2) { //then z0 is in the base
+        isValid = false;
+        break;
+      }
+    }
+
+    return isValid;
   }
 
 }
