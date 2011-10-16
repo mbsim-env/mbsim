383939c03feb8ddc2775db551cfd43895139af58
ts.schindler@gmail.com <ts.schindler@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2011-10-16 15:06:25 +0000

    cosserat : interpolation spline for translations and velocities defined

--- trunk/modules/mbsimFlexibleBody/mbsimFlexibleBody/contours/nurbs_curve_1s.h
+++ trunk/modules/mbsimFlexibleBody/mbsimFlexibleBody/contours/nurbs_curve_1s.h
@@ -23,7 +23,6 @@
 #include "fmatvec.h"
 #include "mbsim/mbsim_event.h"
 #include "mbsim/contours/contour1s.h"
-#include "mbsimFlexibleBody/utils/contact_utils.h"
 
 #ifdef HAVE_NURBS
 #define MY_PACKAGE_BUGREPORT PACKAGE_BUGREPORT
@@ -69,11 +68,7 @@ namespace MBSimFlexibleBody {
        * \brief constructor 
        * \param name of contour
        */
-      NurbsCurve1s(const std::string &name) : MBSim::Contour2s(name), nj(0), nr(0), degU(0), degV(0), Ri(0.), Ra(0.) {
-#ifndef HAVE_NURBS
-        throw MBSim::MBSimError("ERROR(NurbsCurve1s::NurbsCurve1s): External NURBS library not implemented!");
-#endif
-      }
+      NurbsCurve1s(const std::string &name);
 
       /**
        * \brief destructor
@@ -92,174 +87,79 @@ namespace MBSimFlexibleBody {
       /***************************************************/
 
       /* INHERITED INTERFACE OF CONTOUR */
-      virtual void updateKinematicsForFrame(MBSim::ContourPointData &cp, MBSim::FrameFeature ff);
-      virtual void updateJacobiansForFrame(MBSim::ContourPointData &cp);
-      virtual MBSim::ContactKinematics * findContactPairingWith(std::string type0, std::string type1) {
-        return findContactPairingFlexible(type0.c_str(), type1.c_str());
-      }
+      virtual void updateKinematicsForFrame(MBSim::ContourPointData &cp, MBSim::FrameFeature ff) { throw MBSim::MBSimError("ERROR(NurbsCurve1s::updateKinematicsForFrame): Not implemented!"); }
+      virtual void updateJacobiansForFrame(MBSim::ContourPointData &cp) { throw MBSim::MBSimError("ERROR(NurbsCurve1s::updateJacobiansForFrame): Not implemented!"); }
+      virtual MBSim::ContactKinematics *findContactPairingWith(std::string type0, std::string type1) { throw MBSim::MBSimError("ERROR(NurbsCurve1s::findContactPairingWith): Not implemented!"); }
       /***************************************************/
 
 #ifdef HAVE_NURBS
       /**
-       * \brief initialize NURBS disk
+       * \brief initialize NURBS curve 
        * \param stage of initialisation
        */
       void initContourFromBody(MBSim::InitStage stage);
 #endif
 
-      /*! 
-       * \brief transformation cartesian to cylinder system
-       * \param cartesian vector in world system
-       * \return cylindrical coordinates
-       */
-      fmatvec::Vec transformCW(const fmatvec::Vec& WrPoint);
 
 #ifdef HAVE_NURBS
       /*! 
-       * \return derivates of the surface: (first column: deg-th derivative in radial direction / second column: deg-th derivative in azimuthal-direction)
-       * \param radial location
-       * \param azimuthal location
-       * \param order of derivative
-       */
-      fmatvec::Mat computeDirectionalDerivatives(const double &radius, const double &phi, const int &deg);
-#endif
-
-#ifdef HAVE_NURBS
-      /*!
-       * \return curvature on the surface: (first column: radial direction / second column: azimuthal-direction)
-       * \param radial location
-       * \param azimuthal location
-       */
-      fmatvec::Mat computeCurvatures(const double &radius, const double &phi);
-#endif
-
-#ifdef HAVE_NURBS
-      /*! 
-       * \brief computes the U vector of the surface for a closed interpolation
-       * \param
+       * \brief computes the U-Vector
+       * \param number of points
        */
       void computeUVector(const int NbPts); 
 #endif
 
 #ifdef HAVE_NURBS
       /*! 
-       * \brief computes the V-vector of the surface for an open interpolation
-       * \param
+       * \brief interpolates the translations with node-data from body
        */
-      void computeVVector(const int NbPts);
-#endif
-
-#ifdef HAVE_NURBS
-      /*! 
-       * \brief interpolates the surface with node-data from body
-       */
-      void computeSurface();
+      void computeCurveTranslations();
 #endif
 
 #ifdef HAVE_NURBS
       /*!
-       * \brief interpolates the velocities of the surface with the node-data from the body
+       * \brief interpolates the velocities with the node-data from the body
        */
-      void computeSurfaceVelocities();
+      void computeCurveVelocities();
 #endif
-
-#ifdef HAVE_NURBS
-      /*! 
-       * \brief interpolates the Jacobians of translation of the surface with the node-data from the body
-       */
-      void computeSurfaceJacobians();
-#endif
-
-#ifdef HAVE_NURBS
-      /*!
-       * \return control point
-       * \param u location
-       * \param v location
-       */
-      fmatvec::Vec getControlPoints(const int u, const int v);
-#endif
-
-#ifdef HAVE_NURBS
-      /*! 
-       * return U-Vector of the surface (azimuthal direction)
-       */
-      fmatvec::Vec getUVector();
-#endif
-
-#ifdef HAVE_NURBS
-      /*! 
-       * return V-Vector of the surface (radial direction)
-       */
-      fmatvec::Vec getVVector();
-#endif
-
-      /*! 
-       * \return flag, whether the input radius is inside the bounds or the input angle is between 0 and 2 PI
-       * \param parametrisation vector
-       */
-      int testInsideBounds(const fmatvec::Vec &s);
-
-      /*! 
-       * \return norm of the difference between two vectors
-       * \param first vector
-       * \param second vector
-       */
-      double computeError(const fmatvec::Vec &Vec1, const fmatvec::Vec &Vec2);
 
     protected:
-      /** 
-       * \brief number of reference dofs of the flexible body
+      /**
+       * \brief number of elements
        */
-      int RefDofs;
+      int Elements;
 
       /**
-       * \brief number of elements in azimuthal and radial direction
+       * \brief open or closed beam structure
        */
-      int nj, nr;
+      bool openStructure;
 
       /**
-       * \brief interpolation degree azimuthal and radial
+       * \brief length of entire curve
        */
-      int degU, degV;
+      double L;
 
       /**
-       * \brief inner and outer radius
+       * \brief interpolation degree
        */
-      double Ri, Ra;
+      int degU;
 
 #ifdef HAVE_NURBS
-      /**
-       * \brief Jacobians of finite element nodes
+      /** 
+       * \brief interpolated translations of the contour
        */
-      std::vector<MBSim::ContourPointData> jacobians; // size = number of interpolation points
+      PlNurbsCurved *curveTranslations;
 
       /** 
-       * \brief interpolated surface of the contour
+       * \brief interpolated velocities of the contour
        */
-      PlNurbsSurfaced *Surface; // an homogenous surface with accuracy of calculation of double (HSurface<double,3>)
-
-      /** 
-       * \brief interpolated velocities of the surface-points
-       */
-      PlNurbsSurfaced *SurfaceVelocities;
+      PlNurbsCurved *curveVelocities;
 
       /**
-       * \brief interpolated Jacobians of Translation on the surface 
+       * \brief knot vector
        */
-      std::vector<PlNurbsSurfaced> SurfaceJacobiansOfTranslation; // size = number of generalized coordinates
-
-      /**
-       * \brief interpolated Jacobians of Translation on the surface
-       */
-      std::vector<PlNurbsSurfaced> SurfaceJacobiansOfRotation; // size = number of generalized coordinates
-
-      /**
-       * \brief knot vectors, used for the U und V coordinates of the surface
-       */
-      PLib::Vector<double> *uvec; // nurbs++ needs this vector 
-      PLib::Vector<double> *uVec; // knot-vector for azimuthal direction 
-      PLib::Vector<double> *vvec; // nurbs++ needs this vector
-      PLib::Vector<double> *vVec; // knot-vector for radial direction
+      PLib::Vector<double> *uvec; // nurbs++ needs this vector
+      PLib::Vector<double> *uVec; // knot-vector
 #endif
   };
 
