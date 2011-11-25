28f5af2363384d32a08df255c56eb4bd45b3412c
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2011-11-25 16:56:37 +0000

    Update for inverse kinetics.

--- version-3.0.0/mbsim/gear.cc
+++ version-3.0.0/mbsim/gear.cc
@@ -20,16 +20,18 @@
 #include <config.h>
 #include "mbsim/gear.h"
 #include "mbsim/frame.h"
+#include "mbsim/dynamic_system_solver.h"
 
 using namespace std;
 using namespace fmatvec;
 
 namespace MBSim {
 
-  Gear::Gear(const string &name) : LinkMechanics(name) 
+  Gear::Gear(const string &name) : LinkMechanics(name)//, frame(0)
   {
     body.push_back(0); 
-    ratio.push_back(-1);
+    ratio[0].push_back(-1);
+    ratio[1].push_back(-1);
   }
 
   void Gear::calclaSize() {
@@ -42,75 +44,95 @@ namespace MBSim {
     gdSize = 1;
   }
 
-  void Gear::addDependency(RigidBody* body_, double ratio_) {
+  void Gear::addDependency(RigidBody* body_, double ratio1, double ratio2) {
     body.push_back(body_); 
-    ratio.push_back(ratio_);
+    ratio[0].push_back(ratio1);
+    ratio[1].push_back((int)ratio2==0?ratio1:ratio2);
   }
 
   void Gear::updateW(double t, int j) {
-    if(j==0) {
-      for(unsigned i=0; i<body.size(); i++) 
-        W[j][i]-=body[i]->getJRel(j).T()*ratio[i];
-    } else {
-      for(unsigned i=0; i<body.size(); i++) 
-        W[j][i]-=body[i]->getFrameOfReference()->getOrientation()*body[i]->getPJR()*ratio[i];
+    //  if(j==0) {
+    //    for(unsigned i=0; i<body.size(); i++)  {
+    //      W[j][i]-=body[i]->getJRel(j).T()*ratio[i];
+    //    }
+    //  } else {
+    for(unsigned i=0; i<body.size(); i++) {
+      W[j][i]-=body[i]->getFrames()[0]->getJacobianOfRotation(j).T()*(body[i]->getFrameOfReference()->getOrientation()*body[i]->getPJR()*ratio[1][i]);
+      W[j][body.size()]+=frame->getJacobianOfRotation(j).T()*(body[i]->getFrameOfReference()->getOrientation()*body[i]->getPJR()*ratio[1][i]);
     }
+    //  }
   }
 
   void Gear::updateh(double t, int j) {
     la(0) = (*func)(g(0),gd(0));
-    if(j==0) {
-      for(unsigned i=0; i<body.size(); i++) 
-        h[j][i]-=body[i]->getJRel(j).T()*ratio[i]*la;
-    } else {
-      for(unsigned i=0; i<body.size(); i++) 
-        h[j][i]-=body[i]->getFrameOfReference()->getOrientation()*body[i]->getPJR()*ratio[i]*la;
+    for(unsigned i=0; i<body.size(); i++) {
+      h[j][i]-=body[i]->getFrames()[0]->getJacobianOfRotation(j).T()*(body[i]->getFrameOfReference()->getOrientation()*body[i]->getPJR()*ratio[1][i]*la);
+      h[j][body.size()]+=frame->getJacobianOfRotation(j).T()*(body[i]->getFrameOfReference()->getOrientation()*body[i]->getPJR()*ratio[1][i]*la);
     }
   }
 
   void Gear::updateWRef(const Mat &WParent, int j) {
     for(unsigned i=0; i<body.size(); i++) {
       Index J = Index(laInd,laInd+laSize-1);
-      Index I = Index(body[i]->gethInd(j)+(j==0?0:3),body[i]->gethInd(j)+(j==0?0:3)+(j==0?body[i]->getJRel(j).cols():3)-1);
+      Index I = Index(body[i]->gethInd(j),body[i]->gethInd(j)+body[i]->gethSize(j)-1);
 
-      W[j][i].resize()>>WParent(I,J);
+      W[j][i]>>WParent(I,J);
     }
+    Index J = Index(laInd,laInd+laSize-1);
+    Index I = Index(frame->gethInd(j),frame->gethInd(j)+frame->getJacobianOfTranslation(j).cols()-1); // TODO Prüfen ob hSize
+    W[j][body.size()]>>WParent(I,J);
   } 
 
   void Gear::updatehRef(const Vec &hParent, int j) {
     for(unsigned i=0; i<body.size(); i++) {
-      Index I = Index(body[i]->gethInd(j)+(j==0?0:3),body[i]->gethInd(j)+(j==0?0:3)+(j==0?body[i]->getJRel(j).cols():3)-1);
+      Index I = Index(body[i]->gethInd(j),body[i]->gethInd(j)+body[i]->gethSize(j)-1);
       h[j][i]>>hParent(I);
     }
+    Index I = Index(frame->gethInd(j),frame->gethInd(j)+frame->getJacobianOfTranslation(j).cols()-1); // TODO Prüfen ob hSize
+    h[j][body.size()].resize()>>hParent(I);
   } 
 
   void Gear::updateg(double) {
     g.init(0);
     for(unsigned i=0; i<body.size(); i++)
-      g+=body[i]->getuRel()*ratio[i];
+      g+=body[i]->getqRel()*ratio[0][i];
   } 
 
   void Gear::updategd(double) {
     gd.init(0);
-    for(unsigned i=0; i<body.size(); i++)
-      gd-=body[i]->getuRel()*ratio[i];
+  //  Vec buf(1);
+    for(unsigned i=0; i<body.size(); i++) {
+      //buf += (body[i]->getFrameOfReference()->getOrientation()*body[i]->getPJR()*ratio[1][i]).T()*(body[i]->getFrames()[0]->getAngularVelocity()-frame->getAngularVelocity());
+      gd+=body[i]->getuRel()*ratio[0][i];
+    }
   }
 
   bool Gear::isSetValued() const {
     return func?false:true;
   }
 
+  void Gear::updatewb(double t, int j) {
+    //buf += (frame->getOrientation()*body[i]->getPJR()*ratio[1][i]).T()*(body[i]->getFrames()[0]->getAngularVelocity()-frame->getAngularVelocity());
+
+    for(unsigned i=0; i<body.size(); i++)
+      wb += (body[i]->getFrameOfReference()->getOrientation()*body[i]->getPJR()*ratio[1][i]).T()*(body[i]->getFrames()[0]->getGyroscopicAccelerationOfRotation(j) - frame->getGyroscopicAccelerationOfRotation(j)); 
+  }
+
   void Gear::init(InitStage stage) {
     if(stage==unknownStage) {
-      LinkMechanics::init(stage);
+      //LinkMechanics::init(stage);
 
       for(unsigned int i=0; i<body.size(); i++) {
         h[0].push_back(Vec(body[i]->getJRel(0).cols()));
-        h[1].push_back(Vec(3));
+        h[1].push_back(Vec(6));
         W[0].push_back(Mat(body[i]->getJRel(0).cols(),laSize));
-        W[1].push_back(Mat(3,laSize));
+        W[1].push_back(Mat(6,laSize));
         assert(body[i]->getRotation()!=NULL);
       }
+      h[0].push_back(Vec(frame->getJacobianOfTranslation(0).cols()));
+      h[1].push_back(Vec(6));
+      W[0].push_back(Mat(frame->getJacobianOfTranslation(0).cols(),laSize));
+      W[1].push_back(Mat(6,laSize));
     }
     else if(stage==resize) {
       LinkMechanics::init(stage);
@@ -121,6 +143,9 @@ namespace MBSim {
     else if(stage==MBSim::plot) {
       updatePlotFeatures();
       plotColumns.push_back("la(0)");
+      for(unsigned int i=0; i<body.size(); i++) {
+        plotColumns.push_back("M");
+      }
       if(getPlotFeature(plotRecursive)==enabled) {
         LinkMechanics::init(stage);
       }
@@ -132,6 +157,9 @@ namespace MBSim {
 
   void Gear::plot(double t,double dt) {
     plotVector.push_back(la(0));
+    for(unsigned int i=0; i<body.size(); i++) {
+      plotVector.push_back(ratio[1][i]*la(0));
+    }
     if(getPlotFeature(plotRecursive)==enabled) {
       LinkMechanics::plot(t,dt);
     }
