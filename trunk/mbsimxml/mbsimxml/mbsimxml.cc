1e16c8ad145169ca58740daad000a209dd6fe2f4
friedrich.at.gc@gmail.com <friedrich.at.gc@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2011-11-27 15:01:49 +0000

    Removed the script mbsimxml (shell) and mbsimxml.bat (Windows batch) by a c++ program (Issue 5).

---
+++ trunk/mbsimxml/mbsimxml/mbsimxml.cc
@@ -0,0 +1,235 @@
+#include <iostream>
+#include <list>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+#include <fstream>
+#include "env.h"
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#ifdef MBSIMXML_MINGW // Windows
+#  include <Windows.h>
+#endif
+
+using namespace std;
+
+// run command (including params) (OS-independent)
+// returns the return value of the command
+int runcommand(const string &command) {
+  int ret=system(command.c_str());
+#ifdef HAVE_WEXITSTATUS
+  return WEXITSTATUS(ret);
+#else
+  return ret;
+#endif
+}
+
+// return true if filanamea is newer then filenameb (modification time) (OS-independent)
+bool newer(const string &filenamea, const string &filenameb) {
+  struct stat sta, stb;
+  stat(filenamea.c_str(), &sta);
+  stat(filenameb.c_str(), &stb);
+#ifdef HAVE_STAT_ST_MTIM
+  if(sta.st_mtim.tv_sec==stb.st_mtim.tv_sec)
+    return sta.st_mtim.tv_nsec>stb.st_mtim.tv_nsec;
+  else
+    return sta.st_mtim.tv_sec>stb.st_mtim.tv_sec;
+#else
+  return sta.st_mtime>stb.st_mtime;
+#endif
+}
+
+
+// return filename without path but with extension (OS-independent)
+string basename(const string &filename) {
+  int i=filename.rfind('/');
+  int i2=filename.rfind('\\');
+  i=i>i2?i:i2;
+  return i>=0?filename.substr(i):filename;
+}
+
+// touch a file (OS-independent)
+void touch(const string &filename) {
+  ofstream f(filename.c_str());
+  f.close();
+}
+
+int main(int argc, char *argv[]) {
+  // convert args to c++
+  list<string> arg;
+  list<string>::iterator i, i2;
+  for(int i=0; i<argc; i++)
+    arg.push_back(argv[i]);
+
+  // help
+  if(arg.size()<=1 ||
+     (i=std::find(arg.begin(), arg.end(), "-h"))!=arg.end() ||
+     (i=std::find(arg.begin(), arg.end(), "--help"))!=arg.end() ||
+     (i=std::find(arg.begin(), arg.end(), "-?"))!=arg.end()) {
+    cout<<"Usage: mbsimxml [--onlypreprocess|--donotintegrate|--stopafterfirststep|"<<endl
+        <<"                 --autoreload]"<<endl
+        <<"                [--mbsimparam <mbsimparameterfile>] <mbsimfile>"<<endl
+        <<"                [--intparam <integratorparameterfile>] <integratorfile>"<<endl
+        <<""<<endl
+        <<"Copyright (C) 2004-2009 MBSim Development Team"<<endl
+        <<"This is free software; see the source for copying conditions. There is NO"<<endl
+        <<"warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE."<<endl
+        <<""<<endl
+        <<"Licensed under the GNU Lesser General Public License (LGPL)"<<endl
+        <<""<<endl
+        <<"--onlypreprocess     Stop after the preprocessing stage"<<endl
+        <<"--donotintegrate     Stop after the initialization stage, do not integrate"<<endl
+        <<"--stopafterfirststep Stop after outputting the first step (usually at t=0)"<<endl
+        <<"                     This generates a HDF5 output file with only one time serie"<<endl
+        <<"--autoreload         Same as --stopafterfirststep but rerun mbsimxml each time"<<endl
+        <<"                     a input file is newer than the output file"<<endl
+        <<"--mbsimparam <file>  Use <file> as parameter file for mbsim xml file"<<endl
+        <<"<mbsimfile>          Use <mbsimfile> as mbsim xml file"<<endl
+        <<"--intparam <file>    Use <file> as parameter file for mbsim integrator xml file"<<endl
+        <<"<integratorfile>     Use <integratorfile> as mbsim integrator xml file"<<endl;
+    return 0;
+  }
+
+  // get path of this executable
+  char exePath[4096];
+  string EXEEXT;
+#ifdef MBSIMXML_MINGW // Windows
+  GetModuleFileName(NULL, exePath, sizeof(exePath));
+  for(size_t i=0; i<strlen(exePath); i++) if(exePath[i]=='\\') exePath[i]='/'; // convert '\' to '/'
+  *strrchr(exePath, '/')=0; // remove the program name
+  EXEEXT=".exe";
+#else // Linux
+  int exePathLength=readlink("/proc/self/exe", exePath, sizeof(exePath)); // get abs path to this executable
+  exePath[exePathLength]=0; // null terminate
+  *strrchr(exePath, '/')=0; // remove the program name
+  EXEEXT="";
+#endif
+
+  // check for environment variables (none default installation)
+  string MBXMLUTILSBIN;
+  string MBXMLUTILSSCHEMA;
+  string MBSIMXMLBIN;
+  struct stat st;
+  char *env;
+  MBXMLUTILSBIN=MBXMLUTILSBIN_DEFAULT; // default: from build configuration
+  if(stat((MBXMLUTILSBIN+"/mbxmlutilspp"+EXEEXT).c_str(), &st)!=0) MBXMLUTILSBIN=exePath; // use rel path if build configuration dose not work
+  if((env=getenv("MBXMLUTILSBINDIR"))) MBXMLUTILSBIN=env; // overwrite with envvar if exist
+  MBXMLUTILSSCHEMA=MBXMLUTILSSCHEMA_DEFAULT; // default: from build configuration
+  if(stat((MBXMLUTILSSCHEMA+"/http___mbsim_berlios_de_MBSimXML/mbsimxml.xsd").c_str(), &st)!=0) MBXMLUTILSSCHEMA=string(exePath)+"/../share/mbxmlutils/schema"; // use rel path if build configuration dose not work
+  if((env=getenv("MBXMLUTILSSCHEMADIR"))) MBXMLUTILSSCHEMA=env; // overwrite with envvar if exist
+  MBSIMXMLBIN=MBSIMXMLBIN_DEFAULT; // default: from build configuration
+  if(stat((MBSIMXMLBIN+"/mbsimflatxml"+EXEEXT).c_str(), &st)!=0) MBSIMXMLBIN=exePath; // use rel path if build configuration dose not work
+  if((env=getenv("MBSIMXMLBINDIR"))) MBSIMXMLBIN=env; // overwrite with envvar if exist
+
+  // remove program name form args
+  arg.erase(arg.begin());
+
+  // parse parameters
+
+  bool ONLYPP=false;
+  if((i=std::find(arg.begin(), arg.end(), "--onlypreprocess"))!=arg.end()) {
+    ONLYPP=true;
+    arg.erase(i);
+  }
+
+  string NOINT;
+  if((i=std::find(arg.begin(), arg.end(), "--donotintegrate"))!=arg.end()) {
+    NOINT="--donotintegrate";
+    arg.erase(i);
+  }
+
+  string ONLY1OUT;
+  if((i=std::find(arg.begin(), arg.end(), "--stopafterfirststep"))!=arg.end()) {
+    ONLY1OUT="--stopafterfirststep";
+    arg.erase(i);
+  }
+
+  int AUTORELOADTIME=0;
+  if((i=std::find(arg.begin(), arg.end(), "--autoreload"))!=arg.end()) {
+    i2=i; i2++;
+    char *error;
+    ONLY1OUT="--stopafterfirststep";
+    AUTORELOADTIME=strtol(i2->c_str(), &error, 10);
+    // AUTORELOAD is set delayed since we currently do not know the MBSim file name (see later)
+    if(AUTORELOADTIME<0) AUTORELOADTIME=250;
+    arg.erase(i);
+    if(error && strlen(error)==0)
+      arg.erase(i2);
+    else
+      AUTORELOADTIME=250;
+  }
+
+  string PARAM="none";
+  if((i=std::find(arg.begin(), arg.end(), "--mbsimparam"))!=arg.end()) {
+    i2=i; i2++;
+    PARAM=*i2;
+    arg.erase(i); arg.erase(i2);
+  }
+
+  string MBSIM=*arg.begin();
+  arg.erase(arg.begin());
+  string PPMBSIM=".pp."+basename(MBSIM);
+  string DEPMBSIM=".dep."+basename(MBSIM);
+  string ERRFILE=".err."+basename(MBSIM);
+
+  string AUTORELOAD;
+  if(AUTORELOADTIME>0) // AUTORELOAD is now set (see above)
+    AUTORELOAD="--dependencies "+DEPMBSIM;
+
+  string PARAMINT="none";
+  if((i=std::find(arg.begin(), arg.end(), "--intparam"))!=arg.end()) {
+    i2=i; i2++;
+    PARAMINT=*i2;
+    arg.erase(i); arg.erase(i2);
+  }
+
+  string MBSIMINT=*arg.begin();
+  arg.erase(arg.begin());
+  string PPMBSIMINT=".pp."+basename(MBSIMINT);
+
+  // execute
+  int ret; // comamnd return value
+  bool runAgain=true; // always run the first time
+  while(runAgain) {
+    unlink(PPMBSIM.c_str());
+    unlink(PPMBSIMINT.c_str());
+    unlink(ERRFILE.c_str());
+
+    // run preprocessor
+    ret=runcommand(MBXMLUTILSBIN+"/mbxmlutilspp "+AUTORELOAD+" "+
+      PARAM+" "+MBSIM+" "+MBXMLUTILSSCHEMA+"/http___mbsim_berlios_de_MBSimXML/mbsimxml.xsd "+
+      PARAMINT+" "+MBSIMINT+" "+MBXMLUTILSSCHEMA+"/http___mbsim_berlios_de_MBSim/mbsimintegrator.xsd");
+
+    if(!ONLYPP && ret==0)
+      ret=runcommand(MBSIMXMLBIN+"/mbsimflatxml "+NOINT+" "+ONLY1OUT+" "+PPMBSIM+" "+PPMBSIMINT);
+
+    if(ret!=0) touch(ERRFILE);
+
+    runAgain=false; // only run ones except --autoreload is given
+    if(AUTORELOADTIME>0) {
+      // get dependent files
+      vector<string> depfiles;
+      ifstream deps(DEPMBSIM.c_str());
+      while(true) {
+        string depfile;
+        getline(deps, depfile);
+        if(deps.fail()) break;
+        depfiles.push_back(depfile);
+      }
+      deps.close();
+      // check for dependent files being newer then the output file
+      while(!runAgain) {
+        usleep(AUTORELOADTIME*1000);
+        for(size_t i=0; i<depfiles.size(); i++) {
+          if(newer(depfiles[i], PPMBSIM) || newer(depfiles[i], PPMBSIMINT) || newer(depfiles[i], ERRFILE)) {
+            runAgain=true;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  return ret;
+}
