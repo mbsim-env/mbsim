c56524994aa6e0732456fc45fa36fd9c0d88650b
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2011-12-12 17:33:57 +0000

    Adaptions to new kernel.

--- examples/mechanics_contacts_woodpecker_rigid/main.cc
+++ examples/mechanics_contacts_woodpecker_rigid/main.cc
@@ -8,10 +8,14 @@ int main (int argc, char* argv[]) {
 
   Woodpecker sys("MBS");
 //  sys.setImpactSolver(RootFinding);
+  double dt = 1e-4;
+  sys.setLaTol(1e-2*dt);
+  sys.setgdTol(1e-8);
   sys.initialize();
 
   ThetaTimeSteppingIntegrator integrator;
-  integrator.setStepSize(1.e-4);
+  //TimeSteppingIntegrator integrator;
+  integrator.setStepSize(dt);
   //integrator.setDriftCompensation(true);
   integrator.setTheta(0.75);
   integrator.setEndTime(5);
