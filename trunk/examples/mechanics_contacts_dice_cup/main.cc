df804b870eb1c42299dab22c2ff0e770de22578f
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2011-12-12 17:43:30 +0000

    Adaption to new kernel.

--- trunk/examples/mechanics_contacts_dice_cup/main.cc
+++ trunk/examples/mechanics_contacts_dice_cup/main.cc
@@ -9,12 +9,15 @@ int main (int argc, char* argv[]) {
   System *sys = new System("MBS");
 
   sys->setStopIfNoConvergence(true,true);
+  double dt = 5e-5;
+  sys->setLaTol(1e-2*dt);
+  sys->setgdTol(1e-8);
   sys->initialize();
 
   TimeSteppingIntegrator integrator;
 
   integrator.setEndTime(0.5);
-  integrator.setStepSize(5e-5);
+  integrator.setStepSize(dt);
   integrator.setPlotStepSize(5e-3);
 
   integrator.integrate(*sys);
