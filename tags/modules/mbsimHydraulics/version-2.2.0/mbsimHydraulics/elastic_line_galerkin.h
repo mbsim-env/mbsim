3448537be16d14954d2f4d8aa54f83be74a01c19
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2011-12-12 17:10:28 +0000

    Tagging the 2.2.0 release of hydraulics module.

---
+++ tags/modules/mbsimHydraulics/version-2.2.0/mbsimHydraulics/elastic_line_galerkin.h
@@ -0,0 +1,106 @@
+/* Copyright (C) 2004-2009 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: markus.ms.schneider@gmail.com
+ */
+
+#ifndef  _ELASTIC_LINE_GALERKIN_H_
+#define  _ELASTIC_LINE_GALERKIN_H_
+
+#include "hline.h"
+
+class ansatz_function;
+
+namespace MBSimHydraulics {
+
+  /*! ElasticLineGalerkin */
+  class ElasticLineGalerkin : public HLine {
+
+    public:
+      
+      enum AnsatzTypes {
+        BSplineOrd4,
+        BSplineOrd3,
+        Polynom,
+        Harmonic
+      };
+
+      /*! Constructor */
+      ElasticLineGalerkin(const std::string &name);
+      virtual std::string getType() const { return "ElasticLineGalerkin"; }
+
+      /*! set initial pressure of the pipe fluid*/
+      void setp0(double p0_) {p0=p0_; }
+      /*! set the fracAir of the pipe (for E-Modulus calculation*/
+      void setFracAir(double fracAir_) {fracAir=fracAir_; }
+      /*! set delta_h of the pipe*/
+      void setdh(double dh_) {delta_h=dh_; }
+      /*! set damping meassure according Lehr, default=0*/
+      void setDLehr(double DLehr_) {DLehr=DLehr_; }
+      /*! set the method of the ansatz-function and its order; choose one of BSplineOrd3, BSplineOrd4, Polynom, Harmonic*/
+      //void setAnsatzFunction(std::string method_, int nAnsatz_);
+      void setAnsatzFunction(AnsatzTypes method_, int nAnsatz_);
+      /*! set a 2dimension flow recognition*/
+      void setFlow2D(bool flow2d=true){Flow2D=flow2d; }
+      /*! select points in the pipe, for which a output should be created*/
+      void setRelativePlotPoints(const fmatvec::Vec &rPP) {relPlotPoints=rPP; }
+      void setDiameter(double d_) {d=d_; }
+      void setLength(double l_) {l=l_; }
+
+      void setQ0(double Q0_) {Q0=Q0_; }
+
+      fmatvec::Vec getQIn() {return QIn; }
+      fmatvec::Vec getQOut() {return QOut; }
+      fmatvec::Vec getInflowFactor() {return -Area*wE; }
+      fmatvec::Vec getOutflowFactor() {return Area*wA; }
+
+      void init(MBSim::InitStage stage);
+      void calcqSize() {qSize=mdim; }
+      void calcuSize(int j) {uSize[j]=mdim; }
+
+      void updateStateDependentVariables(double t);
+      void updateh(double t);
+      void updateT(double t);
+      void updateM(double t);
+
+      void plot(double t, double dt);
+      void plotParameters();
+
+      void initializeUsingXML(TiXmlElement * element);
+    
+    private:
+      int mdim, plotdim;
+      double nu, g, E, k;
+      fmatvec::Vec WInt, wA, wE, lambda;
+      fmatvec::SymMat MatIntWWT, MatIntWSWST, K, D, N, Omega, MFac;
+      fmatvec::SqrMat phi;
+      ansatz_function * ansatz;
+      fmatvec::Mat plotVecW, plotVecWS;
+      fmatvec::Vec QIn, QOut;
+      double l, d, Area;
+
+    protected:
+      AnsatzTypes ansatzType;
+      bool Flow2D;
+      int nAnsatz;
+      double p0, Q0, fracAir, delta_h, DLehr;
+      fmatvec::Vec relPlotPoints;
+  };
+}
+
+
+#endif   /* ----- #ifndef _HYDLINEGALERKIN_H_  ----- */
+
---
+++ tags/modules/mbsimHydraulics/version-2.2.0/mbsimHydraulics/leakage_line.cc
@@ -0,0 +1,134 @@
+/* Copyright (C) 2004-2009 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: markus.ms.schneider@gmail.com
+ */
+
+#include "mbsimHydraulics/leakage_line.h"
+#include "mbsimHydraulics/environment.h"
+#include "mbsimHydraulics/pressure_loss.h"
+#include "mbsimHydraulics/rigid_line_pressureloss.h"
+#include "mbsimHydraulics/objectfactory.h"
+#include "mbsimControl/signal_.h"
+#include "mbsimHydraulics/obsolet_hint.h"
+
+using namespace std;
+using namespace fmatvec;
+using namespace MBSim;
+using namespace MBSimControl;
+
+namespace MBSimHydraulics {
+
+  double LeakageLine::getGapLength() const {
+    return ((glSignal)?glSignal->getSignal()(0):length);
+  }
+
+  double LeakageLine::getSurface1Velocity() const {
+    return ((s1vSignal)?s1vSignal->getSignal()(0):0);
+  }
+
+  double LeakageLine::getSurface2Velocity() const {
+    return ((s2vSignal)?s2vSignal->getSignal()(0):0);
+  }
+
+  void LeakageLine::init(InitStage stage) {
+    if (stage==MBSim::resolveXMLPath) {
+      RigidHLine::init(stage);
+      if (s1vPath!="")
+        setSurface1VelocitySignal(getByPath<Signal>(process_signal_string(s1vPath)));
+      if (s2vPath!="")
+        setSurface2VelocitySignal(getByPath<Signal>(process_signal_string(s2vPath)));
+      if (glPath!="")
+        setGapLengthSignal(getByPath<Signal>(process_signal_string(glPath)));
+    }
+    else if (stage==MBSim::modelBuildup) {
+      parent->addLink(new RigidLinePressureLoss(name+"/LeakagePressureLoss", this, lpl, false, false));
+      RigidHLine::init(stage);
+    }
+    else
+      RigidHLine::init(stage);
+  }
+
+  void LeakageLine::initializeUsingXML(TiXmlElement * element) {
+    RigidHLine::initializeUsingXML(element);
+    TiXmlElement * e=element->FirstChildElement(MBSIMHYDRAULICSNS"firstSurfaceVelocity");
+    if (e)
+      s1vPath=e->Attribute("ref");
+    e=element->FirstChildElement(MBSIMHYDRAULICSNS"secondSurfaceVelocity");
+    if (e)
+      s2vPath=e->Attribute("ref");
+    e=element->FirstChildElement(MBSIMHYDRAULICSNS"gapLength");
+    if (e)
+      glPath=e->Attribute("ref");
+  }
+
+
+  void PlaneLeakageLine::setPlaneLeakagePressureLoss(PlaneLeakagePressureLoss * plpl) {
+    lpl=plpl;
+  }
+
+  void PlaneLeakageLine::init(InitStage stage) {
+    if (stage==MBSim::preInit) {  
+      LeakageLine::init(stage);
+      double rho=HydraulicEnvironment::getInstance()->getSpecificMass();
+      Mlocal.resize(1, INIT, rho*length/hGap/wGap);
+    }
+    else
+      LeakageLine::init(stage);
+  }
+
+  void PlaneLeakageLine::initializeUsingXML(TiXmlElement * element) {
+    LeakageLine::initializeUsingXML(element);
+    TiXmlElement * e = element->FirstChildElement(MBSIMHYDRAULICSNS"width");
+    setGapWidth(getDouble(e));
+    e = element->FirstChildElement(MBSIMHYDRAULICSNS"height");
+    setGapHeight(getDouble(e));
+    e = element->FirstChildElement(MBSIMHYDRAULICSNS"planeLeakagePressureLoss");
+    PlaneLeakagePressureLoss *p=(PlaneLeakagePressureLoss*)(MBSim::ObjectFactory::getInstance()->createFunction1_SS(e->FirstChildElement()));
+    setPlaneLeakagePressureLoss(p);
+    p->initializeUsingXML(e->FirstChildElement());
+  }
+
+
+  void CircularLeakageLine::setCircularLeakagePressureLoss(CircularLeakagePressureLoss * clpl) {
+    lpl=clpl;
+  }
+
+  void CircularLeakageLine::init(InitStage stage) {
+    if (stage==MBSim::preInit) {  
+      rO=rI+hGap;
+      LeakageLine::init(stage);
+      double rho=HydraulicEnvironment::getInstance()->getSpecificMass();
+      Mlocal.resize(1, INIT, rho*length/(M_PI*(rO*rO-rI*rI)));
+    }
+    else
+      LeakageLine::init(stage);
+  }
+
+  void CircularLeakageLine::initializeUsingXML(TiXmlElement * element) {
+    LeakageLine::initializeUsingXML(element);
+    TiXmlElement * e = element->FirstChildElement(MBSIMHYDRAULICSNS"innerRadius");
+    setInnerRadius(getDouble(e));
+    e = element->FirstChildElement(MBSIMHYDRAULICSNS"height");
+    setGapHeight(getDouble(e));
+    e = element->FirstChildElement(MBSIMHYDRAULICSNS"circularLeakagePressureLoss");
+    CircularLeakagePressureLoss *p=(CircularLeakagePressureLoss*)(MBSim::ObjectFactory::getInstance()->createFunction1_SS(e->FirstChildElement()));
+    setCircularLeakagePressureLoss(p);
+    p->initializeUsingXML(e->FirstChildElement());
+  }
+
+}
+
