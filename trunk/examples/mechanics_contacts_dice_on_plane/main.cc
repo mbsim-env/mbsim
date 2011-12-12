6e4b88b1c146489a8cc37d234d0689c3acf5eaf3
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2011-12-12 17:30:13 +0000

    Adaptions to new kernel and clean up.

--- trunk/examples/mechanics_contacts_dice_on_plane/main.cc
+++ trunk/examples/mechanics_contacts_dice_on_plane/main.cc
@@ -6,12 +6,12 @@ using namespace MBSim;
 
 int main (int argc, char* argv[])
 {
-  System *sys = new System("TS");
-  sys->setImpactSolver(RootFinding);
+  System *sys = new System("MBS");
+  //sys->setImpactSolver(RootFinding);
 
   sys->initialize();
 
-  bool eventDriven = false;
+  bool eventDriven = true;
 
   if(eventDriven) { // Event driven time integration
     LSODARIntegrator integrator;
@@ -20,11 +20,12 @@ int main (int argc, char* argv[])
     integrator.setPlotStepSize(1e-2);
     integrator.setEndTime(2);
     double tol = 1e-4;
-    sys->setgdTol(tol);
-    sys->setLaTol(tol);
-    sys->setlaTol(tol*1e-2);
-    sys->setgddTol(tol*1e-2);
-    //sys->setProjectionTolerance(1e-12);
+    sys->setProjectionTolerance(1e-15);
+    sys->setgTol(1e-6);
+    sys->setgdTol(1e-6);
+    sys->setLaTol(1e-6);
+    sys->setgddTol(1e-8);
+    sys->setlaTol(1e-8);
     integrator.integrate(*sys);
   } 
   else { // time stepping integration
@@ -38,24 +39,6 @@ int main (int argc, char* argv[])
     integrator.integrate(*sys);
   }
 
-  // RKSuiteIntegrator integrator;
-  // RADAU5Integrator integrator;
-  // TimeSteppingIntegrator integrator;
-  // integrator.setdt(1e-4);
-  //
-  //DOPRI5Integrator integrator;
-
-  //integrator.settEnd(10.0);
-
-  //  TimeSteppingIntegrator integrator;
-  //  integrator.setdt(1e-4);
-  //  integrator.setdtPlot(1e-2);
-  //  integrator.settEnd(1.8);
-  //
-  //  integrator.integrate(*sys);
-  //  cout << "finished"<<endl;
-  //  cout << sys->getq() << endl;
-  //  cout << sys->getu() << endl;
   delete sys;
 
   return 0;
