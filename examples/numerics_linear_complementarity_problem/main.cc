9296ef78c674a5210291207a48fdbb61b831c898
kgrundl@gmail.com <kgrundl@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2012-02-20 09:49:57 +0000

    added two numerics examples: 	- MultiDimNewton method compares MultiDim-Netwon method of mbsim/utils with the one of numerics 	- Linear_Complementarity_Problem shows the options of how to solve LCPs

---
+++ examples/numerics_linear_complementarity_problem/main.cc
@@ -0,0 +1,95 @@
+#include <numerics/linear_complementarity_problem/linear_complementarity_problem.h>
+#include <fmatvec.h>
+
+#include <fmatvecTestbench/matgenerator.h>
+
+#include <iostream>
+
+using namespace fmatvec;
+using namespace MBSimNumerics;
+using namespace std;
+
+SymMat makeInfluenceMat(int dim, double influenceDisk, double influenceElement) {
+  SymMat A(dim, NONINIT);
+
+  for(int i=0; i<dim; i++)
+    for(int j =i; j<dim; j++) {
+      int diff = j-i;
+      double elementInfluence = 0;
+      double DiskInfluence = 0;
+      if(diff == 0 or (diff <= 1 and (j%2 == 1)))
+        elementInfluence = influenceElement;
+      DiskInfluence = influenceDisk * cos((double)diff/dim * M_PI);
+      A(i,j) = elementInfluence + DiskInfluence;
+    }
+
+
+  return A;
+}
+
+int main (int argc, char* argv[]) {
+
+  int dim = 300;
+
+  Matgenerator MatGen;
+  MatGen.init();
+
+  MatGen.setDecimalPlaces(4);
+  MatGen.setRange(1);
+
+  Vec q = MatGen.getVec(dim);
+
+  for(int i=0; i<q.size(); i++) {
+    q(i) /= 100;
+    if(q(i) > 0)
+      q(i) *= -1;
+  }
+
+
+  SymMat C = makeInfluenceMat(dim, 10e8, 10e5);
+
+  if(dim < 20) {
+    cout << "C = " << C << endl;
+
+    cout << "q = " << q << endl;
+  }
+
+  LinearComplementarityProblem problem(C, q);
+
+  problem.setDebugLevel(1);
+
+//  map<Index, double> tolerances;
+//  tolerances.insert(pair<Index, double>(Index(0,dim-1), 1e-8));
+//  tolerances.insert(pair<Index, double>(Index(dim,2*dim-1), 1e-3));
+//  LocalResidualCriteriaFunction * residualCriteria = new LocalResidualCriteriaFunction(tolerances);
+//  problem.setNewtonCriteriaFunction(residualCriteria);
+//
+//  LocalShiftCriteriaFunction * shiftCriteria = new LocalShiftCriteriaFunction(tolerances);
+//  problem.setFixpointCriteriaFunction(shiftCriteria);
+
+  Vec result;
+  for(int strategy = 0; strategy < 5; strategy++) {
+    problem.setStrategy(LCPSolvingStrategy(strategy));
+
+    if(LCPSolvingStrategy(strategy) == 2) {
+      for(int jactype = 0; jactype < 2; jactype++) {
+
+        problem.setJacobianType(JacobianType(jactype));
+
+        result = problem.solve();
+      }
+    }
+    else {
+      result = problem.solve();
+    }
+
+    //cout << result << endl;
+
+    //cout << C * result(dim, 2*dim-1) + q - result(0, dim-1) << endl;
+    }
+
+  return 0;
+
+}
+
+
