7ec64a6c84d1e6d50d417e8e9fccd282e53c8b61
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2012-06-16 09:22:58 +0000

    Fixed/Improved/Simplified error handling of mbsimflatxml.

--- trunk/mbsimxml/mbsimxml/mbsimflatxml-main.cc
+++ trunk/mbsimxml/mbsimxml/mbsimflatxml-main.cc
@@ -7,68 +7,30 @@ using namespace std;
 using namespace MBSim;
 
 int main(int argc, char *argv[]) {
-  Integrator *integrator;
-  DynamicSystemSolver *dss;
-
-  bool doNotIntegrate=false;
-  if(argc>=2 && strcmp(argv[1],"--donotintegrate")==0)
-    doNotIntegrate=true;
-
-  int ret=0;
-
   try {
+
+    Integrator *integrator;
+    DynamicSystemSolver *dss;
+  
+    bool doNotIntegrate=false;
+    if(argc>=2 && strcmp(argv[1],"--donotintegrate")==0)
+      doNotIntegrate=true;
+  
     MBSimXML::preInitDynamicSystemSolver(argc, argv, dss);
-  }
-  catch (MBSimError error) {
-    ret=1;
-    error.printExceptionMessage();
-  }
-  if(ret==-1) 
-    return 0; // help message was printed
-  if(ret!=0) 
-    return ret;
-
-  try {
+  
     MBSimXML::initDynamicSystemSolver(argc, argv, dss);
-  }
-  catch (MBSimError error) {
-    ret=1;
-    error.printExceptionMessage();
-  }
-  if(ret!=0) 
-    return ret;
-
-  try {
+  
     MBSimXML::initIntegrator(argc, argv, integrator);
-  }
-  catch (MBSimError error) {
-    ret=1;
-    error.printExceptionMessage();
-  }
-  if(ret!=0) 
-    return ret;
-
-  if(doNotIntegrate==false) {
-    try {
+  
+    if(doNotIntegrate==false) {
       MBSimXML::main(integrator, dss);
     }
-    catch (MBSimError error) {
-      ret=1;
-      error.printExceptionMessage();
-    }
-    if(ret!=0) 
-      return ret;
-  }
-
-  try {
+  
     MBSimXML::postMain(argc, argv, integrator, dss);
   }
   catch (MBSimError error) {
-    ret=1;
     error.printExceptionMessage();
   }
-  if(ret!=0) 
-    return ret;
 
   return 0;
 }
