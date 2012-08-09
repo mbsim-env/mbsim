1f747f6c03623d55521fec863d2d9bd676f3479e
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2012-08-09 13:12:59 +0000

    new branch for mbsimHydraulics

---
+++ foerg/mbsimHydraulics/mbsimHydraulics/rigid_line_pressureloss.h
@@ -0,0 +1,123 @@
+/* Copyright (C) 2004-2009 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: markus.ms.schneider@gmail.com
+ */
+
+#ifndef  _RIGID_LINE_PRESSURELOSS_H_
+#define  _RIGID_LINE_PRESSURELOSS_H_
+
+#include "mbsim/link.h"
+
+namespace MBSim {
+  class GeneralizedForceLaw;
+  class GeneralizedImpactLaw;
+}
+
+namespace MBSimHydraulics {
+
+  class RigidHLine; 
+  class PressureLoss;
+  class LinePressureLoss;
+  class ClosablePressureLoss;
+  class LeakagePressureLoss;
+  class UnidirectionalPressureLoss;
+
+  /*! RigidLinePressureLoss */
+  class RigidLinePressureLoss : public MBSim::Link {
+    public:
+      RigidLinePressureLoss(const std::string &name, RigidHLine * line_, PressureLoss * pressureLoss, bool bilateral_=false, bool unilateral_=false);
+      ~RigidLinePressureLoss();
+      virtual std::string getType() const { return "RigidLinePressureLoss"; }
+      void plot(double t, double dt);
+
+      bool hasSmoothPart() const {return (bilateral || (unilateral && (fabs(dpMin)>1e-6))); }
+
+      void init(MBSim::InitStage stage);
+      // ================================
+      // Methods from init-Process
+      // ================================
+      bool isSetValued() const {return (unilateral || bilateral); }
+      void calcgdSize(int j) {gdSize=active?1:0; }
+      void calcsvSize() {svSize=isSetValued()?1:0; }
+      void updatehRef(const fmatvec::Vec& hRef, int i=0);
+      void updaterRef(const fmatvec::Vec& rRef, int i=0);
+      void updatedhdqRef(const fmatvec::Mat& dhdqRef, int i=0) {};
+      void updatedhduRef(const fmatvec::SqrMat& dhduRef, int i=0);
+      void updatedhdtRef(const fmatvec::Vec& dhdtRef, int i=0);
+      void updaterRef(const fmatvec::Vec& rRef);
+      void updateWRef(const fmatvec::Mat& WRef, int i=0);
+      void updateVRef(const fmatvec::Mat& VRef, int i=0);
+      // ==== END Methods for init-Process ===
+
+      // ================================
+      // Methods for update-Process
+      // ================================
+      void updateg(double t); /* zdotStandard */
+      void checkActive(int j); /* update */
+      //void checkActivegdn(); // event-driven
+      bool gActiveChanged(); /* update */
+      bool isActive() const {return active; }
+      //void calcgdSizeActive() {gdSize=1; }
+      void calclaSize(int j) {laSize=active?1:0; }
+      //void calclaSizeForActiveg() {laSize=0; } // event-driven
+      void calcrFactorSize(int j) {rFactorSize=active?1:0; }
+      void updategd(double t); /* zdotStandard */
+      void updateStopVector(double t); // event-driven
+      void updateh(double t, int j); /* zdotStandard */
+      void updateW(double t, int j); /* zdotStandard */
+      void updatedhdz(double t);
+      // ==== END Methods for update-Process ===
+
+      // ================================
+      // Methods for solve-Process
+      // ================================
+      void checkRoot();
+      void updaterFactors();
+      void solveImpactsFixpointSingle(double dt);
+      void solveConstraintsFixpointSingle();
+      void solveImpactsGaussSeidel(double dt);
+      void solveConstraintsGaussSeidel();
+      void solveImpactsRootFinding(double dt);
+      void solveConstraintsRootFinding();
+      void jacobianImpacts();
+      void jacobianConstraints();
+      void checkImpactsForTermination(double dt);
+      void checkConstraintsForTermination();
+      // ==== END Methods for solve-Process ===
+
+
+    private:
+      RigidHLine * line;
+      bool active, active0;
+      bool unilateral, bilateral;
+      double gdn, gdd;
+      double dpMin;
+      double laSmooth;
+
+      LinePressureLoss * linePressureLoss;
+      ClosablePressureLoss * closablePressureLoss;
+      LeakagePressureLoss * leakagePressureLoss;
+      UnidirectionalPressureLoss * unidirectionalPressureLoss;
+
+      MBSim::GeneralizedForceLaw * gfl;
+      MBSim::GeneralizedImpactLaw * gil;
+  };
+
+}
+
+#endif   /* ----- #ifndef _HYDLINE_PRESSURELOSS_H_  ----- */
+
