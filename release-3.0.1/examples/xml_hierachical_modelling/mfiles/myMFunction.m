fcd833870f81f2239509bf57c93d3d2e25b1cce0
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2012-08-09 10:31:31 +0000

    Removed release-3.0.1

--- release-3.0.1/examples/xml_hierachical_modelling/mfiles/myMFunction.m
+++
@@ -1,2 +0,0 @@
-function ret=myMFunction(a)
-  ret=a;
--- release-3.0.1/thirdparty/nurbs++/nurbs/d_hnurbsS.cpp
+++
@@ -1,9 +0,0 @@
-#include "hnurbsS.cpp"
-
-namespace PLib {
-
-#ifdef NO_IMPLICIT_TEMPLATES
-  template class HNurbsSurface<double,3> ;
-#endif
-
-}
