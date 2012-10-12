facf6fd1e2a7979f99c220ad5e8078530be09022
kgrundl@gmail.com <kgrundl@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2012-10-12 07:06:33 +0000

    mechanics_contacts_maxwell_contact: fitted operator in influence function to updated interface

--- trunk/examples/mechanics_contacts_maxwell_contact/system.cc
+++ trunk/examples/mechanics_contacts_maxwell_contact/system.cc
@@ -42,7 +42,7 @@ class CountourCouplingCantileverBeam : public InfluenceFunction {
     virtual ~CountourCouplingCantileverBeam() {
     }
 
-    double operator()(const fmatvec::Vec &Arg1, const fmatvec::Vec &Arg2, const void * = NULL) {
+    virtual double operator()(const fmatvec::Vec2 &Arg1, const fmatvec::Vec2 &Arg2, const void * = NULL) {
       double i=Arg1(0);  // it is: i < j
       double j=Arg2(0);
       if(i > j)
