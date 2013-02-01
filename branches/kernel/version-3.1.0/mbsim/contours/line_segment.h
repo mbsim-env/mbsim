5941a227d460d154f841abfe3657d731adc17a47
kgrundl@gmail.com <kgrundl@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-02-01 10:27:08 +0000

    Creating new kernel development branch (fixed-size-vectors in flexiblebody-module)

---
+++ branches/kernel/version-3.1.0/mbsim/contours/line_segment.h
@@ -0,0 +1,77 @@
+/* Copyright (C) 2004-2011 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: martin.o.foerg@googlemail.com
+ */
+
+#ifndef _LINE_SEGMENT_H_
+#define _LINE_SEGMENT_H_
+
+#include "mbsim/contour.h"
+
+namespace MBSim {
+
+  class ContourPointData;
+
+  /**
+   * \brief line segment with two bounds
+   * \author Martin Foerg
+   * \date 2010-05-20 initial commit (Martin Foerg)
+   * \date 2011-01-27 some comments (Thomas Cebulla)
+   */ 
+  class LineSegment : public MBSim::RigidContour {	
+    public:
+      /**
+       * \brief constructor
+       * \param name of line segment
+       */
+      LineSegment(const std::string& name);
+
+      /* INHERITED INTERFACE OF ELEMENT */
+      std::string getType() const { return "LineSegment"; }
+      /***************************************************/
+
+      /* INHERITED INTERFACE OF CONTOUR */
+      virtual double computeCurvature(ContourPointData &cp) { return 0; }
+      /***************************************************/
+
+#ifdef HAVE_OPENMBVCPPINTERFACE
+      void enableOpenMBV(bool enable=true, double size=1., int number=10);
+#endif
+
+      virtual void initializeUsingXML(TiXmlElement *element);
+
+      /* GETTER / SETTER */
+      void setBounds(const fmatvec::Vec &bound_);
+      const fmatvec::Vec& getBounds() const { return bound; }
+      const double& getSegmentLength() const {return length; }
+      /***************************************************/
+
+    private:
+      /**
+       * \brief bounds of line segment
+       */
+      fmatvec::Vec bound;
+
+      /**
+       * \brief length of line segment
+       */
+      double length;
+  };      
+}
+
+#endif /* _LINE_SEGMENT_H_ */
+
---
+++ branches/kernel/version-3.1.0/mbsim/integrators/time_stepping_integrator.h
@@ -0,0 +1,104 @@
+/* Copyright (C) 2004-2010 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: martin.o.foerg@googlemail.com
+ */
+
+#ifndef _TIME_STEPPING_INTEGRATOR_H_ 
+#define _TIME_STEPPING_INTEGRATOR_H_
+
+#include<fmatvec.h>
+#include "integrator.h"
+
+namespace MBSim {
+
+  /** 
+   * brief half-explicit time-stepping integrator of first order
+   * \author Martin Foerg
+   * \date 2009-07-13 some comments (Thorsten Schindler)
+   */
+  class TimeSteppingIntegrator : public Integrator { 
+    public:
+      /**
+       * \brief constructor
+       */
+      TimeSteppingIntegrator();
+      
+      /**
+       * \brief destructor
+       */
+      virtual ~TimeSteppingIntegrator() {}
+
+      void preIntegrate(DynamicSystemSolver& system);
+      void subIntegrate(DynamicSystemSolver& system, double tStop);
+      void postIntegrate(DynamicSystemSolver& system);
+
+      /* INHERITED INTERFACE OF INTEGRATOR */
+      virtual void integrate(DynamicSystemSolver& system);
+      virtual void initializeUsingXML(TiXmlElement *element);
+      /***************************************************/
+
+      /* GETTER / SETTER */
+      void setStepSize(double dt_) { dt = dt_; }
+      void setDriftCompensation(bool dc) { driftCompensation = dc; }
+      /***************************************************/
+    
+    private:
+      /**
+       * \brief step size
+       */
+      double dt;
+
+      /**
+       * \brief time and plot time
+       */
+      double t, tPlot;
+
+      /**
+       * \brief iteration counter for constraints, plots, integration, maximum constraints, cummulation constraint
+       */
+      int iter,step, integrationSteps, maxIter, sumIter;
+
+      /**
+       * \brief computing time counter
+       */
+      double s0, time;
+
+      /**
+       * \brief plot step difference
+       */
+      int stepPlot;
+
+      /**
+       * \brief state, position, velocity, order coordinate of dynamical system
+       */
+      fmatvec::Vec z, q, u, x;
+
+      /**
+       * \brief file stream for integration information
+       */
+      std::ofstream integPlot;
+
+      /**
+       * \brief flag for drift compensation
+       */
+      bool driftCompensation;
+  };
+
+}
+
+#endif /* _TIME_STEPPING_INTEGRATOR_H_ */
+
