b05810d8f96c2c4cd3e2bdc7a678a64894c2c3b9
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-02-12 15:46:50 +0000

    New mbsimxml branch

---
+++ branches/user/foerg/mbsimxml/mbsimxml/mbsimflatxml.h
@@ -0,0 +1,22 @@
+#ifndef _MBSIMFLATXML_H_
+#define _MBSIMFLATXML_H_
+
+using namespace std;
+
+namespace MBSim {
+
+  class DynamicSystemSolver;
+  class Integrator;
+
+  class MBSimXML {
+    public:
+      static void preInitDynamicSystemSolver(int argc, char *argv[], DynamicSystemSolver*& dss);
+      static void initDynamicSystemSolver(int argc, char *argv[], DynamicSystemSolver*& dss);
+      static void initIntegrator(int argc, char *argv[], Integrator *&integrator);
+      static void main(Integrator *&integrator, DynamicSystemSolver *&dss);
+      static void postMain(int argc, char *argv[], Integrator *&integrator, DynamicSystemSolver*& dss);
+  };
+
+}
+
+#endif
