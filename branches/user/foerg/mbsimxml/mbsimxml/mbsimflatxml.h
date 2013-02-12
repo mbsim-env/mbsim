0961958214bd32665e0b9d3a9c43ad7759865481
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-02-12 15:52:45 +0000

    Update of mbsimxml branch: uniform behaviour of option "stopafterfirststep"

--- branches/user/foerg/mbsimxml/mbsimxml/mbsimflatxml.h
+++ branches/user/foerg/mbsimxml/mbsimxml/mbsimflatxml.h
@@ -12,6 +12,7 @@ namespace MBSim {
     public:
       static void preInitDynamicSystemSolver(int argc, char *argv[], DynamicSystemSolver*& dss);
       static void initDynamicSystemSolver(int argc, char *argv[], DynamicSystemSolver*& dss);
+      static void plotInitialState(Integrator*& integrator, DynamicSystemSolver*& dss);
       static void initIntegrator(int argc, char *argv[], Integrator *&integrator);
       static void main(Integrator *&integrator, DynamicSystemSolver *&dss);
       static void postMain(int argc, char *argv[], Integrator *&integrator, DynamicSystemSolver*& dss);
