3158a020e24f3daf67f63086864f45ae6378004e
kgrundl@gmail.com <kgrundl@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-02-13 18:55:05 +0000

    catching up with trunk (due too conflicts step by step)

--- branches/user/grundl/modules/mbsimPowertrain/mbsimPowertrain/planetary_gear.cc
+++ branches/user/grundl/modules/mbsimPowertrain/mbsimPowertrain/planetary_gear.cc
@@ -1,10 +1,7 @@
 #include "planetary_gear.h"
 #include "mbsim/rigid_body.h"
 #include "mbsim/constraint.h"
-#include "mbsim/gear.h"
-#include "mbsim/gearing.h"
 #include "mbsim/utils/rotarymatrices.h"
-#include "mbsim/frame.h"
 #ifdef HAVE_OPENMBVCPPINTERFACE
 #include "openmbvcppinterface/frustum.h"
 #include "openmbvcppinterface/compoundrigidbody.h"
@@ -16,7 +13,7 @@ using namespace MBSim;
 
 namespace MBSimPowertrain {
 
-  PlanetaryGear::PlanetaryGear(const std::string &name, int model_) : Group(name), model(model_) {
+  PlanetaryGear::PlanetaryGear(const std::string &name) : Group(name) {
     double mS = 1;
     double mH = 1.5;
     double mT = 0.3;
@@ -45,9 +42,7 @@ namespace MBSimPowertrain {
     housing->setMass(1);
     Theta(2,2) = 1;
     housing->setInertiaTensor(Theta);
-#ifdef HAVE_OPENMBVCPPINTERFACE
     housing->getFrame("C")->enableOpenMBV(0.3);
-#endif
 
     RigidBody* sun = new RigidBody("Sun");
     addObject(sun);
@@ -61,23 +56,19 @@ namespace MBSimPowertrain {
     sun->setInertiaTensor(Theta);
     Vec r(3);
     r(2) = lS/2;
-#ifdef HAVE_OPENMBVCPPINTERFACE
     sun->getFrame("C")->enableOpenMBV(0.3);
-#endif
 
     RigidBody* annulus = new RigidBody("Annulus");
     addObject(annulus);
 
-    r(2) = lS/2-lH/2*1.0;
+    r(2) = lS/2-lH/2*1.01;
     r(1) = 0;
     housing->addFrame("Q",r,BasicRotAKIy(0));
 
     annulus->setFrameOfReference(housing->getFrame("Q"));
     annulus->setFrameForKinematics(annulus->getFrame("C"));
     annulus->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
-#ifdef HAVE_OPENMBVCPPINTERFACE
     annulus->getFrame("C")->enableOpenMBV(0.3);
-#endif
 
     annulus->setMass(mH);
     Theta(2,2) = JH;
@@ -92,14 +83,16 @@ namespace MBSimPowertrain {
     carrier->setFrameOfReference(housing->getFrame("R"));
     carrier->setFrameForKinematics(carrier->getFrame("C"));
     carrier->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
-#ifdef HAVE_OPENMBVCPPINTERFACE
     carrier->getFrame("C")->enableOpenMBV(0.3);
-#endif
 
     carrier->setMass(mT);
     Theta(2,2) = JT;
     carrier->setInertiaTensor(Theta);
 
+    Constraint2* constraint = new Constraint2("C1",carrier);
+    addObject(constraint);
+    constraint->addDependency(sun,0.5*rS/rT2);
+    constraint->addDependency(annulus,0.5*rH/rT2);
 
     r.init(0);
     r(2) = -lT/2-lP/2;
@@ -120,73 +113,16 @@ namespace MBSimPowertrain {
       planet[i]->setFrameOfReference(carrier->getFrame(str.str()));
       planet[i]->setFrameForKinematics(planet[i]->getFrame("C"));
       planet[i]->setRotation(new RotationAboutFixedAxis(Vec("[0;0;1]")));
-#ifdef HAVE_OPENMBVCPPINTERFACE
       planet[i]->getFrame("C")->enableOpenMBV(0.3);
-#endif
 
       planet[i]->setMass(mP);
       Theta(2,2) = JP;
       planet[i]->setInertiaTensor(Theta);
 
-    }
-
-    if(model==1) {
-      GearConstraint* constraint = new GearConstraint("C1",carrier);
+      Constraint2* constraint = new Constraint2(string("C_")+shaftName.str(),planet[i]);
       addObject(constraint);
-      constraint->addDependency(sun,0.5*rS/rT2);
-      constraint->addDependency(annulus,0.5*rH/rT2);
-    } else if(model==0) {
-      Gear *gear;
-      gear = new Gear("Gear1");
-      addLink(gear);
-      gear->setDependentBody(carrier);
-      gear->addDependency(sun,0.5*rS/rT2);
-      gear->addDependency(annulus,0.5*rH/rT2);
-      //gear->setForceFunction(new LinearSpringDamperForce(5,0.5,0));
-
-     // gear->setDependentBody(sun);
-     // gear->addDependency(carrier,rT2/rS-rP/rS);
-     // gear->addDependency(planet[0],-rP/rS);
-     // gear->setRatio2(rT2/rS,1);
-     // gear->setRatio2(-rP/rS,2);
-     // gear->setFrame(housing->getFrame("C"));
-    } else {
-    }
-    for(int i=0; i<numP; i++) {
-      stringstream str;
-      stringstream shaftName;
-      str << "P" << i;
-      shaftName << "Planet" << i;
-      if(model==1) {
-        GearConstraint* constraint = new GearConstraint(string("C_")+shaftName.str(),planet[i]);
-        addObject(constraint);
-        constraint->addDependency(sun,-0.5*(rS/rP+rS/rT2));
-        constraint->addDependency(annulus,0.5*(rH/rP-rH/rT2));
-      } else if (model==0) {
-        Gear *gear;
-        gear = new Gear(string("Gear_")+shaftName.str());
-        addLink(gear);
-        gear->setDependentBody(planet[i]);
-        gear->addDependency(sun,-0.5*(rS/rP+rS/rT2));
-        gear->addDependency(annulus,0.5*(rH/rP-rH/rT2));
-        //gear->setForceFunction(new LinearSpringDamperForce(5,0.5,0));
-
-        //gear->setDependentBody(annulus);
-        //gear->addDependency(carrier,rT2/rH+rP/rH);
-        //gear->addDependency(planet[0],rP/rH);
-        //gear->setRatio2(rT2/rH,1);
-        //gear->setRatio2(rP/rH,2);
-        //gear->setFrame(housing->getFrame("C"));
-
-      } else {
-        Gearing *gear;
-        gear = new Gearing(string("S_")+shaftName.str());
-        addLink(gear);
-        gear->connect(rS,sun->getFrame("C"),rP,planet[i]->getFrame("C"));//,rS,sun->getFrame("C"));
-        gear = new Gearing(string("A_")+shaftName.str(),true);
-        addLink(gear);
-        gear->connect(rP,planet[i]->getFrame("C"),rH,annulus->getFrame("C"));
-      }
+      constraint->addDependency(sun,-0.5*(rS/rP+rS/rT2));
+      constraint->addDependency(annulus,0.5*(rH/rP-rH/rT2));
     }
 
 #ifdef HAVE_OPENMBVCPPINTERFACE
@@ -200,7 +136,6 @@ namespace MBSimPowertrain {
     cylinder->setHeight(lS*5);
     cylinder->setStaticColor(0.1);
     cylinder->setInitialTranslation(0,0,0);
-    cylinder->setName("frustum1");
     sunOMBV->addRigidBody(cylinder);
     cylinder=new OpenMBV::Frustum;
     cylinder->setTopRadius(rS);
@@ -208,7 +143,6 @@ namespace MBSimPowertrain {
     cylinder->setHeight(lS);
     cylinder->setStaticColor(0.1);
     cylinder->setInitialTranslation(0,0,0);
-    cylinder->setName("frustum2");
     sunOMBV->addRigidBody(cylinder);
     sunOMBV->setInitialTranslation(0,0,lS/2);
     sun->setOpenMBVRigidBody(sunOMBV);
