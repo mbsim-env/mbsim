1e2a0b96dab431f94f2d3211b11b6cb74d506bb0
kgrundl@gmail.com <kgrundl@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-02-14 12:44:49 +0000

    catching up with trunk (step by step due to conflicts)

--- branches/user/grundl/modules/mbsimFlexibleBody/mbsimFlexibleBody/contact_kinematics/circle_nurbsdisk2s.h
+++ branches/user/grundl/modules/mbsimFlexibleBody/mbsimFlexibleBody/contact_kinematics/circle_nurbsdisk2s.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2004-2011 MBSim Development Team
+/* Copyright (C) 2004-2010 MBSim Development Team
  *
  * This library is free software; you can redistribute it and/or 
  * modify it under the terms of the GNU Lesser General Public 
@@ -14,7 +14,7 @@
  * License along with this library; if not, write to the Free Software 
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  *
- * Contact: thorsten.schindler@mytum.de
+ * Contact: thschindler@users.berlios.de
  */
 
 #ifndef _CONTACT_KINEMATICS_CIRCLE_NURBSDISK2S_H_
@@ -48,7 +48,7 @@ namespace MBSimFlexibleBody {
 
       /* INHERITED INTERFACE OF CONTACTKINEAMTICS */
       void assignContours(const std::vector<MBSim::Contour*> &contour);
-      virtual void updateg(fmatvec::Vec& g, MBSim::ContourPointData *cpData, int index = 0);
+      virtual void updateg(fmatvec::Vec& g, MBSim::ContourPointData *cpData);   
       virtual void updatewb(fmatvec::Vec& wb, const fmatvec::Vec &g,MBSim::ContourPointData *cpData) { throw MBSim::MBSimError("ERROR (PointNurbsDisk2s::updatewb): not implemented!"); }   
       /***************************************************/
 
