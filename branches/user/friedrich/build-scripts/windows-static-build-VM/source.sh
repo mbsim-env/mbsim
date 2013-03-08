db19fa1ad94d0b1f75a28c709becd60bcc906878
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-03-08 20:59:42 +0000

    Cleanup of build scripts

--- branches/user/friedrich/build-scripts/windows-static-build-VM/source.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# set PKG_CONFIG_PATH
-echo Setting PKG_CONFIG_PATH=/c/project/local/lib/pkgconfig
-export PKG_CONFIG_PATH=/c/project/local/lib/pkgconfig
-
-export MOC=/c/project/qt-everywhere-opensource-src-4.6.3/bin/moc.exe
-export UIC=/c/project/qt-everywhere-opensource-src-4.6.3/bin/uic.exe
-export RCC=/c/project/qt-everywhere-opensource-src-4.6.3/bin/rcc.exe
