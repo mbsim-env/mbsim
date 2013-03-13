b94b28abe82cceceac7321ab6a814531400ca148
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-03-13 09:39:25 +0000

    Tagging the 3.0.3 release of the kernel

---
+++ tags/kernel/release-3.0.3-old-frame-concept/mbsim/frame.cc
@@ -0,0 +1,192 @@
+/* Copyright (C) 2004-2009 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: martin.o.foerg@googlemail.com
+ */
+
+#include <config.h>
+#include "mbsim/frame.h"
+#include "mbsim/utils/utils.h"
+#include "mbsim/utils/rotarymatrices.h"
+//#include "mbsim/object_interface.h"
+#include "mbsim/mbsim_event.h"
+#ifdef HAVE_OPENMBVCPPINTERFACE
+#include <openmbvcppinterface/frame.h>
+#include <openmbvcppinterface/arrow.h>
+#include <openmbvcppinterface/group.h>
+#endif
+
+using namespace std;
+using namespace fmatvec;
+
+namespace MBSim {
+
+  Frame::Frame(const string &name) : Element(name), AWP(EYE) {
+
+    hSize[0] = 0;
+    hSize[1] = 0;
+    hInd[0] = 0;
+    hInd[1] = 0;
+
+#ifdef HAVE_OPENMBVCPPINTERFACE
+    openMBVFrame=0;
+#endif
+  }
+
+  void Frame::plot(double t, double dt) {
+    if(getPlotFeature(plotRecursive)==enabled) {
+      if(getPlotFeature(globalPosition)==enabled) {
+        for(int i=0; i<3; i++)
+          plotVector.push_back(WrOP(i));
+        Vec3 cardan=AIK2Cardan(AWP);
+        for(int i=0; i<3; i++)
+          plotVector.push_back(cardan(i));
+      }
+      if(getPlotFeature(globalVelocity)==enabled) {
+        for(int i=0; i<3; i++)
+          plotVector.push_back(WvP(i));
+        for(int i=0; i<3; i++)
+          plotVector.push_back(WomegaP(i));
+      }
+      if(getPlotFeature(globalAcceleration)==enabled) {
+        for(int i=0; i<3; i++)
+          plotVector.push_back(WaP(i));
+        for(int i=0; i<3; i++)
+          plotVector.push_back(WpsiP(i));
+      }
+#ifdef HAVE_OPENMBVCPPINTERFACE
+      if(getPlotFeature(openMBV)==enabled) {
+        if(openMBVFrame && !openMBVFrame->isHDF5Link()) {
+          vector<double> data;
+          data.push_back(t);
+          data.push_back(WrOP(0));
+          data.push_back(WrOP(1));
+          data.push_back(WrOP(2));
+          Vec3 cardan=AIK2Cardan(AWP);
+          data.push_back(cardan(0));
+          data.push_back(cardan(1));
+          data.push_back(cardan(2));
+          data.push_back(0);
+          openMBVFrame->append(data);
+        }
+      }
+#endif
+      Element::plot(t,dt);
+    }
+  }
+
+  void Frame::closePlot() {
+    if(getPlotFeature(plotRecursive)==enabled) {
+      Element::closePlot();
+    }
+  }
+
+  void Frame::init(InitStage stage) {
+    if(stage==unknownStage) {
+      getJacobianOfTranslation(0).resize(hSize[0]);
+      getJacobianOfRotation(0).resize(hSize[0]);
+      getJacobianOfTranslation(1).resize(hSize[1]);
+      getJacobianOfRotation(1).resize(hSize[1]);
+    }
+    else if(stage==MBSim::plot) {
+      updatePlotFeatures();
+  
+      if(getPlotFeature(plotRecursive)==enabled) {
+        if(getPlotFeature(globalPosition)==enabled) {
+          for(int i=0; i<3; i++)
+            plotColumns.push_back("WrOP("+numtostr(i)+")");
+          plotColumns.push_back("alpha");
+          plotColumns.push_back("beta");
+          plotColumns.push_back("gamma");
+        }
+        if(getPlotFeature(globalVelocity)==enabled) {
+          for(int i=0; i<3; i++)
+            plotColumns.push_back("WvP("+numtostr(i)+")");
+          for(int i=0; i<3; i++)
+            plotColumns.push_back("WomegaP("+numtostr(i)+")");
+        }
+        if(getPlotFeature(globalAcceleration)==enabled) {
+          for(int i=0; i<3; i++)
+            plotColumns.push_back("WaP("+numtostr(i)+")");
+          for(int i=0; i<3; i++)
+            plotColumns.push_back("WpsiP("+numtostr(i)+")");
+        }
+  
+  #ifdef HAVE_OPENMBVCPPINTERFACE
+        if(getPlotFeature(openMBV)==enabled) {
+          if(openMBVFrame) {
+            openMBVFrame->setName(name);
+            parent->getOpenMBVGrp()->addObject(openMBVFrame);
+          }
+        }
+  #endif
+        Element::init(stage);
+      }
+    }
+    else
+      Element::init(stage);
+  }
+
+#ifdef HAVE_OPENMBVCPPINTERFACE
+  void Frame::enableOpenMBV(double size, double offset) {
+    if(size>=0) {
+      openMBVFrame=new OpenMBV::Frame;
+      openMBVFrame->setSize(size);
+      openMBVFrame->setOffset(offset);
+    }
+    else {
+      openMBVFrame=0;
+    }
+  }
+#endif
+
+  void Frame::initializeUsingXML(TiXmlElement *element) {
+    Element::initializeUsingXML(element);
+
+#ifdef HAVE_OPENMBVCPPINTERFACE
+    TiXmlElement *ee;
+    if((ee=element->FirstChildElement(MBSIMNS"enableOpenMBV"))) {
+      enableOpenMBV(getDouble(ee->FirstChildElement(MBSIMNS"size")),
+          getDouble(ee->FirstChildElement(MBSIMNS"offset")));
+
+      // pass a OPENMBV_ID processing instruction to the OpenMBV Frame object
+      for(TiXmlNode *child=ee->FirstChild(); child; child=child->NextSibling()) {
+        TiXmlUnknown *unknown=child->ToUnknown();
+        const size_t length=strlen("?OPENMBV_ID ");
+        if(unknown && unknown->ValueStr().substr(0, length)=="?OPENMBV_ID ")
+          openMBVFrame->setID(unknown->ValueStr().substr(length, unknown->ValueStr().length()-length-1));
+      }
+    }
+#endif
+  }
+
+  TiXmlElement* Frame::writeXMLFile(TiXmlNode *parent) {
+
+    TiXmlElement *ele0 = Element::writeXMLFile(parent);
+
+#ifdef HAVE_OPENMBVCPPINTERFACE
+    if(openMBVFrame) {
+      TiXmlElement *ele1 = new TiXmlElement( MBSIMNS"enableOpenMBV" );
+      addElementText(ele1,MBSIMNS"size",openMBVFrame->getSize());
+      addElementText(ele1,MBSIMNS"offset",openMBVFrame->getOffset());
+      ele0->LinkEndChild(ele1);
+    }
+#endif
+    return ele0;
+  }
+
+}
+
---
+++ tags/kernel/release-3.0.3-old-frame-concept/mbsim/integrators/dopri5_integrator.h
@@ -0,0 +1,90 @@
+/* Copyright (C) 2004-2006  Martin Förg
+ 
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+
+ *
+ * Contact:
+ *   martin.o.foerg@googlemail.com
+ *
+ */
+
+#ifndef _DOPRI5_INTEGRATOR_H_
+#define _DOPRI5_INTEGRATOR_H_
+
+#include "integrator.h"
+
+namespace MBSim {
+
+  /** \brief ODE-Integrator DOPRI5.
+  */
+
+  class DOPRI5Integrator : public Integrator {
+
+    private:
+
+      static void fzdot(int* n, double* t, double* z, double* zd, double* rpar, int* ipar);
+      static void plot(int* nr, double* told, double* t,double* z, int* n, double* con, int* icomp, int* nd, double* rpar, int* ipar, int* irtrn);
+
+      static double tPlot;
+      static double dtOut;
+      static fmatvec::Vec zInp;
+      static bool output_; 
+      static std::ofstream integPlot;
+      static double s0; 
+      static double time;
+      //static int integrationSteps;
+   
+      /** Absolute Toleranz */
+      fmatvec::Vec aTol;
+      /** Relative Toleranz */
+      fmatvec::Vec rTol;
+      /** step size for the first step */
+      double dt0;
+      /** maximum number of steps */
+      int maxSteps;  
+      /** maximale step size */
+      double dtMax;
+
+    public:
+
+      DOPRI5Integrator();
+      ~DOPRI5Integrator() {}
+
+      void setAbsoluteTolerance(const fmatvec::Vec &aTol_) {aTol = aTol_;}
+      void setAbsoluteTolerance(double aTol_) {aTol = fmatvec::Vec(1,fmatvec::INIT,aTol_);}
+      void setRelativeTolerance(const fmatvec::Vec &rTol_) {rTol = rTol_;}
+      void setRelativeTolerance(double rTol_) {rTol = fmatvec::Vec(1,fmatvec::INIT,rTol_);}
+      void setInitialStepSize(double dt0_) {dt0 = dt0_;}
+      void setMaxStepNumber(int maxSteps_) {maxSteps = maxSteps_;}    
+      void setMaximalStepSize(double dtMax_) {dtMax = dtMax_;}
+      const fmatvec::Vec& getAbsoluteTolerance() const { return aTol; }
+      const fmatvec::Vec& getRelativeTolerance() const { return rTol; }
+      double getInitialStepSize() const { return dt0; }
+      int getMaxStepNumber() const { return maxSteps; }
+      double getMaximalStepSize() const { return dtMax; }
+
+      void integrate(DynamicSystemSolver& system);
+
+      virtual void initializeUsingXML(TiXmlElement *element);
+      virtual TiXmlElement* writeXMLFile(TiXmlNode *element);
+
+      using Integrator::writeXMLFile;
+
+      virtual std::string getType() const { return "DOPRI5Integrator"; }
+  };
+
+}
+
+#endif
