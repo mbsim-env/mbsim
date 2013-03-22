0a7adc653a0d129ef6d5783facd22b09de969fc2
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-03-22 17:13:03 +0000

    GUI update: restored event compression.

--- trunk/mbsimgui/mbsimgui/main.cc
+++ trunk/mbsimgui/mbsimgui/main.cc
@@ -24,18 +24,6 @@
 #include <H5Cpp.h>
 #include <QLocale>
 
-class App : public QApplication {
-  public:
-    App( int & argc, char ** argv ) : QApplication(argc,argv) {}
-    bool compressEvent(QEvent *e, QObject *r, QPostEventList *p) {
-      //std::cout << "begin compress" << std::endl;
-      //bool flag =  QApplication::compressEvent(e,r,p);
-      bool flag = false;
-      //std::cout << "end compress" << std::endl;
-      return flag;
-    }
-};
-
 int main(int argc, char *argv[]) {
   // environment variables
   // Disalbe COIN VBO per default (see --help)
@@ -46,7 +34,7 @@ int main(int argc, char *argv[]) {
   H5::Exception::dontPrint();
 
   initializeOctave();
-  App app(argc, argv);
+  QApplication app(argc, argv);
   QLocale::setDefault(QLocale::C);
   setlocale(LC_ALL, "C");
   MainWindow *mainwindow = new MainWindow;
