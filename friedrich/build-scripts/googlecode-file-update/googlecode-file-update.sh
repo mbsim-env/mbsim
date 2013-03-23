9f6cde4942f14b1aaddf1d4a5accb7994a1694d1
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-03-23 17:20:42 +0000

    Fixed upload scripts

--- friedrich/build-scripts/googlecode-file-update/googlecode-file-update.sh
+++ friedrich/build-scripts/googlecode-file-update/googlecode-file-update.sh
@@ -1,13 +1,9 @@
 #! /bin/sh
 
 USER=friedrich.at.gc
+DIRS="/home/user/MBSimLinux/dist_mbsim /home/user/MBSimLinux/dist_openmbv /home/user/MBSimWindows/dist_mbsim /home/user/MBSimWindows/dist_openmbv"
 
-# check
 
-if [ ! -e mbsim-linux-shared-build-xxx.tar.bz2 -a ! -e mbsim-windows-shared-build-xxx.zip -a ! -e openmbv-linux-shared-build-xxx.tar.bz2 -a ! -e openmbv-windows-shared-build-xxx.zip ]; then
-  echo "Usage: None of the distribution files where found in the current directory"
-  exit 1
-fi
 
 # get inputs
 echo -n "Enter Build Number (format e.g. 004): "
@@ -23,34 +19,43 @@ echo
 
 # run
 
-if [ -e mbsim-linux-shared-build-xxx.tar.bz2 ]; then
-  SUMMARY="MBSim (inc. OpenMBV, HDF5Serie, ...) shared Linux build $BUILDNR"
-  PROJECT=mbsim-env
-  OS=Linux
-  FILE=mbsim-linux-shared-build-xxx.tar.bz2
-fi
-if [ -e mbsim-windows-shared-build-xxx.zip ]; then
-  SUMMARY="MBSim (inc. OpenMBV, HDF5Serie, ...) shared Windows build $BUILDNR"
-  PROJECT=mbsim-env
-  OS=Windows
-  FILE=mbsim-windows-shared-build-xxx.zip
-fi
-if [ -e openmbv-linux-shared-build-xxx.tar.bz2 ]; then
-  SUMMARY="OpenMBV shared Linux build $BUILDNR"
-  PROJECT=openmbv
-  OS=Linux
-  FILE=openmbv-linux-shared-build-xxx.tar.bz2
-fi
-if [ -e openmbv-windows-shared-build-xxx.zip ]; then
-  SUMMARY="OpenMBV shared Windows build $BUILDNR"
-  PROJECT=openmbv
-  OS=Windows
-  FILE=openmbv-windows-shared-build-xxx.zip
-fi
+cd $(dirname $0)
+ABSSCRIPTDIR=$(pwd)
 
-FILEBUILDNR=$(echo $FILE | sed -re "s/^(.*-)xxx(\..*)$/\1$BUILDNR\2/")
-ln -s $FILE $FILEBUILDNR
+for D in $DIRS; do
 
-echo "Upload $FILEBUILDNR"
-python2 $(dirname $0)/googlecode_upload.py -s "$SUMMARY" -p "$PROJECT" -u "$USER" -w "$PASSWORD" -l "OpSys-$OS,Type-Archive" $FILEBUILDNR
-rm -f $FILEBUILDNR
+  cd $D
+
+  if [ -e mbsim-linux-shared-build-xxx.tar.bz2 ]; then
+    SUMMARY="MBSim (inc. OpenMBV, HDF5Serie, ...) shared Linux build $BUILDNR"
+    PROJECT=mbsim-env
+    OS=Linux
+    FILE=mbsim-linux-shared-build-xxx.tar.bz2
+  fi
+  if [ -e mbsim-windows-shared-build-xxx.zip ]; then
+    SUMMARY="MBSim (inc. OpenMBV, HDF5Serie, ...) shared Windows build $BUILDNR"
+    PROJECT=mbsim-env
+    OS=Windows
+    FILE=mbsim-windows-shared-build-xxx.zip
+  fi
+  if [ -e openmbv-linux-shared-build-xxx.tar.bz2 ]; then
+    SUMMARY="OpenMBV shared Linux build $BUILDNR"
+    PROJECT=openmbv
+    OS=Linux
+    FILE=openmbv-linux-shared-build-xxx.tar.bz2
+  fi
+  if [ -e openmbv-windows-shared-build-xxx.zip ]; then
+    SUMMARY="OpenMBV shared Windows build $BUILDNR"
+    PROJECT=openmbv
+    OS=Windows
+    FILE=openmbv-windows-shared-build-xxx.zip
+  fi
+  
+  FILEBUILDNR=$(echo $FILE | sed -re "s/^(.*-)xxx(\..*)$/\1$BUILDNR\2/")
+  ln -s $FILE $FILEBUILDNR
+  
+  echo "Upload $FILEBUILDNR"
+  python2 $ABSSCRIPTDIR/googlecode_upload.py -s "$SUMMARY" -p "$PROJECT" -u "$USER" -w "$PASSWORD" -l "OpSys-$OS,Type-Archive" $FILEBUILDNR
+  rm -f $FILEBUILDNR
+
+done
