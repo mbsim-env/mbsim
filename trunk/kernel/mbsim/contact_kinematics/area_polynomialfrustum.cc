e971f5e57a69c9b97d9b86f29cc7f36cec40aea2
kgrundl@gmail.com <kgrundl@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-04-01 15:26:13 +0000

    Enhancing area-polynomial-frustum contact: Now corner-point-contacts are weighted depending on their penetration

--- trunk/kernel/mbsim/contact_kinematics/area_polynomialfrustum.cc
+++ trunk/kernel/mbsim/contact_kinematics/area_polynomialfrustum.cc
@@ -30,13 +30,142 @@ using namespace fmatvec;
 using namespace std;
 
 namespace MBSim {
+  edgePolyFrustum::edgePolyFrustum(const PolynomialFrustum * frustum_) :
+      frustum(frustum_), A(), dir() {
+  }
+
+  edgePolyFrustum::~edgePolyFrustum() {
+  }
+
+  void edgePolyFrustum::setAdir(const Vec3 & A_, const Vec3 & dir_) {
+    A = A_;
+    dir = dir_;
+  }
+
+  Vec edgePolyFrustum::operator ()(const Vec & xin, const void *) {
+    Vec result(1, NONINIT);
+    const double & t = xin(0);
+
+    Vec3 contactPointArea = A + t * dir;
+
+    const double & x = contactPointArea(0);
+    const double phi = ArcTan(contactPointArea(1), contactPointArea(2));
+    const double s = sin(phi);
+    const double c = cos(phi);
+
+    const double fD1 = frustum->getValueD1(x);
+    const double fD1SQp1 = fD1 * fD1 + 1;
+    const double sqrtfD1SQp1 = sqrt(fD1SQp1);
+
+    result(0) = -fD1 * dir(0) + c * dir(1) + s * dir(2);
+    result(0) /= sqrtfD1SQp1;
+
+    return result;
+  }
+
+  edgePolyFrustumCriteria::edgePolyFrustumCriteria(const double & tolerance_) :
+      tolerance(tolerance_), frustumHeight(-1), criteriaResults(0), ax(0.), dx(0.) {
+  }
+
+  int edgePolyFrustumCriteria::operator ()(const Vec & xin, const void *) {
+    criteriaResults.push_back(nrmInf((*function)(xin)));
+
+    if (not inBounds(xin(0)))
+      return -1; //out of bounds --> assume that this thing won't converge
+
+    if (criteriaResults.back() < tolerance)
+      return 0;
+
+    return 1;
+  }
+
+  bool edgePolyFrustumCriteria::isBetter(const Vec & x) {
+    if (not inBounds(x(0)))
+      return false;
+
+    if (nrmInf((*function)(x)) < criteriaResults.back())
+      return true;
+
+    return false;
+  }
+
+  bool edgePolyFrustumCriteria::inBounds(const double & t) {
+    double x = ax + t * dx;
+    if (t < 0 or t > 1 or x < 0 or x > frustumHeight)
+      return false;
+
+    return true;
+  }
+
+  projectAlongNormal::projectAlongNormal(PolynomialFrustum * frustum) :
+      frustum(frustum), referencePoint(), phi(0.) {
+  }
+
+  projectAlongNormal::~projectAlongNormal() {
+
+  }
+
+  void projectAlongNormal::setUpSystemParamters(const Vec3 & referencePoint_, const double & phi_) {
+    referencePoint = referencePoint_;
+    phi = phi_;
+  }
+
+  Vec projectAlongNormal::operator ()(const Vec & xin, const void *) {
+    Vec result(1, NONINIT);
+
+    const double & x = xin(0);
+
+    const double fd = frustum->getValueD1(x);
+
+    //to avoid numerical instabilities chose one of the two options
+    double tmp = referencePoint(1) /  cos(phi);
+    if(fabs(cos(phi)) < 1e-2)
+      tmp = referencePoint(2) / sin(phi);
+
+    result(0) = referencePoint(0) - (frustum->getValue(x) * fd - fd * tmp + x);
+
+    return result;
+  }
+
+  projectAlongNormalJacobian::projectAlongNormalJacobian(PolynomialFrustum * frustum) :
+      frustum(frustum), referencePoint(), phi(0.) {
+  }
+
+  projectAlongNormalJacobian::~projectAlongNormalJacobian() {
+  }
+
+  void projectAlongNormalJacobian::setUpSystemParamters(const Vec3 & referencePoint_, const double & phi_) {
+    referencePoint = referencePoint_;
+    phi = phi_;
+  }
+
+  SqrMat projectAlongNormalJacobian::operator ()(const Vec & xin, const void *) {
+    SqrMat Jac(1, NONINIT);
+
+    const double & x = xin(0);
+
+    const double fd = frustum->getValueD1(x);
+    const double fdd = frustum->getValueD2(x);
+
+    //to avoid numerical instabilities chose one of the two options
+    double tmp = referencePoint(1) /  cos(phi);
+    if(fabs(cos(phi)) < 1e-2)
+      tmp = referencePoint(2) / sin(phi);
+
+    Jac(0,0) =  - fd*fd - frustum->getValue(x) * fdd + fdd * tmp - 1;
+
+    return Jac;
+  }
+
 
   ContactKinematicsAreaPolynomialFrustum::ContactKinematicsAreaPolynomialFrustum() :
-      ContactKinematics(), iarea(-1), ifrustum(-1), area(0), frustum(0), x1(-1), x2(-1), funcAB(0), newtonedge(), jacobian(), criteria(), damping(), ilast(-1), xi(1, INIT, 0.5) {
+      ContactKinematics(), iarea(-1), ifrustum(-1), area(0), frustum(0), x1(-1), x2(-1), funcProjectAlongNormal(0), newtonProjectAlongNormal(), jacobianProjectAlongNormal(0), criteriaProjectAlongNormal(), dampingProjectAlongNormal(),  funcEdge(0), newtonEdge(), jacobianEdge(), criteriaEdge(), dampingEdge(), ilast(-1), xi(1, INIT, 0.5) {
   }
 
   ContactKinematicsAreaPolynomialFrustum::~ContactKinematicsAreaPolynomialFrustum() {
-    delete funcAB;
+    delete funcEdge;
+    delete funcProjectAlongNormal;
+    delete jacobianProjectAlongNormal;
   }
 
   void ContactKinematicsAreaPolynomialFrustum::assignContours(const vector<Contour*> &contour) {
@@ -57,24 +186,32 @@ namespace MBSim {
     x1 = frustum->getHeight() / 2;
     x2 = frustum->getHeight() / 2;
 
-    //initalize edge function
-    funcAB = new edgePolyFrustum(frustum);
-    xi(0) = 0.5;
+    /*Set Up system for projection of contact point onto frustum surface*/
+    funcProjectAlongNormal = new projectAlongNormal(frustum);
+    newtonProjectAlongNormal.setFunction(funcProjectAlongNormal);
+    jacobianProjectAlongNormal = new projectAlongNormalJacobian(frustum);
+    newtonProjectAlongNormal.setJacobianFunction(jacobianProjectAlongNormal);
+    newtonProjectAlongNormal.setCriteriaFunction(&criteriaProjectAlongNormal);
+    newtonProjectAlongNormal.setDampingFunction(&dampingProjectAlongNormal);
 
-    newtonedge.setFunction(funcAB);
-    //newtonedge.setMaximumNumberOfIterations(5);
-    newtonedge.setJacobianFunction(&jacobian);
-    criteria.setFrustumHeight(frustum->getHeight());
-    newtonedge.setCriteriaFunction(&criteria);
+    /*Set Up System for edge contact search */
+    //initalize edge function
+    funcEdge = new edgePolyFrustum(frustum);
+    xi(0) = 0.5;
+    newtonEdge.setFunction(funcEdge);
+    newtonEdge.setJacobianFunction(&jacobianEdge);
+    criteriaEdge.setFrustumHeight(frustum->getHeight());
+    newtonEdge.setCriteriaFunction(&criteriaEdge);
     //REMARK: Damping is evil here, as it always "sees" a solution out of bound and damps this solution over and over again, however it is very likely, that edge that is iterated through will have no contact point
 
     //TODO: check for convexity of frustum
   }
 
-  void ContactKinematicsAreaPolynomialFrustum::setFrustumOrienationKinematics(const double & x, const double & phi, Vec & g, ContourPointData * cpData) {
-    cpData[ifrustum].getFrameOfReference().getOrientation().set(0, frustum->getFrame()->getOrientation() * frustum->computeNormal(x, phi));
-    cpData[ifrustum].getFrameOfReference().getOrientation().set(1, frustum->getFrame()->getOrientation() * frustum->computeTangentRadial(x, phi));
-    cpData[ifrustum].getFrameOfReference().getOrientation().set(2, crossProduct(cpData[ifrustum].getFrameOfReference().getOrientation().col(0), cpData[ifrustum].getFrameOfReference().getOrientation().col(1)));
+  void ContactKinematicsAreaPolynomialFrustum::setFrustumOrienationKinematics(const double & x, const double & phi, ContourPointData * cpData) {
+    SqrMat3 AWF = frustum->getFrame()->getOrientation();
+    cpData[ifrustum].getFrameOfReference().getOrientation().set(0, AWF * frustum->computeNormal(x, phi));
+    cpData[ifrustum].getFrameOfReference().getOrientation().set(1, -AWF * frustum->computeTangentRadial(x, phi));
+    cpData[ifrustum].getFrameOfReference().getOrientation().set(2, AWF * frustum->computeTangentAzimuthal(x, phi));
   }
 
   bool ContactKinematicsAreaPolynomialFrustum::cpLocationInArea(Vec & g, ContourPointData * cpData) {
@@ -160,7 +297,8 @@ namespace MBSim {
     cornerPoints[1] = area->getB();
     cornerPoints[2] = area->getC();
     cornerPoints[3] = area->getD();
-    double gaps[4];
+    double weights[4];
+    double weightsum = 0;
 
     for (int i = 0; i < 4; i++) {
       cornerPoints[i] = frustum->getFrame()->getOrientation().T() * (area->getFrame()->getPosition() + area->getFrame()->getOrientation() * cornerPoints[i] - frustum->getFrame()->getPosition());
@@ -170,31 +308,45 @@ namespace MBSim {
       const double R = frustum->getValue(x);
       if (x >= 0 and x <= frustum->getHeight() and r <= R) {
         //Possible contact point found --> assumed to be at x-position of corner point
-        gaps[i] = r - R;
+        weights[i] = R - r;
+        weightsum += weights[i];
       }
       else
-        gaps[i] = 1.;
+        weights[i] = 0;
     }
 
-    int deepestCornerIndex = 0;
-    for (int i = 1; i < 4; i++) {
-      if (gaps[i] < gaps[deepestCornerIndex])
-        deepestCornerIndex = i;
+    if(not (weightsum > 0.0)) {
+      return false;
     }
 
-    g(0) = gaps[deepestCornerIndex];
+    Vec3 contactPointArea;
+    for (int i = 0; i < 4; i++) {
+      contactPointArea = contactPointArea + cornerPoints[i] * weights[i]/weightsum;
+    }
+
+    double phi = ArcTan(contactPointArea(1), contactPointArea(2));
+
+    // find x-position that intersects with normal for found contact point
+
+    Vec x(1,INIT, contactPointArea(0));
+
+    funcProjectAlongNormal->setUpSystemParamters(contactPointArea, phi);
+    jacobianProjectAlongNormal->setUpSystemParamters(contactPointArea, phi);
+    x = newtonProjectAlongNormal.solve(x);
+
+    Vec3 contactPointFrustum = frustum->computePoint(x(0), phi);
+
+    g(0) = frustum->computeNormal(x(0), phi).T() * (contactPointArea - contactPointFrustum);
 
     if (g(0) < 0.) {
-      const double phi = ArcTan(cornerPoints[deepestCornerIndex](1), cornerPoints[deepestCornerIndex](2));
-      const double & x = cornerPoints[deepestCornerIndex](0);
-
       //Frustum
-      cpData[ifrustum].getFrameOfReference().getPosition() = frustum->getFrame()->getPosition() + frustum->getFrame()->getOrientation() * frustum->computePoint(x, phi);
-      setFrustumOrienationKinematics(x, phi, g, cpData);
+      Vec3 rF = frustum->getFrame()->getPosition();
+      SqrMat3 AWF = frustum->getFrame()->getOrientation();
+      cpData[ifrustum].getFrameOfReference().getPosition() = rF + AWF * contactPointFrustum;
+      setFrustumOrienationKinematics(x(0), phi, cpData);
 
       //Area
-      cpData[iarea].getFrameOfReference().getPosition() = frustum->getFrame()->getPosition() + frustum->getFrame()->getOrientation() * cornerPoints[deepestCornerIndex];
-
+      cpData[iarea].getFrameOfReference().getPosition() = rF + AWF  * contactPointArea;
       cpData[iarea].getFrameOfReference().getOrientation().set(0, -cpData[ifrustum].getFrameOfReference().getOrientation().col(0));
       cpData[iarea].getFrameOfReference().getOrientation().set(1, -cpData[ifrustum].getFrameOfReference().getOrientation().col(1));
       cpData[iarea].getFrameOfReference().getOrientation().set(2, cpData[ifrustum].getFrameOfReference().getOrientation().col(2));
@@ -228,14 +380,14 @@ namespace MBSim {
 
       //set up function for solution
       //Remark: The corner-points positions are assumed to be updated in the cornerContact-routine already --> this always happens if no corner point is a valid contact point
-      funcAB->setAdir(cornerPoints[indi], cornerPoints[indj] - cornerPoints[indi]);
-      criteria.setStartingXCoordinate(cornerPoints[indi](0));
-      criteria.setdirectionXCoordinate(cornerPoints[indj](0) - cornerPoints[indi](0));
+      funcEdge->setAdir(cornerPoints[indi], cornerPoints[indj] - cornerPoints[indi]);
+      criteriaEdge.setStartingXCoordinate(cornerPoints[indi](0));
+      criteriaEdge.setdirectionXCoordinate(cornerPoints[indj](0) - cornerPoints[indi](0));
 
-      xi = newtonedge.solve(xi);
+      xi = newtonEdge.solve(xi);
 
       //two intersection points are found and it is not the same
-      if (newtonedge.getInfo() == 0) {
+      if (newtonEdge.getInfo() == 0) {
 
         const double & t = xi(0);
 
@@ -250,7 +402,7 @@ namespace MBSim {
 
           //Frustum
           cpData[ifrustum].getFrameOfReference().getPosition() = frustum->getFrame()->getPosition() + frustum->getFrame()->getOrientation() * contactPointFrustum;
-          setFrustumOrienationKinematics(x, phi, g, cpData);
+          setFrustumOrienationKinematics(x, phi, cpData);
 
           //Area
           cpData[iarea].getFrameOfReference().getPosition() = frustum->getFrame()->getPosition() + frustum->getFrame()->getOrientation() * contactPointArea;
@@ -296,15 +448,14 @@ namespace MBSim {
 
       //if the area intersects this circle on plane, then search for the contact point
       if (area->Intersect_Circle(rad_inscir, R_cen_inscir)) {
-
         //Check for contact point in the area
         if (cpLocationInArea(g, cpData)) {
           g(0) = distance2Area(cpData[ifrustum].getFrameOfReference().getPosition());
           cpData[iarea].getFrameOfReference().getOrientation() = area->getFrame()->getOrientation();
           cpData[iarea].getFrameOfReference().getPosition() = cpData[ifrustum].getFrameOfReference().getPosition() - area->getFrame()->getOrientation().col(0) * g(0);
           cpData[ifrustum].getFrameOfReference().getOrientation().set(0, -cpData[iarea].getFrameOfReference().getOrientation().col(0));
-          cpData[ifrustum].getFrameOfReference().getOrientation().set(1, -cpData[iarea].getFrameOfReference().getOrientation().col(1));
-          cpData[ifrustum].getFrameOfReference().getOrientation().set(2, cpData[iarea].getFrameOfReference().getOrientation().col(2));
+          cpData[ifrustum].getFrameOfReference().getOrientation().set(1, cpData[iarea].getFrameOfReference().getOrientation().col(1));
+          cpData[ifrustum].getFrameOfReference().getOrientation().set(2, -cpData[iarea].getFrameOfReference().getOrientation().col(2));
           ilast = -1;
           return;
         }
@@ -315,10 +466,8 @@ namespace MBSim {
         else if (edgeContact(g, cpData)) {
           return;
         }
-
       }
     }
-
     g(0) = 1.;
   }
 
@@ -345,105 +494,5 @@ namespace MBSim {
   double ContactKinematicsAreaPolynomialFrustum::distance2Area(const Vec3 & point) {
     return area->getReferenceOrientation().col(0).T() * (point - area->getFrame()->getPosition());
   }
-
-  edgePolyFrustum::edgePolyFrustum(const PolynomialFrustum * frustum_) :
-      frustum(frustum_), A(), dir() {
-  }
-
-  edgePolyFrustum::~edgePolyFrustum() {
-  }
-
-  void edgePolyFrustum::setAdir(const Vec3 & A_, const Vec3 & dir_) {
-    A = A_;
-    dir = dir_;
-  }
-
-  Vec edgePolyFrustum::operator ()(const Vec & xin, const void *) {
-    Vec result(1, NONINIT);
-    const double & t = xin(0);
-
-    Vec3 contactPointArea = A + t * dir;
-
-    const double & x = contactPointArea(0);
-    const double phi = ArcTan(contactPointArea(1), contactPointArea(2));
-    const double s = sin(phi);
-    const double c = cos(phi);
-
-    const double fD1 = frustum->getValueD1(x);
-    const double fD1SQp1 = fD1 * fD1 + 1;
-    const double sqrtfD1SQp1 = sqrt(fD1SQp1);
-
-    result(0) = -fD1 * dir(0) + c * dir(1) + s * dir(2);
-    result(0) /= sqrtfD1SQp1;
-
-    return result;
-  }
-
-  edgePolyFrustumCriteria::edgePolyFrustumCriteria(const double & tolerance_) :
-      tolerance(tolerance_), frustumHeight(-1), criteriaResults(0), ax(0.), dx(0.) {
-  }
-
-  int edgePolyFrustumCriteria::operator ()(const Vec & xin, const void *) {
-    criteriaResults.push_back(nrmInf((*function)(xin)));
-
-    if (not inBounds(xin(0)))
-      return -1; //out of bounds --> assume that this thing won't converge
-
-    if (criteriaResults.back() < tolerance)
-      return 0;
-
-    return 1;
-  }
-
-  bool edgePolyFrustumCriteria::isBetter(const Vec & x) {
-    if (not inBounds(x(0)))
-      return false;
-
-    if (nrmInf((*function)(x)) < criteriaResults.back())
-      return true;
-
-    return false;
-  }
-
-  bool edgePolyFrustumCriteria::inBounds(const double & t) {
-    double x = ax + t * dx;
-    if (t < 0 or t > 1 or x < 0 or x > frustumHeight)
-      return false;
-
-    return true;
-  }
-
-//  fmatvec::SqrMat3 ContactKinematicsAreaPolynomialFrustum::RotatM_2vec(fmatvec::Vec3 X, fmatvec::Vec3 V){
-//    Vec3 Y,Z;
-//    Z(0) = X(1)*V(2)-X(2)*V(1);
-//    Z(1) = X(2)*V(0)-X(0)*V(2);
-//    Z(2) = X(0)*V(1)-X(1)*V(0);
-//    X = X/(nrm2(X));
-//    Z = Z/(nrm2(Z));
-//    Y(0) = Z(1)*X(2)-Z(2)*X(1);
-//    Y(1) = Z(2)*X(0)-Z(0)*X(2);
-//    Y(2) = Z(0)*X(1)-Z(1)*X(0);
-//    SqrMat3 A;
-//    A.col(0)=X;
-//    A.col(1)=Y;
-//    A.col(2)=Z;
-//    A=A.T();
-//    return A;
-//  }
-
-//  MBSim::CP_item_frustum ContactKinematicsAreaPolynomialFrustum::CP_toP_onfrustum3D(fmatvec::Vec3 P){
-//    Vec3 V1,V2,OF,PF,Pnew;
-//    CP_item_frustum cpinfo;
-//    OF=frustum->getReferencePosition();//origin of frustum frame
-//    V1=frustum->getReferenceOrientation().col(0);//V1 assigned to be normal axis of the frustum
-//    V2=P-OF;//V2 assigned to be the vector from the frustum origin to P
-//    SqrMat3 rotationA;
-//    rotationA=RotatM_2vec(V1,V2);//rotation matrix rotate from current frame to the new frame
-//
-//
-//
-//    cpinfo.CP_item_frustum(P,P,0);
-//    return cpinfo;
-//  }
 
 } /* namespace MBSim */
