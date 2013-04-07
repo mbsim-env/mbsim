07e44a1ac050f1b8072f008ad0e774b18c6e424c
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-04-07 10:29:19 +0000

    Cleanup

--- trunk/mbsimgui/mbsimgui/octaveutils.h
+++ trunk/mbsimgui/mbsimgui/octaveutils.h
@@ -23,28 +23,7 @@
 #include <octave/toplev.h>
 #include <iostream>
 
-class TiXmlElement;
-
-extern std::map<std::string, octave_value> currentParam;
-
-enum ValueType {
-  ArbitraryType,
-  ScalarType,
-  VectorType,
-  MatrixType,
-  StringType
-};
-
-struct Param {
-  Param(std::string n, std::string eq, TiXmlElement *e) : name(n), equ(eq), ele(e) {}
-  std::string name, equ;
-  TiXmlElement *ele;
-};
-
-void octaveEvalRet(std::string str, TiXmlElement *e=NULL);
-std::string octaveGetRet(ValueType expectedType=ArbitraryType);
 void initializeOctave();
-int fillParam(std::vector<Param> param);
 
 std::string evalOctaveExpression(const std::string &str);
 
