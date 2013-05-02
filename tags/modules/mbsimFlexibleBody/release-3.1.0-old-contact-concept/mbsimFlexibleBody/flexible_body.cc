29b6043811899476160225748d21546c607dc0d9
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-05-02 15:37:56 +0000

    Tagged mbsimFlexibleBody version 3.1.0

---
+++ tags/modules/mbsimFlexibleBody/release-3.1.0-old-contact-concept/mbsimFlexibleBody/flexible_body.cc
@@ -0,0 +1,182 @@
+/* Copyright (C) 2004-2011 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: thorsten.schindler@mytum.de
+ *          rzander@users.berlios.de
+ */
+
+#include <config.h>
+#include <mbsimFlexibleBody/flexible_body.h>
+#include <mbsim/dynamic_system.h>
+#include <mbsim/frame.h>
+#include <mbsim/contour.h>
+#include <mbsim/utils/function.h>
+#include <mbsim/mbsim_event.h>
+#include <mbsim/contour_pdata.h>
+#include <mbsim/discretization_interface.h>
+
+//#ifdef _OPENMP
+//#include <omp.h>
+//#endif
+
+using namespace fmatvec;
+using namespace MBXMLUtils;
+using namespace std;
+using namespace MBSim;
+
+namespace MBSimFlexibleBody {
+
+  FlexibleBody::FlexibleBody(const string &name) : Body(name), d_massproportional(0.) {}
+
+  FlexibleBody::~FlexibleBody() {
+    for(unsigned int i=0; i<discretization.size(); i++) {
+      if(discretization[i]) { delete discretization[i]; discretization[i] = NULL; }
+    }
+  }
+
+  void FlexibleBody::updateh(double t, int k) {
+    for(int i=0;i<(int)discretization.size();i++) {
+      try { discretization[i]->computeh(qElement[i],uElement[i]); } // compute attributes of finite element
+      catch(MBSimError error) { error.printExceptionMessage(); throw; }
+    }
+    for(int i=0;i<(int)discretization.size();i++) GlobalVectorContribution(i,discretization[i]->geth(),h[k]); // assemble
+
+    if(d_massproportional>0) { // mass proportional damping
+      h[k] -= d_massproportional*(M[k]*u);
+    }
+  }
+
+  void FlexibleBody::updateM(double t, int k) {
+    for(int i=0;i<(int)discretization.size();i++) {
+      try { discretization[i]->computeM(qElement[i]); } // compute attributes of finite element
+      catch(MBSimError error) { error.printExceptionMessage(); throw; }
+    }
+    for(int i=0;i<(int)discretization.size();i++) GlobalMatrixContribution(i,discretization[i]->getM(),M[k]); // assemble
+  }
+
+  void FlexibleBody::updatedhdz(double t) {
+    updateh(t);
+    for(int i=0;i<(int)discretization.size();i++) {
+      try {discretization[i]->computedhdz(qElement[i],uElement[i]); } // compute attributes of finite element
+      catch(MBSimError error) { error.printExceptionMessage(); throw; }
+    }
+   for(int i=0;i<(int)discretization.size();i++) GlobalMatrixContribution(i,discretization[i]->getdhdq(),dhdq); // assemble
+   for(int i=0;i<(int)discretization.size();i++) GlobalMatrixContribution(i,discretization[i]->getdhdu(),dhdu); // assemble
+  }
+
+  void FlexibleBody::updateStateDependentVariables(double t) {
+    BuildElements();
+    for(unsigned int i=0; i<frame.size(); i++) { // frames
+      updateKinematicsForFrame(S_Frame[i],all,frame[i]); 
+    }
+    // TODO contour non native?
+  }
+
+  void FlexibleBody::updateJacobians(double t, int k) {
+    for(unsigned int i=0; i<frame.size(); i++) { // frames
+      updateJacobiansForFrame(S_Frame[i],frame[i]);
+    }
+    // TODO contour non native?
+  }
+
+  void FlexibleBody::plot(double t, double dt) {
+    if(getPlotFeature(plotRecursive)==enabled) {
+      Body::plot(t,dt);
+    }
+  }
+
+  void FlexibleBody::init(InitStage stage) {
+    if(stage==unknownStage) {
+      Body::init(stage);
+      T = SqrMat(qSize,fmatvec::EYE);
+      for(unsigned int i=0; i<frame.size(); i++) { // frames
+        S_Frame[i].getFrameOfReference().getJacobianOfTranslation().resize(uSize[0]);
+        S_Frame[i].getFrameOfReference().getJacobianOfRotation().resize(uSize[0]);
+      }
+    }
+    else if(stage==MBSim::plot) {
+      updatePlotFeatures();
+
+      if(getPlotFeature(plotRecursive)==enabled) {
+        Body::init(stage);
+      }
+    }
+    else
+      Body::init(stage);
+  }
+
+  double FlexibleBody::computeKineticEnergy() {
+    double T = 0.;
+    for(unsigned int i=0; i<discretization.size(); i++) {
+      T += discretization[i]->computeKineticEnergy(qElement[i],uElement[i]);
+    }
+    return T;
+  }
+
+  double FlexibleBody::computePotentialEnergy() {
+    double V = 0.;
+    for(unsigned int i=0; i<discretization.size(); i++) {
+      V += discretization[i]->computeElasticEnergy(qElement[i]) + discretization[i]->computeGravitationalEnergy(qElement[i]);
+    }
+    return V;
+  }
+
+  void FlexibleBody::setFrameOfReference(Frame *frame) { 
+    if(dynamic_cast<DynamicSystem*>(frame->getParent())) 
+      R = frame; 
+    else 
+      throw MBSimError("ERROR (FlexibleBody::setFrameOfReference): Only stationary reference frames are implemented at the moment!"); 
+  }
+
+  void FlexibleBody::addFrame(const string &name, const ContourPointData &S_) {
+    Frame *frame = new Frame(name);
+    addFrame(frame,S_);
+  }
+
+  void FlexibleBody::addFrame(Frame* frame, const ContourPointData &S_) {
+    Body::addFrame(frame);
+    S_Frame.push_back(S_);
+  }
+
+  void FlexibleBody::addFrame(const std::string &name, const int &id) {
+    ContourPointData cp(id);
+    addFrame(name,cp);
+  }
+
+  void FlexibleBody::addFrame(Frame *frame, const  int &id) {
+    ContourPointData cp(id);
+    addFrame(frame,cp);
+  }
+
+  void FlexibleBody::addContour(Contour *contour_) {
+    stringstream frameName;
+    frameName << "ContourFrame" << contour.size();
+    Frame *contourFrame = new Frame(frameName.str());
+//    addFrame(contourFrame,0);
+    contour_->setFrameOfReference(contourFrame);
+    Body::addContour(contour_);
+  }
+
+  void FlexibleBody::initializeUsingXML(TiXmlElement *element) {
+    Body::initializeUsingXML(element);
+    
+    TiXmlElement *e;
+    e=element->FirstChildElement(MBSIMFLEXNS"massProportionalDamping");
+    setMassProportionalDamping(getDouble(e));
+  }
+
+}
+
---
+++ tags/modules/mbsimFlexibleBody/release-3.1.0-old-contact-concept/mbsimFlexibleBody/flexible_body/finite_elements/finite_element_1s_33_rcm/trafo33RCM.h
@@ -0,0 +1,428 @@
+/* Copyright (C) 2004-2011 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: thorsten.schindler@mytum.de
+ */
+
+#ifndef TRAFO33RCM_H_
+#define TRAFO33RCM_H_
+
+#include "fmatvec.h"
+#include "mbsim/utils/function.h"
+#include "mbsimFlexibleBody/pointer.h"
+
+namespace MBSimFlexibleBody {
+
+  /***
+   * \brief computation of FiniteElement1s33RCM position coordinates (cp-angles and bending)
+   * \author Thorsten Schindler
+   * \date 2009-04-24 initial commit (Thorsten Schindler)
+   */
+  class PositionFunction : public MBSim::Function1<fmatvec::Vec,fmatvec::Vec> {    
+    public:
+      /**
+       * \brief constructor
+       * \param reversed Cardan-Object
+       * \param length of FiniteElement1s33RCM
+       * \param angles of COG of rigid left-right connection
+       * \param global bending parameters
+       * \param transpose of difference of right and left beam position
+       */
+      PositionFunction(RevCardanPtr angle_,double l0_,const fmatvec::Vec& pL_,const fmatvec::Vec& pR_,
+          double cL1_,double cR1_,double cL2_,double cR2_,const fmatvec::RowVec& rRrLmH_);
+      
+      /**
+       * \brief destructor 
+       */
+      virtual ~PositionFunction();
+      
+      /* INHERITED INTERFACE */
+      fmatvec::Vec operator()(const fmatvec::Vec& pos, const void * =NULL);
+      /***************************************************/
+
+    private:
+      /** 
+       * \brief reversed Cardan-Object
+       */
+       RevCardanPtr angle;
+
+      /** 
+       * \brief length of FiniteElement1s33RCM 
+       */
+      double l0;
+
+      /** 
+       * \brief angles of COG of rigid left-right connection
+       */
+      fmatvec::Vec pL, pR;
+
+      /** 
+       * \brief global bending parameters
+       */
+      double cL1, cR1, cL2, cR2;
+
+      /** 
+       * \brief transpose of difference of right and left beam position
+       */
+      fmatvec::RowVec rRrLmH;  
+  };
+
+  /**
+   * \brief computation of FiniteElement1s33RCM position JACOBIAN (cp-angles and bending)
+   * \author Thorsten Schindler
+   * \date 2009-04-24 initial commit (Thorsten Schindler)
+   */
+  class PositionJacobian : public MBSim::Function1<fmatvec::SqrMat,fmatvec::Vec> {
+    public:
+      /**
+       * \brief constructor
+       * \param reversed Cardan-Object
+       * \param length of FiniteElement1s33RCM
+       * \param transpose of difference of right and left beam position
+       * \param differentiated centre angles with respect to bending coordinates
+       */
+      PositionJacobian(RevCardanPtr angle_,double l0_,const fmatvec::RowVec &rRrLmH_,const fmatvec::Mat &pSbE_);
+      
+      /**
+       * \brief destructor
+       */
+      virtual ~PositionJacobian();
+      
+      /* INHERITED INTERFACE */
+      fmatvec::SqrMat operator()(const fmatvec::Vec &pos, const void * =NULL);
+      /***************************************************/
+
+    private:
+      /** 
+       * \brief reversed Cardan-Object
+       */
+      RevCardanPtr angle;
+
+      /** 
+       * \brief length of FiniteElement1s33RCM 
+       */
+      double l0;
+
+      /** 
+       * \brief transpose of difference of right and left beam position
+       */
+      fmatvec::RowVec rRrLmH;
+
+      /** 
+       * \brief differentiated centre angles with respect to bending coordinates
+       */
+      fmatvec::Mat pSbE;
+  };
+  /*******************************************************************/
+
+  /*! 
+   * \brief transformation of coordinates for FiniteElement1s33RCM
+   * \author Thorsten Schindler
+   * \date 2009-04-24 initial commit (Thorsten Schindler)
+   */
+  class Trafo33RCM {		
+    public:
+      /**
+       * \brief constructor
+       */
+      Trafo33RCM(RevCardanPtr angle_,double l0_);
+      
+      /**
+       * \brief destructor
+       */
+      virtual ~Trafo33RCM();
+
+      /* GETTER / SETTER */
+      const fmatvec::SqrMat& getJIG() const;		
+      const fmatvec::SqrMat& getJIGt() const;
+      const fmatvec::Vec& getpS() const;
+      const fmatvec::Vec& getpSt() const;
+      const fmatvec::Vec& gettS() const;
+      const fmatvec::Vec& getnS() const;
+      const fmatvec::Vec& getbS() const;
+      const fmatvec::RowVec& getnSH() const;
+      const fmatvec::RowVec& getbSH() const;
+      const fmatvec::Vec& getntilS() const;
+      const fmatvec::Vec& getbtilS() const;
+      const fmatvec::RowVec& getntilSH() const;
+      const fmatvec::RowVec& getbtilSH() const;
+      const fmatvec::Vec& gettSt() const;
+      const fmatvec::Vec& getnSt() const;
+      const fmatvec::Vec& getbSt() const;
+      const fmatvec::RowVec& gettStH() const;
+      const fmatvec::RowVec& getnStH() const;
+      const fmatvec::RowVec& getbStH() const;
+      const fmatvec::RowVec& getntilStH() const;
+      const fmatvec::RowVec& getbtilStH() const;
+      const fmatvec::SqrMat& gettSpS() const;
+      const fmatvec::SqrMat& getnSpS() const;
+      const fmatvec::SqrMat& getbSpS() const;
+      const fmatvec::SqrMat& getntilSpS() const;
+      const fmatvec::SqrMat& getbtilSpS() const;
+      const fmatvec::SqrMat& gettSpSt() const;
+      const fmatvec::SqrMat& getnSpSt() const;
+      const fmatvec::SqrMat& getbSpSt() const;
+      const fmatvec::SqrMat& getntilSpSt() const;
+      const fmatvec::SqrMat& getbtilSpSt() const;
+
+      const fmatvec::Vec& getqIt() const;
+      const double& getepstil() const;
+      const double& getepstilt() const;
+      const fmatvec::Vec& getrS() const;
+      const fmatvec::Vec& getrSt() const;
+      const double& getk0() const;
+      const double& getk0t() const;
+      const fmatvec::Vec& getbe() const;
+      const fmatvec::Vec& getbet() const;
+
+      const double& getxintil() const;
+      const double& getxibtil() const;
+      const double& getetantil() const;
+      const double& getetabtil() const;
+      const double& getxintilt() const;
+      const double& getxibtilt() const;
+      const double& getetantilt() const;
+      const double& getetabtilt() const;
+      const fmatvec::SqrMat& getV() const;
+      /***************************************************/
+
+      /**
+       * \brief compute the internal coordinates
+       * \param global coordinates
+       */
+      void computeqI(const fmatvec::Vec& qG);
+      
+      /**
+       * \brief compute the Jacobian of the trafo 
+       * \param global coordinates
+       */
+      void computeJIG(const fmatvec::Vec& qG);
+      
+      /**
+       * \brief compute the internal state
+       * \param global coordinates
+       * \param global velocities
+       */
+      void computezI(const fmatvec::Vec& qG,const fmatvec::Vec& qGt);
+      
+      /**
+       * \brief compute the internal time differentiated COSY 
+       * \param global coordinates
+       * \param global velocities
+       */
+      void computeCOSYt(const fmatvec::Vec& qG,const fmatvec::Vec& qGt);
+      
+      /**
+       * \brief compute the internal coordinates and velocities as well as JIGt 
+       * \param global coordinates
+       * \param global velocities
+       */
+      void computeTrafo(const fmatvec::Vec& qG,const fmatvec::Vec& qGt);
+
+    private:
+      /** 
+       * \brief reversed Cardan-Object
+       */
+      RevCardanPtr angle;
+
+      /** 
+       * \brief length of FiniteElement1s33RCM and powers
+       */
+      double l0, l0h2, l0h3, l0h4, l0h5;
+      double xstar, xstarh2, xstarh3;
+
+      /** 
+       * \brief internal coordinates of the FiniteElement1s33RCM
+       */
+      double epstil, k0;
+      fmatvec::Vec rS, pS;
+
+      /** 
+       * \brief sum of right and left translational vector as well as transpose of the difference
+       */ 
+      fmatvec::Vec rRrLp;
+      fmatvec::RowVec rRrLmH;
+
+      /** 
+       * \brief bending coordinates
+       */
+      fmatvec::Vec be;
+
+      /** 
+       * \brief COSY definitions 
+       */
+      fmatvec::Vec tS, nS, bS, ntilS, btilS;
+      fmatvec::RowVec nSH, bSH, ntilSH, btilSH;
+      fmatvec::SqrMat tSpS, nSpS, bSpS, ntilSpS, btilSpS;
+      double xibtil, xintil, etabtil, etantil;
+
+      /** 
+       * \brief system matrix and right hand side for computebe() 
+       */
+      fmatvec::Mat SMRHS_Jac;
+
+      /** 
+       * \brief derivative of coefficients of bending polynomials w with respect to bending coordinates
+       */
+      fmatvec::SqrMat V;
+
+      /** 
+       * \brief delta matrices for computebe()
+       */
+      fmatvec::Mat drRdrLp, drRdrLm, pSbE; 		
+
+      /**
+       * \brief COSY definitions for computebe() 
+       */
+      fmatvec::Mat SMRHS, nSbE, bSbE, ntilSbE, btilSbE;
+      fmatvec::RowVec xibtilbE, xintilbE, etabtilbE, etantilbE;
+
+      /** 
+       * \brief derivative of be and COSY with respect to qG
+       */
+      fmatvec::Mat beqG, tSqG, nSqG, bSqG, ntilSqG, btilSqG;
+      fmatvec::RowVec xintilqG, xibtilqG, etantilqG, etabtilqG;
+
+      /** 
+       * \brief Jacobian and differentiated Jacobian
+       */
+      fmatvec::SqrMat JIG, JIGt;
+
+      /** 
+       * \brief derivative of k0, epstilt, qI, rS, be and COSY with respect to time
+       */
+      double k0t, epstilt;
+      fmatvec::Vec qIt, rSt, bet, pSt, tSt, nSt, bSt;
+      fmatvec::RowVec tStH, nStH, bStH, ntilStH, btilStH;
+      fmatvec::SqrMat tSpSt, nSpSt, bSpSt, ntilSpSt, btilSpSt;
+      double xibtilt, xintilt, etabtilt, etantilt;
+
+      /**
+       * \brief computes preliminaries 
+       * \param global coordinates
+       */
+      void computeprelim(const fmatvec::Vec& qG);
+      
+      /**
+       * \brief compute the initial value for computebe() 
+       * \param global coordinates
+       */
+      fmatvec::Vec computes0(const fmatvec::Vec& qG);
+      
+      /**
+       * \brief compute the angle and bending positions
+       * \param global coordinates
+       */
+      void computebe(const fmatvec::Vec& qG);
+      
+      /**
+       * \brief compute the COSY
+       */
+      void computeCOSY();
+      
+      /**
+       * \brief compute the CP, prolongation and torsion
+       * \param global coordinates
+       */
+      void computerSepstk0(const fmatvec::Vec& qG);
+      
+      /**
+       * \brief compute delta matrix for right and left beam end 
+       */
+      void computedrRdrL();
+      
+      /**
+       * \brief compute the derivative of coefficients of bending polynomials w with respect to bending coordinates 
+       */
+      void computeV();
+      
+      /**
+       * \brief compute the derivative of bE with respect to qG
+       */
+      void computebeqG();
+      
+      /**
+       * \brief compute the derivative of COSY with respect to qG
+       */
+      void computeCOSYqG();
+      
+      /**
+       * \brief compute the derivative of COSY with respect to time
+       */
+      void computeCOSYt();
+      
+      /**
+       * \brief compute the derivative of JIG with respect to time
+       * \param global velocities
+       */
+      void computeJIGt(const fmatvec::Vec& qGt);	
+  };
+
+  inline const fmatvec::SqrMat& Trafo33RCM::getJIG() const { return JIG; }
+  inline const fmatvec::SqrMat& Trafo33RCM::getJIGt() const { return JIGt; }
+  inline const fmatvec::Vec& Trafo33RCM::getpS() const { return pS; }
+  inline const fmatvec::Vec& Trafo33RCM::getpSt() const { return pSt; }
+  inline const fmatvec::Vec& Trafo33RCM::gettS() const { return tS; }
+  inline const fmatvec::Vec& Trafo33RCM::getnS() const { return nS; }
+  inline const fmatvec::Vec& Trafo33RCM::getbS() const { return bS; }
+  inline const fmatvec::RowVec& Trafo33RCM::getnSH() const { return nSH; }
+  inline const fmatvec::RowVec& Trafo33RCM::getbSH() const { return bSH; }
+  inline const fmatvec::Vec& Trafo33RCM::getntilS() const { return ntilS; }
+  inline const fmatvec::Vec& Trafo33RCM::getbtilS() const { return btilS; }
+  inline const fmatvec::RowVec& Trafo33RCM::getntilSH() const { return ntilSH; }
+  inline const fmatvec::RowVec& Trafo33RCM::getbtilSH() const { return btilSH; }
+  inline const fmatvec::Vec& Trafo33RCM::gettSt() const { return tSt; }
+  inline const fmatvec::Vec& Trafo33RCM::getnSt() const { return nSt; }
+  inline const fmatvec::Vec& Trafo33RCM::getbSt() const { return bSt; }
+  inline const fmatvec::RowVec& Trafo33RCM::gettStH() const { return tStH; }
+  inline const fmatvec::RowVec& Trafo33RCM::getnStH() const { return nStH; }
+  inline const fmatvec::RowVec& Trafo33RCM::getbStH() const { return bStH; }
+  inline const fmatvec::RowVec& Trafo33RCM::getntilStH() const { return ntilStH; }
+  inline const fmatvec::RowVec& Trafo33RCM::getbtilStH() const { return btilStH; }
+  inline const fmatvec::SqrMat& Trafo33RCM::gettSpS() const { return tSpS; }
+  inline const fmatvec::SqrMat& Trafo33RCM::getnSpS() const { return nSpS; }
+  inline const fmatvec::SqrMat& Trafo33RCM::getbSpS() const { return bSpS; }
+  inline const fmatvec::SqrMat& Trafo33RCM::getntilSpS() const { return ntilSpS; }
+  inline const fmatvec::SqrMat& Trafo33RCM::getbtilSpS() const { return btilSpS; }
+  inline const fmatvec::SqrMat& Trafo33RCM::gettSpSt() const { return tSpSt; }
+  inline const fmatvec::SqrMat& Trafo33RCM::getnSpSt() const { return nSpSt; }
+  inline const fmatvec::SqrMat& Trafo33RCM::getbSpSt() const { return bSpSt; }
+  inline const fmatvec::SqrMat& Trafo33RCM::getntilSpSt() const { return ntilSpSt; }
+  inline const fmatvec::SqrMat& Trafo33RCM::getbtilSpSt() const { return btilSpSt; }
+  inline const fmatvec::Vec& Trafo33RCM::getqIt() const { return qIt; }
+  inline const double& Trafo33RCM::getepstil() const { return epstil; }
+  inline const double& Trafo33RCM::getepstilt() const { return epstilt; }
+  inline const fmatvec::Vec& Trafo33RCM::getrS() const { return rS; }
+  inline const fmatvec::Vec& Trafo33RCM::getrSt() const { return rSt; }
+  inline const double& Trafo33RCM::getk0() const { return k0; }
+  inline const double& Trafo33RCM::getk0t() const { return k0t; }
+  inline const fmatvec::Vec& Trafo33RCM::getbe() const { return be; }
+  inline const fmatvec::Vec& Trafo33RCM::getbet() const { return bet; }
+  inline const double& Trafo33RCM::getxintil() const { return xintil; }
+  inline const double& Trafo33RCM::getxibtil() const { return xibtil; }
+  inline const double& Trafo33RCM::getetantil() const { return etantil; }
+  inline const double& Trafo33RCM::getetabtil() const { return etabtil; }
+  inline const double& Trafo33RCM::getxintilt() const { return xintilt; }
+  inline const double& Trafo33RCM::getxibtilt() const { return xibtilt; }
+  inline const double& Trafo33RCM::getetantilt() const { return etantilt; }
+  inline const double& Trafo33RCM::getetabtilt() const { return etabtilt; }
+  inline const fmatvec::SqrMat& Trafo33RCM::getV() const { return V; } 
+  /*******************************************************************/
+
+}
+
+#endif /*TRAFO33RCM_H_*/
+
