771f572bc158973e17e0cd2c8a87abfc3accabbc
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-05-02 15:37:56 +0000

    Tagged mbsimFlexibleBody version 3.1.0

---
+++ mbsimFlexibleBody/release-3.1.0-old-contact-concept/mbsimFlexibleBody/contours/nurbs_curve_1s.cc
@@ -0,0 +1,292 @@
+/* Copyright (C) 2004-2012 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: thorsten.schindler@mytum.de
+ */
+
+#include<config.h>
+
+#include "mbsimFlexibleBody/contours/nurbs_curve_1s.h"
+#include "mbsimFlexibleBody/flexible_body/flexible_body_1s_cosserat.h"
+
+#ifdef HAVE_NURBS
+using namespace PLib;
+#endif
+
+using namespace std;
+using namespace fmatvec;
+using namespace MBSim;
+
+namespace MBSimFlexibleBody {
+
+  NurbsCurve1s::NurbsCurve1s(const std::string &name) : MBSim::Contour1s(name), Elements(0), openStructure(false), L(0.), degU(0) {
+#ifndef HAVE_NURBS
+    throw MBSim::MBSimError("ERROR(NurbsCurve1s::NurbsCurve1s): External NURBS library not implemented!");
+#endif
+  }
+
+  NurbsCurve1s::~NurbsCurve1s() {
+#ifdef HAVE_NURBS
+    if(curveTranslations) {delete curveTranslations; curveTranslations=NULL;}
+    if(curveVelocities) {delete curveVelocities; curveVelocities=NULL;}
+    if(uvec) {delete uvec; uvec=NULL;}
+    if(uVec) {delete uVec; uVec=NULL;}
+#endif
+  }
+
+  void NurbsCurve1s::updateKinematicsForFrame(ContourPointData &cp, FrameFeature ff) {
+#ifdef HAVE_NURBS
+    if(ff==position || ff==position_cosy || ff==all) {
+      Point3Dd Tmppt = curveTranslations->pointAt(cp.getLagrangeParameterPosition()(0));
+      cp.getFrameOfReference().getPosition()(0) = Tmppt.x();
+      cp.getFrameOfReference().getPosition()(1) = Tmppt.y();
+      cp.getFrameOfReference().getPosition()(2) = Tmppt.z();
+    }
+
+    if(ff==velocity || ff==velocity_cosy || ff==velocities || ff==velocities_cosy || ff==all) {
+      Point3Dd Tmpv = curveVelocities->pointAt(cp.getLagrangeParameterPosition()(0));
+      cp.getFrameOfReference().getVelocity()(0) = Tmpv.x();
+      cp.getFrameOfReference().getVelocity()(1) = Tmpv.y();
+      cp.getFrameOfReference().getVelocity()(2) = Tmpv.z();
+    }
+
+    if(ff==angularVelocity || ff==velocity_cosy || ff==velocities || ff==velocities_cosy || ff==all) {
+      double uStaggered = cp.getLagrangeParameterPosition()(0); // interpolation of angular velocity starts from 0 --> \phi_{1/2} but contour starts from 0 --> r_0 therefore this difference of l0/2
+      double l0 = L/Elements;
+      if (uStaggered < l0/2.)
+        uStaggered = L - l0/2. + uStaggered;
+      else
+        uStaggered -= l0/2.;
+      Point3Dd Tmpav = curveAngularVelocities->pointAt(uStaggered);
+      cp.getFrameOfReference().getAngularAcceleration()(0) = Tmpav.x();
+      cp.getFrameOfReference().getAngularAcceleration()(1) = Tmpav.y();
+      cp.getFrameOfReference().getAngularAcceleration()(2) = Tmpav.z();
+    }
+#endif
+  }
+
+  void NurbsCurve1s::updateJacobiansForFrame(ContourPointData &cp, int j /*=0*/) {
+#ifdef HAVE_NURBS
+    cp.getFrameOfReference().getJacobianOfTranslation().resize(qSize);
+    cp.getFrameOfReference().getJacobianOfRotation().resize(qSize); // TODO open structure
+
+    double uStaggered = cp.getLagrangeParameterPosition()(0); // interpolation of Jacobian of Rotation starts from 0 --> \phi_{1/2} but Jacobian of Translation and contour starts from 0 --> r_0 therefore this difference of l0/2
+    double l0 = L/Elements;
+    if (uStaggered < l0/2.)
+      uStaggered = L - l0/2. + uStaggered;
+    else
+      uStaggered -= l0/2.;
+
+    for(int k=0; k<qSize; k++) {
+      Point3Dd TmpPtTrans = CurveJacobiansOfTranslation[k].pointAt(cp.getLagrangeParameterPosition()(0));
+      Point3Dd TmpPtRot = CurveJacobiansOfRotation[k].pointAt(uStaggered);
+
+      cp.getFrameOfReference().getJacobianOfTranslation()(0,k) = TmpPtTrans.x();
+      cp.getFrameOfReference().getJacobianOfTranslation()(1,k) = TmpPtTrans.y();
+      cp.getFrameOfReference().getJacobianOfTranslation()(2,k) = TmpPtTrans.z();
+
+      cp.getFrameOfReference().getJacobianOfRotation()(0,k) = TmpPtRot.x();
+      cp.getFrameOfReference().getJacobianOfRotation()(1,k) = TmpPtRot.y();
+      cp.getFrameOfReference().getJacobianOfRotation()(2,k) = TmpPtRot.z();
+    }
+#endif
+  }
+
+#ifdef HAVE_NURBS
+  void NurbsCurve1s::initContourFromBody(InitStage stage) {
+    if(stage==resize) {
+      degU = 3;
+      Elements = (static_cast<FlexibleBody1sCosserat*>(parent))->getNumberElements();
+      qSize = (static_cast<FlexibleBody1sCosserat*>(parent))->getNumberDOFs();
+      openStructure = (static_cast<FlexibleBody1sCosserat*>(parent))->isOpenStructure();
+      L = (static_cast<FlexibleBody1sCosserat*>(parent))->getLength();
+
+      if(openStructure) computeUVector(Elements+1);
+      else computeUVector(Elements+degU);
+
+      curveTranslations = new PlNurbsCurved;
+      curveVelocities = new PlNurbsCurved;
+      curveAngularVelocities = new PlNurbsCurved;
+      for(int i=0; i<Elements; i++) { // TODO openstructure: jacobians of Rotation different
+        jacobiansTrans.push_back(ContourPointData(i));
+        jacobiansRot.push_back(ContourPointData(i, STAGGEREDNODE)); // jacobians of rotation are on staggered grid
+      }
+      for(int i=0; i<Elements; i++) { 
+        jacobiansTrans[i].getFrameOfReference().getJacobianOfTranslation().resize();
+        jacobiansRot[i].getFrameOfReference().getJacobianOfRotation().resize();
+      }
+
+      for(int k=0; k<qSize; k++) { // TODO openstructure: jacobians of Rotation different
+        CurveJacobiansOfTranslation.push_back(PlNurbsCurved());
+        CurveJacobiansOfRotation.push_back(PlNurbsCurved());
+      }
+
+      computeCurveTranslations();
+      computeCurveVelocities();
+      computeCurveAngularVelocities();
+    }
+    else if(stage==worldFrameContourLocation) {
+      R->getOrientation() = (static_cast<FlexibleBody1sCosserat*>(parent))->getFrameOfReference()->getOrientation();
+      R->getPosition() = (static_cast<FlexibleBody1sCosserat*>(parent))->getFrameOfReference()->getPosition();
+    }
+  }
+#endif
+
+#ifdef HAVE_NURBS
+  void NurbsCurve1s::computeUVector(const int NbPts) {
+    uvec = new PLib::Vector<double>(NbPts);
+    uVec = new PLib::Vector<double>(NbPts + degU+1);
+
+    const double stepU = L / Elements;
+
+    (*uvec)[0] = 0;
+    for(int i=1;i<uvec->size();i++) {
+      (*uvec)[i] = (*uvec)[i-1] + stepU;
+    }
+
+    if(openStructure) {
+      for(int i=degU+1; i<uVec->size()-(degU+1); i++) {
+        (*uVec)[i] = (*uvec)[i-(degU-1)];
+      }
+      for(int i=0; i<degU+1; i++) {
+        (*uVec)[i] = 0.;
+        (*uVec)[uVec->size()-i-1]= L;
+      }
+    }
+    else {
+      (*uVec)[0] = (-degU) * stepU;
+      for(int i=1; i<uVec->size();i++) {
+        (*uVec)[i] = (*uVec)[i-1] + stepU;
+      }
+    }
+  }
+#endif
+
+#ifdef HAVE_NURBS 
+  void NurbsCurve1s::computeCurveTranslations() {
+    if(openStructure) {
+      PLib::Vector<HPoint3Dd> Nodelist(Elements+1);
+      for(int i=0; i<Elements+1; i++) {
+        ContourPointData cp(i);
+        static_cast<FlexibleBody1sCosserat*>(parent)->updateKinematicsForFrame(cp,position);
+        Nodelist[i] = HPoint3Dd(cp.getFrameOfReference().getPosition()(0),cp.getFrameOfReference().getPosition()(1),cp.getFrameOfReference().getPosition()(2),1);
+      }
+      curveTranslations->globalInterpH(Nodelist, *uvec, *uVec, degU);
+    }
+    else {
+      PLib::Vector<HPoint3Dd> Nodelist(Elements+degU);
+      for(int i=0; i<Elements; i++) {
+        ContourPointData cp(i);
+        static_cast<FlexibleBody1sCosserat*>(parent)->updateKinematicsForFrame(cp,position);
+        Nodelist[i] = HPoint3Dd(cp.getFrameOfReference().getPosition()(0),cp.getFrameOfReference().getPosition()(1),cp.getFrameOfReference().getPosition()(2),1);
+      }
+      for(int i=0;i<degU;i++) {
+        Nodelist[Elements+i] = Nodelist[i];
+      }
+      curveTranslations->globalInterpClosedH(Nodelist, *uvec, *uVec, degU);
+    }
+  }
+#endif
+
+#ifdef HAVE_NURBS
+  void NurbsCurve1s::computeCurveVelocities() {
+    if(openStructure) {
+      PLib::Vector<HPoint3Dd> Nodelist(Elements+1);
+      for(int i=0; i<Elements+1; i++) {
+        ContourPointData cp(i);
+        static_cast<FlexibleBody1sCosserat*>(parent)->updateKinematicsForFrame(cp,velocity);
+        Nodelist[i] = HPoint3Dd(cp.getFrameOfReference().getVelocity()(0),cp.getFrameOfReference().getVelocity()(1),cp.getFrameOfReference().getVelocity()(2),1);
+      }
+      curveVelocities->globalInterpH(Nodelist, *uvec, *uVec, degU);
+    }
+    else {
+      PLib::Vector<HPoint3Dd> Nodelist(Elements+degU);
+      for(int i=0; i<Elements; i++) {
+        ContourPointData cp(i);
+        static_cast<FlexibleBody1sCosserat*>(parent)->updateKinematicsForFrame(cp,velocity);
+        Nodelist[i] = HPoint3Dd(cp.getFrameOfReference().getVelocity()(0),cp.getFrameOfReference().getVelocity()(1),cp.getFrameOfReference().getVelocity()(2),1);
+      }
+      for(int i=0;i<degU;i++) {
+        Nodelist[Elements+i] = Nodelist[i];
+      }
+      curveVelocities->globalInterpClosedH(Nodelist, *uvec, *uVec, degU);
+    }
+  }
+#endif
+
+#ifdef HAVE_NURBS
+  void NurbsCurve1s::computeCurveAngularVelocities() {
+    if(openStructure) {
+      PLib::Vector<HPoint3Dd> Nodelist(Elements+1);
+      for(int i=0; i<Elements+1; i++) {
+        ContourPointData cp(i);
+        static_cast<FlexibleBody1sCosserat*>(parent)->updateKinematicsForFrame(cp,angularVelocity);
+        Nodelist[i] = HPoint3Dd(cp.getFrameOfReference().getAngularVelocity()(0),cp.getFrameOfReference().getAngularVelocity()(1),cp.getFrameOfReference().getAngularVelocity()(2),1);
+      }
+      curveAngularVelocities->globalInterpH(Nodelist, *uvec, *uVec, degU);
+    }
+    else {
+      PLib::Vector<HPoint3Dd> Nodelist(Elements+degU);
+      for(int i=0; i<Elements; i++) {
+        ContourPointData cp(i);
+        static_cast<FlexibleBody1sCosserat*>(parent)->updateKinematicsForFrame(cp,angularVelocity);
+        Nodelist[i] = HPoint3Dd(cp.getFrameOfReference().getAngularVelocity()(0),cp.getFrameOfReference().getAngularVelocity()(1),cp.getFrameOfReference().getAngularVelocity()(2),1);
+      }
+      for(int i=0;i<degU;i++) {
+        Nodelist[Elements+i] = Nodelist[i];
+      }
+      curveAngularVelocities->globalInterpClosedH(Nodelist, *uvec, *uVec, degU);
+    }
+  }
+#endif
+
+#ifdef HAVE_NURBS
+  void NurbsCurve1s::computeCurveJacobians() {
+    if(openStructure) { } // TODO
+    else {
+      PLib::Vector<HPoint3Dd> NodelistTrans(Elements+degU);
+      PLib::Vector<HPoint3Dd> NodelistRot(Elements+degU);
+
+      for(int i=0; i<Elements; i++) {
+        static_cast<FlexibleBody1sCosserat*>(parent)->updateJacobiansForFrame(jacobiansTrans[i]);
+        static_cast<FlexibleBody1sCosserat*>(parent)->updateJacobiansForFrame(jacobiansRot[i]); // jacobians of rotation are on staggered grid
+      }
+      for(int k=0; k<qSize; k++) {
+        for(int i=0; i<Elements; i++) {
+          NodelistTrans[i] = HPoint3Dd(jacobiansTrans[i].getFrameOfReference().getJacobianOfTranslation()(0,k),
+              jacobiansTrans[i].getFrameOfReference().getJacobianOfTranslation()(1,k),
+              jacobiansTrans[i].getFrameOfReference().getJacobianOfTranslation()(2,k),
+              1);
+
+          NodelistRot[i] = HPoint3Dd(jacobiansRot[i].getFrameOfReference().getJacobianOfRotation()(0,k),
+              jacobiansRot[i].getFrameOfReference().getJacobianOfRotation()(1,k),
+              jacobiansRot[i].getFrameOfReference().getJacobianOfRotation()(2,k),
+              1);
+        }
+        for(int i=0;i<degU;i++) {
+          NodelistTrans[Elements+i] = NodelistTrans[i];
+          NodelistRot[Elements+i] = NodelistRot[i];
+        }
+
+        CurveJacobiansOfTranslation[k].globalInterpClosedH(NodelistTrans, *uvec, *uVec, degU);
+        CurveJacobiansOfRotation[k].globalInterpClosedH(NodelistRot, *uvec, *uVec, degU);
+      }
+    }
+  }
+#endif
+
+}
+
---
+++ mbsimFlexibleBody/release-3.1.0-old-contact-concept/mbsimFlexibleBody/flexible_body/finite_elements/finite_element_1s_21_cosserat_translation.h
@@ -0,0 +1,208 @@
+/* Copyright (C) 2004-2012 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: thorsten.schindler@mytum.de
+ */
+
+#ifndef _FINITE_ELEMENT_1S_21_COSSERAT_TRANSLATION_H_
+#define _FINITE_ELEMENT_1S_21_COSSERAT_TRANSLATION_H_
+
+#include "mbsim/discretization_interface.h"
+#include "mbsim/mbsim_event.h"
+#include "mbsim/contour_pdata.h"
+#include "mbsimFlexibleBody/pointer.h"
+#include "fmatvec.h"
+
+namespace MBSimFlexibleBody {
+
+  /**
+   * \brief finite translation element for planar beam using Cosserat model
+   * \author Thomas Cebulla
+   * \author Thorsten Schindler
+   * \author Robert von Zitzewitz
+   * \date 2012-12-14 initial commit (Thomas Cebulla)
+   * \todo computeState only in FlexibleBody TODO
+   * \todo implicit integration TODO
+   *
+   * Cosserat model based on
+   * H. Lang, J. Linn, M. Arnold: Multi-body dynamics simulation of geometrically exact Cosserat rods
+   * but with 
+   *  - Kirchhoff assumption (-> less stiff)
+   *  - Cardan parametrisation (-> less problems with condition and drift for quaternion dae system)
+   *  - piecewise constant Darboux vector with evaluation according to
+   *    I. Romero: The interpolation of rotations and its application to finite element models of
+   *    geometrically exact beams
+   */
+  class FiniteElement1s21CosseratTranslation : public MBSim::DiscretizationInterface {
+    public:
+
+      /**
+       * \brief constructor
+       * \param length of finite element
+       * \param density
+       * \param cross-sectional area
+       * \param Young's modulus
+       * \param shear modulus
+       * \param first area moment of inertia
+       * \param second area moment of inertia
+       * \param torsional moment of inertia
+       * \param acceleration of gravity
+       * \param cardan object
+       */
+      FiniteElement1s21CosseratTranslation(double l0_,double rho_,double A_,double E_,double G_,double I1_,const fmatvec::Vec& g_);
+
+      /**
+       * \brief destructor
+       */
+      virtual ~FiniteElement1s21CosseratTranslation();		
+
+      /* INHERITED INTERFACE OF DISCRETIZATIONINTERFACE */ 
+      virtual const fmatvec::SymMat& getM() const;		
+      virtual const fmatvec::Vec& geth() const;
+      virtual const fmatvec::SqrMat& getdhdq() const;
+      virtual const fmatvec::SqrMat& getdhdu() const;
+      virtual int getqSize() const;
+      virtual int getuSize() const;
+
+      virtual void computeM(const fmatvec::Vec& qG);
+      virtual void computeh(const fmatvec::Vec& qG, const fmatvec::Vec& qGt);
+      virtual void computedhdz(const fmatvec::Vec& qG, const fmatvec::Vec& qGt);
+      virtual double computeKineticEnergy(const fmatvec::Vec& qG, const fmatvec::Vec& qGt);
+      virtual double computeGravitationalEnergy(const fmatvec::Vec& qG);
+      virtual double computeElasticEnergy(const fmatvec::Vec& qG);
+
+      virtual fmatvec::Vec computePosition(const fmatvec::Vec& q, const MBSim::ContourPointData &data);
+      virtual fmatvec::SqrMat computeOrientation(const fmatvec::Vec& q, const MBSim::ContourPointData &data);
+      virtual fmatvec::Vec computeVelocity(const fmatvec::Vec& q, const fmatvec::Vec& u, const MBSim::ContourPointData &data);
+      virtual fmatvec::Vec computeAngularVelocity(const fmatvec::Vec& q, const fmatvec::Vec& u, const MBSim::ContourPointData &data);
+      virtual fmatvec::Mat computeJacobianOfMotion(const fmatvec::Vec& qG, const MBSim::ContourPointData& data);
+
+      /* GETTER / SETTER */
+      void setMaterialDamping(double cEps0D_,double cEps1D_);
+      void setShearCorrectionFactors(double sigma1_, double sigma2_);
+      double getl0() const;
+
+      /**
+       * \brief compute state of finite element
+       * \param global coordinates
+       * \param global velocities
+       * \param LAGRANGIAN parameter in [0,l0]
+       */
+      const fmatvec::Vec& computeStateTranslation(const fmatvec::Vec& qG, const fmatvec::Vec& qGt, double s);
+
+      /**
+       * \brief compute JACOBIAN of contact description in global coordinates
+       * \param global coordinates
+       * \param LAGRANGIAN parameter
+       */
+      fmatvec::Mat computeJXqG(const fmatvec::Vec& qG,double x);
+
+      /**
+       * \brief initialize translational part of mass matrix
+       */
+      void initM();
+
+    private:
+      /**
+       * \brief length of finite element
+       */
+      double l0;
+
+      /**
+       * \brief density
+       */
+      double rho;
+
+      /**
+       * \brief cross sectional area 
+       */
+      double A;
+
+      /**
+       * \brief Young's modulus 
+       */
+      double E;
+
+      /**
+       * \brief shear modulus 
+       */
+      double G;
+
+      /**
+       * \brief geometrical moment of inertia 
+       */
+      double I1, I2, I0;
+
+      /**
+       * \brief gravitation
+       */
+      fmatvec::Vec g;
+
+      /**
+       * \brief strain damping
+       */
+      double cEps0D, cEps1D, cEps2D;
+
+      /**
+       * \brief shear correction factors
+       */
+      double sigma1, sigma2;
+
+      /**
+       * \brief global system description 
+       */
+      fmatvec::SymMat M;
+      fmatvec::Vec h;
+
+      /**
+       * \brief matrices for implicit integration 
+       */
+      fmatvec::SqrMat dhdq, dhdu;
+
+      /**
+       * \brief state at Lagrangian coordinate
+       */
+      fmatvec::Vec X;
+
+      /**
+       * \brief Cardan-object 
+       */
+      CardanPtr ag;
+
+      FiniteElement1s21CosseratTranslation(); // standard constructor
+      FiniteElement1s21CosseratTranslation(const FiniteElement1s21CosseratTranslation&); // copy constructor
+      FiniteElement1s21CosseratTranslation& operator=(const FiniteElement1s21CosseratTranslation&); // assignment operator
+  };
+
+  inline const fmatvec::SymMat& FiniteElement1s21CosseratTranslation::getM() const { return M; }
+  inline const fmatvec::Vec& FiniteElement1s21CosseratTranslation::geth() const { return h; }
+  inline const fmatvec::SqrMat& FiniteElement1s21CosseratTranslation::getdhdq() const { return dhdq; }
+  inline const fmatvec::SqrMat& FiniteElement1s21CosseratTranslation::getdhdu() const { return dhdu; }
+  inline int FiniteElement1s21CosseratTranslation::getqSize() const { return 5; }
+  inline int FiniteElement1s21CosseratTranslation::getuSize() const { return 5; }
+  inline void  FiniteElement1s21CosseratTranslation::computedhdz(const fmatvec::Vec& qG, const fmatvec::Vec& qGt) { throw MBSim::MBSimError("Error(FiniteElement1s21CosseratTranslation::computedhdz): Not implemented"); }
+  inline fmatvec::Vec FiniteElement1s21CosseratTranslation::computePosition(const fmatvec::Vec& q, const MBSim::ContourPointData &data) { throw MBSim::MBSimError("ERROR (FiniteElement1s21CosseratTranslation::computePosition): Not implemented!"); }
+  inline fmatvec::SqrMat FiniteElement1s21CosseratTranslation::computeOrientation(const fmatvec::Vec& q, const MBSim::ContourPointData &data) { throw MBSim::MBSimError("ERROR (FiniteElement1s21CosseratTranslation::computeOrientation): Not implemented!"); }
+  inline fmatvec::Vec FiniteElement1s21CosseratTranslation::computeVelocity(const fmatvec::Vec& q, const fmatvec::Vec& u, const MBSim::ContourPointData &data) { throw MBSim::MBSimError("ERROR (FiniteElement1s21CosseratTranslation::computeVelocity): Not implemented!"); }
+  inline fmatvec::Vec FiniteElement1s21CosseratTranslation::computeAngularVelocity(const fmatvec::Vec& q, const fmatvec::Vec& u, const MBSim::ContourPointData &data) { throw MBSim::MBSimError("ERROR (FiniteElement1s21CosseratTranslation::computeAngularVelocity): Not implemented!"); }
+  inline fmatvec::Mat FiniteElement1s21CosseratTranslation::computeJacobianOfMotion(const fmatvec::Vec& qG,const MBSim::ContourPointData& data) { return computeJXqG(qG,data.getLagrangeParameterPosition()(0)); }
+  inline void FiniteElement1s21CosseratTranslation::setShearCorrectionFactors(double sigma1_, double sigma2_) { sigma1 = sigma1_; sigma2 = sigma2_; }
+  inline double FiniteElement1s21CosseratTranslation::getl0() const { return l0; }
+  inline fmatvec::Mat FiniteElement1s21CosseratTranslation::computeJXqG(const fmatvec::Vec& qG,double x) { throw MBSim::MBSimError("ERROR (FiniteElement1s21CosseratTranslation::computeJXqG): Not implemented!"); }
+
+}
+
+#endif /* _FINITE_ELEMENT_1S_21_COSSERAT_TRANSLATION_H_ */
