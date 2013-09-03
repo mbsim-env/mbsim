3741c6b4100b7f9cc83571034d5de0505f2552b1
markus.ms.schneider@googlemail.com <markus.ms.schneider@googlemail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-09-03 22:12:01 +0000

    fix

---
+++ trunk/modules/mbsimInterface/mbsimInterface/defines.h
@@ -0,0 +1,7 @@
+#ifndef _MBSIMINTERFACE_DEFINES_H_
+#define _MBSIMINTERFACE_DEFINES_H_
+
+#define MBSIMINTERFACENS_ "http://mbsim.berlios.de/MBSimInterface"
+#define MBSIMINTERFACENS "{"MBSIMINTERFACENS_"}"
+
+#endif
