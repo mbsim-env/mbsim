234ea8564fa664888b85b1290b2a09e8fc7d7246
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-10-25 17:14:08 +0000

    Added missing files

---
+++ foerg/mbsimgui/mbsimgui/kinematic_functions_properties.cc
@@ -0,0 +1,119 @@
+/*
+    MBSimGUI - A fronted for MBSim.
+    Copyright (C) 2012 Martin Förg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <config.h>
+#include "kinematic_functions_properties.h"
+#include "kinematic_functions_widgets.h"
+#include "extended_widgets.h"
+#include "mainwindow.h"
+#include <mbxmlutils/octeval.h>
+
+using namespace std;
+using namespace MBXMLUtils;
+
+TranslationAlongFixedAxisProperty::TranslationAlongFixedAxisProperty() {
+  vector<PhysicalVariableProperty> input;
+  input.push_back(PhysicalVariableProperty(new VecProperty(3),"",MBSIMNS"axisOfTranslation"));
+  a.setProperty(new ExtPhysicalVarProperty(input));
+}
+
+TiXmlElement* TranslationAlongFixedAxisProperty::initializeUsingXML(TiXmlElement *element) {
+  a.initializeUsingXML(element);
+  return element;
+}
+
+TiXmlElement* TranslationAlongFixedAxisProperty::writeXMLFile(TiXmlNode *parent) {
+  TiXmlElement *ele0 = FunctionProperty::writeXMLFile(parent);
+  a.writeXMLFile(ele0);
+  return ele0;
+} 
+
+void TranslationAlongFixedAxisProperty::fromWidget(QWidget *widget) {
+  a.fromWidget(static_cast<TranslationAlongFixedAxisWidget*>(widget)->a);
+}
+
+void TranslationAlongFixedAxisProperty::toWidget(QWidget *widget) {
+  a.toWidget(static_cast<TranslationAlongFixedAxisWidget*>(widget)->a);
+}
+
+LinearTranslationProperty::LinearTranslationProperty(int m, int n) : b(0,false) {
+  vector<PhysicalVariableProperty> input;
+  input.push_back(PhysicalVariableProperty(new MatProperty(m,n),"",MBSIMNS"translationVectors"));
+  A.setProperty(new ExtPhysicalVarProperty(input));
+
+  input.clear();
+  input.push_back(PhysicalVariableProperty(new VecProperty(m),"",MBSIMNS"offset"));
+  b.setProperty(new ExtPhysicalVarProperty(input));
+}
+
+int LinearTranslationProperty::getArg1Size() const {
+  string str = OctEval::cast<string>(MainWindow::octEval->stringToOctValue(static_cast<const ExtPhysicalVarProperty*>(A.getProperty())->getCurrentPhysicalVariableProperty().getValue()));
+  vector<vector<string> > A = strToMat(str);
+  return A.size()?A[0].size():0;
+}
+
+TiXmlElement* LinearTranslationProperty::initializeUsingXML(TiXmlElement *element) {
+  A.initializeUsingXML(element);
+  b.initializeUsingXML(element);
+  return element;
+}
+
+TiXmlElement* LinearTranslationProperty::writeXMLFile(TiXmlNode *parent) {
+  TiXmlElement *ele0 = FunctionProperty::writeXMLFile(parent);
+  A.writeXMLFile(ele0);
+  b.writeXMLFile(ele0);
+  return ele0;
+} 
+
+void LinearTranslationProperty::fromWidget(QWidget *widget) {
+  A.fromWidget(static_cast<LinearTranslationWidget*>(widget)->A);
+  b.fromWidget(static_cast<LinearTranslationWidget*>(widget)->b);
+}
+
+void LinearTranslationProperty::toWidget(QWidget *widget) {
+  A.toWidget(static_cast<LinearTranslationWidget*>(widget)->A);
+  b.toWidget(static_cast<LinearTranslationWidget*>(widget)->b);
+}
+
+RotationAboutFixedAxisProperty::RotationAboutFixedAxisProperty() {
+  vector<PhysicalVariableProperty> input;
+  input.push_back(PhysicalVariableProperty(new VecProperty(3),"",MBSIMNS"axisOfRotation"));
+  a.setProperty(new ExtPhysicalVarProperty(input));
+}
+
+TiXmlElement* RotationAboutFixedAxisProperty::initializeUsingXML(TiXmlElement *element) {
+  a.initializeUsingXML(element);
+  return element;
+}
+
+TiXmlElement* RotationAboutFixedAxisProperty::writeXMLFile(TiXmlNode *parent) {
+  TiXmlElement *ele0 = FunctionProperty::writeXMLFile(parent);
+  a.writeXMLFile(ele0);
+  return ele0;
+} 
+
+void RotationAboutFixedAxisProperty::fromWidget(QWidget *widget) {
+  a.fromWidget(static_cast<RotationAboutFixedAxisWidget*>(widget)->a);
+}
+
+void RotationAboutFixedAxisProperty::toWidget(QWidget *widget) {
+  a.toWidget(static_cast<RotationAboutFixedAxisWidget*>(widget)->a);
+}
+
+
