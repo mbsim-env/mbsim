9b8363247463665f6166e4dae871ef0eadb8e86b
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2013-11-16 08:18:47 +0000

    Added missing file

---
+++ foerg/mbsimgui/mbsimgui/function_widget.cc
@@ -0,0 +1,73 @@
+/*
+    MBSimGUI - A fronted for MBSim.
+    Copyright (C) 2012 Martin Förg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <config.h>
+#include "function_widget.h"
+#include "function_property.h"
+#include "mainwindow.h"
+#include "objectfactory.h"
+#include <QComboBox>
+#include <QGridLayout>
+
+extern MainWindow *mw;
+
+FunctionChoiceWidget::FunctionChoiceWidget() {
+//  comboBox = new QComboBox;
+//  QStringList list;
+//  list << "translation x" << "translation y";
+//  comboBox->addItems(list);
+//  varlayout->addWidget(comboBox);
+}
+
+void FunctionChoiceWidget::fromProperty(Property *property) {
+//  comboBox->setCurrentIndex(static_cast<FunctionChoiceProperty*>(property)->getIndex());
+}
+
+void FunctionChoiceWidget::toProperty(Property *property) {
+//  static_cast<FunctionChoiceProperty*>(property)->setIndex(comboBox->currentIndex());
+}
+
+FunctionChoiceContextMenu::FunctionChoiceContextMenu(Property *property, QWidget *parent, bool removable) : PropertyContextMenu(property,parent,removable) {
+  addSeparator();
+  FunctionFactory factory;
+  QActionGroup *actionGroup = new QActionGroup(this);
+  Property *p = property->getParent();
+  for(int i=0; i<factory.size(); i++) {
+    QAction *action=new QAction(QString::fromStdString(factory.getName(i)), this);
+    action->setCheckable(true);
+    actionGroup->addAction(action);
+    addAction(action);
+    actions[action]=i;
+    if(property->getName()==factory.getName(i))
+      action->setChecked(true);
+  }
+  connect(actionGroup,SIGNAL(triggered(QAction*)),this,SLOT(setFunction(QAction*)));
+}
+
+void FunctionChoiceContextMenu::setFunction(QAction *action) {
+  int i = actions[action];
+  Property *parent = property->getParent();
+  delete property;
+
+  FunctionFactory factory;
+  FunctionProperty* body = factory.createFunction(i);
+  parent->setProperty(body);
+  mw->changePropertyItem(parent->getProperty());
+  mw->mbsimxml(1);
+}
