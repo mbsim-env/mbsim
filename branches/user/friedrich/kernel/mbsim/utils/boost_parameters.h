552beae5e52a23537c9c6f615d0899be242da683
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-02-01 07:56:36 +0000

    Added missing file.

---
+++ branches/user/friedrich/kernel/mbsim/utils/boost_parameters.h
@@ -0,0 +1,43 @@
+/* Copyright (C) 2004-2009 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: martin.o.foerg@googlemail.com
+ *          rzander@users.berlios.de
+ */
+
+#ifndef _BOOST_PARAMETERS_H_
+#define _BOOST_PARAMETERS_H_
+
+#include <boost/parameter/name.hpp>
+#include <boost/parameter/keyword.hpp>
+#include <boost/parameter/preprocessor.hpp>
+
+  BOOST_PARAMETER_NAME(scaleLength)
+  BOOST_PARAMETER_NAME(scaleSize)
+  BOOST_PARAMETER_NAME(type)
+  BOOST_PARAMETER_NAME(referencePoint)
+  BOOST_PARAMETER_NAME(diffuseColor)
+  BOOST_PARAMETER_NAME(transparency)
+  BOOST_PARAMETER_NAME(size)
+  BOOST_PARAMETER_NAME(offset)
+  BOOST_PARAMETER_NAME(length)
+  BOOST_PARAMETER_NAME(xLength)
+  BOOST_PARAMETER_NAME(yLength)
+  BOOST_PARAMETER_NAME(zLength)
+  BOOST_PARAMETER_NAME(polynomialPoints)
+  BOOST_PARAMETER_NAME(circularPoints)
+#endif
+
