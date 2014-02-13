68a69e4a5765e9226aa3543a2b87aae09becc801
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-02-13 06:09:55 +0000

    Tagged mbsimHydraulics version 3.3.1

---
+++ tags/modules/mbsimHydraulics/release-3.3.1-old-kinematics-concept/mbsimHydraulics/hydraulic_sensor.cc
@@ -0,0 +1,103 @@
+/* Copyright (C) 2004-2009 MBSim Development Team
+ *
+ * This library is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU Lesser General Public 
+ * License as published by the Free Software Foundation; either 
+ * version 2.1 of the License, or (at your option) any later version. 
+ *  
+ * This library is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * Lesser General Public License for more details. 
+ *  
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this library; if not, write to the Free Software 
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ * Contact: markus.ms.schneider@gmail.com
+ */
+
+#include <config.h>
+#include "mbsimHydraulics/hydraulic_sensor.h"
+#include "mbsimHydraulics/hnode.h"
+#include "mbsimHydraulics/hline.h"
+#include "mbsimHydraulics/environment.h"
+#include "mbsimHydraulics/obsolet_hint.h"
+#include "mbsimHydraulics/defines.h"
+
+using namespace std;
+using namespace MBXMLUtils;
+using namespace fmatvec;
+using namespace MBSim;
+
+namespace MBSimHydraulics {
+
+  MBSIM_OBJECTFACTORY_REGISTERXMLNAME(Element, FlowSensor, MBSIMHYDRAULICSNS"FlowSensor")
+
+  Vec FlowSensor::getSignal() {
+    return line->getQIn(); 
+  }
+
+  void FlowSensor::initializeUsingXML(TiXmlElement * element) {
+    Sensor::initializeUsingXML(element);
+    TiXmlElement *e;
+    e=element->FirstChildElement(MBSIMHYDRAULICSNS"hline");
+    lineString=e->Attribute("ref");
+  }
+
+  void FlowSensor::init(MBSim::InitStage stage) {
+    if (stage==MBSim::resolveXMLPath) {
+      if (lineString!="")
+        setHLine(getByPath<HLine>(process_hline_string(lineString)));
+      Sensor::init(stage);
+    }
+    else
+      Sensor::init(stage);
+  }
+
+  MBSIM_OBJECTFACTORY_REGISTERXMLNAME(Element, PressureSensor, MBSIMHYDRAULICSNS"PressureSensor")
+
+  Vec PressureSensor::getSignal() {
+    return node->getla(); 
+  }
+
+  void PressureSensor::initializeUsingXML(TiXmlElement * element) {
+    Sensor::initializeUsingXML(element);
+    TiXmlElement *e;
+    e=element->FirstChildElement(MBSIMHYDRAULICSNS"hnode");
+    nodeString=e->Attribute("ref");
+  }
+
+  void PressureSensor::init(MBSim::InitStage stage) {
+    if (stage==MBSim::resolveXMLPath) {
+      if (nodeString!="")
+        setHNode(getByPath<HNode>(nodeString));
+      Sensor::init(stage);
+    }
+    else
+      Sensor::init(stage);
+  }
+
+  MBSIM_OBJECTFACTORY_REGISTERXMLNAME(Element, TemperatureSensor, MBSIMHYDRAULICSNS"TemperatureSensor")
+  
+  void TemperatureSensor::init(MBSim::InitStage stage) {
+    if (stage==MBSim::preInit) {
+      T(0)=HydraulicEnvironment::getInstance()->getTemperature();
+      Sensor::init(stage);
+    }
+    else
+      Sensor::init(stage);
+  }
+
+  MBSIM_OBJECTFACTORY_REGISTERXMLNAME(Element, KinematicViscositySensor, MBSIMHYDRAULICSNS"KinematicViscositySensor")
+  
+  void KinematicViscositySensor::init(MBSim::InitStage stage) {
+    if (stage==MBSim::preInit) {
+      nu(0)=HydraulicEnvironment::getInstance()->getKinematicViscosity();
+      Sensor::init(stage);
+    }
+    else
+      Sensor::init(stage);
+  }
+
+}
