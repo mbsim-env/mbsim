7c436e82e90a33bc58cf95654a5d9b4a0a5a26c8
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-02-15 19:10:58 +0000

    Fix in openmbv utils

--- kernel/mbsim/utils/openmbv_utils.cc
+++ kernel/mbsim/utils/openmbv_utils.cc
@@ -131,6 +131,7 @@ namespace MBSim {
 
   OpenMBV::Cuboid* OpenMBVLine::createOpenMBV(TiXmlElement *e) {
     OpenMBV::Cuboid* object = new OpenMBV::Cuboid;
+    if(e) initializeUsingXML(e);
     initializeObject(object);
     return object;
   }
@@ -149,6 +150,7 @@ namespace MBSim {
 
   OpenMBV::Cuboid* OpenMBVPlane::createOpenMBV(TiXmlElement *e) {
     OpenMBV::Cuboid* object = new OpenMBV::Cuboid;
+    if(e) initializeUsingXML(e);
     initializeObject(object);
     return object;
   }
@@ -167,6 +169,7 @@ namespace MBSim {
 
   OpenMBV::Cuboid* OpenMBVCuboid::createOpenMBV(TiXmlElement *e) {
     OpenMBV::Cuboid* object = new OpenMBV::Cuboid;
+    if(e) initializeUsingXML(e);
     initializeObject(object);
     return object;
   }
