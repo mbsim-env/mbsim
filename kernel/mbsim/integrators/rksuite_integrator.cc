267cc4e20e302a61efd3b780762aeec5f90bdd7c
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-03-01 22:55:23 +0000

    Fixed bugs detected by valgrind.

--- kernel/mbsim/integrators/rksuite_integrator.cc
+++ kernel/mbsim/integrators/rksuite_integrator.cc
@@ -36,7 +36,7 @@ namespace MBSim {
 
   MBSIM_OBJECTFACTORY_REGISTERXMLNAME(Integrator, RKSuiteIntegrator, MBSIMINTNS"RKSuiteIntegrator")
 
-  RKSuiteIntegrator::RKSuiteIntegrator() : method(2), thres(1,INIT,1e-10), rTol(1e-6), dt0(0), ndworkarray(100000), messages(0), integrationSteps(0), t(0), tPlot(0), s0(0), time(0), z(0), zdGot(0), zMax(0), integPlot(0) {
+  RKSuiteIntegrator::RKSuiteIntegrator() : method(2), thres(1,INIT,1e-10), rTol(1e-6), dt0(0), ndworkarray(100000), messages(0), integrationSteps(0), t(0), tPlot(0), s0(0), time(0), z(0), zdGot(0), zMax(0) {
   }
 
   void RKSuiteIntegrator::preIntegrate(DynamicSystemSolver& system_) {
