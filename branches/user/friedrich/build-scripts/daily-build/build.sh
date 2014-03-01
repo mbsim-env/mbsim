2f4f4bc533e08af7f6ff16871f3a7fa6c4aba278
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-03-01 21:59:56 +0000

    run valgrind examples in seperate dir to avoid overwriting the examples from the "normal" run.

--- branches/user/friedrich/build-scripts/daily-build/build.sh
+++ branches/user/friedrich/build-scripts/daily-build/build.sh
@@ -6,9 +6,10 @@ svn update $(dirname $0)/build-scripts
 # build and run all examples
 $(dirname $0)/build-scripts/daily-build/build.py "$@" --rotate 30 -j 2 --forceBuild --sourceDir /home/user/MBSimDailyBuild --prefix /home/user/MBSimDailyBuild/local --docOutDir /media/mbsim-env/MBSimDailyBuild/doc --reportOutDir /media/mbsim-env/MBSimDailyBuild/report --url http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimDailyBuild/report --buildType "Daily Build: " --passToConfigure --enable-shared --disable-static --with-qwt-inc-prefix=/usr/include/qwt --with-boost-locale-lib=boost_locale-mt
 
-# run some examples with valgrind
+# run examples with valgrind
 cd $(dirname $0)
 SCRDIR=$(pwd)
 export PKG_CONFIG_PATH=$SCRDIR/local/lib/pkgconfig
-cd $SCRDIR/mbsim/examples
+cd $SCRDIR/mbsim/examples_valgrind
+svn update
 MBSIM_SET_MINIMAL_TEND=1 ./runexamples.py --rotate 30 -j 2 --reportOutDir /media/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report --url http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report --prefixSimulation "valgrind --error-exitcode=200 --trace-children=yes --num-callers=50" --disableCompare --disableValidate --buildType "Daily Build valgrind: "
