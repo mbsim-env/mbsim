64c02a8b1b11ea17d323a90fc4706ea68f809284
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-03-20 10:04:41 +0000

    1) Added torsional stiffness for 1D powertrain analysis 2) Added xml interface

---
+++ trunk/modules/mbsimPowertrain/mbsimPowertrain/defines.h
@@ -0,0 +1,7 @@
+#ifndef _MBSIMPOWERTRAIN_DEFINES_H_
+#define _MBSIMPOWERTRAIN_DEFINES_H_
+
+#define MBSIMPOWERTRAINNS_ "http://mbsim.berlios.de/MBSimPowertrain"
+#define MBSIMPOWERTRAINNS "{"MBSIMPOWERTRAINNS_"}"
+
+#endif
