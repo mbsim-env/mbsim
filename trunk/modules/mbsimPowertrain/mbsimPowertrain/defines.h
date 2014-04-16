64aa47f63034aed11151af53a138d4933fe92479
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-04-16 05:49:02 +0000

    Switched xml interface from tinyxml to xercesc.

--- trunk/modules/mbsimPowertrain/mbsimPowertrain/defines.h
+++ trunk/modules/mbsimPowertrain/mbsimPowertrain/defines.h
@@ -1,7 +1,4 @@
 #ifndef _MBSIMPOWERTRAIN_DEFINES_H_
 #define _MBSIMPOWERTRAIN_DEFINES_H_
 
-#define MBSIMPOWERTRAINNS_ "http://mbsim.berlios.de/MBSimPowertrain"
-#define MBSIMPOWERTRAINNS "{"MBSIMPOWERTRAINNS_"}"
-
 #endif
