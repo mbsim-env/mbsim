3383adbb157f5bca7a65dce3424f1d23ae1f5579
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-05-16 17:26:38 +0000

    Show possible memory leaks in daily valgrind runs.

--- branches/user/friedrich/build-scripts/daily-build/build.sh
+++ branches/user/friedrich/build-scripts/daily-build/build.sh
@@ -12,4 +12,4 @@ SRCDIR=$(pwd)
 export PKG_CONFIG_PATH=$SRCDIR/local/lib/pkgconfig
 cd $SRCDIR/mbsim/examples_valgrind
 svn update
-MBSIM_SET_MINIMAL_TEND=1 ./runexamples.py --rotate 30 -j 2 --reportOutDir /media/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report --url http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report --prefixSimulation "valgrind --error-exitcode=200 --trace-children=yes --num-callers=50 --gen-suppressions=all --suppressions=$SRCDIR/build-scripts/daily-build/valgrind-mbsim.supp --leak-check=full" --disableCompare --disableValidate --buildType "Daily Build valgrind: "
+MBSIM_SET_MINIMAL_TEND=1 ./runexamples.py --rotate 30 -j 2 --reportOutDir /media/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report --url http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report --prefixSimulation "valgrind --error-exitcode=200 --trace-children=yes --num-callers=150 --gen-suppressions=all --suppressions=$SRCDIR/build-scripts/daily-build/valgrind-mbsim.supp --leak-check=full --show-reachable=yes" --disableCompare --disableValidate --buildType "Daily Build valgrind: "
