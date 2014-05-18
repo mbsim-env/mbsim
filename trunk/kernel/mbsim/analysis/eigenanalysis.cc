967d6444a7fb7a5ec9d1693798adbf9855a87453
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-05-18 17:51:57 +0000

    Improved the eigenanalysis: we now compute the equilibrium position of the dynamic system and plot its movement about this equilibrium. At present all eigenmodes are plotted at once. In the future we should be able to plot only certain modes.

--- trunk/kernel/mbsim/analysis/eigenanalysis.cc
+++ trunk/kernel/mbsim/analysis/eigenanalysis.cc
@@ -21,39 +21,72 @@
 #include "eigenanalysis.h"
 #include "mbsim/dynamic_system_solver.h"
 #include "mbsim/utils/eps.h"
+#include "fmatvec/linear_algebra_complex.h"
+#include "mbsim/utils/nonlinear_algebra.h"
 #include <iostream>
 
 using namespace std;
 using namespace fmatvec;
 
+// TODO: remove the following two functions and provide an uniform concept to
+// cast between complex and double
+Vector<Ref, complex<double> > toComplex(const Vector<Ref, double> &x) {
+  Vector<Ref, complex<double> > y(x.size(),NONINIT);
+  for(int i=0; i<x.size(); i++)
+    y(i) = complex<double>(x(i),0);
+  return y;
+}
+const Vector<Ref, double> fromComplex(const Vector<Ref, complex<double> > &x) {
+  Vector<Ref, double> y(x.size(),NONINIT);
+  for(int i=0; i<x.size(); i++)
+    y(i) = x(i).real();
+  return y;
+}
+
 namespace MBSim {
 
   DynamicSystemSolver * Eigenanalysis::system = 0;
 
+  Eigenanalysis::Residuum::Residuum(DynamicSystemSolver *sys_, double t_) : sys(sys_), t(t_) {}
+
+  Vec Eigenanalysis::Residuum::operator()(const Vec &z) {
+      Vec res;
+      res = sys->zdot(z,t);
+      return res;
+    } 
+
   void Eigenanalysis::analyse(DynamicSystemSolver& system_) {
     system = &system_;
 
-    double t=t0;
+    double t=tStart;
     int zSize=system->getzSize();
     Vec z(zSize);
     if(z0.size())
       z = z0;
     else
       system->initz(z);          
+
+    Residuum f(system,t);
+    MultiDimNewtonMethod newton(&f);
+    newton.setLinearAlgebra(1);
+    Vec zEq = newton.solve(z);
+    if(newton.getInfo() != 0)
+      throw MBSimError("ERROR in Eigenanalysis: computation of equilibrium position failed!");
+
     double delta = epsroot();
-    SqrMat A(z.size());
+    SqrMat A(zEq.size());
     Vec zd, zdOld;
-    zdOld = system->zdot(z,t);
+    zdOld = system->zdot(zEq,t);
     for (int i = 0; i < z.size(); i++) {
-      double ztmp = z(i);
-      z(i) += delta;
-      zd = system->zdot(z,t);
+      double ztmp = zEq(i);
+      zEq(i) += delta;
+      zd = system->zdot(zEq,t);
       A.col(i) = (zd - zdOld) / delta;
-      z(i) = ztmp;
+      zEq(i) = ztmp;
     }
-    SquareMatrix<Ref, complex<double> > B;
+    SquareMatrix<Ref, complex<double> > V;
     Vector<Ref, complex<double> > w;
-    eigvec(A,B,w);
+    eigvec(A,V,w);
     ofstream os("Eigenanalysis.mat");
     os << "# name: lambda" << endl;
     os << "# type: complex matrix" << endl;
@@ -64,14 +97,27 @@ namespace MBSim {
     os << endl;
     os << "# name: V" << endl;
     os << "# type: complex matrix" << endl;
-    os << "# rows: " << B.rows() << endl;
-    os << "# columns: " << B.cols() << endl;
-    for (int i=0; i < B.rows(); ++i) {
-      for (int j=0; j < B.cols(); ++j) 
-        os << setw(26) << B.e(i,j);
+    os << "# rows: " << V.rows() << endl;
+    os << "# columns: " << V.cols() << endl;
+    for (int i=0; i < V.rows(); ++i) {
+      for (int j=0; j < V.cols(); ++j) 
+        os << setw(26) << V.e(i,j);
       os << endl;
     }
     os.close();
+
+    if(deltaz0.size()==0)
+      deltaz0.resize(zEq.size());
+    Vector<Ref, complex<double> > c = slvLU(V,toComplex(deltaz0));
+    Vector<Ref, complex<double> > deltaz(zEq.size(),NONINIT);
+
+    for(double t=tStart; t<tEnd+dtPlot; t+=dtPlot) {
+      deltaz.init(0);
+      for(int i=0; i<w.size(); i++)
+        deltaz += c(i)*V.col(i)*exp(w(i)*t); 
+      z = zEq + fromComplex(deltaz);
+      system->plot(z,t);
+    }
   }
 
 }
