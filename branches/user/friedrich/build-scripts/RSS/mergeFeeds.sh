22169b025bb5c111896529ec69b0693b6be1ae46
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-06-26 18:55:45 +0000

    Switched to web server of VM

--- branches/user/friedrich/build-scripts/RSS/mergeFeeds.sh
+++ branches/user/friedrich/build-scripts/RSS/mergeFeeds.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-WEBDIR=/media/mbsim-env/mergedFeeds
+WEBDIR=/var/www/html/mbsim-env/mergedFeeds
 
 # Build feeds
 /home/user/Tools/rsstool-1.0.1rc2-src/src/rsstool -r --rss -o=$WEBDIR/build2.rss.xml \
-http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimDailyBuild/report/result.rss.xml \
-http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimDailyBuild/report/result_current/runexamples_report/result.rss.xml \
-http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report/result.rss.xml \
-http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimLinux/report/result.rss.xml \
-http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimLinux/report/result_current/runexamples_report/result.rss.xml \
-http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimLinux/report_distribute/result.rss.xml \
-http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimWindows/report/result.rss.xml \
-http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimWindows/report/result_current/runexamples_report/result.rss.xml \
-http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimWindows/report_distribute/result.rss.xml
+http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimDailyBuild/report/result.rss.xml \
+http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimDailyBuild/report/result_current/runexamples_report/result.rss.xml \
+http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report/result.rss.xml \
+http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimLinux/report/result.rss.xml \
+http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimLinux/report/result_current/runexamples_report/result.rss.xml \
+http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimLinux/report_distribute/result.rss.xml \
+http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimWindows/report/result.rss.xml \
+http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimWindows/report/result_current/runexamples_report/result.rss.xml \
+http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimWindows/report_distribute/result.rss.xml
 # fix CEST to CET (CEST does not work on all clients)
 sed -re "s|<pubDate>(.*)CEST</pubDate>|<pubDate>\1CET</pubDate>|g" < $WEBDIR/build2.rss.xml > $WEBDIR/build.rss.xml
 
--- branches/user/friedrich/build-scripts/daily-build/build.sh
+++ branches/user/friedrich/build-scripts/daily-build/build.sh
@@ -4,7 +4,7 @@
 svn update $(dirname $0)/build-scripts
 
 # build and run all examples
-$(dirname $0)/build-scripts/daily-build/build.py "$@" --rotate 30 -j 2 --forceBuild --sourceDir /home/user/MBSimDailyBuild --prefix /home/user/MBSimDailyBuild/local --docOutDir /media/mbsim-env/MBSimDailyBuild/doc --reportOutDir /media/mbsim-env/MBSimDailyBuild/report --url http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimDailyBuild/report --buildType "Daily Build: " --passToConfigure --enable-shared --disable-static --with-qwt-inc-prefix=/usr/include/qwt --with-boost-locale-lib=boost_locale-mt
+$(dirname $0)/build-scripts/daily-build/build.py "$@" --rotate 30 -j 2 --forceBuild --sourceDir /home/user/MBSimDailyBuild --prefix /home/user/MBSimDailyBuild/local --docOutDir /var/www/html/mbsim-env/MBSimDailyBuild/doc --reportOutDir /var/www/html/mbsim-env/MBSimDailyBuild/report --url http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimDailyBuild/report --buildType "Daily Build: " --passToConfigure --enable-shared --disable-static --with-qwt-inc-prefix=/usr/include/qwt --with-boost-locale-lib=boost_locale-mt
 
 # run examples with valgrind
 cd $(dirname $0)
@@ -12,4 +12,4 @@ SRCDIR=$(pwd)
 export PKG_CONFIG_PATH=$SRCDIR/local/lib/pkgconfig
 cd $SRCDIR/mbsim/examples_valgrind
 svn update
-MBSIM_SET_MINIMAL_TEND=1 ./runexamples.py --rotate 30 -j 2 --reportOutDir /media/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report --url http://www4.amm.mw.tu-muenchen.de/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report --prefixSimulation "valgrind --error-exitcode=200 --trace-children=yes --num-callers=150 --gen-suppressions=all --suppressions=$SRCDIR/build-scripts/daily-build/valgrind-mbsim.supp --leak-check=full --show-reachable=yes" --disableCompare --disableValidate --buildType "Daily Build valgrind: "
+MBSIM_SET_MINIMAL_TEND=1 ./runexamples.py --rotate 30 -j 2 --reportOutDir /var/www/html/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report --url http://www4.amm.mw.tu-muenchen.de:8080/mbsim-env/MBSimDailyBuild/report/runexamples_valgrind_report --prefixSimulation "valgrind --error-exitcode=200 --trace-children=yes --num-callers=150 --gen-suppressions=all --suppressions=$SRCDIR/build-scripts/daily-build/valgrind-mbsim.supp --leak-check=full --show-reachable=yes" --disableCompare --disableValidate --buildType "Daily Build valgrind: "
