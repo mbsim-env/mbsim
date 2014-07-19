866dc9e4b2d2633ba89f21b9fdcabfdf22010045
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-07-19 20:45:40 +0000

    1) Added missing files. 2) Clean up.

---
+++ mbsimgui/mbsimgui/solver.cc
@@ -0,0 +1,118 @@
+/*
+    MBSimGUI - A fronted for MBSim.
+    Copyright (C) 2012 Martin Förg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include <config.h>
+#include "solver.h"
+#include "dynamic_system_solver.h"
+#include "objectfactory.h"
+#include "mainwindow.h"
+#include <QtGui/QMenu>
+#include <QtGui/QFileDialog>
+#include <QtGui/QHBoxLayout>
+#include <boost/bind.hpp>
+
+using namespace std;
+using namespace MBXMLUtils;
+using namespace xercesc;
+using namespace boost;
+
+namespace MBSimGUI {
+
+  extern MainWindow *mw;
+  extern bool absolutePath;
+  extern QDir mbsDir;
+
+  Solver::Solver() : name("Solver"), embed(0,false) {
+
+    embed.setProperty(new EmbedProperty(boost::bind(&Solver::getName, this)));
+  }
+
+  Solver::~Solver() {
+  }
+
+  void Solver::initializeUsingXML(DOMElement *element) {
+  }
+
+  DOMElement* Solver::writeXMLFile(DOMNode *parent) {
+    DOMDocument *doc=parent->getNodeType()==DOMNode::DOCUMENT_NODE ? static_cast<DOMDocument*>(parent) : parent->getOwnerDocument();
+    DOMElement *ele0=D(doc)->createElement(MBSIMINT%getType());
+    parent->insertBefore(ele0, NULL);
+
+    return ele0;
+  }
+
+  void Solver::initializeUsingXMLEmbed(DOMElement *element) {
+    embed.initializeUsingXML(element);
+    embed.setActive(true);
+  }
+
+  DOMElement* Solver::writeXMLFileEmbed(DOMNode *parent) {
+    DOMDocument *doc=parent->getOwnerDocument();
+    DOMElement *ele = embed.writeXMLFile(parent);
+
+    //  if(static_cast<const EmbedProperty*>(embed.getProperty())->hasParameterFile()) {
+    //    string absFileName =  static_cast<const EmbedProperty*>(embed.getProperty())->getParameterFile();
+    //    string relFileName =  mbsDir.relativeFilePath(QString::fromStdString(absFileName)).toStdString();
+    //    shared_ptr<DOMDocument> doc=MainWindow::parser->createDocument();
+    //    DOMElement *ele1 = D(doc)->createElement(PARAM%string("Parameter"));
+    //    doc->insertBefore( ele1, NULL );
+    //    for(int i=0; i<parameter.size(); i++)
+    //      parameter[i]->writeXMLFile(ele1);
+    //    string name=absolutePath?(mw->getUniqueTempDir().generic_string()+"/"+relFileName):absFileName;
+    //    QFileInfo info(QString::fromStdString(name));
+    //    QDir dir;
+    //    if(!dir.exists(info.absolutePath()))
+    //      dir.mkpath(info.absolutePath());
+    //    DOMParser::serialize(doc.get(), (name.length()>4 && name.substr(name.length()-4,4)==".xml")?name:name+".xml");
+    //  }
+    //  else {
+    //    DOMElement *ele1 = D(doc)->createElement(PARAM%string("Parameter"));
+    //    ele->insertBefore( ele1, NULL );
+    //    for(int i=0; i<parameter.size(); i++)
+    //      parameter[i]->writeXMLFile(ele1);
+    //  }
+
+    if(!static_cast<const EmbedProperty*>(embed.getProperty())->hasFile())
+      writeXMLFile(ele);
+    else {
+      string absFileName =  static_cast<const EmbedProperty*>(embed.getProperty())->getFile();
+      string relFileName =  mbsDir.relativeFilePath(QString::fromStdString(absFileName)).toStdString();
+      string name=absolutePath?(mw->getUniqueTempDir().generic_string()+"/"+relFileName):absFileName;
+      writeXMLFile(name);
+    }
+    return ele;
+  }
+
+  Solver* Solver::readXMLFile(const string &filename) {
+    MBSimObjectFactory::initialize();
+    shared_ptr<DOMDocument> doc=MainWindow::parser->parse(filename);
+    DOMElement *e=doc->getDocumentElement();
+    Solver *solver=ObjectFactory::getInstance()->createSolver(e);
+    if(solver)
+      solver->initializeUsingXML(e);
+    return solver;
+  }
+
+  void Solver::writeXMLFile(const string &name) {
+    shared_ptr<DOMDocument> doc=MainWindow::parser->createDocument();
+    writeXMLFile(doc.get());
+    DOMParser::serialize(doc.get(), (name.length()>4 && name.substr(name.length()-4,4)==".xml")?name:name+".xml");
+  }
+
+}
