37ba2e8e89dab1e1dcbdb40db9a5f69b01c82e75
ts.schindler@gmail.com <ts.schindler@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-08-29 13:21:27 +0000

    example for fmi with events

--- mbsimfmi/examples/runExamplesEvent.sh
+++ mbsimfmi/examples/runExamplesEvent.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm *.tmp
 echo "Start 1st test"
-bash ../script/pack_FMU.sh ../../../../MBSim/Install/ ../../../../MBSim/mbsim/examples/xmlflat/tippe_top/MBS.mbsimprj.flat.xml 1>/dev/null
-./fmuCheck.linux64 mbsim.fmu >out_tippe.tmp 2>&1 #1>&-
-mv mbsim.fmu mbsimT.fmu
+bash ../script/pack_FMU.sh ../../../../MBSim/Install/ ../../../../MBSim/mbsim/examples/xmlflat/bouncing_ball/MBS.mbsimprj.flat.xml 1>/dev/null
+./fmuCheck.linux64 mbsim.fmu >out_bouncing.tmp 2>&1 #1>&-
+mv mbsim.fmu mbsimB.fmu
 echo "Clean folder"
 #rm ./*.h5
 #rm ./*.xml
