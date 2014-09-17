499c7251ce1c1b5f471dab4ab420cf83fc3468c5
ts.schindler@gmail.com <ts.schindler@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-09-17 13:04:52 +0000

    trying another solver to advance to impact situations

--- examples/mechanics/basics/pendulum_with_joints/main.cc
+++ examples/mechanics/basics/pendulum_with_joints/main.cc
@@ -8,15 +8,13 @@ using namespace std;
 int main (int argc, char* argv[]) {
 
   System *sys = new System("MBS");
-
+  //sys->setConstraintSolver(DynamicSystemSolver::LinearEquations);
   sys->initialize();
 
   HETS2Integrator integrator;
-
   integrator.setStepSize(1e-4);
   integrator.setEndTime(10.1);
   integrator.setPlotStepSize(1e-2);
-
   integrator.integrate(*sys);
 
   cout << "finished"<<endl;
