fd8c82322b1c14db0ff54578ba328f350b46685e
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-09-27 18:09:14 +0000

    Fixed/Improved build system

--- friedrich/build-scripts/linux-shared-build-VM/release.sh
+++ friedrich/build-scripts/linux-shared-build-VM/release.sh
@@ -15,9 +15,9 @@ cat << EOF > $OUTDIR/report_distribute/result.rss.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
-    <title>Linux Release Build: Release Distribution</title>
+    <title>Daily Linux Release Build</title>
     <link>$URL/report_distribute/distribute.out</link>
-    <description>Linux Release Build: Result RSS feed of the MBSim Linux shared build/distribution</description>
+    <description>Daily Linux Release Build: Result RSS feed of the MBSim daily Linux build/distribution</description>
     <language>en-us</language>
     <managingEditor>friedrich.at.gc@googlemail.com (friedrich)</managingEditor>
     <atom:link href="$URL/report_distribute/result.rss.xml" rel="self" type="application/rss+xml"/>
@@ -25,7 +25,7 @@ EOF
 if [ $1 -eq 1 ]; then
 cat << EOF >> $OUTDIR/report_distribute/result.rss.xml
     <item>
-      <title>Linux Release Build: creating distribution failed</title>
+      <title>Daily Linux Release Build: creating distribution failed</title>
       <link>$URL/report_distribute/distribute.out</link>
       <guid isPermaLink="false">$URL/report_distribute/rss_id_$DATE1</guid>
       <pubDate>$DATE2</pubDate>
@@ -48,7 +48,7 @@ rss 0
 
 rm -rf $SRCDIR/local/share/mbxmlutils
 
-$SRCDIR/build-scripts/daily-build/build.py --forceBuild "$@" --rotate 30 -j 2 --sourceDir $SRCDIR --prefix $SRCDIR/local --reportOutDir $OUTDIR/report --url $URL/report --buildType "Linux Release Build: " --passToConfigure --enable-shared --disable-static --with-qwt-inc-prefix=/usr/include/qwt --with-boost-locale-lib=boost_locale-mt --passToRunexamples --disableCompare --disableValidate xmlflat/hierachical_modelling xml/hierachical_modelling xml/time_dependent_kinematics xml/hydraulics_ballcheckvalve
+$SRCDIR/build-scripts/daily-build/build.py --forceBuild "$@" --rotate 30 -j 2 --sourceDir $SRCDIR --prefix $SRCDIR/local --reportOutDir $OUTDIR/report --url $URL/report --buildType "Linux Release Build: " --passToConfigure --enable-shared --disable-static --with-qwt-inc-prefix=/usr/include/qwt --with-boost-locale-lib=boost_locale-mt --passToRunexamples --disableCompare --disableValidate
 
 RET=0
 
