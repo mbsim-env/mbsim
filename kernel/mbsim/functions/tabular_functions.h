7889bd4fad137b975151a8c0371eca5feb199695
martin.o.foerg@gmail.com <martin.o.foerg@gmail.com@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-11-09 15:03:58 +0000

    Revision of code.

--- kernel/mbsim/functions/tabular_functions.h
+++ kernel/mbsim/functions/tabular_functions.h
@@ -43,9 +43,9 @@ namespace MBSim {
           xIndexOld = 0;
           return FromVecV<Ret>::cast(trans(y.row(0)));
         }
-        else if (xVal >= x(xSize - 1)) {
-          xIndexOld = xSize - 1;
-          return FromVecV<Ret>::cast(trans(y.row(xSize - 1)));
+        else if (xVal >= x(x.size() - 1)) {
+          xIndexOld = x.size() - 1;
+          return FromVecV<Ret>::cast(trans(y.row(x.size() - 1)));
         }
         else if (xVal <= x(i)) {
           while (xVal < x(i))
@@ -70,7 +70,8 @@ namespace MBSim {
         e = MBXMLUtils::E(element)->getFirstElementChildNamed(MBSIM%"xy");
         if (e) {
           fmatvec::MatV xy = Element::getMat(e);
-          assert(xy.cols() > 1);
+          if(xy.cols() <= 1)
+            THROW_MBSIMERROR("Dimension missmatch in size of xy");
           x = xy.col(0);
           y = xy(fmatvec::Range<fmatvec::Var, fmatvec::Var>(0, xy.rows() - 1), fmatvec::Range<fmatvec::Var, fmatvec::Var>(1, xy.cols() - 1));
         }
@@ -78,17 +79,18 @@ namespace MBSim {
       void init(Element::InitStage stage) {
         Function<Ret(Arg)>::init(stage);
         if(stage==Element::preInit) {
-          for (int i = 1; i < x.size(); i++)
-            assert(x(i) > x(i - 1));
-          assert(x.size() == y.rows());
-          xSize = x.size();
+          for(int i=1; i<x.size(); i++)
+            if(x(i) <= x(i-1))
+              THROW_MBSIMERROR("Values of x must be strictly monotonic increasing!");
+          if(y.rows() != x.size())
+            THROW_MBSIMERROR("Dimension missmatch in size of x");
         }
       }
     protected:
       fmatvec::VecV x;
       fmatvec::MatV y;
     private:
-      int xIndexOld, xSize;
+      int xIndexOld;
   };
 
   template<typename Sig> class PeriodicTabularFunction; 
@@ -124,11 +126,9 @@ namespace MBSim {
   template<typename Sig> class TwoDimensionalTabularFunction; 
 
   template<typename Ret, typename Arg1, typename Arg2>
-  class TwoDimensionalTabularFunction<Ret(Arg1,Arg2)> : public Function<Ret(Arg1, Arg2)> {
+  class TwoDimensionalTabularFunction<Ret(Arg1, Arg2)> : public Function<Ret(Arg1, Arg2)> {
     public:
-      TwoDimensionalTabularFunction() :
-          xSize(0), ySize(0), x0Index(0), x1Index(0), y0Index(0), y1Index(0), func_value(1, fmatvec::INIT, 0), xy(4, fmatvec::INIT, 1), XYval(4, fmatvec::INIT, 0), XYfac(4, 4, fmatvec::INIT, 0) {
-      }
+      TwoDimensionalTabularFunction() : x0Index(0), x1Index(0), y0Index(0), y1Index(0), xy(4, fmatvec::INIT, 1), XYval(4, fmatvec::INIT, 0), XYfac(4, 4, fmatvec::INIT, 0) { }
       /* INHERITED INTERFACE OF FUNCTION2 */
       virtual void initializeUsingXML(xercesc::DOMElement *element) {
         xercesc::DOMElement * e = MBXMLUtils::E(element)->getFirstElementChildNamed(MBSIM%"xValues");
@@ -144,8 +144,8 @@ namespace MBSim {
       virtual Ret operator()(const Arg1& x_, const Arg2& y_) {
         double x = ToDouble<Arg1>::cast(x_);
         double y = ToDouble<Arg2>::cast(y_);
-        calcIndex(&x, xVec, &xSize, &x0Index, &x1Index);
-        calcIndex(&y, yVec, &ySize, &y0Index, &y1Index);
+        calcIndex(x, xVec, xVec.size(), x0Index, x1Index);
+        calcIndex(y, yVec, yVec.size(), y0Index, y1Index);
 
         xy(1) = x;
         xy(2) = y;
@@ -180,87 +180,62 @@ namespace MBSim {
       }
       /***************************************************/
       /* GETTER / SETTER */
-      void setXValues(const fmatvec::Vec &xVec_) {
-        xVec << xVec_;
-        xSize = xVec.size();
+      void setXValues(const fmatvec::Vec &xVec_) { xVec = xVec_; }
+      void setYValues(const fmatvec::Vec &yVec_) { yVec = yVec_; }
+      void setXYMat(const fmatvec::Mat &XY_) { XY = XY_; }
 
-        for (int i = 1; i < xVec.size(); i++)
-          if (xVec(i - 1) >= xVec(i))
-            THROW_MBSIMERROR("xVec must be strictly monotonic increasing!");
-        xSize = xVec.size();
-      }
-      void setYValues(const fmatvec::Vec &yVec_) {
-        yVec << yVec_;
-        ySize = yVec.size();
-
-        for (int i = 1; i < yVec.size(); i++)
-          if (yVec(i - 1) >= yVec(i))
-            THROW_MBSIMERROR("yVec must be strictly monotonic increasing!");
-      }
-      void setXYMat(const fmatvec::Mat &XY_) {
-        XY << XY_;
-
-        if (xSize == 0)
-          fmatvec::Atom::msg(fmatvec::Atom::Warn) << "It is strongly recommended to set x file first! Continuing anyway." << std::endl;
-        else if (ySize == 0)
-          fmatvec::Atom::msg(fmatvec::Atom::Warn) << "It is strongly recommended to set y file first! Continuing anyway." << std::endl;
-        else {
-          if (XY.cols() != xSize)
-            THROW_MBSIMERROR("Dimension missmatch in xSize");
-          else if (XY.rows() != ySize)
-            THROW_MBSIMERROR("Dimension missmatch in ySize");
-        }
-      }
-
-      double getxMin() {
-        return xVec(0);
-      }
-      double getxMax() {
-        return xVec(xVec.size() - 1);
-      }
-      double getyMin() {
-        return yVec(0);
-      }
-      double getyMax() {
-        return yVec(yVec.size() - 1);
-      }
-
+      double getxMin() { return xVec(0); }
+      double getxMax() { return xVec(xVec.size() - 1); }
+      double getyMin() { return yVec(0); }
+      double getyMax() { return yVec(yVec.size() - 1); }
       /***************************************************/
 
+      void init(Element::InitStage stage) {
+        Function<Ret(Arg1, Arg2)>::init(stage);
+        if(stage==Element::preInit) {
+          if (XY.cols() != xVec.size())
+            THROW_MBSIMERROR("Dimension missmatch in xSize");
+          if (XY.rows() != yVec.size())
+            THROW_MBSIMERROR("Dimension missmatch in ySize");
+          for (int i = 1; i < xVec.size(); i++)
+            if (xVec(i - 1) >= xVec(i))
+              THROW_MBSIMERROR("x values must be strictly monotonic increasing!");
+          for (int i = 1; i < yVec.size(); i++)
+            if (yVec(i - 1) >= yVec(i))
+              THROW_MBSIMERROR("y values must be strictly monotonic increasing!");
+        }
+      }
     protected:
       fmatvec::Vec xVec;
       fmatvec::Vec yVec;
       fmatvec::Mat XY;
 
-      int xSize;
-      int ySize;
       int x0Index, x1Index;
       int y0Index, y1Index;
 
-      fmatvec::Vec func_value;
       fmatvec::Vec xy;
       fmatvec::Vec XYval;
       fmatvec::Mat XYfac;
 
-      void calcIndex(const double * x, fmatvec::Vec X, int * xSize, int * xIndexMinus, int * xIndexPlus) {
-        if (*x <= X(0)) {
-          *xIndexPlus = 1;
-          *xIndexMinus = 0;
-          fmatvec::Atom::msg(fmatvec::Atom::Warn) << "TwoDimensionalTabularFunction: Value (" << *x << ") is smaller than the smallest table value(" << X(0) << ")!" << std::endl;
+      void calcIndex(double x, fmatvec::Vec X, int xSize, int &xIndexMinus, int &xIndexPlus) {
+        if (x <= X(0)) {
+          xIndexPlus = 1;
+          xIndexMinus = 0;
+          fmatvec::Atom::msg(fmatvec::Atom::Warn) << "TwoDimensionalTabularFunction: Value (" << x << ") is smaller than the smallest table value(" << X(0) << ")!" << std::endl;
         }
-        else if (*x >= X(*xSize - 1)) {
-          *xIndexPlus = *xSize - 1;
-          *xIndexMinus = *xSize - 2;
-          fmatvec::Atom::msg(fmatvec::Atom::Warn) << "TwoDimensionalTabularFunction: Value (" << *x << ") is greater than the greatest table value(" << X(*xSize - 1) << ")!" << std::endl;
+        else if (x >= X(xSize - 1)) {
+          xIndexPlus = xSize - 1;
+          xIndexMinus = xSize - 2;
+          fmatvec::Atom::msg(fmatvec::Atom::Warn) << "TwoDimensionalTabularFunction: Value (" << x << ") is greater than the greatest table value(" << X(xSize - 1) << ")!" << std::endl;
         }
         else {
-          if (*x < X(*xIndexPlus))
-            while (*x < X(*xIndexPlus - 1) && *xIndexPlus > 1)
-              (*xIndexPlus)--;
-          else if (*x > X(*xIndexPlus))
-            while (*x > X(*xIndexPlus) && *xIndexPlus < *xSize - 1)
-              (*xIndexPlus)++;
-          *xIndexMinus = *xIndexPlus - 1;
+          if (x < X(xIndexPlus))
+            while (x < X(xIndexPlus - 1) && xIndexPlus > 1)
+              xIndexPlus--;
+          else if (x > X(xIndexPlus))
+            while (x > X(xIndexPlus) && xIndexPlus < xSize - 1)
+              xIndexPlus++;
+          xIndexMinus = xIndexPlus - 1;
         }
       }
 
