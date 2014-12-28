14389659f47b5cd71ca8216ad23676478a766845
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2014-12-28 13:57:08 +0000

    Add NamespaceURI definition to mbsimxml.h. Just for use in other modules/tools.

--- trunk/mbsimxml/mbsimxml/mbsimxml.h
+++ trunk/mbsimxml/mbsimxml/mbsimxml.h
@@ -2,8 +2,11 @@
 #define _MBSIMXML_MBSIMXML_H_
 
 #include <boost/filesystem.hpp>
+#include <mbxmlutilshelper/dom.h>
 
 namespace MBSim {
+
+  const MBXMLUtils::NamespaceURI MBSIMXML("http://mbsim.berlios.de/MBSimXML");
 
   void generateMBSimXMLSchema(const boost::filesystem::path &mbsimxml_xsd, const boost::filesystem::path &MBXMLUTILSSCHEMA);
 
