acf2cfe7c78151e53300315a799e2472bd851813
friedrich.at.gc <friedrich.at.gc@c123ba7f-e748-9643-39d5-9b7b2de17e7a>
2015-01-09 20:48:41 +0000

    MBSimFMI is now stable (on the build system) -> removed the willFail flag.

--- branches/user/friedrich/build-scripts/daily-build/build.py
+++ branches/user/friedrich/build-scripts/daily-build/build.py
@@ -210,7 +210,7 @@ def main():
         pj('openmbv', 'mbxmlutils'),
         pj('mbsim', 'mbsimxml')
       ])],
-    pj('mbsim', 'mbsimfmi'): [True, set([ # depends on
+    pj('mbsim', 'mbsimfmi'): [False, set([ # depends on
         pj('mbsim', 'kernel'),
         pj('mbsim', 'mbsimxml'),
         pj('mbsim', 'modules', 'mbsimControl')
