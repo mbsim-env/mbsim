cf38bd45e8c55fa4843b99fa39072ce7d45d911e
foerg <martin.o.foerg@gmail.com>
2015-09-04 17:36:49 +0200

    Fix

--- kernel/mbsim/observers/rigid_body_group_observer.cc
+++ kernel/mbsim/observers/rigid_body_group_observer.cc
@@ -103,16 +103,16 @@ namespace MBSim {
       Vec3 &pd = macc;
       Vec3 L, Ld;
       for(unsigned int i=0; i<body.size(); i++) {
-        mpos += body[i]->getMass()*body[i]->getFrame("C")->getPosition();
-        mvel += body[i]->getMass()*body[i]->getFrame("C")->getVelocity();
-        macc += body[i]->getMass()*body[i]->getFrame("C")->getAcceleration();
+        mpos += body[i]->getMass()*body[i]->getFrame("C")->getPosition(t);
+        mvel += body[i]->getMass()*body[i]->getFrame("C")->getVelocity(t);
+        macc += body[i]->getMass()*body[i]->getFrame("C")->getAcceleration(t);
       }
       Vec3 rOS = mpos/m;
       Vec3 vS = mvel/m;
       Vec3 aS = macc/m;
-      Vec3 rOR = ref?ref->getPosition():rOS;
-      Vec3 vR = ref?ref->getVelocity():vS;
-      Vec3 aR = ref?ref->getAcceleration():aS;
+      Vec3 rOR = ref?ref->getPosition(t):rOS;
+      Vec3 vR = ref?ref->getVelocity(t):vS;
+      Vec3 aR = ref?ref->getAcceleration(t):aS;
       for(unsigned int i=0; i<body.size(); i++) {
         SqrMat3 AIK = body[i]->getFrame("C")->getOrientation();
         Vec3 rRSi = body[i]->getFrame("C")->getPosition() - rOR;
