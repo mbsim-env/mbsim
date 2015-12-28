148297bba9b61cc569b125528d9012bbac33c649
Markus Friedrich <friedrich.at.gc@googlemail.com>
2015-12-28 22:36:46 +0100

    More build fixes and first win64 tests.

--- misc/BuildService/scripts/linux64-dailyrelease.py
+++ misc/BuildService/scripts/linux64-dailyrelease.py
@@ -19,14 +19,14 @@ os.environ["CXXFLAGS"]="-g -O2"
 os.environ["CFLAGS"]="-g -O2"
 os.environ["FFLAGS"]="-g -O2"
 
-if subprocess.call([SCRIPTDIR+"/build.py", "--rotate", "14", "-j", "2", "--sourceDir", SRCDIR, "--prefix", SRCDIR+"/local", "--reportOutDir", OUTDIR+"/report", "--url", URL+"/report", "--buildType", "Linux64 Daily Release: ", "--passToConfigure", "--enable-shared", "--disable-static", "--with-qwt-inc-prefix=/usr/include/qwt", "--with-swigpath=/home/mbsim/3rdparty/swig-local-linux64/bin", "--with-javajnicflags=-I/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.37.x86_64/include -I/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.37.x86_64/include/linux", "--passToRunexamples", "--disableCompare", "--disableValidate", "xmlflat/hierachical_modelling", "xml/hierachical_modelling", "xml/time_dependent_kinematics", "xml/hydraulics_ballcheckvalve", "fmi/simple_test", "fmi/hierachical_modelling", "fmi/sphere_on_plane", "mechanics/basics/hierachical_modelling", "mechanics/basics/time_dependent_kinematics"])!=0:
+if subprocess.call([SCRIPTDIR+"/build.py", "--rotate", "14", "-j", "2", "--sourceDir", SRCDIR, "--prefix", SRCDIR+"/local", "--reportOutDir", OUTDIR+"/report", "--url", URL+"/report", "--buildType", "linux64-dailyrelease", "--passToConfigure", "--enable-shared", "--disable-static", "--with-qwt-inc-prefix=/usr/include/qwt", "--with-swigpath=/home/mbsim/3rdparty/swig-local-linux64/bin", "--with-javajnicflags=-I/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.37.x86_64/include -I/usr/lib/jvm/java-1.6.0-openjdk-1.6.0.37.x86_64/include/linux", "--passToRunexamples", "--disableCompare", "--disableValidate", "xmlflat/hierachical_modelling", "xml/hierachical_modelling", "xml/time_dependent_kinematics", "xml/hydraulics_ballcheckvalve", "fmi/simple_test", "fmi/hierachical_modelling", "fmi/sphere_on_plane", "mechanics/basics/hierachical_modelling", "mechanics/basics/time_dependent_kinematics"])!=0:
   print("linux64-dailyrelease failed.")
 
 f=open(OUTDIR+"/report_distribute/distribute.out", "w")
 if subprocess.call([SCRIPTDIR+"/linux64-dailyrelease-distribute.sh"], stderr=subprocess.STDOUT, stdout=f)!=0:
   import addBuildSystemFeed
-  addBuildSystemFeed.add("Linux64 Daily Release distribution", "Linux64 Daily Release: creating distribution failed",
-                         "See the linked web page for details", URL+"/report_distribute/distribute.out")
+  addBuildSystemFeed.add("linux64-dailyrelease-distribution", "Distribution: linux64-dailyrelease",
+                         "Unable to create the binary distribution file.", URL+"/report_distribute/distribute.out")
 f.close()
 
 shutil.copy(SRCDIR+"/dist_mbsim/mbsim-env-linux64-shared-build-xxx.tar.bz2", OUTDIR+"/download/")
