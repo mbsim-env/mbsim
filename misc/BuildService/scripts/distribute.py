8cffacab3d9e64c97e06e1546e6a33c1a1b02f05
Markus Friedrich <friedrich.at.gc@googlemail.com>
2016-01-08 23:42:04 +0100

    Imporved distribution generation

--- misc/BuildService/scripts/distribute.py
+++ misc/BuildService/scripts/distribute.py
@@ -12,6 +12,7 @@ import subprocess
 import StringIO
 import time
 import re
+import shutil
 
 args=None
 platform=None
@@ -64,12 +65,12 @@ def addDepsFor(name):
   noDepsFor=["_OpenMBV.so", "_OpenMBV.pyd"]
   return not os.path.basename(name) in noDepsFor
 
-def addToDist(name, arcname, addDepLibs=True):
+def addFileToDist(name, arcname, addDepLibs=True):
   import deplibs
   # do not add a file more than once
-  if arcname in addToDist.content:
+  if arcname in addFileToDist.content:
     return
-  addToDist.content.add(arcname)
+  addFileToDist.content.add(arcname)
   # add
 
   # dir link -> error
@@ -81,76 +82,116 @@ def addToDist(name, arcname, addDepLibs=True):
     link=os.readlink(name) # add also the reference
     if "/" in link: # but only if to the same dire
       raise RuntimeError("Only links to the same dir are supported.")
-    addToDist(os.path.dirname(name)+"/"+link, os.path.dirname(arcname)+"/"+link) # recursive call
+    addFileToDist(os.path.dirname(name)+"/"+link, os.path.dirname(arcname)+"/"+link) # recursive call
   # file -> add as file
   elif os.path.isfile(name):
-    # check for not stripped library or executable
+    # file type
     content=subprocess.check_output(["file", name])
-    if (re.search('ELF [0-9]+-bit LSB', content)!=None and re.search('not stripped', content)!=None) or \
-       (re.search('PE32\+? executable', content)!=None and re.search('stripped to external PDB', content)==None):
-      # strip file and add
+    if re.search('ELF [0-9]+-bit LSB', content)!=None or re.search('PE32\+? executable', content)!=None:
+      # binary file
+      # fix rpath (remove all absolute componentes from rpath)
       outdir=os.path.dirname(args.distFile)
       basename=os.path.basename(name)
       try:
-        subprocess.check_call(["objcopy", "--only-keep-debug", name, outdir+"/"+basename+".debug"])
-        subprocess.check_call(["objcopy", "--strip-all", name, outdir+"/"+basename])
-        subprocess.check_call(["objcopy", "--add-gnu-debuglink="+outdir+"/"+basename+".debug", outdir+"/"+basename])
-        if platform=="linux":
-          distFile.add(outdir+"/"+basename, arcname)
-          if name.startswith("/home/"): # do not add debug files of system files
-            distDebugFile.add(outdir+"/"+basename+".debug", arcname+".debug")
-        if platform=="win":
-          distFile.write(outdir+"/"+basename, arcname)
-          if name.startswith("/home/"): # do not add debug files of system files
-            distDebugFile.write(outdir+"/"+basename+".debug", arcname+".debug")
+        shutil.copy(name, outdir+"/"+basename+".rpath")
+        fnull=open(os.devnull, 'w')
+        if re.search('ELF [0-9]+-bit LSB', content)!=None:
+          try:
+            for line in subprocess.check_output(["chrpath", "-l", outdir+"/"+basename+".rpath"]).splitlines():
+              match=re.search(".* RPATH=(.*)", line)
+              if match!=None:
+                vnewrpath=[]
+                for p in match.expand("\\1").split(':'):
+                  if p[0]!='/':
+                    vnewrpath.append(p)
+                if subprocess.call(["chrpath", "-r", ":".join(vnewrpath), outdir+"/"+basename+".rpath"], stdout=fnull)!=0:
+                  raise RuntimeError("chrpath -r ... failed")
+                break
+          except subprocess.CalledProcessError as ex:
+            pass
+        # strip or not
+        if (re.search('ELF [0-9]+-bit LSB', content)!=None and re.search('not stripped', content)!=None) or \
+           (re.search('PE32\+? executable', content)!=None and re.search('stripped to external PDB', content)==None):
+          # not stripped binary file
+          try:
+            subprocess.check_call(["objcopy", "--only-keep-debug", outdir+"/"+basename+".rpath", outdir+"/"+basename+".debug"])
+            subprocess.check_call(["objcopy", "--strip-all", outdir+"/"+basename+".rpath", outdir+"/"+basename])
+            subprocess.check_call(["objcopy", "--add-gnu-debuglink="+outdir+"/"+basename+".debug", outdir+"/"+basename])
+            if platform=="linux":
+              distFile.add(outdir+"/"+basename, arcname)
+              if name.startswith("/home/"): # do not add debug files of system files
+                distDebugFile.add(outdir+"/"+basename+".debug", arcname+".debug")
+            if platform=="win":
+              distFile.write(outdir+"/"+basename, arcname)
+              if name.startswith("/home/"): # do not add debug files of system files
+                distDebugFile.write(outdir+"/"+basename+".debug", arcname+".debug")
+          finally:
+            if os.path.exists(outdir+"/"+basename): os.remove(outdir+"/"+basename)
+            if os.path.exists(outdir+"/"+basename+".debug"): os.remove(outdir+"/"+basename+".debug")
+        else:
+          # stripped binary file
+          if platform=="linux":
+            distFile.add(outdir+"/"+basename+".rpath", arcname)
+          if platform=="win":
+            distFile.write(outdir+"/"+basename+".rpath", arcname)
       finally:
-        if os.path.exists(outdir+"/"+basename): os.remove(outdir+"/"+basename)
-        if os.path.exists(outdir+"/"+basename+".debug"): os.remove(outdir+"/"+basename+".debug")
+        if os.path.exists(outdir+"/"+basename+".rpath"): os.remove(outdir+"/"+basename+".rpath")
+      # add also all dependent libs to the lib/bin dir
+      if addDepLibs and addDepsFor(name):
+        for deplib in deplibs.depLibs(name):
+          if platform=="linux":
+            subdir="lib"
+          if platform=="win":
+            subdir="bin"
+          addFileToDist(deplib, "mbsim-env/"+subdir+"/"+os.path.basename(deplib), False)
     else:
-      # add file
+      # none binary file
       if platform=="linux":
         distFile.add(name, arcname)
       if platform=="win":
         distFile.write(name, arcname)
-    # check for a excutable/libray. If so add also all dependent libs to the lib/bin dir
-    if addDepLibs and addDepsFor(name):
-      for deplib in deplibs.depLibs(name):
-        if platform=="linux":
-          subdir="lib"
-        if platform=="win":
-          subdir="bin"
-        addToDist(deplib, "mbsim-env/"+subdir+"/"+os.path.basename(deplib), False)
   # dir -> add recursively
   elif os.path.isdir(name):
     for dirpath, dirnames, filenames in os.walk(name):
       for file in filenames:
-        addToDist(dirpath+"/"+file, arcname+dirpath[len(name):]+"/"+file)
+        addFileToDist(dirpath+"/"+file, arcname+dirpath[len(name):]+"/"+file)
   else:
     raise RuntimeError("Unknown file type.")
-addToDist.content=set()
+addFileToDist.content=set()
+
+def addStrToDist(text, arcname, exeBit=False):
+  if platform=="linux":
+    tarinfo=tarfile.TarInfo(arcname)
+    tarinfo.size=len(text)
+    tarinfo.mtime=time.time()
+    if exeBit:
+      tarinfo.mode=0o755
+    distFile.addfile(tarinfo, StringIO.StringIO(text))
+  if platform=="win":
+    distFile.writestr(arcname, text)
 
 
 
 def addQtPlugins():
   print("Add Qt plugins: qsvg and qsvgicon")
-  print("")
+
   if platform=="linux":
-    addToDist("/usr/lib64/qt4/plugins/imageformats/libqsvg.so", "mbsim-env/bin/imageformats/libqsvg.so")
-    addToDist("/usr/lib64/qt4/plugins/iconengines/libqsvgicon.so", "mbsim-env/bin/iconengines/libqsvgicon.so")
+    addFileToDist("/usr/lib64/qt4/plugins/imageformats/libqsvg.so", "mbsim-env/bin/imageformats/libqsvg.so")
+    addFileToDist("/usr/lib64/qt4/plugins/iconengines/libqsvgicon.so", "mbsim-env/bin/iconengines/libqsvgicon.so")
   if platform=="win":
-    addToDist("/usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt4/plugins/imageformats/qsvg4.dll", "mbsim-env/bin/imageformats/qsvg4.dll")
-    addToDist("/usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt4/plugins/iconengines/qsvgicon4.dll", "mbsim-env/bin/iconengines/qsvgicon4.dll")
+    addFileToDist("/usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt4/plugins/imageformats/qsvg4.dll", "mbsim-env/bin/imageformats/qsvg4.dll")
+    addFileToDist("/usr/x86_64-w64-mingw32/sys-root/mingw/lib/qt4/plugins/iconengines/qsvgicon4.dll", "mbsim-env/bin/iconengines/qsvgicon4.dll")
 
 
 
 def addReadme():
   print("Add README.txt file")
-  print("")
+
   if platform=="linux":
-    note="This binary Linux64 build requires a Linux distribution with glibc >= 2.17."
+    note="This binary Linux64 build requires a Linux 64-bit operating system with glibc >= 2.17."
     scriptExt=""
   if platform=="win":
-    note="This binary build requires a 64-Bit Windows operating system."
+    note="This binary Win64 build requires a Windows 64-bit operating system."
     scriptExt=".bat"
 
   text='''Using the MBSim-Environment:
@@ -164,8 +205,12 @@ NOTE:
   is unpacked does not contain any spaces.)
 - Test the installation:
   1) Run the program <install-dir>/mbsim-env/bin/mbsim-env-test%s to check the
-     installation. This will run some MBSim examples as well as the
-     program h5plotserie, openmbv and mbsimgui.
+     installation. This will run some MBSim examples, some OpenMBVC++Interface
+     SWIG examples and the programs h5plotserie, openmbv and mbsimgui.
+  2) To Enable also the OpenMBVC++Interface SWIG python 2.7 example ensure that
+     "python" is in your PATH and set the envvar MBSIMENV_TEST_PYTHON=1.
+     To Enable also the OpenMBVC++Interface SWIG java example ensure that
+     "java" is in your PATH and set the envvar MBSIMENV_TEST_JAVA=1.
 - Try any of the programs in <install-dir>/mbsim-env/bin
 - Build your own models using XML and run it with
   <install-dir>/mbsim-env/bin/mbsimxml <mbsim-project-file.xml>
@@ -174,57 +219,70 @@ NOTE:
 
 Have fun!'''%(note, scriptExt)
 
-  if platform=="linux":
-    tarinfo=tarfile.TarInfo('mbsim-env/README.txt')
-    tarinfo.size=len(text)
-    tarinfo.mtime=time.time()
-    distFile.addfile(tarinfo, StringIO.StringIO(text))
-  if platform=="win":
-    distFile.writestr('mbsim-env/README.txt', text)
+  addStrToDist(text, 'mbsim-env/README.txt')
 
 
 
 def addMBSimEnvTest():
   print("Add test script mbsim-env-test[.bat]")
-  print("")
+
   if platform=="linux":
     text='''#! /bin/sh
 
 set -e
 INSTDIR="$(readlink -f $(dirname $0)/..)"
 
-cd $INSTDIR/examples
-
 echo "XMLFLAT_HIERACHICAL_MODELLING"
-cd xmlflat/hierachical_modelling
+cd $INSTDIR/examples/xmlflat/hierachical_modelling
 $INSTDIR/bin/mbsimflatxml MBS.mbsimprj.flat.xml
-cd ../..
 echo "DONE"
 
 echo "XML_HIERACHICAL_MODELLING"
-cd xml/hierachical_modelling
+cd $INSTDIR/examples/xml/hierachical_modelling
 $INSTDIR/bin/mbsimxml MBS.mbsimprj.xml
-cd ../..
 echo "DONE"
 
 echo "XML_TIME_DEPENDENT_KINEMATICS"
-cd xml/time_dependent_kinematics
+cd $INSTDIR/examples/xml/time_dependent_kinematics
 $INSTDIR/bin/mbsimxml MBS.mbsimprj.xml
-cd ../..
 echo "DONE"
 
 echo "XML_HYDRAULICS_BALLCHECKVALVE"
-cd xml/hydraulics_ballcheckvalve
+cd $INSTDIR/examples/xml/hydraulics_ballcheckvalve
 $INSTDIR/bin/mbsimxml MBS.mbsimprj.xml
-cd ../..
 echo "DONE"
 
+
+export OPENMBVCPPINTERFACE_PREFIX="$INSTDIR"
+
+echo "OPENMBVCPPINTERFACE_SWIG_OCTAVE"
+cd $INSTDIR/share/openmbvcppinterface/examples/swig
+$INSTDIR/bin/octave octavetest.m
+echo "DONE"
+
+if [ "_$MBSIMENV_TEST_PYTHON" == "_1" ]; then
+  echo "OPENMBVCPPINTERFACE_SWIG_PYTHON"
+  cd $INSTDIR/share/openmbvcppinterface/examples/swig
+  python pythontest.py
+  echo "DONE"
+fi
+
+if [ "_$MBSIMENV_TEST_JAVA" == "_1" ]; then
+  echo "OPENMBVCPPINTERFACE_SWIG_JAVA"
+  cd $INSTDIR/share/openmbvcppinterface/examples/swig
+  java -classpath .:$INSTDIR/bin/openmbv.jar javatest
+  echo "DONE"
+fi
+
+
 echo "STARTING H5PLOTSERIE"
-$INSTDIR/bin/h5plotserie xml/hierachical_modelling/TS.mbsim.h5
+cd $INSTDIR/examples/xml/hierachical_modelling
+$INSTDIR/bin/h5plotserie TS.mbsim.h5
 echo "DONE"
 
 echo "STARTING OPENMBV"
-$INSTDIR/bin/openmbv xml/hierachical_modelling/TS.ombv.xml
+cd $INSTDIR/examples/xml/hierachical_modelling
+$INSTDIR/bin/openmbv TS.ombv.xml
 echo "DONE"
 
 echo "STARTING MBSIMGUI"
@@ -240,43 +298,65 @@ set PWD=%CD%
 
 set INSTDIR=%~dp0..
 
-cd "%INSTDIR%\examples"
-
 echo XMLFLAT_HIERACHICAL_MODELLING
-cd xmlflat\hierachical_modelling
+cd %INSTDIR%\examples\xmlflat\hierachical_modelling
 "%INSTDIR%\bin\mbsimflatxml.exe" MBS.mbsimprj.flat.xml
 if ERRORLEVEL 1 goto end
-cd ..\..
 echo DONE
 
 echo XML_HIERACHICAL_MODELLING
-cd xml\hierachical_modelling
+cd %INSTDIR%\examples\xml\hierachical_modelling
 "%INSTDIR%\bin\mbsimxml.exe" MBS.mbsimprj.xml
 if ERRORLEVEL 1 goto end
-cd ..\..
 echo DONE
 
 echo XML_TIME_DEPENDENT_KINEMATICS
-cd xml\time_dependent_kinematics
+cd %INSTDIR%\examples\xml\time_dependent_kinematics
 "%INSTDIR%\bin\mbsimxml.exe" MBS.mbsimprj.xml
 if ERRORLEVEL 1 goto end
-cd ..\..
 echo DONE
 
 echo XML_HYDRAULICS_BALLCHECKVALVE
-cd xml/hydraulics_ballcheckvalve
+cd %INSTDIR%\examples\xml\hydraulics_ballcheckvalve
 "%INSTDIR%\bin\mbsimxml.exe" MBS.mbsimprj.xml
 if ERRORLEVEL 1 goto end
-cd ..\..
 echo DONE
 
+
+set OPENMBVCPPINTERFACE_PREFIX=%INSTDIR%
+
+echo OPENMBVCPPINTERFACE_SWIG_OCTAVE
+cd %INSTDIR%\share\openmbvcppinterface\examples\swig
+%INSTDIR%\bin\octave.exe octavetest.m
+if ERRORLEVEL 1 goto end
+echo DONE
+
+IF "%MBSIMENV_TEST_PYTHON%"=="1" (
+  echo OPENMBVCPPINTERFACE_SWIG_PYTHON
+  cd %INSTDIR%\share\openmbvcppinterface\examples\swig
+  python pythontest.py
+  if ERRORLEVEL 1 goto end
+  echo DONE
+)
+
+IF "%MBSIMENV_TEST_JAVA%"=="1" (
+  echo OPENMBVCPPINTERFACE_SWIG_JAVA
+  cd %INSTDIR%\share\openmbvcppinterface\examples\swig
+  java -classpath .;%INSTDIR%/bin/openmbv.jar javatest
+  if ERRORLEVEL 1 goto end
+  echo DONE
+)
+
+
 echo STARTING H5PLOTSERIE
-"%INSTDIR%\bin\h5plotserie.exe" xml\hierachical_modelling\TS.mbsim.h5
+cd %INSTDIR%\examples\xml\hierachical_modelling
+"%INSTDIR%\bin\h5plotserie.exe" TS.mbsim.h5
 if ERRORLEVEL 1 goto end
 echo DONE
 
 echo STARTING OPENMBV
-"%INSTDIR%\bin\openmbv.exe" xml\hierachical_modelling\TS.ombv.xml
+cd %INSTDIR%\examples\xml\hierachical_modelling
+"%INSTDIR%\bin\openmbv.exe" TS.ombv.xml
 if ERRORLEVEL 1 goto end
 echo DONE
 
@@ -290,30 +370,48 @@ echo ALL TESTS DONE
 :end
 cd "%PWD%"'''
 
-  if platform=="linux":
-    tarinfo=tarfile.TarInfo('mbsim-env/bin/mbsim-env-test')
-    tarinfo.size=len(text)
-    tarinfo.mode=0o755
-    tarinfo.mtime=time.time()
-    distFile.addfile(tarinfo, StringIO.StringIO(text))
-  if platform=="win":
-    distFile.writestr('mbsim-env/bin/mbsim-env-test.bat', text)
+  addStrToDist(text, 'mbsim-env/bin/mbsim-env-test'+('.bat' if platform=="win" else ""), True)
 
 
 
-def addOctaveShare():
+def addQtConf():
+  print("Add qt.conf file")
+
+  text='''[Paths]
+Plugins = '.'
+'''
+  addStrToDist(text, 'mbsim-env/bin/qt.conf')
+
+
+
+def addOctave():
   print("Add octave share dir")
-  print("")
+
   if platform=="linux":
-    addToDist("/usr/share/octave", "mbsim-env/share/octave")
+    addFileToDist("/usr/share/octave", "mbsim-env/share/octave")
   if platform=="win":
-    addToDist("/home/mbsim/3rdparty/octave-local-win64/share/octave", "mbsim-env/share/octave")
+    addFileToDist("/home/mbsim/3rdparty/octave-local-win64/share/octave", "mbsim-env/share/octave")
+
+  print("Add octave executable")
+
+  if platform=="linux":
+    addStrToDist('''#!/bin/sh
+INSTDIR="$(readlink -f $(dirname $0)/..)"
+export OCTAVE_HOME="$INSTDIR"
+export LD_LIBRARY_PATH="$INSTDIR/lib"
+$INSTDIR/bin/.octave-3.8.2.envvar "$@"
+''', "mbsim-env/bin/octave", True)
+    addFileToDist("/usr/bin/octave-3.8.2", "mbsim-env/bin/.octave-3.8.2.envvar")
+    addFileToDist("/usr/bin/octave-cli-3.8.2", "mbsim-env/bin/octave-cli-3.8.2")
+  if platform=="win":
+    addFileToDist("/home/mbsim/3rdparty/octave-local-win64/bin/octave-3.8.2.exe", "mbsim-env/bin/octave.exe")
+    addFileToDist("/home/mbsim/3rdparty/octave-local-win64/bin/octave-cli-3.8.2.exe", "mbsim-env/bin/octave-cli-3.8.2.exe")
 
 
 
 def addExamples():
   print("Add some examples")
-  print("")
+
   for ex in [
     "xmlflat/hierachical_modelling",
     "xml/hierachical_modelling",
@@ -321,7 +419,7 @@ def addExamples():
     "xml/hydraulics_ballcheckvalve",
   ]:
     for file in subprocess.check_output(["git", "ls-files"], cwd=args.prefix+"/../mbsim/examples/"+ex).rstrip().splitlines():
-      addToDist(args.prefix+"/../mbsim/examples/"+ex+"/"+file, "mbsim-env/examples/"+ex+"/"+file)
+      addFileToDist(args.prefix+"/../mbsim/examples/"+ex+"/"+file, "mbsim-env/examples/"+ex+"/"+file)
 
 
 
@@ -333,7 +431,7 @@ def main():
   # open archives
   print("Create binary distribution")
   print("")
-  print("")
+
   global distFile, distDebugFile
   if platform=="linux":
     distFile=tarfile.open(args.distFile+".tar.bz2", mode='w:bz2')
@@ -345,13 +443,13 @@ def main():
   # add special files
   addReadme()
   addMBSimEnvTest()
+  addQtConf()
 
   # add prefix
   print("Add prefix dir of mbsim-env")
-  print("")
-  addToDist(args.prefix, "mbsim-env")
-  # add octave share
-  addOctaveShare()
+  addFileToDist(args.prefix, "mbsim-env")
+  # add octave
+  addOctave()
 
   # add qt plugins
   addQtPlugins()
@@ -360,6 +458,7 @@ def main():
   addExamples()
 
   # close archives
+  print("")
   print("Finished")
   distFile.close()
   distDebugFile.close()
