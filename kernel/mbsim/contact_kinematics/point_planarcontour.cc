fa35f0e07b705e95aca77dedc19beba868dd2ea4
foerg <martin.o.foerg@gmail.com>
2016-06-05 07:14:05 +0200

    Revise code

--- kernel/mbsim/contact_kinematics/point_planarcontour.cc
+++ kernel/mbsim/contact_kinematics/point_planarcontour.cc
@@ -108,10 +108,10 @@ namespace MBSim {
     const Mat3x3 tOm1 = tilde(Om1);
     const Mat3x3 tOm2 = tilde(Om2);
 
-    wb(0) += (vC2-vC1).T()*N1*zetad(0)+n1.T()*tOm2*R2*zetad(1)-n1.T()*tOm1*(vC2-vC1);
+    wb(0) += (vC2-vC1).T()*N1*zetad(0)+n1.T()*(tOm2*R2*zetad(1)-tOm1*(vC2-vC1));
     if (wb.size()>1) {
       const Vec3 U1=-n1;
-      wb(1) += (vC2-vC1).T()*U1*zetad(0)+u1.T()*tOm2*R2*zetad(1)-u1.T()*tOm1*(vC2-vC1);
+      wb(1) += (vC2-vC1).T()*U1*zetad(0)+u1.T()*(tOm2*R2*zetad(1)-tOm1*(vC2-vC1));
     }
   }
 
