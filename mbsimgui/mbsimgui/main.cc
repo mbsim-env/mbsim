583084ce3027cc4c2a961da02218b5e356e5b832
Markus Friedrich <friedrich.at.gc@googlemail.com>
2016-12-31 11:42:47 +0100

    Fix

--- mbsimgui/mbsimgui/main.cc
+++ mbsimgui/mbsimgui/main.cc
@@ -29,7 +29,7 @@ using namespace MBSimGUI;
 int main(int argc, char *argv[]) {
   // environment variables
   // Disable COIN VBO per default (see --help)
-  char COIN_VBO[strlen("COIN_VBO=0")+1];
+  static char COIN_VBO[strlen("COIN_VBO=0")+1];
   if(getenv("COIN_VBO")==NULL) putenv(strcpy(COIN_VBO, "COIN_VBO=0"));
 
   QStringList arg;
