b42b83e17e643c5940c8861671736e4e5c85deb9
foerg <martin.o.foerg@gmail.com>
2017-12-23 19:29:37 +0100

    Adjust solver parameters
    Use the solver parameters from the example tippe top. These parameters
    work for all integrators.

--- examples/mechanics/contacts/sphere_on_plane/main.cc
+++ examples/mechanics/contacts/sphere_on_plane/main.cc
@@ -16,6 +16,13 @@ int main (int argc, char* argv[]) {
 
   if(type==0) { // Event driven time integration
     LSODARIntegrator integrator;
+    sys->setProjectionTolerance(1e-15);
+    sys->setGeneralizedRelativePositionTolerance(1e-6);
+    sys->setGeneralizedRelativeVelocityTolerance(1e-6);
+    sys->setGeneralizedImpulseTolerance(1e-6);
+    sys->setGeneralizedForceTolerance(1e-8);
+    sys->setGeneralizedRelativeAccelerationTolerance(1e-8);
+    integrator.setInitialStepSize(1e-8);
     integrator.setPlotOnRoot(false);
     integrator.setPlotStepSize(1e-2);
     integrator.setEndTime(1.5);
@@ -24,6 +31,8 @@ int main (int argc, char* argv[]) {
   else if(type==1) { // time stepping integration
     double dt = 1e-4;
     TimeSteppingIntegrator integrator;
+    sys->setGeneralizedImpulseTolerance(1e-2*dt);
+    sys->setGeneralizedRelativeVelocityTolerance(1e-8);
     integrator.setStepSize(dt);
     integrator.setPlotStepSize(1e-2);
     integrator.setEndTime(1.5);
@@ -31,6 +40,13 @@ int main (int argc, char* argv[]) {
   }
   else { // EVent driven using boost odeint runge-kutta dopri5
     BoostOdeintDOS<runge_kutta_dopri5<fmatvec::Vec>, BoostOdeintHelper::SystemTag> integrator;
+    sys->setProjectionTolerance(1e-15);
+    sys->setGeneralizedRelativePositionTolerance(1e-6);
+    sys->setGeneralizedRelativeVelocityTolerance(1e-6);
+    sys->setGeneralizedImpulseTolerance(1e-6);
+    sys->setGeneralizedForceTolerance(1e-8);
+    sys->setGeneralizedRelativeAccelerationTolerance(1e-8);
+    integrator.setInitialStepSize(1e-8);
     integrator.setPlotOnRoot(false);
     integrator.setPlotStepSize(1e-2);
     integrator.setEndTime(1.5);
