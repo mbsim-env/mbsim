00844eaab273094857ff9a8da0e346e806e1ae1c
Markus Friedrich <friedrich.at.gc@googlemail.com>
2017-12-23 12:48:28 +0100

    Fixed some odeint examples.
    Do not set initialStepSize to 0.
    
    Disabled fmi_cosim/sphere_on_plane.
    This example does not work do to wrong root switching of a spatial
    friction contact.
    
    Added option to run mechanics/contacts/sphere_on_plane/main.cc also with
    odeint integrator.
    With LSODARIntegrator and TimeStepping this example works. With odeint
    it also hangs due to root switching of a spaticl friction contact.
    Adapting single_contact.cc:240 solves the problem with odeint but then
    LSODAR does not work anymore.
    
    Solution currently unknown.

--- examples/mechanics/contacts/sphere_on_plane/main.cc
+++ examples/mechanics/contacts/sphere_on_plane/main.cc
@@ -1,24 +1,27 @@
 #include "system.h"
 #include <mbsim/integrators/integrators.h>
+#include <mbsim/integrators/boost_odeint_integrator.h>
+#include <boost/numeric/odeint.hpp>
 
 using namespace std;
 using namespace MBSim;
 using namespace MBSimIntegrator;
+using namespace boost::numeric::odeint;
 
 int main (int argc, char* argv[]) {
   System *sys = new System("TS");
 
   sys->initialize();
-  bool eventDriven = false;
+  int type = 1;
 
-  if(eventDriven) { // Event driven time integration
+  if(type==0) { // Event driven time integration
     LSODARIntegrator integrator;
     integrator.setPlotOnRoot(false);
     integrator.setPlotStepSize(1e-2);
     integrator.setEndTime(1.5);
     integrator.integrate(*sys);
   } 
-  else { // time stepping integration
+  else if(type==1) { // time stepping integration
     double dt = 1e-4;
     TimeSteppingIntegrator integrator;
     integrator.setStepSize(dt);
@@ -26,6 +29,14 @@ int main (int argc, char* argv[]) {
     integrator.setEndTime(1.5);
     integrator.integrate(*sys);
   }
+  else { // EVent driven using boost odeint runge-kutta dopri5
+    BoostOdeintDOS<runge_kutta_dopri5<fmatvec::Vec>, BoostOdeintHelper::SystemTag> integrator;
+    integrator.setPlotOnRoot(false);
+    integrator.setPlotStepSize(1e-2);
+    integrator.setEndTime(1.5);
+    integrator.setSystem(sys);
+    integrator.integrate();
+  } 
 
   delete sys;
 
