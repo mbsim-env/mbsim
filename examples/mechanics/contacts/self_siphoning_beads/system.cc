0718c7d31439f4f1c55fc550db93578bdd8fc6e2
foerg <martin.o.foerg@gmail.com>
2018-01-16 10:29:55 +0100

    Fix example

--- examples/mechanics/contacts/self_siphoning_beads/system.cc
+++ examples/mechanics/contacts/self_siphoning_beads/system.cc
@@ -165,7 +165,7 @@ SelfSiphoningBeats::SelfSiphoningBeats(const string &projectName, int elements, 
 
       if (ele > 1) {
         IsotropicRotationalSpringDamper * iso = new IsotropicRotationalSpringDamper("Iso" + toString(ele));
-        iso->setParameters(0, isoDamping, 0);
+        iso->setMomentFunction(new LinearSpringDamperForce(0, isoDamping));
         iso->connect(balls[ele - 1]->getFrameC(), balls[ele]->getFrameC());
         addLink(iso);
       }
