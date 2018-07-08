79477c63f5ed3a865d6f2354f2c5d6b138507b1f
Markus Friedrich <friedrich.at.gc@googlemail.com>
2018-07-08 18:15:17 +0200

    Moved misc to new mbsim-env repository "build"

--- misc/BuildService/scripts/buildPreparation/runHDF5TryRunCommandsWithWine.sh
+++
@@ -1,8 +0,0 @@
-for i in $(sed -rne "s/\<SET *\( *(.*)/\1/"p TryRunResults.cmake | grep -v __TRYRUN_OUTPUT); do
-  EXE=$(find -name "*$i.exe")
-  echo
-  echo
-  echo "START $EXE"
-  wine $EXE
-  echo "RET $?"
-done
