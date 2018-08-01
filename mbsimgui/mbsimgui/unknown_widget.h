5d5fedef25d4231a4802385db07e0a35c27f81e0
foerg <martin.o.foerg@gmail.com>
2018-08-01 19:34:12 +0200

    Enable handling of functions that are not known by the GUI

---
+++ mbsimgui/mbsimgui/unknown_widget.h
@@ -0,0 +1,42 @@
+/*
+    MBSimGUI - A fronted for MBSim.
+    Copyright (C) 2012 Martin Förg
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#ifndef _UNKNOWN_WIDGET_H_
+#define _UNKNOWN_WIDGET_H_
+
+#include "widget.h"
+
+namespace MBSimGUI {
+
+  class ExtWidget;
+
+  class UnknownWidget : public Widget {
+    public:
+      UnknownWidget();
+      virtual QString getType() const { return "UnknownWidget"; }
+      xercesc::DOMElement* initializeUsingXML(xercesc::DOMElement *element) override;
+      xercesc::DOMElement* writeXMLFile(xercesc::DOMNode *parent, xercesc::DOMNode *ref=nullptr) override;
+    protected:
+      MBXMLUtils::FQN tagName;
+      ExtWidget *editor;
+  };
+
+}
+
+#endif
