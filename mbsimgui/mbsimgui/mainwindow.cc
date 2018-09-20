9166b49514b12ccc2192abc8a8db8d7e0acdf323
Martin Förg <martin.o.foerg@gmail.com>
2018-09-20 19:17:37 +0200

    Fix help view

--- mbsimgui/mbsimgui/mainwindow.cc
+++ mbsimgui/mbsimgui/mainwindow.cc
@@ -1024,7 +1024,7 @@ namespace MBSimGUI {
       xmlHelpDialog->setWindowTitle("MBSimGUI - MBSim XML Documentation");
     }
     if(url.isEmpty())
-      xmlHelpDialog->load(QUrl(((MBXMLUtils::getInstallPath()/"share"/"mbxmlutils"/"doc").string()+"/http___www_mbsim-env_de_MBSim/index.html").c_str()));
+      xmlHelpDialog->load(QUrl(("file://"+(MBXMLUtils::getInstallPath()/"share"/"mbxmlutils"/"doc").string()+"/http___www_mbsim-env_de_MBSim/index.html").c_str()));
     else
       xmlHelpDialog->load(QUrl(url));
     xmlHelpDialog->show();
