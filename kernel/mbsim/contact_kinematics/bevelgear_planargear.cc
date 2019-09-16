7e3983b99dc036ca758dccb157a6c7212c1577bc
Martin Förg <martin.o.foerg@gmail.com>
2019-09-16 18:00:41 +0200

    Revise code

--- kernel/mbsim/contact_kinematics/bevelgear_planargear.cc
+++ kernel/mbsim/contact_kinematics/bevelgear_planargear.cc
@@ -40,27 +40,14 @@ namespace MBSim {
       bevelgear = static_cast<BevelGear*>(contour[1]);
       planargear = static_cast<PlanarGear*>(contour[0]);
     }
-    m = bevelgear->getModule();
-    al0 = bevelgear->getPressureAngle();
-    double phi0 = tan(al0)-al0;
-    double s0 = m*M_PI/2;
-    double dk[2];
-    z[0] = bevelgear->getNumberOfTeeth();
     beta[0] = bevelgear->getHelixAngle();
     beta[1] = planargear->getHelixAngle();
-    d0[0] = m*z[0]/cos(beta[0]);
-    db[0] = d0[0]*cos(al0);
-    dk[0] = d0[0]+2*m;
-    rb[0] = db[0]/2;
-    sb[0] = db[0]*(s0/d0[0]+phi0)-bevelgear->getBacklash();
-    ga[0] = sb[0]/rb[0]/2;
-    delmin[0] = -al0-ga[0];
-    delmax[0] = tan(acos(db[0]/dk[0]))-al0-ga[0];
-    delmin[1] = -m/cos(al0);
-    delmax[1] = m/cos(al0);
+    m = bevelgear->getModule()/cos(beta[0]);
+    al0 = bevelgear->getPressureAngle();
+    z[0] = bevelgear->getNumberOfTeeth();
     z[1] = planargear->getNumberOfTeeth();
-    sb[1] = m*(M_PI/2 + 2*tan(al0)) - planargear->getBacklash();
-    d0[1] = m*z[1]/cos(beta[1]);
+    d0[0] = m*z[0];
+    d0[1] = m*z[1];
   }
 
   void ContactKinematicsBevelGearPlanarGear::updateg(SingleContact &contact, int ii) {
@@ -83,20 +70,20 @@ namespace MBSim {
         double rsi = 1e10;
         int signk = (phi2 - signi*delh2)>0?-1:1;
         for(int k_=0; k_<z[1]; k_++) {
-          double rsi_ = fabs(signk*k_*2*M_PI/z[1] - signi*delh2 + phi2);
+          double rsi_ = fabs(phi2 + signk*k_*2*M_PI/z[1] - signi*delh2);
           if(rsi_<rsi) {
             rsi = rsi_;
             k = k_;
           }
         }
         v[1].push_back(signk*k);
-        double phi1corr = ((signk*k*2*M_PI/z[1] - signi*delh2 + phi2)*z[1])/z[0];
+        double phi1corr = ((phi2 + signk*k*2*M_PI/z[1] - signi*delh2)*z[1])/z[0];
 
         k = 0;
         rsi = 1e10;
         signk = (phi1 + signi*delh1 - phi1corr)>0?-1:1;
         for(int k_=0; k_<z[0]; k_++) {
-          double rsi_ = fabs(signk*k_*2*M_PI/z[0] + signi*delh1 + phi1 - phi1corr);
+          double rsi_ = fabs(phi1 + signk*k_*2*M_PI/z[0] + signi*delh1 - phi1corr);
           if(rsi_<rsi) {
             rsi = rsi_;
             k = k_;
@@ -114,37 +101,37 @@ namespace MBSim {
           k[0] = i0;
           k[1] = i1;
           if(ii==0 or not(k[0]==ksave[0][0] and k[1]==ksave[0][1])) {
-            double phi2q = -signi*delh2+k[1]*2*M_PI/z[1]+phi2;
+            double phi2q = phi2+k[1]*2*M_PI/z[1]-signi*delh2;
             double l = -signi*sin(phi2q)/cos(phi2q-beta[1])*sin(al)*d0[1]/2;
             rSP[1](0) = signi*l*sin(al)*cos(beta[1]);
             rSP[1](1) = l*cos(al);
             rSP[1](2) = signi*l*sin(al)*sin(beta[1])+d0[1]/2;
-            rSP[1] = planargear->getFrame()->getOrientation()*BasicRotAIKy(-signi*delh2+k[1]*2*M_PI/z[1])*rSP[1];
+            rSP[1] = planargear->getFrame()->getOrientation()*BasicRotAIKy(k[1]*2*M_PI/z[1]-signi*delh2)*rSP[1];
             rOP[1] = planargear->getFrame()->getPosition() + rSP[1];
 
-            double phi1q = signi*delh1+k[0]*2*M_PI/z[0]+phi1;
+            double phi1q = phi1+k[0]*2*M_PI/z[0]+signi*delh1;
             phi2q = -d0[0]/d0[1]*phi1q;
             l = signi*sin(phi2q)/cos(phi2q-beta[0])*sin(al)*d0[1]/2;
-            double a = -signi*l*sin(al)*cos(phi2q-beta[0])+d0[1]/2*sin(phi2q);
+            double a = signi*l*sin(al)*cos(phi2q-beta[0])-d0[1]/2*sin(phi2q);
             double b = l*cos(al)-d*sin(be);
             double c = signi*l*sin(al)*sin(phi2q-beta[0])+d0[1]/2*cos(phi2q)-d*cos(be);
-            rSP[0](0) = -a*cos(phi1q)+b*sin(phi1q)*cos(be)-c*sin(phi1q)*sin(be);
-            rSP[0](1) = a*sin(phi1q)+b*cos(phi1q)*cos(be)-c*cos(phi1q)*sin(be);
+            rSP[0](0) = a*cos(phi1q)+b*sin(phi1q)*cos(be)-c*sin(phi1q)*sin(be);
+            rSP[0](1) = -a*sin(phi1q)+b*cos(phi1q)*cos(be)-c*cos(phi1q)*sin(be);
             rSP[0](2) = b*sin(be)+c*cos(be);
-            rSP[0] = bevelgear->getFrame()->getOrientation()*BasicRotAIKz(signi*delh1+k[0]*2*M_PI/z[0])*rSP[0];
+            rSP[0] = bevelgear->getFrame()->getOrientation()*BasicRotAIKz(k[0]*2*M_PI/z[0]+signi*delh1)*rSP[0];
             rOP[0] = bevelgear->getFrame()->getPosition() + rSP[0];
 
             Vec3 u2(NONINIT);
             u2(0) = signi*sin(al)*cos(beta[1]);
             u2(1) = cos(al);
             u2(2) = signi*sin(al)*sin(beta[1]);
-            u2 = planargear->getFrame()->getOrientation()*BasicRotAIKy(-signi*delh2+k[1]*2*M_PI/z[1])*u2;
+            u2 = planargear->getFrame()->getOrientation()*BasicRotAIKy(k[1]*2*M_PI/z[1]-signi*delh2)*u2;
 
             Vec3 v2(NONINIT);
             v2(0) = signi*sin(beta[1]);
             v2(1) = 0;
             v2(2) = -signi*cos(beta[1]);
-            v2 = planargear->getFrame()->getOrientation()*BasicRotAIKy(-signi*delh2+k[1]*2*M_PI/z[1])*v2;
+            v2 = planargear->getFrame()->getOrientation()*BasicRotAIKy(k[1]*2*M_PI/z[1]-signi*delh2)*v2;
 
             Vec3 n2 = crossProduct(u2,v2);
 
@@ -152,8 +139,8 @@ namespace MBSim {
             if(/**g>-0.5*M_PI*d0[0]/z[0] and **/g<contact.getGeneralizedRelativePosition(false)(0)) {
               ksave[ii][0] = k[0];
               ksave[ii][1] = k[1];
-              etasave[ii][0] = -delh1-signi*(k[0]*2*M_PI/z[0]+phi1);
-              etasave[ii][1] = delh2-signi*(k[1]*2*M_PI/z[1]+phi2);
+              etasave[ii][0] = -delh1-signi*(phi1+k[0]*2*M_PI/z[0]);
+              etasave[ii][1] = delh2-signi*(phi2+k[1]*2*M_PI/z[1]);
               signisave[ii] = signi;
 
               contact.getContourFrame(iplanargear)->setPosition(rOP[1]);
