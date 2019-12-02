356cbbb88b7455f44cbf998bf8cc00d4b9cab4a8
Markus Friedrich <friedrich.at.gc@googlemail.com>
2019-12-02 22:49:11 +0100

    Fixed example build deps on mingw

--- examples/default_build.mk
+++ examples/default_build.mk
@@ -35,16 +35,18 @@ all: main$(EXEEXT)
 # FMI export target
 fmiexport: mbsimfmi_model$(SHEXT)
 
+# mingw -Wl,-Map generates some dependencies named rtr0*.o which needs to be removed
+
 # link main executable with pkg-config options from PACKAGES (runexamples.py executes always ./main)
 main$(EXEEXT): $(OBJECTS)
 	$(CXX) -Wl,-Map=$@.linkmap -o $@ $(OBJECTS) $(LDFLAGS) $(shell pkg-config --libs $(PACKAGES))
-	@sed -rne "/^LOAD /s/^LOAD (.*)$$/ \1 \\\/p" $@.linkmap > $@.d2
+	@sed -rne "/^LOAD /s/^LOAD (.*)$$/ \1 \\\/p" $@.linkmap | grep -Ev rtr[0-9]+\.o > $@.d2
 	@echo "$@: \\" > $@.d && cat $@.d2 >> $@.d && rm -f $@.linkmap $@.d2
 
 # FMI export target
 mbsimfmi_model$(SHEXT): $(OBJECTS)
 	$(CXX) -shared $(LDFLAGSRPATH) -Wl,-rpath,\$$ORIGIN,-Map=$@.linkmap -o $@ $(OBJECTS) $(LDFLAGS) $(shell pkg-config --libs $(PACKAGES))
-	@sed -rne "/^LOAD /s/^LOAD (.*)$$/ \1 \\\/p" $@.linkmap > $@.d2
+	@sed -rne "/^LOAD /s/^LOAD (.*)$$/ \1 \\\/p" $@.linkmap | grep -Ev rtr[0-9]+\.o > $@.d2
 	@echo "$@: \\" > $@.d && cat $@.d2 >> $@.d && rm -f $@.linkmap $@.d2
 
 rpath: $(OBJECTS)
