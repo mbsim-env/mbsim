9f2409f4573c9b6392b7bf4bd02059e54265dce8
Martin Förg <martin.o.foerg@gmail.com>
2021-12-21 18:04:01 +0100

    Switch to LGPL.
    In order to allow for proprietary plugins we consistently switch to LGPL for all mbsim-env projects.

--- mbsimgui/mbsimgui/mainwindow.cc
+++ mbsimgui/mbsimgui/mainwindow.cc
@@ -2,19 +2,19 @@
     MBSimGUI - A fronted for MBSim.
     Copyright (C) 2012 Martin Förg
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  This library is free software; you can redistribute it and/or 
+  modify it under the terms of the GNU Lesser General Public 
+  License as published by the Free Software Foundation; either 
+  version 2.1 of the License, or (at your option) any later version. 
+   
+  This library is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+  Lesser General Public License for more details. 
+   
+  You should have received a copy of the GNU Lesser General Public 
+  License along with this library; if not, write to the Free Software 
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 */
 
 #include <config.h>
@@ -1190,7 +1190,7 @@ namespace MBSimGUI {
            + tr("<p>See <a href=\"https://www.mbsim-env.de\">MBSim-Environment</a> for more information.</p>"
              "<h2>Copyright</h2>"
              "<p>Copyright &copy; Martin Foerg <tt>&lt;martin.o.foerg@googlemail.com&gt;</tt><p/>"
-             "<p>Licensed under the General Public License (see file COPYING).</p>"
+             "<p>Licensed under the Lesser General Public License (see file COPYING).</p>"
              "<p>This is free software; see the source for copying conditions. There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.</p>"
              "<h2>Dependencies:</h2>"
              "<pre>"
