8e94d8bcf4c9b06bbb01934c1e80657b67204b5a
Martin Förg <martin.o.foerg@gmail.com>
2022-10-01 09:55:04 +0200

    Revert last commit

--- modules/mbsimFlexibleBody/mbsimFlexibleBody/flexible_body/generic_flexible_ffr_body.cc
+++ modules/mbsimFlexibleBody/mbsimFlexibleBody/flexible_body/generic_flexible_ffr_body.cc
@@ -251,6 +251,9 @@ namespace MBSimFlexibleBody {
     for(unsigned int i=0; i<K0om.size(); i++)
       Oe1[i] += K0om[i];
 
+    if(not(De0.size()))
+      De0 <<= beta.e(0)*Me + beta.e(1)*Ke0;
+
     if(Knl1.size()) {
       Ke1.resize(Knl1.size());
       if(Knl2.size()) Ke2.resize(Knl2.size(),vector<SqrMatV>(Knl2.size()));
@@ -260,35 +263,6 @@ namespace MBSimFlexibleBody {
           Ke2[i][j] <<= 0.5*Knl2[i][j];
       }
     }
-
-    if(mDamping.size()) {
-      if(mDamping.size()!=Pdm.cols())
-	throwError(string("(GenericFlexibleFfrBody::init): size of modal damping does not match, must be ") +
-	    to_string(Pdm.cols()) + ", but is " + to_string(mDamping.size()) + ".");
-      SquareMatrix<Ref,double> V;
-      Vector<Ref,double> w;
-      eigvec(Ke0,SymMat(PPdm[0][0]+PPdm[1][1]+PPdm[2][2]),V,w);
-      Pdm <<= Pdm*V;
-      for(int i=0; i<3; i++) {
-	rPdm[i] <<= rPdm[i]*V;
-	for(int j=0; j<3; j++)
-	  PPdm[i][j] <<= V.T()*PPdm[i][j]*V;
-      }
-      Ke0 <<= JTMJ(Ke0,V);
-      for(size_t i=0; i<Phi.size(); i++)
-	Phi[i] <<= Phi[i]*V;
-      for(size_t i=0; i<Psi.size(); i++)
-	Psi[i] <<= Psi[i]*V;
-      for(size_t i=0; i<sigmahel.size(); i++)
-	sigmahel[i] <<= sigmahel[i]*V;
-      De0.resize(Pdm.cols(),INIT,0);
-      for(int i=0; i<De0.size(); i++)
-	De0(i,i) = 2*sqrt((PPdm[0][0](i,i)+PPdm[1][1](i,i)+PPdm[2][2](i,i))*Ke0(i,i))*mDamping(i);
-    }
-    else if(beta.e(0)>0 or beta.e(1)>0)
-      De0 <<= beta.e(0)*Me + beta.e(1)*Ke0;
-    else if(not(De0.size()))
-      De0.resize(Pdm.cols(),INIT,0);
   }
 
   void GenericFlexibleFfrBody::prefillMassMatrix() {
@@ -309,6 +283,30 @@ namespace MBSimFlexibleBody {
 
   void GenericFlexibleFfrBody::init(InitStage stage, const InitConfigSet &config) {
     if(stage==preInit) {
+      if(not(De0.size()) and mDamping.size()) {
+	if(mDamping.size()!=(int)Pdm.cols())
+	  throwError(string("(GenericFlexibleFfrBody::init): size of modal damping does not match, must be ") + to_string(Pdm.cols()) +
+		", but is " + to_string(mDamping.size()) + ".");
+	SquareMatrix<Ref,double> V;
+	Vector<Ref,double> w;
+	eigvec(Ke0,SymMat(PPdm[0][0]+PPdm[1][1]+PPdm[2][2]),V,w);
+	Pdm <<= Pdm*V;
+	for(int i=0; i<3; i++) {
+	   rPdm[i] <<= rPdm[i]*V;
+	  for(int j=0; j<3; j++)
+	    PPdm[i][j] <<= V.T()*PPdm[i][j]*V;
+	}
+	Ke0 <<= JTMJ(Ke0,V);
+	for(size_t i=0; i<Phi.size(); i++)
+	  Phi[i] <<= Phi[i]*V;
+	for(size_t i=0; i<Psi.size(); i++)
+	  Psi[i] <<= Psi[i]*V;
+	for(size_t i=0; i<sigmahel.size(); i++)
+	  sigmahel[i] <<= sigmahel[i]*V;
+	De0.resize(V.cols(),INIT,0);
+	for(int i=0; i<De0.size(); i++)
+	  De0(i,i) = 2*sqrt((PPdm[0][0](i,i)+PPdm[1][1](i,i)+PPdm[2][2](i,i))*Ke0(i,i))*mDamping(i);
+      }
 
       if(generalizedVelocityOfRotation==unknown)
         throwError("Generalized velocity of rotation unknown");
