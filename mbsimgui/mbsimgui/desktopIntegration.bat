97472845a551143352c2cdb5c00e44c0fb68f1eb
Markus Friedrich <friedrich@rcstengel.de>
2023-06-29 10:44:38 +0200

    call regedit only ones

--- mbsimgui/mbsimgui/desktopIntegration.bat
+++ mbsimgui/mbsimgui/desktopIntegration.bat
@@ -12,6 +12,9 @@ REM destination dirs
 set STARTMENU=%APPDATA%\Microsoft\Windows\Start Menu\Programs
 set DESKTOP=%USERPROFILE%\Desktop
 
+REM create reg file
+echo Windows Registry Editor Version 5.00 > %TEMP%\mbsim-env.reg
+
 for %%i in (%WINDOWSDIR%\mbsim-env.de.*.source.bat) do call :LOOPSTART %%i
 goto:LOOPEND
 :LOOPSTART
@@ -19,8 +22,7 @@ goto:LOOPEND
   call %1
 
   REM registry
-  REM create reg file
-  echo Windows Registry Editor Version 5.00 > %TEMP%\mbsim-env.reg
+  REM append to reg file
   echo. >> %TEMP%\mbsim-env.reg
   echo [HKEY_CURRENT_USER\Software\Classes\mbsim-env-%NAME%\shell\open\command] >> %TEMP%\mbsim-env.reg
   echo @="\"%BINDIR:\=\\%\\%EXE%\" \"%%1\"" >> %TEMP%\mbsim-env.reg
@@ -30,8 +32,6 @@ goto:LOOPEND
   echo. >> %TEMP%\mbsim-env.reg
   echo [HKEY_CURRENT_USER\Software\Classes\%EXT%\DefaultIcon] >> %TEMP%\mbsim-env.reg
   echo @="%BINDIR:\=\\%\\%EXE%" >> %TEMP%\mbsim-env.reg
-  REM apply reg file
-  regedit /s /c %TEMP%\mbsim-env.reg
 
   REM desktop
   cscript /B %WINDOWSDIR%\createLnk.vbs "%DESKTOP%\%NAME%.lnk" "%BINDIR%\%EXE%"
@@ -40,6 +40,9 @@ goto:LOOPEND
   cscript /B %WINDOWSDIR%\createLnk.vbs "%STARTMENU%\%NAME%.lnk" "%BINDIR%\%EXE%"
 goto:eof
 :LOOPEND
+
+REM apply reg file
+regedit /s /c %TEMP%\mbsim-env.reg
 
 REM update cache (if possible)
 ie4uinit.exe -show 2> nul
