86663f8585eccd10c129f9646e0ca4a6e34ae49c
Markus Friedrich <friedrich@rcstengel.de>
2023-08-28 15:37:36 +0200

    Switched to python 3.6

--- mbsimgui/mbsimgui/freedesktop.org/desktopIntegration.py
+++ mbsimgui/mbsimgui/freedesktop.org/desktopIntegration.py
@@ -7,20 +7,6 @@ try:
   import subprocess
   import platform
   import configparser
-  # MISSING: remove this is mbsimenv/buildwin64 is using python >= 3.6
-  def f(s, locals=None):
-    import re
-    formatRE=re.compile(r"{[^}]+}")
-    ret=""
-    last=0
-    for m in re.finditer(formatRE, s):
-      m.start(0)
-      m.end(0)
-      ret+=s[last:m.start(0)]+str(eval(m.group(0)[1:-1], None, locals))
-      last=m.end(0)
-    ret+=s[last:]
-    return ret
-  # MISSING END
   
   # Install all mbsim-env freedesktop.org modules
 
@@ -44,42 +30,42 @@ try:
   
   
     # source dirs
-    PREFIX=f("{os.path.dirname(__file__)}/..")
-    FREEDESKTOPORGDIR=f("{PREFIX}/share/mbsim-env/freedesktop.org")
-    BINDIR=f("{PREFIX}/bin")
+    PREFIX=f"{os.path.dirname(__file__)}/.."
+    FREEDESKTOPORGDIR=f"{PREFIX}/share/mbsim-env/freedesktop.org"
+    BINDIR=f"{PREFIX}/bin"
     HOME=os.environ["HOME"]
     
     # destination dirs
-    DATAHOME=os.environ.get("XDG_DATA_HOME", f("{HOME}/.local/share"))
-    CONFIG=os.environ.get("XDG_CONFIG_HOME", f("{HOME}/.config"))
-    DESKTOP=os.environ.get("XDG_DESKTOP_DIR", f("{HOME}/Desktop"))
-    if os.path.isfile(f("{CONFIG}/user-dirs.dirs")):
-      with open(f("{CONFIG}/user-dirs.dirs"), "rt") as ff:
+    DATAHOME=os.environ.get("XDG_DATA_HOME", f"{HOME}/.local/share")
+    CONFIG=os.environ.get("XDG_CONFIG_HOME", f"{HOME}/.config")
+    DESKTOP=os.environ.get("XDG_DESKTOP_DIR", f"{HOME}/Desktop")
+    if os.path.isfile(f"{CONFIG}/user-dirs.dirs"):
+      with open(f"{CONFIG}/user-dirs.dirs", "rt") as ff:
         for line in ff.readlines():
           if line.startswith('XDG_DESKTOP_DIR="$HOME'):
             DESKTOP=HOME+line[len('XDG_DESKTOP_DIR="$HOME'):-2]
             break
     
     # svgs
-    for F in glob.glob(f("{DATAHOME}/icons/hicolor/scalable/apps/mbsim-env.*.svg")):
+    for F in glob.glob(f"{DATAHOME}/icons/hicolor/scalable/apps/mbsim-env.*.svg"):
       os.unlink(F)
-    os.makedirs(f("{DATAHOME}/icons/hicolor/scalable/apps"), exist_ok=True)
-    for F in glob.glob(f("{FREEDESKTOPORGDIR}/*.svg")):
-      shutil.copyfile(F, f("{DATAHOME}/icons/hicolor/scalable/apps/mbsim-env.{os.path.basename(F)}"))
+    os.makedirs(f"{DATAHOME}/icons/hicolor/scalable/apps", exist_ok=True)
+    for F in glob.glob(f"{FREEDESKTOPORGDIR}/*.svg"):
+      shutil.copyfile(F, f"{DATAHOME}/icons/hicolor/scalable/apps/mbsim-env.{os.path.basename(F)}")
     
     # mimeapps
-    os.makedirs(f("{HOME}/.config"), exist_ok=True)
+    os.makedirs(f"{HOME}/.config", exist_ok=True)
     cp=configparser.ConfigParser(delimiters=('='), comment_prefixes=('#'), strict=False, interpolation=None)
     cp.optionxform=str
-    cp.read([f("{HOME}/.config/mimeapps.list")]+glob.glob(f("{FREEDESKTOPORGDIR}/mimeapps-*.list")))
-    with open(f("{HOME}/.config/mimeapps.list"), "wt") as ff:
+    cp.read([f"{HOME}/.config/mimeapps.list"]+glob.glob(f"{FREEDESKTOPORGDIR}/mimeapps-*.list"))
+    with open(f"{HOME}/.config/mimeapps.list", "wt") as ff:
       cp.write(ff, space_around_delimiters=False)
     
     # apps
-    for F in glob.glob(f("{DATAHOME}/applications/mbsim-env.*.desktop"))+glob.glob(f("{DESKTOP}/mbsim-env.*.desktop")):
+    for F in glob.glob(f"{DATAHOME}/applications/mbsim-env.*.desktop")+glob.glob(f"{DESKTOP}/mbsim-env.*.desktop"):
       os.unlink(F)
-    os.makedirs(f("{DATAHOME}/applications"), exist_ok=True)
-    for F in glob.glob(f("{FREEDESKTOPORGDIR}/mbsim-env.*.desktop")):
+    os.makedirs(f"{DATAHOME}/applications", exist_ok=True)
+    for F in glob.glob(f"{FREEDESKTOPORGDIR}/mbsim-env.*.desktop"):
       cp=configparser.ConfigParser(delimiters=('='), comment_prefixes=('#'), strict=False, interpolation=None)
       cp.optionxform=str
       cp.read(F)
@@ -88,19 +74,19 @@ try:
         de[key]=de[key].replace("@bindir@", BINDIR)
       if not copyToMenu:
         de["NoDisplay"]="true"
-      with open(f("{DATAHOME}/applications/{os.path.basename(F)}"), "wt") as ff:
+      with open(f"{DATAHOME}/applications/{os.path.basename(F)}", "wt") as ff:
         cp.write(ff, space_around_delimiters=False)
       if de.get("NoDisplay", "false")!="true" and copyToDesktop:
-        shutil.copyfile(f("{DATAHOME}/applications/{os.path.basename(F)}"), f("{DESKTOP}/{os.path.basename(F)}"))
-        os.chmod(f("{DESKTOP}/{os.path.basename(F)}"), mode=0o755)
+        shutil.copyfile(f"{DATAHOME}/applications/{os.path.basename(F)}", f"{DESKTOP}/{os.path.basename(F)}")
+        os.chmod(f"{DESKTOP}/{os.path.basename(F)}", mode=0o755)
     
     # mime types
-    for F in glob.glob(f("{DATAHOME}/mime/packages/mbsim-env.*.xml")):
+    for F in glob.glob(f"{DATAHOME}/mime/packages/mbsim-env.*.xml"):
       os.unlink(F)
-    os.makedirs(f("{DATAHOME}/mime/packages"), exist_ok=True)
-    for F in glob.glob(f("{FREEDESKTOPORGDIR}/mbsim-env.*.xml")):
-      shutil.copyfile(F, f("{DATAHOME}/mime/packages/{os.path.basename(F)}"))
-    subprocess.check_call(["update-mime-database", f("{DATAHOME}/mime")])
+    os.makedirs(f"{DATAHOME}/mime/packages", exist_ok=True)
+    for F in glob.glob(f"{FREEDESKTOPORGDIR}/mbsim-env.*.xml"):
+      shutil.copyfile(F, f"{DATAHOME}/mime/packages/{os.path.basename(F)}")
+    subprocess.check_call(["update-mime-database", f"{DATAHOME}/mime"])
 
     if addToPATH:
       def addPath(file):
@@ -113,10 +99,10 @@ try:
         if not skip:
           with open(file, "at") as f:
             print("export PATH=$PATH:"+BINDIR, file=f)
-      if os.path.isfile(f("{HOME}/.bashrc")):
-        addPath(f("{HOME}/.bashrc"))
-      if os.path.isfile(f("{HOME}/.kshrc")):
-        addPath(f("{HOME}/.kshrc"))
+      if os.path.isfile(f"{HOME}/.bashrc"):
+        addPath(f"{HOME}/.bashrc")
+      if os.path.isfile(f"{HOME}/.kshrc"):
+        addPath(f"{HOME}/.kshrc")
   
   
   
@@ -129,18 +115,18 @@ try:
   
   
     # source dirs
-    PREFIX=f("{os.path.dirname(__file__)}/..")
-    WINDOWSDIR=f("{PREFIX}/share/mbsim-env/windows")
-    BINDIR=f("{PREFIX}/bin")
-    FREEDESKTOPORGDIR=f("{PREFIX}/share/mbsim-env/freedesktop.org")
+    PREFIX=f"{os.path.dirname(__file__)}/.."
+    WINDOWSDIR=f"{PREFIX}/share/mbsim-env/windows"
+    BINDIR=f"{PREFIX}/bin"
+    FREEDESKTOPORGDIR=f"{PREFIX}/share/mbsim-env/freedesktop.org"
   
     # destination dirs
-    STARTMENU=f("{os.environ['APPDATA']}/Microsoft/Windows/Start Menu/Programs")
-    DESKTOP=f("{os.environ['USERPROFILE']}/Desktop")
+    STARTMENU=f"{os.environ['APPDATA']}/Microsoft/Windows/Start Menu/Programs"
+    DESKTOP=f"{os.environ['USERPROFILE']}/Desktop"
   
     # mimetype -> extension
     mimeTypeExt={}
-    for F in glob.glob(f("{FREEDESKTOPORGDIR}/mbsim-env.*.xml")):
+    for F in glob.glob(f"{FREEDESKTOPORGDIR}/mbsim-env.*.xml"):
       for mimeTypeEle in xml.etree.ElementTree.parse(F).getroot().findall("{http://www.freedesktop.org/standards/shared-mime-info}mime-type"):
         for globEle in mimeTypeEle.findall("{http://www.freedesktop.org/standards/shared-mime-info}glob"):
           mimeTypeExt.setdefault(mimeTypeEle.attrib["type"], []).append(globEle.attrib["pattern"])
@@ -148,18 +134,22 @@ try:
     # default associations
     cp=configparser.ConfigParser(delimiters=('='), comment_prefixes=('#'), strict=False, interpolation=None)
     cp.optionxform=str
-    cp.read(glob.glob(f("{FREEDESKTOPORGDIR}/mimeapps-*.list")))
+    cp.read(glob.glob(f"{FREEDESKTOPORGDIR}/mimeapps-*.list"))
     da=cp["Default Applications"]
   
     def createShortcut(shortcutFile, target, iconFile, workingDir=''):
       os.makedirs(os.path.dirname(shortcutFile), exist_ok=True)
-      vb=f(r'''set shell=CreateObject("WScript.Shell")
-  set sc=shell.CreateShortcut("{shortcutFile.replace("\\", "/")}")
-  sc.TargetPath="{target.replace("\\", "/")}"
-  sc.WorkingDirectory="{workingDir.replace("\\", "/")}"
-  sc.IconLocation="{iconFile.replace("\\", "/")}"
+      shortcutFileForward=shortcutFile.replace("\\", "/")
+      targetForward=target.replace("\\", "/")
+      workingDirForward=workingDir.replace("\\", "/")
+      iconFileForward=iconFile.replace("\\", "/")
+      vb=fr'''set shell=CreateObject("WScript.Shell")
+  set sc=shell.CreateShortcut("{shortcutFileForward}")
+  sc.TargetPath="{targetForward}"
+  sc.WorkingDirectory="{workingDirForward}"
+  sc.IconLocation="{iconFileForward}"
   sc.Save
-  ''', locals())
+  '''
       fd, path=tempfile.mkstemp('.vbs')
       try:
         with os.fdopen(fd, 'w') as ff:
@@ -183,7 +173,7 @@ try:
   
     # registry
     with winreg.CreateKey(winreg.HKEY_CURRENT_USER, r'Software\Classes') as classes:
-      for F in glob.glob(f("{FREEDESKTOPORGDIR}/mbsim-env.*.desktop")):
+      for F in glob.glob(f"{FREEDESKTOPORGDIR}/mbsim-env.*.desktop"):
         progID=os.path.splitext(os.path.basename(F))[0]
         cp=configparser.ConfigParser(delimiters=('='), comment_prefixes=('#'), strict=False, interpolation=None)
         cp.optionxform=str
@@ -217,13 +207,13 @@ try:
             winreg.SetValue(classes, progID+r'\FriendlyTypeName', winreg.REG_SZ, de["Name"])
             winreg.SetValue(classes, progID+r'\Infotipp', winreg.REG_SZ, de["Comment"])
             winreg.SetValue(classes, progID+r'\DefaultIcon', winreg.REG_SZ, de["IconWindows"])
-            winreg.SetValue(classes, progID+r'\shell\open', winreg.REG_SZ, f(r'Open with {de["Name"]}'))
+            winreg.SetValue(classes, progID+r'\shell\open', winreg.REG_SZ, fr'Open with {de["Name"]}')
             winreg.SetValue(classes, progID+r'\shell\open\command', winreg.REG_SZ, de["ExecWindows"])
             for section in cp:
               if section.startswith("Windows-shell-"):
                 ws=cp[section]
-                winreg.SetValue(classes, progID+f(r'\shell\{ws["ID"]}'), winreg.REG_SZ, ws["Name"])
-                winreg.SetValue(classes, progID+f(r'\shell\{ws["ID"]}\command'), winreg.REG_SZ, ws["Exec"])
+                winreg.SetValue(classes, progID+fr'\shell\{ws["ID"]}', winreg.REG_SZ, ws["Name"])
+                winreg.SetValue(classes, progID+fr'\shell\{ws["ID"]}\command', winreg.REG_SZ, ws["Exec"])
   
             execFile=de["Exec"].split(" ")[0]
             if execFile[0]=='"' and execFile[-1]=='"' or execFile[0]=="'" and execFile[-1]=="'":
@@ -231,22 +221,22 @@ try:
   
             # desktop
             if copyToDesktop:
-              createShortcut(f(r'{DESKTOP}/{de["Name"]}.lnk'), execFile, execFile+".exe,0")
+              createShortcut(fr'{DESKTOP}/{de["Name"]}.lnk', execFile, execFile+".exe,0")
   
             # start menu
             if copyToMenu:
-              createShortcut(f(r'{STARTMENU}/{de["Name"]}.lnk'), execFile, execFile+".exe,0")
+              createShortcut(fr'{STARTMENU}/{de["Name"]}.lnk', execFile, execFile+".exe,0")
         if de["Type"]=="Link":
           url=de["URL"]
           if url.startswith("file://"):
             url=url[7:]
           # desktop
           if copyToDesktop:
-            createShortcut(f(r'{DESKTOP}/{de["Name"]}.lnk'), url, de["IconWindows"])
+            createShortcut(fr'{DESKTOP}/{de["Name"]}.lnk', url, de["IconWindows"])
   
           # start menu
           if copyToMenu:
-            createShortcut(f(r'{STARTMENU}/{de["Name"]}.lnk'), url, de["IconWindows"])
+            createShortcut(fr'{STARTMENU}/{de["Name"]}.lnk', url, de["IconWindows"])
 
     if addToPATH:
       with winreg.CreateKey(winreg.HKEY_CURRENT_USER, r'Environment') as env:
