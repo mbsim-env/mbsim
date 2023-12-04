a77bc8f33f04f37234709b5a943d9090471720cc
Martin Förg <martin.o.foerg@gmail.com>
2023-12-04 08:12:26 +0100

    Update mbsimgui

--- mbsimgui/mbsimgui/kinetics_widgets.h
+++ mbsimgui/mbsimgui/kinetics_widgets.h
@@ -289,7 +289,7 @@ namespace MBSimGUI {
       xercesc::DOMElement* writeXMLFile(xercesc::DOMNode *parent, xercesc::DOMNode *ref=nullptr) override;
     protected:
       void inputFileChanged(const QString &fileName);
-      ExtWidget *inputDataFile, *tyreSide, *mck, *cpt, *ts, *six, *siy, *p, *cz, *dz, *sfFx, *sfFy, *sfMx, *sfMy, *sfMz, *sfs, *sfmux, *sfmuy, *sfkx, *sfky, *sfkg, *sfkm;
+      ExtWidget *inputDataFile, *tyreSide, *mck, *cpt, *ts, *six, *siy, *p, *cz, *dz, *rtw, *sfFx, *sfFy, *sfMx, *sfMy, *sfMz, *sfs, *sfmux, *sfmuy, *sfkx, *sfky, *sfkg, *sfkm;
   };
 
 }
