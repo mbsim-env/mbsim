b11ca00366d17c4a5105008403ad4debae455db9
Markus Friedrich <fm12111+github@googlemail.com>
2023-12-10 13:39:59 +0100

    Added missing files

---
+++ mbsimgui/mbsimgui/single_line_delegate.h
@@ -0,0 +1,39 @@
+/*
+    MBSimGUI - A fronted for MBSim.
+    Copyright (C) 2012 Martin Förg
+
+  This library is free software; you can redistribute it and/or 
+  modify it under the terms of the GNU Lesser General Public 
+  License as published by the Free Software Foundation; either 
+  version 2.1 of the License, or (at your option) any later version. 
+   
+  This library is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+  Lesser General Public License for more details. 
+   
+  You should have received a copy of the GNU Lesser General Public 
+  License along with this library; if not, write to the Free Software 
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+*/
+
+#ifndef MBSIMGUI_SINGLE_LINE_DELEGATE_H
+#define MBSIMGUI_SINGLE_LINE_DELEGATE_H
+
+#include <QItemDelegate>
+#include <QTreeView>
+
+namespace MBSimGUI {
+
+class SingleLineDelegate : public QItemDelegate {
+  public:
+    SingleLineDelegate(QTreeView *tv);
+    void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+    QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+  private:
+    QTreeView *treeView;
+};
+
+}
+
+#endif
