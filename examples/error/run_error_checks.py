b3dc4f07388924bdeab6df455d7f404d2aeed23b
Markus Friedrich <fm12111+github@googlemail.com>
2024-11-03 10:54:03 +0100

    Rename and more tests

--- examples/error/run_error_checks.py
+++ examples/error/run_error_checks.py
@@ -5,6 +5,7 @@ import os
 import sys
 
 UPDATE=len(sys.argv)>=2 and sys.argv[1]=="update"
+SHOWDIFF=len(sys.argv)>=2 and sys.argv[1]=="showdiff"
 
 global ERROR
 ERROR=0
@@ -27,37 +28,45 @@ def checkErrorFormat(dir, errorFormat):
   os.chdir(oldDir)
   cur=cur.decode("utf-8")
 
-  if not UPDATE:
+  if UPDATE:
+    # update reference error output
+    with open(dir+"/error-"+errorFormat+".errorOutput", "wt") as f:
+      f.write(cur)
+  else:
     # check error output
     if os.path.isfile(dir+"/error-"+errorFormat+".errorOutput"):
       with open(dir+"/error-"+errorFormat+".errorOutput", "rt") as f:
         ref=f.read()
       if ref!=cur:
-        print(dir+": "+errorFormat+": error output does not match reference"); ERROR+=1
+        if SHOWDIFF:
+          curFile="/tmp/run_error_checks.py.tmpfile"
+          with open(curFile, "wt") as f:
+            f.write(cur)
+          subprocess.check_call(["nvim-qt", "--nofork", "--no-ext-tabline", "--no-ext-popupmenu", "--", "-d", dir+"/error-"+errorFormat+".errorOutput", curFile])
+        else:
+          print(dir+": "+errorFormat+": error output does not match reference"); ERROR+=1
     else:
       print(dir+": "+errorFormat+": no reference"); ERROR+=1
-  else:
-    # update reference error output
-    with open(dir+"/error-"+errorFormat+".errorOutput", "wt") as f:
-      f.write(cur)
 
 def check(dir):
   checkErrorFormat(dir, "GCC")
   checkErrorFormat(dir, "HTMLFILELINE")
   checkErrorFormat(dir, "HTMLXPATH")
 
-check("hierachical_modelling/ppError")
+check("hierachical_modelling/paraHRefParseError")
+check("hierachical_modelling/paraHRefPPError")
+check("hierachical_modelling/paraInlineParseError")
+check("hierachical_modelling/paraInlinePPError")
 check("hierachical_modelling/parseError")
+check("hierachical_modelling/ppError")
 check("hierachical_modelling/initXMLError")
 check("hierachical_modelling/mbsimError")
-check("hierachical_modelling/paraHRefError")
-check("hierachical_modelling/paraInlineError")
-check("hierachical_modelling_inlineembed1/ppError")
 check("hierachical_modelling_inlineembed1/parseError")
+check("hierachical_modelling_inlineembed1/ppError")
 check("hierachical_modelling_inlineembed1/initXMLError")
 check("hierachical_modelling_inlineembed1/mbsimError")
-check("hierachical_modelling_inlineembed2/ppError")
 check("hierachical_modelling_inlineembed2/parseError")
+check("hierachical_modelling_inlineembed2/ppError")
 check("hierachical_modelling_inlineembed2/initXMLError")
 check("hierachical_modelling_inlineembed2/mbsimError")
 
