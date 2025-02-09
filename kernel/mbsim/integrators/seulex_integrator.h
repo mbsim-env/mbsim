4f6aa2e4d311215c7ca99dfe9ee9abc409a53633
Martin Förg <martin.o.foerg@gmail.com>
2025-02-09 10:42:24 +0100

    Update implementation of SEULEX integrator according to commit 44eedd

--- kernel/mbsim/integrators/seulex_integrator.h
+++ kernel/mbsim/integrators/seulex_integrator.h
@@ -23,34 +23,31 @@
 #ifndef _SEULEX_INTEGRATOR_H_
 #define _SEULEX_INTEGRATOR_H_
 
-#include "root_finding_integrator.h"
+#include "dae_integrator.h"
 
 namespace MBSim {
 
   /** \brief DAE-Integrator SEULEX
   */
-  class SEULEXIntegrator : public RootFindingIntegrator {
-
-    public:
-      enum Formalism {
-        ODE=0,
-        DAE1,
-        unknown
-      };
+  class SEULEXIntegrator : public DAEIntegrator {
 
     private:
+      double delta(int i, double z) const override;
       typedef void (*Fzdot)(int* n, double* t, double* y, double* yd, double* rpar, int* ipar);
+      typedef void (*Jac)(int* n, double *t, double *y, double *J, int *nn, double *rpar, int *ipar);
       typedef void (*Mass)(int* n, double* m, int* lmas, double* rpar, int* ipar);
       static Fzdot fzdot[2];
+      static Jac jac[2];
       static Mass mass[2];
       static void fzdotODE(int* n, double* t, double* z, double* zd, double* rpar, int* ipar);
       static void fzdotDAE1(int* n, double* t, double* y, double* yd, double* rpar, int* ipar);
+      static void jacODE(int* n, double *t, double *z, double *J, int *nn, double *rpar, int *ipar);
+      static void jacDAE1(int* n, double *t, double *y, double *J, int *nn, double *rpar, int *ipar);
       static void massFull(int* n, double* m, int* lmas, double* rpar, int* ipar);
       static void massReduced(int* n, double* m, int* lmas, double* rpar, int* ipar);
       static void plot(int* nr, double* told, double* x, double *y, double *rc, int* lrc, int* ic, int* lic, int* n, double* rpar, int* ipar, int* irtrn);
 
-      void calcSize();
-      void reinit();
+      void reinit() override;
 
       double tPlot{0};
       double dtOut{0};
@@ -67,16 +64,16 @@ namespace MBSim {
       int maxSteps{std::numeric_limits<int>::max()};
       /** maximal step size */
       double dtMax{0};
-      /** formalism **/
-      Formalism formalism{ODE};
-      /** reduced form **/
-      bool reduced{false};
       /** autonomous system **/
       bool autonom{false};
 
-      int neq, mlJac, muJac;
+      int mlJac, muJac;
       fmatvec::VecInt iWork;
       fmatvec::Vec work;
+
+      bool drift { false };
+
+      std::exception_ptr exception;
 
     public:
       ~SEULEXIntegrator() override = default;
@@ -88,8 +85,6 @@ namespace MBSim {
       void setInitialStepSize(double dt0_) { dt0 = dt0_; }
       void setMaximumStepSize(double dtMax_) { dtMax = dtMax_; }
       void setStepLimit(int maxSteps_) { maxSteps = maxSteps_; }
-      void setFormalism(Formalism formalism_) { formalism = formalism_; }
-      void setReducedForm(bool reduced_) { reduced = reduced_; }
       void setAutonomousSystem(bool autonom_) { autonom = autonom_; }
 
       using Integrator::integrate;
